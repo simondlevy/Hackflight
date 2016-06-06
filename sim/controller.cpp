72e3d118121e412700179b67596d289ea9947f40
simondlevy <simon.d.levy@gmail.com>
2016-06-05 22:42:29 -0400

    Added headers

--- sim/controller.cpp
+++ sim/controller.cpp
@@ -1,5 +1,24 @@
-// Adapted from  http://www.cplusplus.com/forum/general/5304/
-// Key bindings: http://www.flightsimbooks.com/flightsimhandbook/keyboardcontrols.php
+/*
+   controller.cpp : support for various kinds of control devices
+
+   Copyright (C) Simon D. Levy 2016
+
+   This file is part of Hackflight.
+
+   Hackflight is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   Hackflight is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with Hackflight.  If not, see <http://www.gnu.org/licenses/>.
+
+
+   Adapted from  http://www.cplusplus.com/forum/general/5304/
+*/
 
 #include "controller.hpp"
 
--- sim/controller.hpp
+++ sim/controller.hpp
@@ -1,3 +1,22 @@
+/*
+   controller.hpp : header for support for various kinds of control devices
+
+   Copyright (C) Simon D. Levy 2016
+
+   This file is part of Hackflight.
+
+   Hackflight is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   Hackflight is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with Hackflight.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #pragma once
 
 #include <stdio.h>
@@ -33,6 +52,11 @@ class PS3Controller : public AxialController {
     void getDemands(float & pitchDemand, float & rollDemand, float & yawDemand, float & throttleDemand);
 };
 
+/**
+  *
+  * Uses key bindings from <a href="http://www.flightsimbooks.com/flightsimhandbook/keyboardcontrols.php">
+  * Microsoft Flight Simulator</a>. 
+  */
 class KeyboardController : public Controller {
 
     private:
