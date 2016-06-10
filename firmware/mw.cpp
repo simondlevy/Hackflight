5f0dce01c93a0ad66f281efa696565db7001c34b
simondlevy <simon.d.levy@gmail.com>
2016-06-09 21:15:52 -0400

    Removed unused code

--- firmware/mw.cpp
+++ firmware/mw.cpp
@@ -125,21 +125,6 @@ void setup(void)
 
 } // setup
 
-const char *byte_to_binary(int x)
-{
-    static char b[9];
-    b[0] = '\0';
-
-    int z;
-    for (z = 128; z > 0; z >>= 1)
-    {
-        strcat(b, ((x & z) == z) ? "1" : "0");
-    }
-
-    return b;
-}
-
-
 void loop(void)
 {
     static uint32_t rcTime = 0;
