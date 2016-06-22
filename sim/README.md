efbff9d1bf5af47485e2f8c9798fcf10e4100349
Simon D. Levy <simon.d.levy@gmail.com>
2016-06-22 14:40:50 -0400

    Simulator working with Windows but no other OS

--- sim/README.md
+++ sim/README.md
@@ -1,36 +1,3 @@
 <img src="hackflight.png">
 
-This directory contains code that you can use to build HackflightSim, a flight
-simulator that uses the Hackflight firmware and the [Virtual Robot
-Experimentation Platform](http://www.coppeliarobotics.com/) from Coppelia
-Robotics.  
-
-To build and run the simulator you will need download V-REP, and you will need to
-use the Linux operating system.  (We are working on a version for Windows 10.)
-The simulator works best with an R/C transmitter:  Taranis transmitter with USB
-mini-B cable, or Spektrum transmitter with 
-[GWS adapter cable] (https://www.amazon.com/gp/product/B000RO7JAI)
-(which is unfortunately no longer available).
-You can also fly it with a PS3 controller, Logitech ExtremePro3D joystick,
-or even the numeric keypad on your
-keyboard, using the [key
-mappings](http://www.flightsimbooks.com/flightsimhandbook/keyboardcontrols.php)
-for the Microsoft Flight Simulator. 
-For keyboard control you have to focus the terminal window from which you launched V-REP in order
-for the simulator to detect the keystrokes.  On the PS3, I have found it useful
-to wiggle the sticks a bit when starting, to make sure the program is detecting
-the controller.
-
-The simulator uses a V-REP plugin for optimal speed and simplicity.  To build
-the plugin you should clone the hackflight repository, cd to
-<b>hackflight/sim</b>, and edit the value of <tt>VREP\_DIR</tt> in the Makefile
-to reflect where you installed V-REP. In <b>v_repExtHackflight</b>
-you should also adjust the <tt>#define</tt> for the controller to
-match the controller you're using.  Typing
-<tt>make install</tt> will build the plugin library file
-<b>libv_repExtHackflight.so</b> and copy it to the V-REP folder. You should not
-have V-REP running while you do this, or you'll likely cause it to shut itself
-down.
-
-You can then launch V-REP, open the scene file <b>hackflight.ttt</b>, and click
-the play button to start the simuation.
+THE SIMULATOR IS CURRENTLY DOWN FOR REPAIRS.
--- sim/v_repExtHackflight.cpp
+++ sim/v_repExtHackflight.cpp
@@ -17,14 +17,9 @@
    along with Hackflight.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-// ==================================================================================
-// Choose your controller
-#define CONTROLLER_TARANIS
-//#define CONTROLLER_SPEKTRUM
-//#define CONTROLLER_EXTREME3DPRO
-//#define CONTROLLER_PS3
-//#define CONTROLLER_KEYBOARD
-// ==================================================================================
+static enum Controller { TARANIS, SPEKTRUM, EXTREMEPRO3D, PS3 };
+
+static Controller controller = EXTREMEPRO3D;
 
 #include "v_repExtHackflight.h"
 #include "scriptFunctionData.h"
@@ -88,229 +83,6 @@ static float thrusts[4];
 // 100 Hz timestep, used for simulating microsend timer
 static double timestep = .01;
 
-// Joystick support for Linux, OS X ==========================================
-#ifdef __linux__
-#define JOY_DEV "/dev/input/js0"
-#include <linux/joystick.h>
-#include <termios.h>
-#ifdef CONTROLLER_KEYBOARD
-static struct termios oldSettings;
-#else
-static int joyfd;
-#endif
-#endif
-
-#ifdef __APPLE__
-#include <SDL.h>
-SDL_Joystick * sdljoy;
-#endif
-
-#ifdef CONTROLLER_KEYBOARD
-void kbinit(void)
-{
-#ifdef __linux__
-    struct termios newSettings;
-
-    // Save keyboard settings for restoration later
-    tcgetattr(fileno( stdin ), &oldSettings);
-
-    // Create new keyboard settings
-    newSettings = oldSettings;
-    newSettings.c_lflag &= (~ICANON & ~ECHO);
-    tcsetattr(fileno(stdin), TCSANOW, &newSettings);
-#endif
-}
-
-int _kbhit(void)
-{
-    char c = 0;
-
-#ifdef __linux__
-    fd_set set;
-    struct timeval tv;
-
-    tv.tv_sec = 0;
-    tv.tv_usec = 1000;
-
-    FD_ZERO( &set );
-    FD_SET( fileno( stdin ), &set );
-
-    int res = select( fileno( stdin )+1, &set, NULL, NULL, &tv );
-
-    if( res > 0 )
-    {
-        read(fileno( stdin ), &c, 1);
-    }
-
-    else if( res < 0 ) {
-        perror( "select error" );
-    }
-#endif
-
-    return (int)c;
-}
-
-static void kbdone(void)
-{
-#ifdef __linux__
-    tcsetattr(fileno(stdin), TCSANOW, &oldSettings);
-#endif
-}
-#endif
-
-// Controller axis maps
-
-#ifdef CONTROLLER_TARANIS
-static int axes[5] = {0, 1, 2, 3, 4}; // roll, pitch, yaw, throttle, aux
-#endif
-
-#ifdef CONTROLLER_SPEKTRUM
-static int axes[5] = {1, 2, 5, 0, 3}; 
-#endif
-
-#ifdef CONTROLLER_EXTREME3DPRO
-static int axes[5] = {0, 1, 2, 3, 4}; 
-#endif
-
-#ifdef CONTROLLER_PS3
-static int axes[5] = {2, 3, 0, 1, -1};  // aux unused
-#endif
-
-#ifdef CONTROLLER_KEYBOARD
-static int axes[5] = {-1, -1, -1, -1, -1};  // all unused
-#endif
-
-#define KEYBOARD_INC 10
-
-#ifndef _WIN32
-
-#define LUA_GET_JOYSTICK_COUNT_COMMAND "simExtJoyGetCount"
-#define LUA_GET_JOYSTICK_DATA_COMMAND  "simExtJoyGetData"
-
-void LUA_GET_JOYSTICK_COUNT_CALLBACK(SLuaCallBack* p)
-{
-	// Prepare the return value:
-	p->outputArgCount=1; // 1 return value
-
-    // x return values takes x*2 simInt for the type and size buffer
-	p->outputArgTypeAndSize=(simInt*)simCreateBuffer(p->outputArgCount*2*sizeof(simInt)); 
-	p->outputArgTypeAndSize[2*0+0]=sim_lua_arg_int;	        // The return value is an int
-	p->outputArgTypeAndSize[2*0+1]=1;					    // Not used (table size if the return value was a table)
-	p->outputInt=(simInt*)simCreateBuffer(1*sizeof(int));   // One int return value
-
-    // Assume no joystick (return 0)
-    int retval = 0;
-
-#ifdef CONTROLLER_KEYBOARD
-    kbinit();
-#else
-#ifdef __linux
-    joyfd = open(JOY_DEV, O_RDONLY);
-    if (joyfd > 0) {
-        fcntl(joyfd, F_SETFL, O_NONBLOCK);
-        retval = 1;
-    }
-#endif
-#ifdef __APPLE__
-    if (SDL_Init(SDL_INIT_JOYSTICK)) {
-        retval = 1;
-    }
-#endif
-#endif
-
-    p->outputInt[0] = retval;     // return 1 on success, 0 on failure
-}
-
-#ifndef _WIN32
-static int scaleAxis(int value)
-{
-    return 1000 * value / 32767.;
-}
-
-void LUA_GET_JOYSTICK_DATA_CALLBACK(SLuaCallBack* p)
-{
-	bool error=true;
-	if (p->inputArgCount>0)
-	{ // Ok, we have at least 1 input argument
-		if (p->inputArgTypeAndSize[0*2+0]==sim_lua_arg_int)
-		{ // Ok, we have an int as argument 1
-			if ( (p->inputInt[0]<1)&&(p->inputInt[0]>=0) )
-			{ // Ok, there is a device at this index!
-				error=false;
-			}
-			else
-				simSetLastError(LUA_GET_JOYSTICK_DATA_COMMAND,"Invalid index."); // output an error
-		}
-		else
-			simSetLastError(LUA_GET_JOYSTICK_DATA_COMMAND,"Wrong argument type/size."); // output an error
-	}
-	else
-		simSetLastError(LUA_GET_JOYSTICK_DATA_COMMAND,"Not enough arguments."); // output an error
-
-
-	// Now we prepare the return value(s):
-	if (error) {
-		p->outputArgCount=0; // 0 return values --> nil (error)
-	}
-	else {
-		p->outputArgCount=5; // 5 return values
-
-        // x return values takes x*2 simInt for the type and size buffer
-		p->outputArgTypeAndSize=(simInt*)simCreateBuffer(p->outputArgCount*10*sizeof(simInt)); 
-		p->outputArgTypeAndSize[2*0+0]=sim_lua_arg_int|sim_lua_arg_table;	// The return value is an int table
-		p->outputArgTypeAndSize[2*0+1]=3;					// table size is 3 (the 3 axes)
-		p->outputArgTypeAndSize[2*1+0]=sim_lua_arg_int;	// The return value is an int
-		p->outputArgTypeAndSize[2*1+1]=1;					// Not used (not a table)
-		p->outputArgTypeAndSize[2*2+0]=sim_lua_arg_int|sim_lua_arg_table;	// The return value is an int table
-		p->outputArgTypeAndSize[2*2+1]=3;					// table size is 3 (the 3 rot axes)
-		p->outputArgTypeAndSize[2*3+0]=sim_lua_arg_int|sim_lua_arg_table;	// The return value is an int table
-		p->outputArgTypeAndSize[2*3+1]=2;					// table size is 2 (the 2 sliders)
-		p->outputArgTypeAndSize[2*4+0]=sim_lua_arg_int|sim_lua_arg_table;	// The return value is an int table
-		p->outputArgTypeAndSize[2*4+1]=4;					// table size is 4 (the 4 pov values)
-
-        // 13 int return values (3 for the axes + 1 for the buttons + 3 for rot axis, +2 for slider, +4 for pov)
-		p->outputInt=(simInt*)simCreateBuffer(13*sizeof(int)); 
-
-        static int roll, pitch, yaw, throttle, aux;
-
-#ifdef __linux__
-        if (joyfd > 0) {
-
-            struct js_event js;
-
-            read(joyfd, &js, sizeof(struct js_event));
-
-            if (js.type & JS_EVENT_AXIS) {
-
-                    if (js.number == axes[0]) 
-                        roll = scaleAxis(js.value);
-
-                    if (js.number == axes[1]) 
-                        pitch = scaleAxis(js.value);
-
-                    if (js.number == axes[2]) 
-                        yaw = scaleAxis(js.value);
-
-                    if (js.number == axes[3]) 
-                        throttle = scaleAxis(js.value);
-
-                    if (js.number == axes[4]) 
-                        aux = scaleAxis(js.value);
-                }
-
-        }
-#endif
-        // We only need to specify these five return values
-        p->outputInt[1]= roll;
-        p->outputInt[2]= pitch;
-        p->outputInt[4]= yaw;
-        p->outputInt[0]= throttle;
-        p->outputInt[6]= aux;
-    }
-}
-#endif
-#endif
-
 
 // --------------------------------------------------------------------------------------
 // simExtHackflight_start
@@ -343,8 +115,10 @@ void LUA_START_CALLBACK(SScriptCallBack* cb)
 #define LUA_UPDATE_COMMAND "simExtHackflight_update"
 
 static const int inArgs_UPDATE[]={
-    4,
-    sim_script_arg_int32|sim_script_arg_table,5,  // RC axis values
+    6,
+    sim_script_arg_int32|sim_script_arg_table,3,  // primary axis values
+	sim_script_arg_int32|sim_script_arg_table,3,  // rotational axis values
+	sim_script_arg_int32|sim_script_arg_table,2,  // sliders
     sim_script_arg_double|sim_script_arg_table,3, // Gyro values
     sim_script_arg_double|sim_script_arg_table,3, // Accelerometer values
     sim_script_arg_int32                          // Barometric pressure
@@ -356,45 +130,75 @@ void LUA_UPDATE_CALLBACK(SScriptCallBack* cb)
     if (D.readDataFromStack(cb->stackID,inArgs_UPDATE,inArgs_UPDATE[0],LUA_UPDATE_COMMAND)) {
         std::vector<CScriptFunctionDataItem>* inData=D.getInDataPtr();
 
-        // Read RC axes 
-#ifndef CONTROLLER_KEYBOARD
-        for (int k=0; k<5; ++k)
-            demands[k] = inData->at(0).int32Data[k];
-#endif
+		int axes[3], rotAxes[3], sliders[2];
 
-#if defined(CONTROLLER_PS3) || defined(CONTROLLER_EXTREME3DPRO)
+        // Read joystick axes
+        for (int k=0; k<3; ++k) {
+            axes[k] = inData->at(0).int32Data[k]; 
+			rotAxes[k] = inData->at(1).int32Data[k]; 
+		}
 
-        // Scale down non-RC controllers
-        demands[0] /= 2;
-        demands[1] /= 2;
+		// Read sliders
+        for (int k=0; k<2; ++k) {
+            sliders[k] = inData->at(2).int32Data[k]; 
+		}
 
-        // and negate their throttle and pitch values
-        demands[1] *= -1;
-        demands[3] *= -1;
-#endif
+		// Handle each controller differently
+		switch (controller) {
 
-#ifdef CONTROLLER_PS3
+		case TARANIS:
+			demands[0] = axes[0];     // roll
+			demands[1] = axes[1];     // pitch
+			demands[2] = axes[2];     // yaw
+			demands[3] = rotAxes[0];  // throttle			
+			break;
 
-        // PS3 spring-mounted throttle requires special handling
-        ps3throttle += demands[3] * PS3_THROTTLE_INC;     
-        if (ps3throttle < -1000)
-            ps3throttle = -1000;
-        if (ps3throttle > 1000)
-            ps3throttle = 1000;
-        demands[3] = ps3throttle;
-#endif
+		case SPEKTRUM:
+			demands[0] = axes[1];		// roll
+			demands[1] = axes[2];		// pitch
+			demands[2] = rotAxes[2];	// yaw
+			demands[3] = axes[0];		// throttle
+			break;
+
+		case EXTREMEPRO3D:
+			demands[0] = axes[0];		// roll
+			demands[1] = axes[1];		// pitch
+			demands[2] = rotAxes[2];	// yaw
+			demands[3] = -sliders[0];	// throttle
+			break;
+
+		case PS3:
+
+			demands[0] = axes[2];		// roll
+			demands[1] = -rotAxes[2];	// pitch
+			demands[2] = axes[0];		// yaw
+			demands[3] = -axes[1];		// throttle
+
+			// PS3 spring-mounted throttle requires special handling
+			ps3throttle += demands[3] * PS3_THROTTLE_INC;     
+			if (ps3throttle < -1000)
+				ps3throttle = -1000;
+			if (ps3throttle > 1000)
+				ps3throttle = 1000;
+			demands[3] = ps3throttle;
+
+			break;
+		}
+
+		// XXX Don't support aux switch for now
+		demands[4] = -1000;
 
         //printf("r: %4d    p: %4d    y: %4d    t: %4d    a: %4d\n", demands[0], demands[1], demands[2], demands[3], demands[4]);
 
 
         // Read gyro, accelerometer
         for (int k=0; k<3; ++k) {
-            gyro[k]   = inData->at(1).doubleData[k]; 
-            accel[k]  = inData->at(2).doubleData[k]; 
+            gyro[k]   = inData->at(3).doubleData[k]; 
+            accel[k]  = inData->at(4).doubleData[k]; 
         }
 
         // Read barometer
-        baroPressure = inData->at(3).int32Data[0];
+        baroPressure = inData->at(5).int32Data[0];
 
         // Set thrust for each motor
         for (int i=0; i<4; ++i) {
@@ -421,18 +225,6 @@ void LUA_UPDATE_CALLBACK(SScriptCallBack* cb)
 
 void LUA_STOP_CALLBACK(SScriptCallBack* cb)
 {
-#ifdef CONTROLLER_KEYBOARD
-    kbdone();
-#else
-#ifdef __linux__
-    close(joyfd);
-#endif
-#ifdef __APPLE__
-    SDL_JoystickClose(sdljoy);
-    SDL_Quit();
-#endif
-#endif
-
 	CScriptFunctionData D;
 	D.pushOutData(CScriptFunctionDataItem(true));
 	D.writeDataToStack(cb->stackID);
@@ -493,18 +285,6 @@ VREP_DLLEXPORT unsigned char v_repStart(void* reservedPointer,int reservedInt)
 	simRegisterScriptCallbackFunction(strConCat(LUA_START_COMMAND,"@",PLUGIN_NAME),strConCat("boolean result=",LUA_START_COMMAND,"(number HackflightHandle,number duration,boolean returnDirectly=false)"),LUA_START_CALLBACK);
     simRegisterScriptCallbackFunction(strConCat(LUA_UPDATE_COMMAND,"@",PLUGIN_NAME), NULL, LUA_UPDATE_CALLBACK);
 	simRegisterScriptCallbackFunction(strConCat(LUA_STOP_COMMAND,"@",PLUGIN_NAME),strConCat("boolean result=",LUA_STOP_COMMAND,"(number HackflightHandle)"),LUA_STOP_CALLBACK);
-
-#ifndef _WIN32
-	// Register joystick commands for Linux, OS X (already provided in Windows)
-	int inArgs1[]={0};
-	simRegisterCustomLuaFunction(LUA_GET_JOYSTICK_COUNT_COMMAND,strConCat("number count=",
-                LUA_GET_JOYSTICK_COUNT_COMMAND,"()"),inArgs1,LUA_GET_JOYSTICK_COUNT_CALLBACK);
-
-	int inArgs2[]={1,sim_lua_arg_int};
-	simRegisterCustomLuaFunction(LUA_GET_JOYSTICK_DATA_COMMAND,
-            strConCat("table_3 axes, number buttons,table_3 rotAxes,table_2 slider,table_4 pov=",
-                LUA_GET_JOYSTICK_DATA_COMMAND,"(number deviceIndex)"),inArgs2,LUA_GET_JOYSTICK_DATA_CALLBACK);
-#endif
 
 	return 8; // initialization went fine, we return the version number of this plugin (can be queried with simGetModuleName)
 }
