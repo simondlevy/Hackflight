461ac3d8802a50cc06fadcc4b3cd1af09c7b4a26
simondlevy <simon.d.levy@gmail.com>
2016-07-31 14:43:22 -0400

    Simplified

--- slam/3dslamvis.py
+++ slam/3dslamvis.py
@@ -28,40 +28,51 @@ CONE_LENGTH  =   30
 CONE_YOFFSET =   20
 CONE_RADIUS  =   10
 
-from visual import box, display, cone, color, rate, vector, materials
+from visual import box, display, cone, color, vector, materials
 from time import sleep
-from math import pi, radians
+from math import radians
 
 class ThreeDSlamVis(object):
 
-    def __init__(self, display_size_pixels=1000, map_size_cm=1000):
+    def __init__(self, display_size_pixels=1000, map_size_cm=1000, obstacle_size_cm=10):
 
         display(width=display_size_pixels,height=display_size_pixels,title='3D SLAM')
 
-        floor = box(pos = (0, map_size_cm*FLOOR_Y, map_size_cm*FLOOR_Z), 
-                length=map_size_cm, height=1, width=map_size_cm, 
-                material=materials.rough)
+        # Floor for reference
+        box(pos = (0, map_size_cm*FLOOR_Y, map_size_cm*FLOOR_Z), 
+                   length=map_size_cm, height=1, width=map_size_cm, 
+                   material=materials.rough)
 
         self.vehicle = cone(pos = (0, map_size_cm*FLOOR_Y+CONE_YOFFSET, map_size_cm*FLOOR_Z), 
                 axis=(CONE_LENGTH,0,0), radius=CONE_RADIUS, color=color.red)
 
-        self.pose = 0,0,0,0
+        self.obstacle_size_cm = obstacle_size_cm
+
+        self.theta = 0
 
     def addObstacle(self, x, y, z):
 
-        return
+        box(pos = (x, z,y), 
+            length=self.obstacle_size_cm, height=self.obstacle_size_cm, width=self.obstacle_size_cm, 
+            material=materials.rough)
+
 
     def setPose(self, x, y, z, theta):
         '''
-        Sets vehicle pose (X,Y,Z coordinates in cm and heading theta in degrees).
+        Sets vehicle pose: 
+        X: left/right   (cm)
+        Y: forward/back (cm)
+        Z: up/down      (cm)
+        theta: degrees
         '''
 
-        self.vehicle.rotate(angle=radians(self.pose[3] - theta), axis=(0,1,0))
+        # Rotate vehicle by difference between current angle and desired angle
+        self.vehicle.rotate(angle=radians(self.theta - theta), axis=(0,1,0))
 
         # VPython uses Y for up/down; we use Z
         self.vehicle.pos = vector(x, z, y)
 
-        self.pose = x, y, z, theta
+        self.theta = theta
 
 if __name__ == '__main__':
 
@@ -76,7 +87,5 @@ if __name__ == '__main__':
         sleep(.1)
 
         theta = (theta + 10) % 360
-        x += 10
-        y += 20
-        x += 10
+        z += 2
 
