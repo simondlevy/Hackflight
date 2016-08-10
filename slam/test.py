dea42949a16c6dcf610a478fd1471c9dedd03823
simondlevy <simon.d.levy@gmail.com>
2016-08-09 22:40:57 -0400

    Generating correct test distances

--- slam/test.py
+++ slam/test.py
@@ -25,14 +25,42 @@ from microslam import MicroSLAM
 from slamvis2d import TwoDSlamVis
 
 from time import sleep
+from math import tan, radians, sqrt
 
 slam = MicroSLAM()
 
 disp = TwoDSlamVis()
 
 x,y,theta = 0,0,0
+WALLDIST  = 100
 
 while True:
+
+    theta = (theta + 5) % 360
+
+    octant = theta // 45
+
+    t = theta
+
+    if octant == 1:
+        t = 90 - theta
+    elif octant == 2:
+        t = theta - 90
+    elif octant == 3:
+        t = 180 - theta
+    elif octant == 5:
+        t = 270 - theta
+    elif octant == 6:
+        t = theta - 270
+
+    x = WALLDIST
+    y = x * tan(radians(t))
+
+    d = int(sqrt(x*x + y*y))
+
+    print('%3d %3d %d %3d' % (theta, t, octant, d))
+
+    #slam.update((d,)
 
     try:
         sleep(.25)
