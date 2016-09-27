06d58d7fd50e78c048bedc20f2245fad759a04ba
simondlevy <simon.d.levy@gmail.com>
2016-09-27 16:50:48 -0400

    Axes mapped correctly

--- parser/msppg_resources/bluefly.py
+++ parser/msppg_resources/bluefly.py
@@ -25,6 +25,9 @@ along with this code.  If not, see <http:#www.gnu.org/licenses/>.
 # MAC address of your Bluetooth adapter
 BT_ADDR = "00:06:66:73:e3:a6"
 
+# Map from controller axes to RC channels: Roll, Pitch, Yaw, Throttle
+CHANMAP = [2,3,0,1]
+
 import pygame
 from pygame.locals import KEYDOWN, K_ESCAPE, QUIT
  
@@ -114,13 +117,15 @@ class App:
             self.draw_text("Axes (%d)" % self.my_joystick.get_numaxes(), 
                            5, 25, (255, 255, 255))
  
+            print('Roll: %+3.3f | Pitch: %+3.3f | Yaw: %+3.3f | Throttle: %+3.3f' % 
+                    (self._chanval(0), self._chanval(1), self._chanval(2), self._chanval(3)))
+
             for i in range(0, self.my_joystick.get_numaxes()):
-                if (self.my_joystick.get_axis(i)):
-                    pygame.draw.circle(self.screen, (0, 0, 200), 
-                                       (20 + (i * 30), 50), 10, 0)
+                axval = self.my_joystick.get_axis(i)
+                if axval:
+                    pygame.draw.circle(self.screen, (0, 0, 200), (20 + (i * 30), 50), 10, 0)
                 else:
-                    pygame.draw.circle(self.screen, (255, 0, 0), 
-                                       (20 + (i * 30), 50), 10, 0)
+                    pygame.draw.circle(self.screen, (255, 0, 0), (20 + (i * 30), 50), 10, 0)
  
                 self.center_text("%d" % i, 20 + (i * 30), 50, (255, 255, 255))
  
@@ -129,11 +134,9 @@ class App:
  
             for i in range(0, self.my_joystick.get_numbuttons()):
                 if (self.my_joystick.get_button(i)):
-                    pygame.draw.circle(self.screen, (0, 0, 200), 
-                                       (20 + (i * 30), 100), 10, 0)
+                    pygame.draw.circle(self.screen, (0, 0, 200), (20 + (i * 30), 100), 10, 0)
                 else:
-                    pygame.draw.circle(self.screen, (255, 0, 0), 
-                                       (20 + (i * 30), 100), 10, 0)
+                    pygame.draw.circle(self.screen, (255, 0, 0), (20 + (i * 30), 100), 10, 0)
  
                 self.center_text("%d" % i, 20 + (i * 30), 100, (255, 255, 255))
  
@@ -142,15 +145,16 @@ class App:
  
             for i in range(0, self.my_joystick.get_numhats()):
                 if (self.my_joystick.get_hat(i) != (0, 0)):
-                    pygame.draw.circle(self.screen, (0, 0, 200), 
-                                       (20 + (i * 30), 150), 10, 0)
+                    pygame.draw.circle(self.screen, (0, 0, 200), (20 + (i * 30), 150), 10, 0)
                 else:
-                    pygame.draw.circle(self.screen, (255, 0, 0), 
-                                       (20 + (i * 30), 150), 10, 0)
+                    pygame.draw.circle(self.screen, (255, 0, 0), (20 + (i * 30), 150), 10, 0)
  
                 self.center_text("%d" % i, 20 + (i * 30), 100, (255, 255, 255))
  
             pygame.display.flip()
+
+    def _chanval(self, index):
+        return self.my_joystick.get_axis(CHANMAP[index])
  
     def quit(self):
         pygame.display.quit()
