2cc2c3cce7e2c8fc4e3965fe0feac0c7c59afc4b
simondlevy <simon.d.levy@gmail.com>
2016-09-27 19:10:30 -0400

    PWM correct for all axes

--- parser/msppg_resources/bluefly.py
+++ parser/msppg_resources/bluefly.py
@@ -26,10 +26,16 @@ BT_ADDR = "00:06:66:73:e3:a6"
 # Map from controller axes to RC channels: Roll, Pitch, Yaw, Throttle
 CHANMAP = [2,3,0,1]
 
-# Special handling for throttle: a tiny increment up or down
+# Special handling for throttle: starting at zero, then a tiny increment up or down
 THROTINC = 1e-5
 
+# PWM range
+PWMMIN = 1000
+PWMMAX = 2000
+
 import pygame
+
+from msppg import serialize_SET_RAW_RC
  
 class App(object):
 
@@ -48,7 +54,7 @@ class App(object):
         self.joystick = pygame.joystick.Joystick(0)
         self.joystick.init()
 
-        self.throttle = 0
+        self.throttle = -1
  
     def main(self):
 
@@ -59,19 +65,34 @@ class App(object):
                 pygame.event.get()
 
                 # Special handling for throttle
-                self.throttle -= self._chanval(3) * THROTINC
-                self.throttle = max(min(self.throttle, 1), 0)
+                self.throttle -= self._axval(3) * THROTINC
+                self.throttle = max(min(self.throttle, +1), -1)
      
-                print('Roll: %+3.3f | Pitch: %+3.3f | Yaw: %+3.3f | Throttle: %+3.3f' % 
-                        (self._chanval(0), self._chanval(1), self._chanval(2), self.throttle))
+                print('Roll: %4d | Pitch: %4d | Yaw: %4d | Throttle: %4d' % 
+                        (self._axis2pwm(0), self._axis2pwm(1,-1), self._axis2pwm(2), self._axval2pwm(self.throttle)))
 
             except KeyboardInterrupt:
 
                 break
 
-    def _chanval(self, index):
+    def _axval(self, index):
 
         return self.joystick.get_axis(CHANMAP[index])
+
+    def _axval2pwm(self, axval):
+
+        c = axval
+        a = -1
+        b = +1
+        z = PWMMAX
+        y = PWMMIN
+
+        return int((c - a) * (z - y) / (b - a) + y)
+
+    def _axis2pwm(self, index, sgn=+1):
+
+        return self._axval2pwm(sgn*self._axval(index))
  
 app = App()
+
 app.main()
