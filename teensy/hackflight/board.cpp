1c775aecb3da7c4c12d271d420ecd3c2ea71b184
simondlevy <simon.d.levy@gmail.com>
2016-10-10 17:34:07 -0400

    Works with bolderflight MPU9250 driver

--- teensy/hackflight/board.cpp
+++ teensy/hackflight/board.cpp
@@ -25,7 +25,7 @@
 
 #include <Arduino.h>
 #include <ArduinoRXInterrupt.h>
-#include <i2c_t3.h>
+#include <MPU9250.h>
 
 #include "board.hpp"
 #include "rc.hpp"
@@ -33,40 +33,12 @@
 #define IMU_LOOPTIME_USEC       3500
 #define CALIBRATING_GYRO_MSEC   3500
 
-#define PPM_INPUT_PIN           6
+// an MPU9250 object with its I2C address 
+// of 0x68 (ADDR to GRND) and on Teensy bus 0
+// using pins 16 and 17 instead of 18 and 19
+// and internal pullups instead of external.
+MPU9250 imu(0x68, 0, I2C_PINS_16_17, I2C_PULLUP_INT);
 
-#define MPU9250_ADDRESS  0x68  
-
-#define GYRO_CONFIG      0x1B
-#define ACCEL_CONFIG     0x1C
-#define ACCEL_CONFIG2    0x1D
-#define SMPLRT_DIV       0x19
-#define CONFIG           0x1A
-#define INT_PIN_CFG      0x37
-#define INT_ENABLE       0x38
-#define ACCEL_XOUT_H     0x3B
-#define GYRO_XOUT_H      0x43
-#define PWR_MGMT_1       0x6B // Device defaults to the SLEEP mode
-#define WHO_AM_I_MPU9250 0x75 // Should return 0x71
-
-// Set initial input parameters
-enum Ascale {
-    AFS_2G = 0,
-    AFS_4G,
-    AFS_8G,
-    AFS_16G
-};
-
-enum Gscale {
-    GFS_250DPS = 0,
-    GFS_500DPS,
-    GFS_1000DPS,
-    GFS_2000DPS
-};
-
-// Specify sensor full scale
-static uint8_t Gscale = GFS_2000DPS;
-static uint8_t Ascale = AFS_8G;
 
 // https://www.tindie.com/products/onehorse/dc-motor-controller-board-for-teensy-31-/
 // Multiwii M1 = Controller M1 = Pin 23
@@ -75,70 +47,10 @@ static uint8_t Ascale = AFS_8G;
 // Multiwii M4 = Controller M4 = Pin 22
 static const uint8_t MOTOR_PINS[4] = {23, 3, 4, 22};
 
-
-
+// for old-school PWM receiver like FrSky VD5M
 int RX_PINS[5] = {5, 6, 18, 19, 20};
 
-  
 
-// I^2C utility functions --------------------------------------------------------------
-
-static void writeByte(uint8_t address, uint8_t subAddress, uint8_t data)
-{
-    Wire.beginTransmission(address);  // Initialize the Tx buffer
-    Wire.write(subAddress);           // Put slave register address in Tx buffer
-    Wire.write(data);                 // Put data in Tx buffer
-    Wire.endTransmission();           // Send the Tx buffer
-}
-
-uint8_t readByte(uint8_t address, uint8_t subAddress)
-{
-    uint8_t data; // `data` will store the register data   
-    Wire.beginTransmission(address);         // Initialize the Tx buffer
-    Wire.write(subAddress);                  // Put slave register address in Tx buffer
-    Wire.endTransmission(I2C_NOSTOP);        // Send the Tx buffer, but send a restart to keep connection alive
-    //  Wire.endTransmission(false);             // Send the Tx buffer, but send a restart to keep connection alive
-    //  Wire.requestFrom(address, 1);  // Read one byte from slave register address 
-    Wire.requestFrom(address, (size_t) 1);  // Read one byte from slave register address 
-    data = Wire.read();                      // Fill Rx buffer with result
-    return data;                             // Return data read from slave register
-}
-
-static void readBytes(uint8_t address, uint8_t subAddress, uint8_t count, uint8_t * dest)
-{  
-    Wire.beginTransmission(address);   // Initialize the Tx buffer
-    Wire.write(subAddress);            // Put slave register address in Tx buffer
-    Wire.endTransmission(I2C_NOSTOP);  // Send the Tx buffer, but send a restart to keep connection alive
-    //  Wire.endTransmission(false);       // Send the Tx buffer, but send a restart to keep connection alive
-    uint8_t i = 0;
-    //        Wire.requestFrom(address, count);  // Read bytes from slave register address 
-    Wire.requestFrom(address, (size_t) count);  // Read bytes from slave register address 
-    while (Wire.available()) {
-        dest[i++] = Wire.read(); }         // Put read results in the Rx buffer
-}
-
-// IMU utility functions -----------------------------------------------------------------------
-
-static void readAccelData(int16_t * destination)
-{
-    uint8_t rawData[6];  // x/y/z accel register data stored here
-    readBytes(MPU9250_ADDRESS, ACCEL_XOUT_H, 6, &rawData[0]);  // Read the six raw data registers into data array
-    destination[0] = ((int16_t)rawData[0] << 8) | rawData[1] ;  // Turn the MSB and LSB into a signed 16-bit value
-    destination[1] = ((int16_t)rawData[2] << 8) | rawData[3] ;  
-    destination[2] = ((int16_t)rawData[4] << 8) | rawData[5] ; 
-}
-
-
-static void readGyroData(int16_t * destination)
-{
-    uint8_t rawData[6];  // x/y/z gyro register data stored here
-    readBytes(MPU9250_ADDRESS, GYRO_XOUT_H, 6, &rawData[0]);  // Read the six raw data registers sequentially into data array
-    destination[0] = ((int16_t)rawData[0] << 8) | rawData[1] ;  // Turn the MSB and LSB into a signed 16-bit value
-    destination[1] = ((int16_t)rawData[2] << 8) | rawData[3] ;  
-    destination[2] = ((int16_t)rawData[4] << 8) | rawData[5] ; 
-}
-
-// Board method implementations -------------------------------------------------------------------
 
 void Board::debug(char c)
 {
@@ -148,71 +60,13 @@ void Board::debug(char c)
 void Board::imuInit(uint16_t & acc1G, float & gyroScale)
 {
     // wake up device
-    writeByte(MPU9250_ADDRESS, PWR_MGMT_1, 0x00); // Clear sleep mode bit (6), enable all sensors 
-    delay(100); // Wait for all registers to reset 
-
-    // get stable time source
-    writeByte(MPU9250_ADDRESS, PWR_MGMT_1, 0x01);  // Auto select clock source to be PLL gyroscope reference if ready else
-    delay(200); 
-
-    // Configure Gyro and Thermometer
-    // Disable FSYNC and set thermometer and gyro bandwidth to 41 and 42 Hz, respectively; 
-    // minimum delay time for this setting is 5.9 ms, which means sensor fusion update rates cannot
-    // be higher than 1 / 0.0059 = 170 Hz
-    // DLPF_CFG = bits 2:0 = 011; this limits the sample rate to 1000 Hz for both
-    // With the MPU9250, it is possible to get gyro sample rates of 32 kHz (!), 8 kHz, or 1 kHz
-    writeByte(MPU9250_ADDRESS, CONFIG, 0x03);  
-
-    // Set sample rate = gyroscope output rate/(1 + SMPLRT_DIV)
-    writeByte(MPU9250_ADDRESS, SMPLRT_DIV, 0x04);  // Use a 200 Hz rate; a rate consistent with the filter update rate 
-    // determined inset in CONFIG above
-
-    // Set gyroscope full scale range
-    // Range selects FS_SEL and AFS_SEL are 0 - 3, so 2-bit values are left-shifted into positions 4:3
-    uint8_t c = readByte(MPU9250_ADDRESS, GYRO_CONFIG); // get current GYRO_CONFIG register value
-    // c = c & ~0xE0; // Clear self-test bits [7:5] 
-    c = c & ~0x02; // Clear Fchoice bits [1:0] 
-    c = c & ~0x18; // Clear AFS bits [4:3]
-    c = c | Gscale << 3; // Set full scale range for the gyro
-    // c =| 0x00; // Set Fchoice for the gyro to 11 by writing its inverse to bits 1:0 of GYRO_CONFIG
-    writeByte(MPU9250_ADDRESS, GYRO_CONFIG, c ); // Write new GYRO_CONFIG value to register
-
-    // Set accelerometer full-scale range configuration
-    c = readByte(MPU9250_ADDRESS, ACCEL_CONFIG); // get current ACCEL_CONFIG register value
-    // c = c & ~0xE0; // Clear self-test bits [7:5] 
-    c = c & ~0x18;  // Clear AFS bits [4:3]
-    c = c | Ascale << 3; // Set full scale range for the accelerometer 
-    writeByte(MPU9250_ADDRESS, ACCEL_CONFIG, c); // Write new ACCEL_CONFIG register value
-
-    // Set accelerometer sample rate configuration
-    // It is possible to get a 4 kHz sample rate from the accelerometer by choosing 1 for
-    // accel_fchoice_b bit [3]; in this case the bandwidth is 1.13 kHz
-    c = readByte(MPU9250_ADDRESS, ACCEL_CONFIG2); // get current ACCEL_CONFIG2 register value
-    c = c & ~0x0F; // Clear accel_fchoice_b (bit 3) and A_DLPFG (bits [2:0])  
-    c = c | 0x03;  // Set accelerometer rate to 1 kHz and bandwidth to 41 Hz
-    writeByte(MPU9250_ADDRESS, ACCEL_CONFIG2, c); // Write new ACCEL_CONFIG2 register value
-
-    // The accelerometer, gyro, and thermometer are set to 1 kHz sample rates, 
-    // but all these rates are further reduced by a factor of 5 to 200 Hz because of the SMPLRT_DIV setting
-
-    // Configure Interrupts and Bypass Enable
-    // Set interrupt pin active high, push-pull, hold interrupt pin level HIGH until interrupt cleared,
-    // clear on read of INT_STATUS, and enable I2C_BYPASS_EN so additional chips 
-    // can join the I2C bus and all can be controlled by the Arduino as master
-    writeByte(MPU9250_ADDRESS, INT_PIN_CFG, 0x22);    
-    writeByte(MPU9250_ADDRESS, INT_ENABLE, 0x01);  // Enable data ready (bit 0) interrupt
+    imu.begin(ACCEL_RANGE_8G,GYRO_RANGE_2000DPS);
  
     // Accel scale 8g (4096 LSB/g)
     acc1G = 4096;
 
     // 16.4 dps/lsb scalefactor for all Invensense devices
     gyroScale = (1.0f / 16.4f) * (M_PI / 180.0f);
-}
-
-void Board::imuRead(int16_t accADC[3], int16_t gyroADC[3])
-{
-    readAccelData(accADC);  
-    readGyroData(gyroADC);
 }
 
 void Board::init(uint32_t & looptimeMicroseconds, uint32_t & calibratingGyroMsec)
@@ -224,20 +78,25 @@ void Board::init(uint32_t & looptimeMicroseconds, uint32_t & calibratingGyroMsec
   
     // Set up LED
     pinMode(13, OUTPUT);
-    
-    // Set up I^2C
-    Wire.begin(I2C_MASTER, 0x00, I2C_PINS_16_17, I2C_PULLUP_INT, I2C_RATE_400);
-    
+
     // Set up PWM receiver
     initChannels(RX_PINS, 5);
 
     // Set up serial communication over USB
     Serial.begin(115200);
-    Serial1.begin(115200);
 
     // XXX these values should probably be #define'd generally for all physical (non-simulated) boards
     looptimeMicroseconds = IMU_LOOPTIME_USEC;
     calibratingGyroMsec  = CALIBRATING_GYRO_MSEC;
+}
+
+
+void Board::imuRead(int16_t accADC[3], int16_t gyroADC[3])
+{
+    imu.getMotion6Counts(&accADC[1], &accADC[0], &accADC[2], &gyroADC[1], &gyroADC[0], &gyroADC[2]);
+
+    accADC[2]  = -accADC[2];
+    gyroADC[2] = -gyroADC[2];
 }
 
 void Board::delayMilliseconds(uint32_t msec)
