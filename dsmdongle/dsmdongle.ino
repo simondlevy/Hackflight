21a846180383eb6ddded1142e9ec885466ebd1b9
simondlevy <simon.d.levy@gmail.com>
2017-01-19 13:38:51 -0500

    Added header comments

--- dsmdongle/dsmdongle.ino
+++ dsmdongle/dsmdongle.ino
@@ -1,5 +1,21 @@
+/*
+   dsmdongle.ino : Arduino/Teensy sketch for sending channel values from 
+   Spektrum DSM receiver to host computer.
 
-#include <Arduino.h>
+   This file is part of Hackflight.
+
+   Hackflight is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   Hackflight is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with Hackflight.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 
 // https://github.com/simondlevy/SpektrumDSM
 #include <SpektrumDSM.h>
