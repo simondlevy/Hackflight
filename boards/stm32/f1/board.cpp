66263f8aa9fbec09dc2a7c644a058e67b4c723fb
simondlevy <simon.d.levy@gmail.com>
2017-02-06 23:21:00 -0500

    More MPU6050 C++

--- boards/stm32/f1/board.cpp
+++ boards/stm32/f1/board.cpp
@@ -69,8 +69,8 @@ void Board::init(uint32_t & looptimeMicroseconds, uint32_t & calibratingGyroMsec
 
     Serial.begin(115200);
 
-    //Wire.begin();
-    i2cInit(I2CDEV_2);
+    Wire.begin();
+    //i2cInit(I2CDEV_2);
 
     pwmInit(USE_CPPM, PWM_FILTER, FAST_PWM, MOTOR_PWM_RATE, PWM_IDLE_PULSE);
 
