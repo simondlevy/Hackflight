ce76f512c761ce8360a6013feffcca72318ee31a
Simon D. Levy <simon.d.levy@gmail.com>
2017-02-24 12:38:55 -0500

    Cleanup

--- boards/teensy/hackflight/hackflight.hpp
+++ boards/teensy/hackflight/hackflight.hpp
@@ -46,4 +46,14 @@ void debug(const char * format, ...);
 #define constrain(val, lo, hi) (val) < (lo) ? lo : ((val) > hi ? hi : val) 
 #endif
 
-#include "config.hpp"
+// Config =====================================================
+
+#define CONFIG_MAGNETIC_DECLINATION                 0
+
+#define CONFIG_CALIBRATING_ACC_MSEC                 1400
+
+#define CONFIG_YAW_CONTROL_DIRECTION                1    // 1 or -1 
+#define CONFIG_RC_LOOPTIME_MSEC                     20
+#define CONFIG_CALIBRATE_ACCTIME_MSEC               500
+#define CONFIG_SMALL_ANGLE                          250  // tenths of a degree
+#define CONFIG_ALTITUDE_UPDATE_MSEC                 25   // based on accelerometer low-pass filter
--- boards/teensy/hackflight/rc.hpp
+++ boards/teensy/hackflight/rc.hpp
@@ -33,6 +33,8 @@ enum {
 
 // Define number of RC channels
 #define CONFIG_RC_CHANS 8
+#define CONFIG_PWM_MIN  990
+#define CONFIG_PWM_MAX  2010
 
 // For logical combinations of stick positions (low, center, high)
 #define ROL_LO (1 << (2 * DEMAND_ROLL))
