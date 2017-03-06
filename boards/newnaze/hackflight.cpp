e2c207250d7accdd04bca92be0dcf9384352b041
Simon D. Levy <simon.d.levy@gmail.com>
2017-03-06 00:42:06 -0500

    Successfully compiling & printing millisecond values

--- boards/newnaze/hackflight.cpp
+++ boards/newnaze/hackflight.cpp
@@ -1,29 +1,17 @@
 #include <hackflight.hpp>
 #include "naze.hpp"
 
-#include <stm32f10x_conf.h>
+#include <Arduino.h>
 
-#include <drv_gpio.h>
-#include <drv_system.h>
-#include <drv_serial.h>
-#include <drv_uart.h>
-#include <drv_serial.h>
+hf::Hackflight h;
 
-#include <stdlib.h>
-
-int main(void)
+void setup(void)
 {
-    void SetSysClock(bool overclock);
-    void systemInit(void);
+    Serial.begin(115200);
+    //h.init(new hf::Naze());
+}
 
-    SetSysClock(0);
-
-    systemInit();
-
-    hf::Hackflight h;
-    h.init(new hf::Naze());
-
-    while (true) {
-        h.update();
-    }
+void loop(void)
+{
+    Serial.printf("%ld\n", millis());
 }
