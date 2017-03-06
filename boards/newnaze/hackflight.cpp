1222866fa7c1fd18a6af25a2e488576528f846a6
simondlevy <simon.d.levy@gmail.com>
2017-03-06 15:28:04 -0500

    Cleanup

--- boards/newnaze/hackflight.cpp
+++ boards/newnaze/hackflight.cpp
@@ -9,27 +9,6 @@ void setup(void)
 {
     Serial.begin(115200);
     h.init(new hf::Naze());
-
-    /*
-    hf::Board * board = new hf::Naze();
-
-    board->init();
-
-    board->setLed(0, false);
-    board->setLed(1, false);
-
-    for (uint8_t i = 0; i < 10; i++) {
-        board->setLed(0, true);
-        board->setLed(1, false);
-        board->delayMilliseconds(50);
-        board->setLed(0, false);
-        board->setLed(1, true);
-        board->delayMilliseconds(50);
-     }
-
-    board->setLed(0, false);
-    board->setLed(1, false);
-    */
 }
 
 void loop(void)
