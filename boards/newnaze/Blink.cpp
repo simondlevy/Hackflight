448fc20d8f23d21a3c5bb0e29c482479da80504a
Simon D. Levy <simon.d.levy@gmail.com>
2017-03-05 23:30:46 -0500

    Attempting to remove Blink.cpp

--- boards/newnaze/Blink.cpp
+++
@@ -1,9 +0,0 @@
-#include <Arduino.h>
-
-// the setup routine runs once when you press reset:
-void setup() {                
-}
-
-// the loop routine runs over and over again forever:
-void loop() {
-}
