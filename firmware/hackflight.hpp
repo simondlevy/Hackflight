7e2db8373d2160a28bab77a7702f595a534debcd
simondlevy <simon.d.levy@gmail.com>
2017-03-09 02:01:00 -0500

    Cleanup

--- firmware/hackflight.hpp
+++ firmware/hackflight.hpp
@@ -39,12 +39,9 @@
 // Config =====================================================
 
 #define CONFIG_CALIBRATING_ACC_MSEC                 1400
-
-#define CONFIG_YAW_CONTROL_DIRECTION                1    // 1 or -1 
 #define CONFIG_RC_LOOPTIME_MSEC                     21
 #define CONFIG_CALIBRATE_ACCTIME_MSEC               500
 #define CONFIG_SMALL_ANGLE                          250  // tenths of a degree
-#define CONFIG_ALTITUDE_UPDATE_MSEC                 25   // based on accelerometer low-pass filter
 
 namespace hf {
 
