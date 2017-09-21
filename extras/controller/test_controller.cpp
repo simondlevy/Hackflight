e4c691271852e4c3d5db42bb3ec1af9d8a8f7283
Dr. Simon Levy <slevy@fred.cs.wlu.edu>
2017-09-21 18:37:58 -0400

    Move new Controller class to src/receivers/controller

--- extras/controller/test_controller.cpp
+++
@@ -1,7 +0,0 @@
-#include "controller.hpp"
-
-int main(int argc, char ** argv)
-{
-
-    return 0;
-}
---
+++ src/receivers/controller/test_controller.cpp
@@ -0,0 +1,9 @@
+#include "hackflight.hpp"
+#include "controller2.hpp"
+
+int main(int argc, char ** argv)
+{
+    hf::Controller controller;
+
+    return 0;
+}
