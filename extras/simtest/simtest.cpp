beb74ea3d294585bfb037e6e17358677e5272168
Dr. Simon Levy <slevy@fred.cs.wlu.edu>
2017-10-20 21:04:08 -0400

    =Moar simtest

--- extras/simtest/simtest.cpp
+++ extras/simtest/simtest.cpp
@@ -2,6 +2,43 @@
 
 #include <hackflight.hpp>
 
+namespace hf {
+
+class NullBoard : public Board {
+
+    virtual void init(Config& config) override
+    {
+        (void)config;
+    }
+
+    virtual void delayMilliseconds(uint32_t msec) override
+    {
+    }
+
+    virtual void getImu(float eulerAnglesRadians[3], int16_t gyroRaw[3]) override
+    {
+        for (int k=0; k<3; ++k) {
+            eulerAnglesRadians[k] = 0;
+            gyroRaw[k] = 0;
+        }
+    }
+
+    virtual uint64_t getMicros() override
+    {
+        return 0;
+    }
+
+    virtual void writeMotor(uint8_t index, float value) override
+    {
+        (void)index;
+        (void)value;
+    }
+
+
+}; // class NullBoard
+
+} // namespace hf
+
 int main(int argc, char ** argv)
 {
     return 0;
