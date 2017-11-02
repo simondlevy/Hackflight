a79e5f0a687784d946c25ea1579bc0bc7d90a654
simondlevy <simon.d.levy@gmail.com>
2017-11-02 12:58:08 -0400

    Simplfied receiver API

--- src/hackflight.hpp
+++ src/hackflight.hpp
@@ -211,7 +211,7 @@ void Hackflight::updateRc(void)
         auxState = receiver->getAuxState();
         if (board->extrasHaveBaro()) {
             if (auxState > 0) {
-                //alti.start(receiver->commands[DEMAND_THROTTLE]);
+                //alti.start(receiver->demandThrottle);
             }
             else {
                 //alti.stop();
@@ -246,15 +246,14 @@ void Hackflight::updateImu(void)
     // If barometer avaialble, update accelerometer for altitude fusion, then modify throttle demand
     if (board->extrasHaveBaro()) {
         alti.updateAccelerometer(eulerAnglesRadians, armed);
-        //alti.modifyThrottleDemand(receiver->commands[DEMAND_THROTTLE]);
+        //alti.modifyThrottleDemand(receiver->demandThrottle);
     }
 
-   // Stabilization is synced to IMU update.  Stabilizer also uses raw gyro values.
-    stab.update(receiver->commands[DEMAND_ROLL], receiver->commands[DEMAND_PITCH], receiver->commands[DEMAND_YAW],
-            gyroRaw, eulerAnglesDegrees);
+   // Stabilization is synced to IMU update.  Stabilizer also uses RC demands and raw gyro values.
+    stab.update(receiver->demandRoll, receiver->demandPitch, receiver->demandYaw, gyroRaw, eulerAnglesDegrees);
 
     // Update mixer
-    mixer.update(receiver->commands[DEMAND_THROTTLE], stab.pidRoll, stab.pidPitch, stab.pidYaw, armed);
+    mixer.update(receiver->demandThrottle, stab.pidRoll, stab.pidPitch, stab.pidYaw, armed);
 
     // Update serial comms
     msp.update(eulerAnglesDegrees, armed);
--- src/receiver.hpp
+++ src/receiver.hpp
@@ -39,36 +39,39 @@ protected:
 
 private:
 
-    void adjustCommand(uint8_t channel);
+    float adjustCommand(float command, uint8_t channel);
 
-    void applyPitchRollFunction(uint8_t channel);
-    void makePositiveCommand(uint8_t channel);
+    float applyPitchRollFunction(float command);
+    float makePositiveCommand(uint8_t channel);
 
     static float rcFun(float x, float e, float r);
     static float throttleFun(float x, float e, float m);
 
-    uint8_t commandDelay;                               // cycles since most recent movement
-    int32_t averageIndex;
+    uint8_t commandDelay;     // cycles since most recent movement
+    int32_t ppmAverageIndex;  // help with noisy CPPM receivers
 
     ReceiverConfig config;
 
 public:
 
+    float   rawvals[CONFIG_RC_CHANS];  // raw [-1,+1] from receiver, for MSP
+
+    uint8_t sticks;                    // stick positions for command combos
+
+    float   demandThrottle;
+    float   demandRoll;
+    float   demandPitch;
+    float   demandYaw;
+
     void    init(const ReceiverConfig& rcConfig);
     void    update(void);
-    float   rawvals[CONFIG_RC_CHANS];  // raw [-1,+1] from receiver
-    float   commands[4];
-    uint8_t sticks;                // stick positions for command combos
     bool    changed(void);
     void    computeExpo(void);
     uint8_t getAuxState(void);
     bool    throttleIsDown(void);
 
-    int16_t scaleup(float x, float in_min, float in_max, int16_t out_min, int16_t out_max);
-
     // Override this if your receiver provides RSSI or other weak-signal detection
-    virtual bool     lostSignal(void) { return false; }
-
+    virtual bool lostSignal(void) { return false; }
 };
 
 
@@ -83,7 +86,7 @@ void Receiver::init(const ReceiverConfig& rcConfig)
 
     commandDelay = 0;
     sticks = 0;
-    averageIndex = 0;
+    ppmAverageIndex = 0;
 }
 
 void Receiver::update()
@@ -100,13 +103,13 @@ void Receiver::update()
     // Other kinds of receivers require average of channel values to remove noise
     else {
         for (uint8_t chan = 0; chan < 5; chan++) {
-            averageRaw[chan][averageIndex % 4] = readChannel(chan);
+            averageRaw[chan][ppmAverageIndex % 4] = readChannel(chan);
             rawvals[chan] = 0;
             for (uint8_t i = 0; i < 4; i++)
                 rawvals[chan] += averageRaw[chan][i];
             rawvals[chan] /= 4;
         }
-        averageIndex++;
+        ppmAverageIndex++;
     }
 
     // check stick positions, updating command delay
@@ -134,31 +137,36 @@ bool Receiver::changed(void)
 void Receiver::computeExpo(void)
 {
     // Convert raw [-1,+1] to absolute value
-    makePositiveCommand(DEMAND_ROLL);
-    makePositiveCommand(DEMAND_PITCH);
-    makePositiveCommand(DEMAND_YAW);
+    demandRoll  = makePositiveCommand(DEMAND_ROLL);
+    demandPitch = makePositiveCommand(DEMAND_PITCH);
+    demandYaw   = makePositiveCommand(DEMAND_YAW);
 
     // Apply expo nonlinearity to roll, pitch
-    applyPitchRollFunction(DEMAND_ROLL);
-    applyPitchRollFunction(DEMAND_PITCH);
+    demandRoll  = applyPitchRollFunction(demandRoll);
+    demandPitch = applyPitchRollFunction(demandPitch);
 
     // Put sign back on command, yielding [-0.5,+0.5]
-    adjustCommand(DEMAND_ROLL);
-    adjustCommand(DEMAND_PITCH);
-    adjustCommand(DEMAND_YAW);
+    demandRoll  = adjustCommand(demandRoll, DEMAND_ROLL);
+    demandPitch = adjustCommand(demandPitch, DEMAND_PITCH);
+    demandYaw   = adjustCommand(demandYaw, DEMAND_YAW);
 
     // Yaw demand needs to be reversed
-    commands[DEMAND_YAW] = -commands[DEMAND_YAW];
+    demandYaw = -demandYaw;
 
     // Special handling for throttle
     float tmp = (rawvals[DEMAND_THROTTLE] + 1) / 2; // [-1,+1] -> [0,1]
-    commands[DEMAND_THROTTLE] = throttleFun(tmp, config.throttleExpo, config.throttleMid);
+    demandThrottle = throttleFun(tmp, config.throttleExpo, config.throttleMid);
 
 } // computeExp
 
-void Receiver::applyPitchRollFunction(uint8_t channel)
+float Receiver::makePositiveCommand(uint8_t channel)
 {
-    commands[channel] = rcFun(commands[channel], config.pitchRollExpo, config.pitchRollRate);
+    return std::abs(rawvals[channel]);
+}
+
+float Receiver::applyPitchRollFunction(float command)
+{
+    return rcFun(command, config.pitchRollExpo, config.pitchRollRate);
 }
 
 float Receiver::rcFun(float x, float e, float r)
@@ -166,25 +174,22 @@ float Receiver::rcFun(float x, float e, float r)
     return (1 + e*(x*x - 1)) * x * r;
 }
 
+float Receiver::adjustCommand(float command, uint8_t channel)
+{
+    command /= 2;
+
+    if (rawvals[channel] < 0) {
+        command = -command;
+    }
+
+    return command;
+}
+
 float Receiver::throttleFun(float x, float e, float mid)
 {
     float tmp   = x - mid;
     float y = tmp>0 ? 1-mid : (tmp<0 ? mid : 1);
     return mid + tmp*(1-e + e * (tmp*tmp) / (y*y));
-}
-
-void Receiver::makePositiveCommand(uint8_t channel)
-{
-    commands[channel] = std::abs(rawvals[channel]);
-}
-
-void Receiver::adjustCommand(uint8_t channel)
-{
-    commands[channel] /= 2;
-
-    if (rawvals[channel] < 0) {
-        commands[channel] = -commands[channel];
-    }
 }
 
 uint8_t Receiver::getAuxState(void) 
@@ -197,11 +202,6 @@ uint8_t Receiver::getAuxState(void)
 bool Receiver::throttleIsDown(void)
 {
     return rawvals[DEMAND_THROTTLE] < -1 + config.margin;
-}
-
-int16_t Receiver::scaleup(float x, float in_min, float in_max, int16_t out_min, int16_t out_max)
-{
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
 } // namespace
