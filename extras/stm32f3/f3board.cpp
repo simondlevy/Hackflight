66b174aef43eeb2d301d27e1cd72b7201189f783
simondlevy <simon.d.levy@gmail.com>
2018-08-07 14:02:55 -0400

    Begin F3Board implementation

---
+++ extras/stm32f3/f3board.cpp
@@ -0,0 +1,23 @@
+/*
+f3board.cpp : Board implementation for STM32F3 boards
+
+Copyright (C) 2018 Simon D. Levy 
+
+This file is part of Hackflight.
+
+Hackflight is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Hackflight is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Hackflight.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "f3board.h"
+
--- src/board.hpp
+++ src/board.hpp
@@ -21,6 +21,7 @@
 #pragma once
 
 #include <stdarg.h>
+#include <stdint.h>
 
 namespace hf {
 
