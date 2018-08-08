23ae00d1e0c22421f07108a491b8d03a2e9ee424
simondlevy <simon.d.levy@gmail.com>
2018-08-08 14:58:57 -0400

    Successfully detecting MPU6050

--- extras/stm32f3/Wire.h
+++ extras/stm32f3/Wire.h
@@ -1,22 +1,22 @@
 /*
-Wire.h : Arduino API header for BreezySTM32 I^2C library
+Wire.h : Arduino-style API for SMT32F boards
 
-Copyright (C) 2017 Simon D. Levy 
+Copyright (C) 2018 Simon D. Levy 
 
-This file is part of BreezySTM32.
+This file is part of Hackflight.
 
-BreezySTM32 is free software: you can redistribute it and/or modify
+Hackflight is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-BreezySTM32 is distributed in the hope that it will be useful,
+Hackflight is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with BreezySTM32.  If not, see <http://www.gnu.org/licenses/>.
+along with Hackflight.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 extern "C" {
