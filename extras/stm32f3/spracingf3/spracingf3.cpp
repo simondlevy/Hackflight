4c6683ab7af449c9dfdb982790fbf73816f34dac
simondlevy <simon.d.levy@gmail.com>
2018-08-13 21:52:24 -0400

    Begin SPRacingF3 implementation

---
+++ extras/stm32f3/spracingf3/spracingf3.cpp
@@ -0,0 +1,84 @@
+/*
+   spracingf3.cpp Board class implementation for F3Board
+
+   Copyright (c) 2018 Simon D. Levy
+
+   This file is part of Hackflight.
+
+   Hackflight is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Hackflight is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with Hackflight.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <f3_board.h>
+#include <motor.h>
+#include <debug.hpp>
+#include <MPU6050.h>
+#include <Wire.h>
+
+void F3Board::imuInit(void)
+{
+    Wire.begin(1);
+
+    MPU6050 * imu = new MPU6050(AFS_2G, GFS_250DPS);
+
+    switch (imu->begin()) {
+
+        case MPU_ERROR_ID:
+            error("Bad device ID");
+            break;
+        case MPU_ERROR_SELFTEST:
+            error("Failed self-test");
+            break;
+        default:
+            break;
+    }
+ 
+    _imu = imu;
+}
+
+bool F3Board::imuRead(void)
+{
+    MPU6050 * imu = (MPU6050 *)_imu;
+
+    if (imu->checkNewData()) {  
+
+        imu->readAccelerometer(_ax, _ay, _az);
+        imu->readGyrometer(_gx, _gy, _gz);
+
+        return true;
+
+    }  
+
+    return false;
+}
+
+void F3Board::writeMotor(uint8_t index, float value)
+{
+    (void)index;
+    (void)value;
+}
+
+void F3Board::motorInit(void)
+{
+}
+
+extern "C" {
+
+#include "platform.h"
+#include "serial.h"
+
+    USART_TypeDef * getDsmUart()
+    {
+        return USART2;
+    }
+
+} // extern "C"
---
+++ extras/stm32f3/spracingf3/target.h
@@ -0,0 +1,154 @@
+/*
+ * This file is part of Hackflight.
+ *
+ * Hackflight is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Hackflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Hackflight.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define TARGET_BOARD_IDENTIFIER "SRF3"
+
+#define DEFAULT_PID_PROCESS_DENOM 1
+#define DEFAULT_GYRO_SAMPLE_HZ 2000
+
+#define LED0_GPIO   GPIOB
+#define LED0_PIN    Pin_3
+#define LED0_PERIPHERAL RCC_AHBPeriph_GPIOB
+
+#define BEEP_GPIO   GPIOC
+#define BEEP_PIN    Pin_15
+#define BEEP_PERIPHERAL RCC_AHBPeriph_GPIOC
+#define BEEPER_INVERTED
+
+#define USABLE_TIMER_CHANNEL_COUNT 17
+
+#define USE_MPU_DATA_READY_SIGNAL
+#define ENSURE_MPU_DATA_READY_IS_LOW
+
+#define USE_MAG_DATA_READY_SIGNAL
+#define ENSURE_MAG_DATA_READY_IS_HIGH
+
+
+#define GYRO
+#define USE_GYRO_MPU6050
+#define GYRO_MPU6050_ALIGN CW270_DEG
+
+#define ACC
+#define USE_ACC_MPU6050
+#define ACC_MPU6050_ALIGN CW270_DEG
+
+#define LED0
+
+#define USE_UART1
+#define USE_UART2
+#define USE_UART3
+#define USE_SOFTSERIAL1
+#define USE_SOFTSERIAL2
+#define SERIAL_PORT_COUNT 5
+
+#define USE_UART1_TX_DMA
+#define UART1_TX_PIN        GPIO_Pin_9  // PA9
+#define UART1_RX_PIN        GPIO_Pin_10 // PA10
+#define UART1_GPIO          GPIOA
+#define UART1_GPIO_AF       GPIO_AF_7
+#define UART1_TX_PINSOURCE  GPIO_PinSource9
+#define UART1_RX_PINSOURCE  GPIO_PinSource10
+
+#define UART2_TX_PIN        GPIO_Pin_14 // PA14 / SWCLK
+#define UART2_RX_PIN        GPIO_Pin_15 // PA15
+#define UART2_GPIO          GPIOA
+#define UART2_GPIO_AF       GPIO_AF_7
+#define UART2_TX_PINSOURCE  GPIO_PinSource14
+#define UART2_RX_PINSOURCE  GPIO_PinSource15
+
+#define UART3_TX_PIN        GPIO_Pin_10 // PB10 (AF7)
+#define UART3_RX_PIN        GPIO_Pin_11 // PB11 (AF7)
+#define UART3_GPIO_AF       GPIO_AF_7
+#define UART3_GPIO          GPIOB
+#define UART3_TX_PINSOURCE  GPIO_PinSource10
+#define UART3_RX_PINSOURCE  GPIO_PinSource11
+
+#define SOFTSERIAL_1_TIMER TIM3
+#define SOFTSERIAL_1_TIMER_RX_HARDWARE 4 // PWM 5
+#define SOFTSERIAL_1_TIMER_TX_HARDWARE 5 // PWM 6
+#define SOFTSERIAL_2_TIMER TIM3
+#define SOFTSERIAL_2_TIMER_RX_HARDWARE 6 // PWM 7
+#define SOFTSERIAL_2_TIMER_TX_HARDWARE 7 // PWM 8
+
+#define USE_I2C
+#define I2C_DEVICE (I2CDEV_1) // PB6/SCL, PB7/SDA
+
+#define USE_SPI
+#define USE_SPI_DEVICE_2 // PB12,13,14,15 on AF5
+
+#define M25P16_CS_GPIO          GPIOB
+#define M25P16_CS_PIN           GPIO_Pin_12
+#define M25P16_SPI_INSTANCE     SPI2
+
+#define USE_ADC
+#define BOARD_HAS_VOLTAGE_DIVIDER
+
+#define ADC_INSTANCE                ADC2
+#define ADC_DMA_CHANNEL             DMA2_Channel1
+#define ADC_AHB_PERIPHERAL          RCC_AHBPeriph_DMA2
+
+#define ADC0_GPIO                   GPIOA
+#define ADC0_GPIO_PIN               GPIO_Pin_4
+#define ADC0_CHANNEL                ADC_Channel_1
+
+#define ADC1_GPIO                   GPIOA
+#define ADC1_GPIO_PIN               GPIO_Pin_5
+#define ADC1_CHANNEL                ADC_Channel_2
+
+#define ADC2_GPIO                   GPIOB
+#define ADC2_GPIO_PIN               GPIO_Pin_2
+#define ADC2_CHANNEL                ADC_Channel_12
+
+#define ADC_CHANNEL_COUNT 3
+
+#define ADC_BATTERY     ADC_CHANNEL0
+#define ADC_AMPERAGE     ADC_CHANNEL1
+#define ADC_RSSI        ADC_CHANNEL2
+
+#define LED_STRIP
+#define LED_STRIP_TIMER TIM1
+
+#define WS2811_GPIO                     GPIOA
+#define WS2811_GPIO_AHB_PERIPHERAL      RCC_AHBPeriph_GPIOA
+#define WS2811_GPIO_AF                  GPIO_AF_6
+#define WS2811_PIN                      GPIO_Pin_8
+#define WS2811_PIN_SOURCE               GPIO_PinSource8
+#define WS2811_TIMER                    TIM1
+#define WS2811_TIMER_APB2_PERIPHERAL    RCC_APB2Periph_TIM1
+#define WS2811_DMA_CHANNEL              DMA1_Channel2
+#define WS2811_IRQ                      DMA1_Channel2_IRQn
+#define WS2811_DMA_TC_FLAG              DMA1_FLAG_TC2
+#define WS2811_DMA_HANDLER_IDENTIFER    DMA1Channel2Descriptor
+
+
+#define DEFAULT_RX_FEATURE FEATURE_RX_PPM
+
+#define USE_EXTI
+
+// UART3,
+#define BIND_PORT  GPIOB
+#define BIND_PIN   Pin_11
+
+#define USE_SERIAL_4WAY_BLHELI_INTERFACE
+
+// IO - stm32f303cc in 48pin package
+#define TARGET_IO_PORTA 0xffff
+#define TARGET_IO_PORTB 0xffff
+#define TARGET_IO_PORTC (BIT(13)|BIT(14)|BIT(15))
+#define TARGET_IO_PORTF (BIT(0)|BIT(1))
