36620af525309a02a99b8eb8b3310ebeb269b9d9
simondlevy <simon.d.levy@gmail.com>
2018-08-17 13:49:40 -0400

    =Cleanup

--- extras/newstm32f/spracingf3/sketch.cpp
+++ extras/newstm32f/spracingf3/sketch.cpp
@@ -1,37 +1,36 @@
+#include <hackflight.hpp>
+
 extern "C" {
 
-#include <stdbool.h>
-#include <stdint.h>
-
+// Cleanflight includes
 #include "platform.h"
-
 #include "drivers/io.h"
 #include "drivers/system.h"
 #include "drivers/timer.h"
 #include "drivers/time.h"
 #include "drivers/pwm_output.h"
 
-#define BRUSHED_MOTORS_PWM_RATE 16000
-#define BRUSHLESS_MOTORS_PWM_RATE 480
+    static const uint16_t BRUSHED_MOTORS_PWM_RATE   = 16000;
+    static const uint16_t BRUSHLESS_MOTORS_PWM_RATE = 480;
 
-typedef enum {
-    MOTOR_UNKNOWN = 0,
-    MOTOR_BRUSHED,
-    MOTOR_BRUSHLESS
-} HardwareMotorTypes_e;
+    typedef enum {
+        MOTOR_UNKNOWN = 0,
+        MOTOR_BRUSHED,
+        MOTOR_BRUSHLESS
+    } HardwareMotorTypes_e;
 
-static float motorval;
+    static float motorval;
 
-void setup(void)
-{
-    motorval = .01;
-}
+    void setup(void)
+    {
+        motorval = .01;
+    }
 
-void loop(void)
-{
-    pwmWriteMotor(0, motorval);
-    motorval += .01;
-    delay(10);
-}
+    void loop(void)
+    {
+        pwmWriteMotor(0, motorval);
+        motorval += .01;
+        delay(10);
+    }
 
 } // extern "C"
