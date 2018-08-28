6af72d83e3cea2a610926f91af0bc14ecd46f442
simondlevy <simon.d.levy@gmail.com>
2018-08-27 22:50:15 -0400

    =More Hyperion F3 Evo Brushed Board implementation

--- extras/stm32f/f3evobrushed/mpu6500.cpp
+++ extras/stm32f/f3evobrushed/mpu6500.cpp
@@ -90,10 +90,9 @@ enum accel_fsr_e {
 
     bool MPU6500::read(int16_t & ax, int16_t & ay, int16_t & az, int16_t & gx, int16_t & gy, int16_t & gz)
     {
+        /*
         static const uint8_t dataToSend[7] = {GYRO_XOUT_H | 0x80, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-
         uint8_t data[7];
-
         if (!spiBusTransfer(_bus, dataToSend, data, 7)) {
             return false;
         }
@@ -101,12 +100,22 @@ enum accel_fsr_e {
         gy = (int16_t)((data[1] << 8) | data[2]);
         gx = (int16_t)((data[3] << 8) | data[4]);
         gz = (int16_t)((data[5] << 8) | data[6]);
+        */
+
+        uint8_t data[6];
+        
+        if (!spiBusReadRegisterBuffer(_bus, GYRO_XOUT_H | 0x80, data, 6)) {
+            return false;
+        }
+
+        gx = (int16_t)((data[0] << 8) | data[1]);
+        gy = (int16_t)((data[2] << 8) | data[3]);
+        gz = (int16_t)((data[4] << 8) | data[5]);
 
         spiBusReadRegisterBuffer(_bus, ACCEL_XOUT_H | 0x80, data, 6);
 
-        // Note reversed X/Y order because of IMU rotation
-        ax = (int16_t)((data[0] << 8) | data[3]);
-        ay = (int16_t)((data[2] << 8) | data[1]);
+        ax = (int16_t)((data[0] << 8) | data[1]);
+        ay = (int16_t)((data[2] << 8) | data[3]);
         az = (int16_t)((data[4] << 8) | data[5]);
 
         return true;
