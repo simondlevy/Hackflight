bb3d97d2bd6776f5029d613e795fb5acbd853798
simondlevy <simon.d.levy@gmail.com>
2018-09-04 18:10:30 -0400

    Begin BetaFPVF3 sketch

---
+++ extras/stm32f/boards/betafpvf3/betafpvf3.cpp
@@ -0,0 +1,173 @@
+/*
+   betafpvf3.h : Board implmentation for BetaFPV F3 Brushed board
+
+   Copyright (C) 2018 Simon D. Levy 
+
+   This file is part of Hackflight.
+
+   Hackflight is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Hackflight is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Hackflight.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "betafpvf3.h"
+
+static const uint16_t BRUSHED_PWM_RATE     = 32000;
+static const uint16_t BRUSHED_IDLE_PULSE   = 0; 
+
+static const float    MOTOR_MIN = 1000;
+static const float    MOTOR_MAX = 2000;
+
+// Here we put code that interacts with Cleanflight
+extern "C" {
+
+    // Cleanflight includes
+#include "platform.h"
+#include "drivers/system.h"
+#include "drivers/timer.h"
+#include "drivers/time.h"
+#include "drivers/pwm_output.h"
+#include "drivers/light_led.h"
+#include "drivers/serial.h"
+#include "drivers/serial_uart.h"
+#include "drivers/serial_usb_vcp.h"
+#include "io/serial.h"
+#include "target.h"
+#include "stm32f30x.h"
+
+    // Hackflight includes
+#include "../../common/spi.h"
+
+    static serialPort_t * _serial0;
+
+    BetaFPVF3::BetaFPVF3(void)
+    {
+        initMotors();
+        initUsb();
+        initImu();
+
+        RealBoard::init();
+    }
+
+    void BetaFPVF3::initImu(void)
+    {
+        spi_init(MPU6000_SPI_INSTANCE, IOGetByTag(IO_TAG(MPU6000_CS_PIN)));
+
+        _imu = new MPU6000(MPUIMU::AFS_2G, MPUIMU::GFS_250DPS);
+
+        switch (_imu->begin()) {
+
+            case MPUIMU::ERROR_IMU_ID:
+                error("Bad device ID");
+                break;
+            case MPUIMU::ERROR_SELFTEST:
+                error("Failed self-test");
+                break;
+            default:
+                break;
+        }
+    }
+
+    void BetaFPVF3::initUsb(void)
+    {
+        _serial0 = usbVcpOpen();
+    }
+
+    void BetaFPVF3::initMotors(void)
+    {
+        motorDevConfig_t dev;
+
+        dev.motorPwmRate = BRUSHED_PWM_RATE;
+        dev.motorPwmProtocol = PWM_TYPE_BRUSHED;
+        dev.motorPwmInversion = false;
+        dev.useUnsyncedPwm = true;
+        dev.useBurstDshot = false;
+
+        dev.ioTags[0] = timerioTagGetByUsage(TIM_USE_MOTOR, 0);
+        dev.ioTags[1] = timerioTagGetByUsage(TIM_USE_MOTOR, 1);
+        dev.ioTags[2] = timerioTagGetByUsage(TIM_USE_MOTOR, 2);
+        dev.ioTags[3] = timerioTagGetByUsage(TIM_USE_MOTOR, 3);
+
+        motorDevInit(&dev, BRUSHED_IDLE_PULSE, 4);
+
+        pwmEnableMotors();
+    }
+
+    void BetaFPVF3::writeMotor(uint8_t index, float value)
+    {
+        pwmWriteMotor(index, MOTOR_MIN + value*(MOTOR_MAX-MOTOR_MIN));
+    }
+
+    void BetaFPVF3::delaySeconds(float sec)
+    {
+        delay((uint16_t)(sec*1000));
+    }
+
+    void BetaFPVF3::setLed(bool isOn)
+    {
+        ledSet(0, isOn);
+    }
+
+    uint32_t BetaFPVF3::getMicroseconds(void)
+    {
+        return micros();
+    }
+
+    void BetaFPVF3::reboot(void)
+    {
+        systemResetToBootloader();
+    }
+
+    uint8_t BetaFPVF3::serialAvailableBytes(void)
+    {
+        return serialRxBytesWaiting(_serial0);
+    }
+
+    uint8_t BetaFPVF3::serialReadByte(void)
+    {
+        return serialRead(_serial0);
+    }
+
+    void BetaFPVF3::serialWriteByte(uint8_t c)
+    {
+        serialWrite(_serial0, c);
+    }
+
+    bool BetaFPVF3::imuRead(void)
+    {
+        if (_imu->checkNewData()) {  
+
+            _imu->readAccelerometer(_ax, _ay, _az);
+            _imu->readGyrometer(_gx, _gy, _gz);
+
+            // Negate for IMU orientation
+            _ax = -_ax;
+            _ay = -_ay;
+            _gx = -_gx;
+            _gy = -_gy;
+
+            //hf::Debug::printf("%d\n", micros());
+
+            return true;
+        }  
+
+        return false;
+    }
+
+    void hf::Board::outbuf(char * buf)
+    {
+        for (char *p=buf; *p; p++)
+            serialWrite(_serial0, *p);
+    }
+
+
+} // extern "C"
---
+++ extras/stm32f/boards/betafpvf3/sketch.cpp
@@ -0,0 +1,54 @@
+/*
+   Sketch for Furious BetaFPV F3 brushed board with Spektrum DSMX receiver
+
+   Copyright (c) 2018 Simon D. Levy
+
+   This file is part of Hackflight.
+
+   Hackflight is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Hackflight is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with Hackflight.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <hackflight.hpp>
+#include <mixers/quadx.hpp>
+#include "betafpvf3.h"
+
+constexpr uint8_t CHANNEL_MAP[6] = {0, 1, 2, 3, 6, 4};
+
+static hf::Hackflight h;
+
+extern "C" {
+
+#include "../../common/dsmx.h"
+
+    void setup(void)
+    {
+           hf::Stabilizer * stabilizer = new hf::Stabilizer(
+           0.10f,      // Level P
+           0.125f,     // Gyro cyclic P
+           0.001875f,  // Gyro cyclic I
+           0.175f,     // Gyro cyclic D
+           0.625f,    // Gyro yaw P
+           0.005625f); // Gyro yaw I
+         
+        DSMX_Receiver * rc = new DSMX_Receiver(UARTDEV_2, CHANNEL_MAP);
+
+        // Initialize Hackflight firmware
+        h.init(new BetaFPVF3(), rc, new hf::MixerQuadX(), stabilizer);
+    }
+
+    void loop(void)
+    {
+        h.update();
+    }
+
+} // extern "C"
