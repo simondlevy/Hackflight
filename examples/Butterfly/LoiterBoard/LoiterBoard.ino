a91fc9f9da9aee67d80168343cf33740aabefd37
simondlevy <simon.d.levy@gmail.com>
2018-09-13 13:48:03 -0400

    Reading flow sensor periodically

--- examples/Butterfly/LoiterBoard/LoiterBoard.ino
+++ examples/Butterfly/LoiterBoard/LoiterBoard.ino
@@ -12,7 +12,7 @@
 
    This file is part of Hackflight.
 
-   Hackflight is free software: you can redistribute it and/or modify
+   Hackflight is free software: you can redistribute it and/or modiflowy
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
@@ -32,6 +32,8 @@
 
 //#include "hackflight.hpp"
 
+static uint16_t FLOW_UPDATE_HZ = 20;
+
 static const uint8_t VCC_PIN = A0;
 static const uint8_t GND_PIN = A1;
 
@@ -40,6 +42,8 @@ static const uint8_t CS_PIN  = 10;
 static VL53L1X _distanceSensor;
 
 static Bitcraze_PMW3901 _flowSensor(CS_PIN);
+
+static uint32_t _flowUpdateMicros = 1000000 / FLOW_UPDATE_HZ;
 
 static void powerPin(uint8_t pin, uint8_t value)
 {
@@ -84,24 +88,29 @@ void loop(void)
 {
     // Declare measurement variables static so they'll persist between calls to loop()
     static uint16_t distance;
+    static int16_t flowx, flowy;
 
+    // Read distance sensor when its data is available
     if (_distanceSensor.newDataReady()) {
         distance = _distanceSensor.getDistance(); //Get the result of the measurement from the sensor
     }
 
+    // Read flow sensor periodically
+    static uint32_t _time;
+    uint32_t time = micros();
+    if (time-_time > _flowUpdateMicros) {
+        _flowSensor.readMotionCount(&flowx, &flowy);
+        _time = time;
+    }
+
     Serial.print("Distance: ");
     Serial.print(distance);
-    Serial.println(" mm");
+    Serial.print(" mm; Flow: ");
+    Serial.print(flowx);
+    Serial.print(" ");
+    Serial.println(flowy);
 
     /*
-    int16_t fx = 0, fy = 0;
-    _flowSensor.readMotionCount(&fx, &fy);
-    Serial.print(fx);
-    Serial.print(" ");
-    Serial.println(fy);
-
-    delay(50);
-
     static uint8_t c;
     Serial1.write(c);
     c = (c+1) % 0xFF;
