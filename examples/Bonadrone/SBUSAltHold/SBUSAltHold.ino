0f551b8d9b54033204bbb43ca86029c7cff0abbf
Juan Gallostra <juangallostra@gmail.com>
2018-10-17 21:40:49 +0200

    Add alt hold controller and sketch
    More altitude hold work
    
    Fix integral error computation
    
    Remove unused variable
    
    Take time into account

---
+++ examples/Bonadrone/SBUSAltHold/SBUSAltHold.ino
@@ -0,0 +1,129 @@
+/*
+   SBUSAltHold.ino : Hackflight sketch for Bonadrone flight controller with SBUS receiver
+   that allows altitude hold flight mode
+
+   Additional libraries needed:
+
+       https://github.com/simondlevy/LSM6DSM
+       https://github.com/simondlevy/CrossPlatformDataBus
+       https://github.com/simondlevy/SBUSRX
+       https://github.com/simondlevy/VL53L1X
+
+   Hardware support for Bonadrone flight controller:
+
+       https://github.com/simondlevy/grumpyoldpizza
+
+   Copyright (c) 2018 Juan Gallostra & Simon D. Levy
+
+   This file is part of Hackflight.
+
+   Hackflight is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Hackflight is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with Hackflight.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <Arduino.h>
+#include <VL53L1X.h>
+
+#include "hackflight.hpp"
+#include "boards/bonadrone.hpp"
+#include "receivers/sbus.hpp"
+#include "mixers/quadx.hpp"
+
+#include "sensors/rangefinder.hpp"
+
+#include "pidcontrollers/level.hpp"
+#include "pidcontrollers/althold.hpp"
+
+class VL53L1X_Rangefinder : public hf::Rangefinder {
+
+    private:
+
+        VL53L1X _distanceSensor;
+
+    protected:
+
+        virtual bool distanceAvailable(float & distance) override
+        {
+            if (_distanceSensor.newDataReady()) {
+                distance = _distanceSensor.getDistance() / 1000.f; // mm => m
+                return true;
+            }
+            return false;
+        }
+
+    public:
+
+        void begin(void)
+        {
+            _distanceSensor.begin();
+        }
+}; // class VL53L1X_Rangefinder 
+
+// Change this as needed
+#define SBUS_SERIAL Serial1
+
+static constexpr uint8_t CHANNEL_MAP[6] = {2,0,1,3,5,4};
+
+hf::Hackflight h;
+
+hf::SBUS_Receiver rc = hf::SBUS_Receiver(CHANNEL_MAP, SERIAL_SBUS, &SBUS_SERIAL);
+
+hf::MixerQuadX mixer;
+
+VL53L1X_Rangefinder rangefinder;
+
+hf::Rate ratePid = hf::Rate(
+        0.10f,  // Gyro Roll/Pitch P
+        0.01f,  // Gyro Roll/Pitch I
+        0.05f,  // Gyro Roll/Pitch D
+        0.10f,  // Gyro yaw P
+        0.01f,  // Gyro yaw I
+        5.00f); // Demands to rate
+
+hf::Level level = hf::Level(
+        0.25f,   // Roll Level P
+        0.25f);  // Pitch Level P
+
+hf::AltitudeHold althold = hf::AltitudeHold(
+        0.30f,   // Altitude Hold P
+        0.20f,   // Altitude Hold Velocity P
+        0.00f,   // Altitude Hold Velocity I
+        1.00f);  // Altitude Hold Velocity D
+
+
+void setup(void)
+{
+    // begin the serial port for the ESP32
+    Serial4.begin(115200);
+
+    // Trim receiver via software
+    rc.setTrimRoll(-0.0012494f);
+    rc.setTrimPitch(-0.0058769f);
+    rc.setTrimYaw(-0.0192190f);
+
+    // 0 means the controller will always be active, but by changing
+    // that number it can be linked to a different aux state
+    h.addPidController(&althold, 2);
+    h.addPidController(&level, 0);
+
+    h.init(new hf::BonadroneMultiShot(), &rc, &mixer, &ratePid);
+    
+    // Add rangefinder sensor
+    rangefinder.begin();
+    h.addSensor(&rangefinder);
+    
+}
+
+void loop(void)
+{
+    h.update();
+}
---
+++ src/pidcontrollers/althold.hpp
@@ -0,0 +1,113 @@
+/*
+   althold.hpp : Altitude hold PID controller
+
+   Copyright (c) 2018 Juan Gallostra and Simon D. Levy
+
+   This file is part of Hackflight.
+
+   Hackflight is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Hackflight is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with Hackflight.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <cstdint>
+#include <cstring>
+#include <algorithm>
+#include <limits>
+#include <cmath>
+
+#include "receiver.hpp"
+#include "filters.hpp"
+#include "debug.hpp"
+#include "datatypes.hpp"
+#include "pidcontroller.hpp"
+
+namespace hf {
+
+    class AltitudeHold : public PID_Controller {
+
+        friend class Hackflight;
+
+        private: 
+
+            // Arbitrary constants
+            const float WINDUP_MAX             = 0.40f;
+            const float HOVER_THROTTLE         = 0.05f;
+            const float MIN_ALTITUDE           = 0.10f;
+
+            // PID constants set in constructor
+            float _altHoldP;
+            float _altHoldVelP;
+            float _altHoldVelI;
+            float _altHoldVelD;
+            
+            // Required constants
+            float _lastError;
+            float _deltaError;
+            float _integralError;
+            float _velocityTarget;
+            float _altitudeTarget;
+            uint32_t _previousTime;
+            
+
+            void resetErrors(void)
+            {
+                _lastError = 0;
+                _deltaError = 0;
+                _integralError = 0;
+            }
+
+        public:
+
+            AltitudeHold(float altHoldP, float altHoldVelP, float altHoldVelI, float altHoldVelD) :
+                _altHoldP(altHoldP), 
+                _altHoldVelP(altHoldVelP), 
+                _altHoldVelI(altHoldVelI),
+                _altHoldVelD(altHoldVelD) 
+            {
+              // Initialize errors
+              resetErrors();
+              _previousTime = micros();
+            }
+
+            bool modifyDemands(state_t & state, demands_t & demands)
+            {
+              uint32_t _currentTime = micros();
+              float dt = (_currentTime - _previousTime) / 1000000.0f;
+              _previousTime = _currentTime;
+              _altitudeTarget = 0.20f;
+              // Compute velocity setpoint
+              _velocityTarget = (_altitudeTarget - state.altitude)*_altHoldP;
+              // Compute errors
+
+              float velocityError = _velocityTarget - state.variometer;
+              // Update error integral and error derivative
+              _integralError = Filter::constrainAbs(_integralError + velocityError * dt, WINDUP_MAX);
+              _deltaError = (velocityError - _lastError) / dt;
+              _lastError = velocityError;
+              float throttleCorrection = _altHoldVelP * velocityError +
+                                         _altHoldVelD * _deltaError +
+                                         _altHoldVelI * _integralError;
+              demands.throttle = HOVER_THROTTLE + throttleCorrection;
+
+              return true;
+            }
+            
+            virtual bool shouldFlashLed(void) override 
+            {
+                return true;
+            }
+
+    };  // class AltitudeHold
+
+} // namespace
