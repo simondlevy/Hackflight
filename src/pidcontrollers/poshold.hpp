383c8c400b0a91e478e3de26d051aabcfe328270
simondlevy <simon.d.levy@gmail.com>
2018-10-30 14:18:30 -0400

    Begin iNav-style pos-hold

--- src/pidcontrollers/poshold.hpp
+++ src/pidcontrollers/poshold.hpp
@@ -1,6 +1,8 @@
 /*
    poshold.hpp : PID-based position hold
 
+   Based on https://raw.githubusercontent.com/wiki/iNavFlight/inav/images/nav_poshold_pids_diagram.jpg
+
    Copyright (c) 2018 Simon D. Levy
 
    This file is part of Hackflight.
@@ -34,7 +36,7 @@ namespace hf {
 
         public:
 
-        PositionHold(float posP, float posrP, float posrI, float posrD)
+        PositionHold(float posP, float posrP, float posrI, float posrD=0.0f)
         {
             _posP = posP;
             _posrP = posrP;
@@ -46,7 +48,12 @@ namespace hf {
 
         virtual bool modifyDemands(state_t & state, demands_t & demands, float currentTime) 
         {
-            return false;
+            (void)currentTime;
+
+            demands.pitch = adjustCyclic(demands.pitch, state.velocityForward);
+            demands.roll  = adjustCyclic(demands.roll,  state.velocityRightward);
+
+            return true;
         }
 
         virtual bool shouldFlashLed(void) override 
@@ -54,12 +61,25 @@ namespace hf {
             return true;
         }
 
+        private:
+
         // set by constructor
         float _posP;
         float _posrP;
         float _posrI;
         float _posrD;
 
+        bool inBand(float demand)
+        {
+            return fabs(demand) < Receiver::STICK_DEADBAND; 
+        }
+
+        float adjustCyclic(float demand, float velocity)
+        {
+            // Inside throttle deadband, adjust pitch/roll demand by PD controller; outside deadband, leave it as-is
+            return inBand(demand) ? demand - _posrP*velocity: demand; 
+        }
+
     };  // class PositionHold
 
-} // namespace
+} // namespace hf
