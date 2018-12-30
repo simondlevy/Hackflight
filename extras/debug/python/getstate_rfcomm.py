6ca23a643125acdccc71c5fe96619962010ba881
simondlevy <simon.d.levy@gmail.com>
2018-12-30 14:55:00 -0500

    Uses new msppg.Parser API

--- extras/debug/python/getstate_rfcomm.py
+++ extras/debug/python/getstate_rfcomm.py
@@ -23,24 +23,23 @@ along with this code.  If not, see <http:#www.gnu.org/licenses/>.
 BD_ADDR = '00:06:66:73:E3:E8'
 BD_PORT = 1
 
-from msppg import MSP_Parser as Parser, serialize_STATE_Request
+from msppg import Parser, serialize_STATE_Request
 import bluetooth
-from time import time, sleep
+
+request = serialize_STATE_Request()
+
+class StateParser(Parser):
+
+    def handle_STATE(altitude, variometer, positionX, positionY, heading, velocityForward, velocityRightward):
+        print(altitude, variometer, positionX, positionY, heading, velocityForward, velocityRightward)
+        sock.send(request)
 
 parser = Parser()
-request = serialize_STATE_Request()
 
 sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 sock.connect((BD_ADDR, BD_PORT))
 
 print('connected to ' + BD_ADDR)
-
-def handler(altitude, variometer, positionX, positionY, heading, velocityForward, velocityRightward):
-
-    print(altitude, variometer, positionX, positionY, heading, velocityForward, velocityRightward)
-    sock.send(request)
-
-parser.set_STATE_Handler(handler)
 
 sock.send(request)
 
@@ -48,7 +47,9 @@ while True:
 
         try:
 
-            parser.parse(sock.recv(1))
+            c = sock.recv(1)
+            print('%02x' % ord(c)) 
+            parser.parse(c)
 
         except KeyboardInterrupt:
 
