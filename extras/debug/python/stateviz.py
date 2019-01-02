a2e173732dc61e4b5ba48328dc3df828612d2d8a
simondlevy <simon.d.levy@gmail.com>
2019-01-02 14:30:53 -0500

    =More stateviz work

--- extras/debug/python/stateviz.py
+++ extras/debug/python/stateviz.py
@@ -25,13 +25,8 @@ import argparse
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                    help='an integer for the accumulator')
-    parser.add_argument('--sum', dest='accumulate', action='store_const',
-                    const=sum, default=max,
-                    help='sum the integers (default: find the max)')
-
+    parser = argparse.ArgumentParser(description='Visualize incoming vehicle-state messages.')
+    parser.add_argument('-v', '--verbosity', help='increase output verbosity')
     args = parser.parse_args()
-    print(args.accumulate(args.integers))
+    print(args)
 
