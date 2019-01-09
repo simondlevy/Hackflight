cd33d6156faeda4288b65d9f07aacb29105e4908
simondlevy <simon.d.levy@gmail.com>
2019-01-08 20:01:29 -0500

    Begin OpenMV project

---
+++ extras/openmv/camera.py
@@ -0,0 +1,19 @@
+# Adapted from hello_world.py example: https://docs.openmv.io/openmvcam/tutorial/script_structure.html
+
+import sensor, image, time
+import msppg
+
+sensor.reset()                      # Reset and initialize the sensor.
+sensor.set_pixformat(sensor.RGB565) # Set pixel format to RGB565 (or GRAYSCALE)
+sensor.set_framesize(sensor.QQVGA)  # Set frame size to QVGA (160x120)
+sensor.skip_frames(time = 2000)     # Wait for settings take effect.
+clock = time.clock()                # Create a clock object to track the FPS.
+
+# Invert the image
+sensor.__write_reg(0x0C, sensor.__read_reg(0x0C) | (1 << 7))
+
+while(True):
+    clock.tick()                    # Update the FPS clock.
+    img = sensor.snapshot()         # Take a picture and return the image.
+    print(clock.fps())              # Note: OpenMV Cam runs about half as fast when connected
+                                    # to the IDE. The FPS should increase once disconnected.
