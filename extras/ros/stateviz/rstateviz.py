064931d90bf554d04c073178065fa32eeb43e2c2
simondlevy <simon.d.levy@gmail.com>
2019-01-18 23:18:28 +0200

    Fixed typo

--- extras/ros/stateviz/rstateviz.py
+++ extras/ros/stateviz/rstateviz.py
@@ -21,7 +21,7 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU Lesser General Public License 
 along with this code.  If not, see <http:#www.gnu.org/licenses/>.
-''''
+'''
 
 import rospy
 import copy
