4fc07747acb05e159b6d782d6a8023b5419d5fce
simondlevy <simon.d.levy@gmail.com>
2019-05-18 20:34:20 +0300

    Compile OmnibusF3 header-only

--- extras/stm32f/examples/omnibusf3_dsmx/omnibusf3_dsmx.cpp
+++ extras/stm32f/examples/omnibusf3_dsmx/omnibusf3_dsmx.cpp
@@ -21,7 +21,7 @@
 #include <hackflight.hpp>
 #include <mixers/quadxcf.hpp>
 #include <pidcontrollers/level.hpp>
-#include "omnibusf3.h"
+#include "omnibusf3.hpp"
 
 constexpr uint8_t CHANNEL_MAP[6] = {0, 1, 2, 3, 6, 4};
 
