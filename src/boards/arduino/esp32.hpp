5691768d87880f2514b1a7d4083ea7961f756c93
unknown <simondlevy>
2019-07-01 16:40:40 +0300

    Begin ESP32 support

---
+++ src/boards/arduino/esp32.hpp
@@ -0,0 +1,37 @@
+/*
+   esp32.hpp : Board subclass for ESP32
+
+   Copyright (c) 2019 Simon D. Levy
+
+   This file is part of Hackflight.
+
+   Hackflight is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Hackflight is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with Hackflight.  If not, see <http://www.gnu.org/licenses/>.
+   */
+
+#pragma once
+
+#include "boards/arduino/mock.hpp"
+
+namespace hf {
+
+    class ESP32 : public MockBoard {
+
+        public:
+
+            ESP32(void) : MockBoard(2, true)
+            {
+            }
+
+    }; // class ESP32
+
+} // namespace hf
