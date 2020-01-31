bb6462f62ba421b2ece68950335e550a99eafdcc
Levy <levys@wlu.edu>
2020-01-31 18:34:47 -0500

    Use DSMX receiver

--- examples/Teensy40/Teensy40_USFS_DSMX/Teensy40_USFS_DSMX.ino
+++ examples/Teensy40/Teensy40_USFS_DSMX/Teensy40_USFS_DSMX.ino
@@ -31,15 +31,18 @@
 #include "hackflightfull.hpp"
 #include "boards/realboards/arduino/teensy40.hpp"
 #include "imus/usfs.hpp"
-#include "receivers/mock.hpp"
+#include "receivers/arduino/dsmx/dsmx_serial1.hpp"
 #include "motors/mock.hpp"
 #include "mixers/quadxcf.hpp"
+
+static constexpr uint8_t CHANNEL_MAP[6] = {0, 1, 2, 3, 6, 4};
+static constexpr float DEMAND_SCALE = 8.58f;
 
 hf::HackflightFull h;
 
 hf::USFS imu;
 
-hf::MockReceiver rc;
+hf::DSMX_Receiver_Serial1 rc = hf::DSMX_Receiver_Serial1(CHANNEL_MAP, DEMAND_SCALE);  
 
 hf::MixerQuadXCF mixer;
 
