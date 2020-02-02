3d10764d8f9e2e8a93ab7e11e45018ffc353d600
Levy <levys@wlu.edu>
2020-02-01 23:52:51 -0500

    Running all channels (TAER, Aux1)

--- examples/dsmx2sbus/dsmx2sbus.ino
+++ examples/dsmx2sbus/dsmx2sbus.ino
@@ -21,23 +21,48 @@
 #include <SBUS.h>
 #include <DSMRX.h>
 
+static const uint8_t CHANNELS_IN  = 8;
+static const uint8_t CHANNELS_OUT = 16;
+
 SBUS sbus = SBUS(Serial1);
+
+DSM2048 rx;
+
+void serialEvent2(void)
+{
+    while (Serial2.available()) {
+        rx.handleSerialEvent(Serial2.read(), micros());
+    }
+}
 
 void setup(void)
 {
     sbus.begin();
+
+    // For DSMX in
+    Serial2.begin(115000);
+
+    // For debugging
+    Serial.begin(115000);
 }
 
 void loop(void)
 {
-      static float chanvals[16];
+    float invals[CHANNELS_IN] = {0};
 
-      chanvals[0] = -1;
+    rx.getChannelValuesNormalized(invals, CHANNELS_IN);
 
-      chanvals[5] = millis() > 3000 ? +1 : -1;
+    static float outvals[CHANNELS_OUT];
 
-      sbus.writeCal(chanvals);
+    outvals[0] = invals[0]; // Throttle
+    outvals[1] = invals[1]; // Roll
+    outvals[2] = invals[2]; // Pitch
+    outvals[3] = invals[3]; // Yaw
 
-      delay(10);
+    outvals[5] = invals[6]; // DSXM Aux1 => SBUS Aux2
+
+    sbus.writeCal(outvals);
+
+    delay(10);
 }
 
