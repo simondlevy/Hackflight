ae9bed1df6cfa34c24c6a6d3f33857e3b0584e31
Levy <levys@wlu.edu>
2020-09-16 12:45:32 -0400

    Begin USFS BetaFPV IMU configuration

--- src/imus/usfs/usfs_betafpv.hpp
+++ src/imus/usfs/usfs_betafpv.hpp
@@ -1,19 +1,15 @@
 /*
-   Hackflight sketch for BetaFPV config with USFS IMU and mock motors/receiver
-
-   Additional libraries needed:
-
-       https://github.com/simondlevy/USFS
-       https://github.com/simondlevy/CrossPlatformDataBus
-
+   Support for USFS IMU on BetaFPV-style flight controller
 
    Copyright (c) 2020 Simon D. Levy
 
    This file is part of Hackflight.
+
    Hackflight is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
+
    Hackflight is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -22,25 +18,14 @@
    along with Hackflight.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "hackflight.hpp"
-#include "boards/realboards/tinypico.hpp"
-#include "receivers/mock.hpp"
-#include "actuators/mixers/quadxcf.hpp"
-#include "motors/mock.hpp"
-#include "imus/usfs/usfs_betafpv.hpp"
+#pragma once
 
-hf::Hackflight   h;
-hf::MockReceiver rc;
-hf::MixerQuadXCF mixer;
-hf::USFS_BetaFPV imu;
-hf::MockMotor    motors;
+#include "imus/usfs.hpp"
 
-void setup(void)
-{
-    h.init(new hf::TinyPico(), &imu, &rc, &mixer, &motors);
-}
+namespace hf {
 
-void loop(void)
-{
-    h.update();
-}
+    class USFS_BetaFPV : public USFS {
+
+    }; // class USFS_BetaFPV
+
+} // namespace hf
