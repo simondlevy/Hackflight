e759d05add09daa9383dbc5d9d7fe6f65b1ec9fe
Levy <levys@wlu.edu>
2020-09-17 16:04:23 -0400

    Added DSMRX receiver

--- examples/BetaFPV/USFS/USFS.ino
+++ examples/BetaFPV/USFS/USFS.ino
@@ -5,7 +5,7 @@
 
        https://github.com/simondlevy/USFS
        https://github.com/simondlevy/CrossPlatformDataBus
-
+       https://github.com/simondlevy/SpektrumDSM
 
    Copyright (c) 2020 Simon D. Levy
 
@@ -24,20 +24,47 @@
 
 #include "hackflight.hpp"
 #include "boards/realboards/tinypico.hpp"
-#include "receivers/mock.hpp"
+#include "receivers/arduino/dsmx.hpp"
 #include "actuators/mixers/quadxcf.hpp"
 #include "motors/mock.hpp"
 #include "imus/usfs/usfs_betafpv.hpp"
 
-hf::Hackflight   h;
-hf::MockReceiver rc;
-hf::MixerQuadXCF mixer;
-hf::USFS_BetaFPV imu;
-hf::MockMotor    motors;
+static const uint8_t SERIAL1_RX = 32;
+static const uint8_t SERIAL1_TX = 33; // unused
+
+static constexpr uint8_t CHANNEL_MAP[6] = {0, 1, 2, 3, 6, 4};
+static constexpr float DEMAND_SCALE = 8.0f;
+
+hf::Hackflight    h;
+hf::DSMX_Receiver rc = hf::DSMX_Receiver(CHANNEL_MAP, DEMAND_SCALE);  
+hf::MixerQuadXCF  mixer;
+hf::USFS_BetaFPV  imu;
+hf::MockMotor     motors;
+
+// Timer task for DSMX serial receiver
+static void receiverTask(void * params)
+{
+    while (true) {
+
+        if (Serial1.available()) {
+            rc.handleSerialEvent(Serial1.read(), micros());
+        }
+
+        delay(1);
+    }
+}
 
 void setup(void)
 {
+    // Start receiver on Serial1
+    Serial1.begin(115000, SERIAL_8N1, SERIAL1_RX, SERIAL1_TX);
+
+    // Start Hackflight
     h.init(new hf::TinyPico(), &imu, &rc, &mixer, &motors);
+
+    // Start the receiver timed task
+    TaskHandle_t task;
+    xTaskCreatePinnedToCore(receiverTask, "Task", 10000, NULL, 1, &task, 0);
 }
 
 void loop(void)
