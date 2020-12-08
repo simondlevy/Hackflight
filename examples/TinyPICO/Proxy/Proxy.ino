a608ce5efe5628fe55c07446a6467c431a20935b
simondlevy <simon.d.levy@gmail.com>
2020-12-07 21:04:16 -0500

    Begin TinyPICO proxy sketch

---
+++ examples/TinyPICO/Proxy/Proxy.ino
@@ -0,0 +1,78 @@
+/*
+   Use TinyPICO as a proxy between DSMX receiver in and SBUS out
+
+   Additional libraries needed:
+
+       https://github.com/simondlevy/DSMRX
+
+
+   Copyright (c) 2020 Simon D. Levy
+
+   This file is part of Hackflight.
+   Hackflight is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   Hackflight is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with Hackflight.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "hackflight.hpp"
+#include "boards/realboards/tinypico.hpp"
+#include "receivers/arduino/dsmx.hpp"
+#include "actuators/mixers/quadxcf.hpp"
+#include "motors/mock.hpp"
+#include "imus/mock.hpp"
+
+static const uint8_t SERIAL1_RX = 32;
+static const uint8_t SERIAL1_TX = 33; // unused
+
+static constexpr uint8_t CHANNEL_MAP[6] = {0, 1, 2, 3, 6, 4};
+
+static constexpr float DEMAND_SCALE = 8.0f;
+
+hf::Hackflight h;
+
+hf::DSMX_Receiver rc = hf::DSMX_Receiver(CHANNEL_MAP, DEMAND_SCALE);  
+
+hf::MixerQuadXCF mixer;
+
+hf::MockIMU imu;
+
+hf::MockMotor motors;
+
+// Timer task for DSMX serial receiver
+static void receiverTask(void * params)
+{
+    while (true) {
+
+        if (Serial1.available()) {
+            rc.handleSerialEvent(Serial1.read(), micros());
+        }
+
+        delay(1);
+    }
+}
+
+
+void setup(void)
+{
+    // Start receiver on Serial1
+    Serial1.begin(115000, SERIAL_8N1, SERIAL1_RX, SERIAL1_TX);
+
+    // Initialize Hackflight firmware
+    h.init(new hf::TinyPico(), &imu, &rc, &mixer, &motors);
+
+    // Start the receiver timed task
+    TaskHandle_t task;
+    xTaskCreatePinnedToCore(receiverTask, "Task", 10000, NULL, 1, &task, 0);
+}
+
+void loop(void)
+{
+    h.update();
+}
