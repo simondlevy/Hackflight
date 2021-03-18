9a558b7a9d5c7240f61bb777b09aa9f5ec4525b1
simondlevy <simon.d.levy@gmail.com>
2021-03-17 21:00:22 -0400

    geteuler.py script working with new parser

--- extras/debug/geteuler.py
+++ extras/debug/geteuler.py
@@ -7,17 +7,27 @@ Copyright (C) Simon D. Levy 2021
 MIT License
 '''
 
-BAUD = 115200
-
-#PORT = 'COM22'          # Windows
-PORT = '/dev/ttyACM1' # Linux
-
-from msppg import Parser, serialize_ATTITUDE_RADIANS_Request
+from msp import Parser
 import serial
 from time import sleep
 from sys import stdout
+import argparse
+import struct
 
 class AttitudeParser(Parser):
+
+    def dispatchMessage(self):
+
+        if self.message_id == 121:
+            self.handle_RC_NORMAL(*struct.unpack('=ffffff', self.message_buffer))
+
+        if self.message_id == 122:
+            self.handle_ATTITUDE_RADIANS(*struct.unpack('=fff', self.message_buffer))
+
+    @staticmethod
+    def serialize_ATTITUDE_RADIANS_Request():
+        msg = '$M<' + chr(0) + chr(122) + chr(122)
+        return bytes(msg, 'utf-8')
 
     def handle_ATTITUDE_RADIANS(self, roll, pitch, yaw):
 
@@ -27,9 +37,15 @@ class AttitudeParser(Parser):
 
 if __name__ == '__main__':
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument(dest='com_port', help='COM port')
+
+    # Parse and print the results
+    args = parser.parse_args()
+
     parser = AttitudeParser()
-    request = serialize_ATTITUDE_RADIANS_Request()
-    port = serial.Serial(PORT, BAUD)
+    request = AttitudeParser.serialize_ATTITUDE_RADIANS_Request()
+    port = serial.Serial(args.com_port, 115200)
 
     # Connecting causes reboot on ESP32
     sleep(1)
