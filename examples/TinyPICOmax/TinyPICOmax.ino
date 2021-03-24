bb2d678956f45886d5f8fea57c0706a1469884d8
simondlevy <simon.d.levy@gmail.com>
2021-03-24 15:02:03 -0400

    More USFSMAX work

--- examples/TinyPICOmax/TinyPICOmax.ino
+++ examples/TinyPICOmax/TinyPICOmax.ino
@@ -19,14 +19,34 @@
 
 #include "hackflight.hpp"
 #include "mixers/quadxcf.hpp"
+#include "pidcontrollers/rate.hpp"
+#include "pidcontrollers/level.hpp"
 #include "sensors/usfsmax.hpp"
 
-#include "receivers/mock.hpp"
+#include "receivers/arduino/dsmx.hpp"
 
 
 // Receiver --------------------------------------------------------------
 
-hf::MockReceiver receiver;
+static const uint8_t SERIAL1_RX = 4;
+static const uint8_t SERIAL1_TX = 14;  // unused
+
+static constexpr uint8_t CHANNEL_MAP[6] = {0, 1, 2, 3, 6, 4};
+static constexpr float DEMAND_SCALE = 8.0f;
+
+hf::DSMX_Receiver receiver = hf::DSMX_Receiver(CHANNEL_MAP, DEMAND_SCALE);  
+
+static void receiverTask(void * params)
+{
+    while (true) {
+
+        if (Serial1.available()) {
+            receiver.handleSerialEvent(Serial1.read(), micros());
+        }
+
+        delay(1);
+    }
+}
 
 // Motors ----------------------------------------------------------------
 
@@ -37,15 +57,28 @@ rft::MockMotor motors;
 rft::TinyPico board;
 static hf::UsfsQuat quat;
 static hf::UsfsGyro gyro;
+static hf::RatePid ratePid = hf::RatePid( 0.05f, 0.00f, 0.00f, 0.10f, 0.01f); 
+static hf::LevelPid levelPid = hf::LevelPid(0.20f);
 static hf::MixerQuadXCF mixer(&motors);
 
 static hf::Hackflight h(&board, &receiver, &mixer);
 
 void setup(void)
 {
+    // Start receiver on Serial1
+    Serial1.begin(115000, SERIAL_8N1, SERIAL1_RX, SERIAL1_TX);
+
     // Add gyro, quaternion sensors
     h.addSensor(&quat);
     h.addSensor(&gyro);
+
+    // Add PID controllers
+    h.addClosedLoopController(&levelPid);
+    h.addClosedLoopController(&ratePid);
+
+    // Start the receiver timed task
+    TaskHandle_t task;
+    xTaskCreatePinnedToCore(receiverTask, "ReceiverTask", 10000, NULL, 1, &task, 1);
 
     // Initialize Hackflight firmware
     h.begin();
