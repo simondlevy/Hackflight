85a83425383b893febc4d41a5c85e211ad6134a1
simondlevy <simon.d.levy@gmail.com>
2021-03-24 15:05:40 -0400

    Begin ESP32 Serial1 receiver support [Issue 98]

---
+++ src/receivers/arduino/dsmx/dsmx_esp32.hpp
@@ -0,0 +1,47 @@
+/*
+   Spektrum DSMX support for ESP32 flight controllers using Serial1
+
+   Copyright (c) 2021 Simon D. Levy
+
+   MIT License
+ */
+
+#pragma once
+
+#include "receivers/arduino/dsmx.hpp"
+#include <DSMRX.h>
+
+static hf::DSMX_Receiver * _dsmx_rx;
+
+void serialEvent1(void)
+{
+    while (Serial1.available()) {
+
+        _dsmx_rx->handleSerialEvent(Serial1.read(), micros());
+    }
+}
+
+namespace hf {
+
+    class DSMX_Receiver_Serial1 : public DSMX_Receiver {
+
+        protected:
+
+            void begin(void) override 
+            {
+                Receiver::begin();
+
+                Serial1.begin(115200);
+            }
+
+        public:
+
+            DSMX_Receiver_Serial1(const uint8_t channelMap[6], const float demandScale)
+                :  DSMX_Receiver(channelMap, demandScale) 
+            { 
+                _dsmx_rx = this;
+            }
+
+    }; // class DSMX_Receiver_Serial1
+
+} // namespace hf
