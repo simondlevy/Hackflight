2ba194c8a2209a278dfaa1bed411ff266f1ec8fc
simondlevy <simon.d.levy@gmail.com>
2021-04-12 13:44:55 -0400

    Begin factoring yaw PID from rate PID

--- src/pidcontrollers/yaw.hpp
+++ src/pidcontrollers/yaw.hpp
@@ -1,7 +1,7 @@
 /*
-   Angular-velocity-based PID controller for roll, pitch, yaw
+   Yaw PID controller based on angular velocity
 
-   Copyright (c) 2021 Juan Gallostra and Simon D. Levy
+   Copyright (c) 2021 Simon D. Levy
 
    MIT License
  */
@@ -18,7 +18,7 @@
 
 namespace hf {
 
-    class RatePid : public rft::PidController {
+    class YawPid : public rft::PidController {
 
         private: 
 
@@ -26,27 +26,18 @@ namespace hf {
             static constexpr float BIG_YAW_DEMAND = 0.1f;
 
             // Rate mode uses a rate controller for roll, pitch
-            _AngularVelocityPid _rollPid;
-            _AngularVelocityPid _pitchPid;
             _AngularVelocityPid _yawPid;
 
         public:
 
-            RatePid(const float Kp, const float Ki, const float Kd, const float Kp_yaw, const float Ki_yaw) 
+            YawPid(const float Kp, const float Ki) 
             {
-                _rollPid.begin(Kp, Ki, Kd);
-                _pitchPid.begin(Kp, Ki, Kd);
-                _yawPid.begin(Kp_yaw, Ki_yaw, 0);
+                _yawPid.begin(Kp, Ki, 0);
             }
 
             void modifyDemands(rft::State * state, float * demands)
             {
                 float * x = ((State *)state)->x;
-
-                demands[DEMANDS_ROLL]  = _rollPid.compute(demands[DEMANDS_ROLL], x[State::STATE_DPHI]);
-
-                demands[DEMANDS_PITCH] = _pitchPid.compute(demands[DEMANDS_PITCH], x[State::STATE_DTHETA]);
-                demands[DEMANDS_YAW]   = _yawPid.compute(demands[DEMANDS_YAW], x[State::STATE_DPSI]);
 
                 // Prevent "yaw jump" during correction
                 demands[DEMANDS_YAW] =
@@ -61,8 +52,6 @@ namespace hf {
             virtual void resetOnInactivity(bool inactive) override
             {
                 // Check throttle-down for integral reset
-                _rollPid.resetOnInactivity(inactive);
-                _pitchPid.resetOnInactivity(inactive);
                 _yawPid.resetOnInactivity(inactive);
             }
 
