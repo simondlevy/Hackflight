971cbaf78e02b6577b452d9f6f40c4b21e085b67
simondlevy <simon.d.levy@gmail.com>
2021-04-17 23:35:04 -0400

    Cleanup

--- src/state.hpp
+++ src/state.hpp
@@ -15,27 +15,30 @@ namespace hf {
 
     class State : public rft::State{
 
+        friend class Hackflight;
+
         private:
 
             static constexpr float MAX_ARMING_ANGLE_DEGREES = 25.0f;
-
-        // XXX protected
-        public:
-
-            // See Bouabdallah et al. (2004)
-            enum {X, DX, Y, DY, Z, DZ, PHI, DPHI, THETA, DTHETA, PSI, DPSI, SIZE};
-
-            float x[SIZE];
 
             bool safeAngle(uint8_t axis)
             {
                 return fabs(x[axis]) < rft::Filter::deg2rad(MAX_ARMING_ANGLE_DEGREES);
             }
 
+        protected:
+
             bool safeToArm(void)
             {
                 return safeAngle(PHI) && safeAngle(THETA);
             }
+
+        public:
+
+            // See Bouabdallah et al. (2004)
+            enum {X, DX, Y, DY, Z, DZ, PHI, DPHI, THETA, DTHETA, PSI, DPSI, SIZE};
+
+            float x[SIZE];
 
     }; // class State
 
