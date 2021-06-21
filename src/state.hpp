07d6f6e751101a56110e19f7df387522056fdf02
simondlevy <simon.d.levy@gmail.com>
2021-06-21 19:43:09 -0400

    More RFT migration

--- src/state.hpp
+++ src/state.hpp
@@ -8,14 +8,33 @@
 
 #pragma once
 
+#include <RFT_state.hpp>
+#include <RFT_filters.hpp>
+
 namespace hf {
 
-    class State {
+    class State : public rft::State {
+
+        friend class Hackflight;
+
+        private:
+
+            static constexpr float MAX_ARMING_ANGLE_DEGREES = 25.0f;
+
+            bool safeAngle(uint8_t axis)
+            {
+                return fabs(x[axis]) < rft::Filter::deg2rad(MAX_ARMING_ANGLE_DEGREES);
+            }
+
+        protected:
+
+            bool safeToArm(void)
+            {
+                return safeAngle(PHI) && safeAngle(THETA);
+            }
+
 
         public:
-
-            bool armed;
-            bool failsafe;
 
             // See Bouabdallah et al. (2004)
             enum {X, DX, Y, DY, Z, DZ, PHI, DPHI, THETA, DTHETA, PSI, DPSI, SIZE};
