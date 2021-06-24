f278879e10ced77c2347632bf2aed1a5c0a47089
simondlevy <simon.d.levy@gmail.com>
2021-06-24 19:49:43 -0400

    Begin restoring thrust-vectoring mixer

--- src/mixers/thrustvec.hpp
+++ src/mixers/thrustvec.hpp
@@ -16,13 +16,22 @@ namespace hf {
 
         public:
 
-            MixerThrustVector(void) 
+            MixerThrustVector(
+                    rft::RotaryMotor * rotor1,
+                    rft::RotaryMotor * rotor2,
+                    rft::ServoMotor * servo1,
+                    rft::ServoMotor * servo1)
             {
                 //                     Th   RR   PF  YR
                 motorDirections[0] = { +1,  0,   0, +1 };   // rotor 1
                 motorDirections[1] = { +1,  0,   0, -1 };   // rotor 2
                 motorDirections[2] = {  0, +1,   0,  0 };   // servo 1
                 motorDirections[3] = {  0,  0 , +1,  0 };   // servo 2
+
+                Mixer::addMotor(rotor1);
+                Mixer::addMotor(rotor2);
+                Mixer::addMotor(servo1);
+                Mixer::addMotor(servo2);
              }
 
         protected:
