5123e8f40eec1b0b42cddb1e7da8fee75cb59a81
simondlevy <simon.d.levy@gmail.com>
2021-06-29 18:07:14 -0400

    =More rangefinder work

--- src/sensors/vl53l1x.hpp
+++ src/sensors/vl53l1x.hpp
@@ -20,27 +20,49 @@ namespace hf {
 
         private:
 
-            VL53L1X vl53l1x;
+            VL53L1X _vl53l1x;
+
+            // Support using temporal first different to compute State::DZ
+            float _dist_prev = 0;
+            float _time_prev = 0;
 
         protected:
 
             virtual void modifyState(rft::State * state, float time) override
             {
-                (void)time;
-
                 State * hfstate = (State *)state;
+
+                float dist = _vl53l1x.getDistance();
+
+                hfstate->x[State::Z] = dist;
+
+                if (_time_prev > 0) {
+                    hfstate->x[State::DZ] = (dist - _dist_prev) / (time - _time_prev);
+                }
+
+                _dist_prev = dist;
+                _time_prev = time;
 
             }
 
             virtual void begin(void) override
             {
-                vl53l1x.begin();
+                _vl53l1x.begin();
             }
 
             virtual bool ready(float time) override
             {
-                return false;
+                return _vl53l1x.newDataReady();
             }
+
+        public:
+
+          Vl53l1xRangefinder(void)
+          {
+            _dist_prev = 0;
+            _time_prev = 0;
+          }
+
 
     };  // class Vl53l1xRangefinder 
 
