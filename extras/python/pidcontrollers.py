97e4c15a812bac489d06507060c3056be3288344
simondlevy <simon.d.levy@gmail.com>
2021-07-12 22:35:08 -0400

    Restored RatePid original API [Issue 139]

--- extras/python/pidcontrollers.py
+++ extras/python/pidcontrollers.py
@@ -18,63 +18,6 @@ def _constrainAbs(val, lim):
     return -lim if val < -lim else (+lim if val > +lim else val)
 
 
-class _DofPid:
-    '''
-    PID controller for a single degree of freedom.  Because time differences
-    (dt) appear more-or-less constant, we avoid incoroporating them into the
-    code i.e., they are "absorbed" into tuning constants Ki and Kd.
-    '''
-
-    def __init__(self, Kp, Ki, Kd, windupMax=0.4):
-
-        # Constants
-        self.Kp = Kp
-        self.Ki = Ki
-        self.Kd = Kd
-        self.windupMax = windupMax
-
-        # Initialize state
-        self.reset()
-
-    def compute(self, target, actual):
-
-        # Compute error as scaled target minus actual
-        error = target - actual
-
-        # Compute P term
-        pterm = error * self.Kp
-
-        # Compute I term
-        errorI = _DofPid.constrainAbs(self.state['errorI'] + error,
-                                      self.windupMax)
-        iterm = errorI * self.Ki
-
-        # Compute D term
-        deltaError = error - self.state['lastError']
-        dterm = ((self.state['deltaError1'] +
-                  self.state['deltaError2'] + deltaError) * self.Kd)
-
-        # Update state
-        self.state = {'deltaError2': self.state['deltaError1'],
-                      'deltaError1': deltaError,
-                      'errorI': errorI,
-                      'lastError': error}
-
-        return pterm + iterm + dterm, self.state
-
-    def reset(self):
-
-        self.state = {'deltaError1': 0,
-                      'deltaError2': 0,
-                      'errorI': 0,
-                      'lastError': 0}
-
-    @staticmethod
-    def constrainAbs(val, lim):
-
-        return -lim if val < -lim else (+lim if val > +lim else val)
-
-
 class LevelPid:
 
     MAX_ANGLE_DEGREES = 45
@@ -105,51 +48,110 @@ class LevelPid:
         return newdemands, None
 
 
-class _AngularVelocityPid(_DofPid):
+class RatePid:
 
     # Arbitrary constants
     BIG_DEGREES_PER_SECOND = 40.0
     WINDUP_MAX = 6.0
 
-    def __init__(self, Kp, Ki, Kd):
+    def __init__(self, Kp, Ki, Kd, windupMax=0.4):
 
-        _DofPid.__init__(self, Kp, Ki, Kd, self.WINDUP_MAX)
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+        self.windupMax = windupMax
 
         # Convert degree parameters to radians for use later
         self.bigAngularVelocity = np.radians(self.BIG_DEGREES_PER_SECOND)
 
-    def compute(self, demand, angularVelocity):
+        # Initialize state
+        self.state = [RatePid._newstate(), RatePid._newstate()]
 
-        # Reset integral on quick angular velocity change
-        if abs(angularVelocity) > self.bigAngularVelocity:
-            self.reset()
-
-        return _DofPid.compute(self, demand, angularVelocity)
-
-
-class RatePid:
-
-    def __init__(self, Kp, Ki, Kd):
-
-        self.rollPid = _AngularVelocityPid(Kp, Ki, Kd)
-        self.pitchPid = _AngularVelocityPid(Kp, Ki, Kd)
+    @staticmethod
+    def _newstate():
+        return {'deltaError1': 0,
+                'deltaError2': 0,
+                'errorI': 0,
+                'lastError': 0}
 
     def modifyDemands(self, state, demands):
 
-        newdmnds = demands.copy()
+        newdemands = demands.copy()
 
-        # Roll angle and roll demand are both positive for starboard right down
-        roll, rstate = self.rollPid.compute(demands[DEMANDS_ROLL],
-                                            state[STATE_DPHI])
-        newdmnds[DEMANDS_ROLL] = roll
+        newdemands[DEMANDS_ROLL], self.state[0] = \
+            RatePid._compute(self.Kp,
+                             self.Ki,
+                             self.Kd,
+                             self.windupMax,
+                             self.bigAngularVelocity,
+                             demands,
+                             DEMANDS_ROLL,
+                             state,
+                             STATE_DPHI,
+                             self.state[0])
 
-        # Pitch demand is postive for stick forward, but pitch angle is
-        # positive for nose up.  So we negate pitch angle to compute demand
-        pitch, pstate = self.pitchPid.compute(demands[DEMANDS_PITCH],
-                                              -state[STATE_DTHETA])
-        newdmnds[DEMANDS_PITCH] = pitch
+        newdemands[DEMANDS_PITCH], self.state[1] = \
+            RatePid._compute(self.Kp,
+                             self.Ki,
+                             self.Kd,
+                             self.windupMax,
+                             self.bigAngularVelocity,
+                             demands,
+                             DEMANDS_PITCH,
+                             state,
+                             STATE_DTHETA,
+                             self.state[1],
+                             -1)
 
-        return newdmnds, (rstate, pstate)
+        return newdemands, self.state
+
+    @staticmethod
+    def _compute(Kp,
+                 Ki,
+                 Kd,
+                 windupMax,
+                 bigAngularVelocity,
+                 demands,
+                 demand_index,
+                 vehicle_state,
+                 vehicle_state_index,
+                 controller_state,
+                 state_direction=+1):
+        '''
+        Handles one degree of freedom of angular velocity.  We specify the
+        direction of the velocity because pitch demand is postive for stick
+        forward, but pitch angle is positive for nose up.
+        '''
+
+        angularVelocity = state_direction * vehicle_state[vehicle_state_index]
+
+        # Reset integral on quick angular velocity change
+        if abs(angularVelocity) > bigAngularVelocity:
+            controller_state = RatePid._newstate()
+
+        # Compute error as scaled target minus actual
+        error = demands[demand_index] - angularVelocity
+
+        # Compute P term
+        pterm = error * Kp
+
+        # Compute I term
+        errorI = _constrainAbs(controller_state['errorI'] + error,
+                               windupMax)
+        iterm = errorI * Ki
+
+        # Compute D term
+        deltaError = error - controller_state['lastError']
+        dterm = ((controller_state['deltaError1'] +
+                  controller_state['deltaError2'] + deltaError) * Kd)
+
+        # Update controller state
+        controller_state['deltaErrro2'] = controller_state['deltaError1']
+        controller_state['deltaErrro1'] = deltaError
+        controller_state['errorI'] = errorI
+        controller_state['lasteError'] = error
+
+        return pterm + iterm + dterm, controller_state
 
 
 class YawPid:
@@ -178,4 +180,3 @@ class YawPid:
         newdemands[DEMANDS_YAW] = error * self.Kp + errorI * self.Ki
 
         return newdemands, self.state
-
