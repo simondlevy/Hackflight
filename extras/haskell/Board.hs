8a6e01e78ecf8f634d56948040969194c613a334
simondlevy <simon.d.levy@gmail.com>
2021-07-15 14:44:08 -0400

    =More Haskell work

---
+++ extras/haskell/Board.hs
@@ -0,0 +1,12 @@
+{--
+  Board datatype for Hackflight
+
+  Copyright(C) 2021 Simon D.Levy
+
+  MIT License
+--}
+
+module Board where
+
+-- See Bouabdallah et al. (2004)
+data Board = Board deriving (Show)
