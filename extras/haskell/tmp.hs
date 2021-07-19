a3532b093224039dae2189ea484bdc873a6a453f
simondlevy <simon.d.levy@gmail.com>
2021-07-19 00:18:22 -0400

    =More Haskell work

--- extras/haskell/tmp.hs
+++ extras/haskell/tmp.hs
@@ -1,21 +1,12 @@
-{--
-  Socket-based multicopter control
-
-  Copyright(C) 2021 Simon D.Levy
-
-  MIT License
---}
-
 module Tmp
 
 where
 
-import State
 import Demands
 
 data PidState = AltHoldState Double Double
 
-type PidFun = Time -> Demands -> PidState -> (Demands, PidState)
+type PidFun = Double -> Demands -> PidState -> (Demands, PidState)
 
 loop :: (PidFun, PidState) -> (PidFun, PidState)
 
