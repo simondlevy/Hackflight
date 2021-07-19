f3530ec12b3155f684226e0c8bbf1064610f7594
simondlevy <simon.d.levy@gmail.com>
2021-07-19 19:36:03 -0400

    =More Haskell work

--- extras/haskell/Hackflight.hs
+++ extras/haskell/Hackflight.hs
@@ -19,17 +19,22 @@ type HackflightFun = Demands -> VehicleState -> [PidController] -> Mixer
                      -> (Motors, [PidController])
 
 hackflightFun :: HackflightFun
+
+-- Base case: apply the mixer to the demands to get the motor values
+hackflightFun demands _vehicleState [] mixer = 
+    (mixer demands, [])
+
 hackflightFun demands vehicleState pidControllers mixer = 
 
-  let pidController = pidControllers!!0
+    let pidController = pidControllers!!0
       
-      controllerFun = pidFun pidController
+        controllerFun = pidFun pidController
 
-      -- Run the PID controller to get new demands and controller state
-      (newDemands, newControllerState) = controllerFun vehicleState
-                                                       demands 
-                                                       (pidState pidController)
+        -- Run the PID controller to get new demands and controller state
+        (newDemands, newControllerState) = controllerFun vehicleState
+                                                         demands 
+                                                         (pidState pidController)
 
-  -- Run the mixer on the new demands to get the motors, then return them and a PID
-  -- controller made from the new controller state
-  in ((mixer newDemands), [(newPidController controllerFun newControllerState)])
+    -- Run the mixer on the new demands to get the motors, then return them and a PID
+    -- controller made from the new controller state
+    in ((mixer newDemands), [(newPidController controllerFun newControllerState)])
