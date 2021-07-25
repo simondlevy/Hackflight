33eda176b84dab90332b756fe5a0eb1676c09f7d
simondlevy <simon.d.levy@gmail.com>
2021-07-25 01:44:48 -0400

    =More Haskell work

--- extras/haskell/src/Mixer.hs
+++ extras/haskell/src/Mixer.hs
@@ -24,12 +24,37 @@ motorValues motors = [m1 motors, m2 motors, m3 motors, m4 motors]
 type Mixer = Demands -> Motors
 
 quadXAPMixer :: Mixer
+
 quadXAPMixer demands = 
+
     let t = (throttle demands)
         r = (roll demands)
         p = (pitch demands)
         y = (yaw demands)
+
     in Motors (constrain (t - r - p + y))
               (constrain (t + r + p + y))
               (constrain (t + r - p - y))
               (constrain (t - r + p - y))
+
+------------------------------------------------------------------------------
+
+data NewMixer = QuadXAPMixer 
+
+data Motor = Motor { motorValue :: Double }
+
+data NewMotors = QuadMotors {newm1 :: Motor, newm2 :: Motor, newm3 :: Motor, newm4 :: Motor}
+
+getMotors :: NewMixer -> Demands -> NewMotors
+
+getMotors QuadXAPMixer demands =
+
+    let t = (throttle demands)
+        r = (roll demands)
+        p = (pitch demands)
+        y = (yaw demands)
+
+    in QuadMotors (Motor (constrain (t - r - p + y)))
+                  (Motor (constrain (t + r + p + y)))
+                  (Motor (constrain (t + r - p - y)))
+                  (Motor (constrain (t - r + p - y)))
