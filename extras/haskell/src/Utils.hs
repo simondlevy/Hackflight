3a4310f0734b5b5bcc4a6ddedd887a8c9a14eaca
simondlevy <simon.d.levy@gmail.com>
2021-07-26 15:11:07 -0400

    =More Haskell work

--- extras/haskell/src/Utils.hs
+++ extras/haskell/src/Utils.hs
@@ -25,6 +25,11 @@ deg2rad d = d * pi / 180
 
 constrain :: Double -> Double
 constrain x = if x < 0 then 0 else if x > 1 then 1 else x
+
+-- https://stackoverflow.com/a/47157839
+compose :: Foldable t => t1 -> t (t1 -> t1) -> t1
+compose x ys = foldl (\a b -> b a) x ys
+
 -- https://stackoverflow.com/a/20918430
 
 doublesToBytes :: [Double] -> ByteString
