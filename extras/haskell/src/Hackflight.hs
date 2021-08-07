ae1818b43b895226016c5865b062f04da9b99de2
simondlevy <simon.d.levy@gmail.com>
2021-08-06 21:59:17 -0400

    =More Haskell work

--- extras/haskell/src/Hackflight.hs
+++ extras/haskell/src/Hackflight.hs
@@ -29,22 +29,27 @@ hackflight receiver sensors pidControllers mixer =
 
     let rxDemands = getDemands receiver
 
+        -- Get the vehicle state by running the sensors
         vehicleState = foldr modifyState initialVehicleState sensors
 
         -- Repclicate receiver demands for mapping
         dlist = [rxDemands|_<-[1..(length pidControllers)]]
 
-        combo = zip dlist pidControllers
+        -- Map the PID update function to the pid controllers and demands
+        newControl = map (pidUpdate vehicleState) (zip dlist pidControllers)
 
-        foo = map (pidUpdate vehicleState) combo
+        -- Extract the new PID controllers
+        newPidControllers = map (\p -> snd p) newControl
 
-        newNewPidControllers = map (\p -> snd p) foo
+        -- Extract the new list of demands
+        newDemandsList = map (\p -> fst p) newControl
 
-        newDemandsList = map (\p -> fst p) foo
-
+        -- Sum over the list of demands to get the final demands
         newDemands = foldr addDemands (Demands 0 0 0 0) newDemandsList
 
-    in ((getMotors mixer newDemands), newNewPidControllers)
+    -- Send the final demands to the mixer, returning the resulting motor
+    -- values and the new PID controller states
+    in ((getMotors mixer newDemands), newPidControllers)
 
 addDemands :: Demands -> Demands -> Demands
 addDemands d1 d2 = Demands ((throttle d1) + (throttle d2))
