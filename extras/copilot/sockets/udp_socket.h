8af72d78a7c7c59915a163d41d98ba3ab2ad5d52
simondlevy <simon.d.levy@gmail.com>
2021-08-14 18:26:47 -0400

    =More Haskel Copilot socket work

--- extras/copilot/sockets/udp_socket.h
+++ extras/copilot/sockets/udp_socket.h
@@ -8,30 +8,39 @@
 
 #include "socket_compat.hpp"
 
-class UdpSocket : public Socket {
+typedef struct {
 
-    protected:
+    socket_t socket;
+    struct sockaddr_in si_other;
+    socklen_t slen = sizeof(_si_other);
 
-        struct sockaddr_in _si_other;
-        socklen_t _slen = sizeof(_si_other);
+} udp_socket_t;
 
-        void setupTimeout(uint32_t msec)
-        {
-            if (msec > 0) {
-                Socket::setUdpTimeout(msec);
-            }
-        }
+void setupTimeout(udp_socket_t udp_socket, uint32_t msec)
+{
+    if (msec > 0) {
+        setUdpTimeout(udp_socket.socket, msec);
+    }
+}
 
-    public:
+void sendData(udp_socket_t udp_socket, void * buf, size_t len)
+{
+    sendto(udp_socket.socket.sock,
+           (const char *)buf,
+           (int)len,
+           0,
+           (struct sockaddr *)&(udp_socket.si_other),
+           (int)udp_socket.slen);
 
-        void sendData(void * buf, size_t len)
-        {
-            sendto(_sock, (const char *)buf, (int)len, 0, (struct sockaddr *) &_si_other, (int)_slen);
+}
 
-        }
-
-        bool receiveData(void * buf, size_t len)
-        {
-            return recvfrom(_sock, (char *)buf, (int)len, 0, (struct sockaddr *) &_si_other, &_slen) == _slen;
-        }
-};
+bool receiveData(udp_socket_t udp_socket, void * buf, size_t len)
+{
+    return recvfrom(udp_socket.socket.sock,
+                    (char *)buf,
+                    (int)len,
+                    0,
+                    (struct sockaddr *) &(udp_socket.si_other),
+                    &(udp_socket.slen))
+        == _slen;
+}
