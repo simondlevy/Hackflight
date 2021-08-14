809503cea411c0dd2d104866913946ed9bc4eac4
simondlevy <simon.d.levy@gmail.com>
2021-08-14 19:31:01 -0400

    =More Haskel Copilot socket work

--- extras/copilot/udp_sockets.h
+++ extras/copilot/udp_sockets.h
@@ -7,19 +7,9 @@
 
 #pragma once
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
 #include <stdbool.h>
-#include <string.h>
-
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <sys/time.h>
-
 #include <netdb.h>
-#include <unistd.h>
-#include <arpa/inet.h>
 
 typedef struct {
 
