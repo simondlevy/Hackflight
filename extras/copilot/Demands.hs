d891db1ad308d70f0c0e3e25543e09a2ead22f82
simondlevy <simon.d.levy@gmail.com>
2021-08-15 14:58:26 -0400

    =More Haskell Copilot work

--- extras/copilot/Demands.hs
+++ extras/copilot/Demands.hs
@@ -16,11 +16,6 @@ data Demands = Demands { throttle :: Stream Double
                        , yaw :: Stream Double  
                      } deriving (Show)
 
-{--
-initialDemands :: Demands
-
-initialDemands = Demands 0 0 0 0
-
 
 addDemands :: Demands -> Demands -> Demands
 
@@ -28,5 +23,6 @@ addDemands d1 d2 = Demands ((throttle d1) + (throttle d2))
                            ((roll d1) + (roll d2))
                            ((pitch d1) + (pitch d2))
                            ((yaw d1) + (yaw d2))
---}
+initialDemands :: Demands
 
+initialDemands = Demands 0 0 0 0
