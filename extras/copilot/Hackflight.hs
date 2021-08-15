3c4aa1048930360ee894a96c04cf98de5d65ca22
simondlevy <simon.d.levy@gmail.com>
2021-08-15 02:55:11 -0400

    =More Haskell Copilot work

--- extras/copilot/Hackflight.hs
+++ extras/copilot/Hackflight.hs
@@ -11,11 +11,13 @@ receiverDemands :: Stream (Array 4 Double)
 receiverDemands  = extern "receiverDemands" Nothing
 
 motorValues :: Stream (Array 4 Double)
-motorValues = [array [0,0,0,0]] ++ motorValues
+motorValues = [array [0, 0, 0, 0]] ++ motorValues
 
 spec = do
 
-  trigger "runMotors" true [arg motorValues]
+  let newMotorValues = [array [0.1, 0.2, 0.3, 0.4]] ++ motorValues
+
+  trigger "runMotors" true [arg newMotorValues]
 
 -- Compile the spec
 main = reify spec >>= compile "hackflight"
