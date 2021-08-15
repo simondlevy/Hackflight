0b5d1dd08a77d9695b292ad16d978ca8a254169e
simondlevy <simon.d.levy@gmail.com>
2021-08-15 14:55:12 -0400

    =More Haskell Copilot work

---
+++ extras/copilot/Demands.hs
@@ -0,0 +1,32 @@
+{--
+  Control demands
+
+  Copyright(C) 2021 Simon D.Levy
+
+  MIT License
+--}
+
+module Demands where
+
+import Language.Copilot
+
+data Demands = Demands { throttle :: Stream Double
+                       , roll :: Stream Double  
+                       , pitch :: Stream Double  
+                       , yaw :: Stream Double  
+                     } deriving (Show)
+
+{--
+initialDemands :: Demands
+
+initialDemands = Demands 0 0 0 0
+
+
+addDemands :: Demands -> Demands -> Demands
+
+addDemands d1 d2 = Demands ((throttle d1) + (throttle d2))
+                           ((roll d1) + (roll d2))
+                           ((pitch d1) + (pitch d2))
+                           ((yaw d1) + (yaw d2))
+--}
+
--- extras/copilot/Hackflight.hs
+++ extras/copilot/Hackflight.hs
@@ -14,16 +14,13 @@ module Main where
 import Language.Copilot
 import Copilot.Compile.C99
 
+import Demands
+
 receiverDemands :: Stream (Array 4 Double)
 receiverDemands  = extern "receiverDemands" Nothing
 
 motorValues :: Stream (Array 4 Double)
 motorValues = [array [0, 0, 0, 0]] ++ motorValues
-
-data Demands = Demands { throttle :: Stream Double
-                       , roll     :: Stream Double
-                       , pitch    :: Stream Double
-                       , yaw      :: Stream Double }
 
 spec = do
 
