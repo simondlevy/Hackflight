13f12113fdddac1ebdd9ab66a1c5d250edfd3ac4
simondlevy <simon.d.levy@gmail.com>
2021-08-15 20:57:09 -0400

    =More Haskell Copilot work

--- extras/copilot/Sensor.hs
+++ extras/copilot/Sensor.hs
@@ -18,5 +18,20 @@ data Sensor = Gyrometer { gyroX :: Stream Double
                         , gyroY :: Stream Double
                         , gyroZ :: Stream Double } 
 
--- modifyState :: Sensor -> VehicleState -> VehicleState
 
+modifyState :: Sensor -> VehicleState -> VehicleState
+
+modifyState (Gyrometer gx gy gz) vehicleState = 
+
+  VehicleState (x vehicleState)
+               (dx vehicleState)
+               (y vehicleState)
+               (dy vehicleState)
+               (z vehicleState)
+               (dz vehicleState)
+               (phi vehicleState)
+               gx
+               (theta vehicleState)
+               gy
+               (psi vehicleState)
+               gz
