1990aff390a9ee0ab6d596591e15971be1b1bb05
simondlevy <simon.d.levy@gmail.com>
2021-08-16 15:27:06 -0400

    =More Haskell Copilot work

--- extras/copilot/Quaternion.hs
+++ extras/copilot/Quaternion.hs
@@ -23,6 +23,14 @@ quatModifyState :: VehicleState
 
 quatModifyState = VehicleState 0 0 0 0 0 0 phi 0 theta 0 psi 0
 
-  where phi = 0
+  where qw = quat.!!0
+        qx = quat.!!1
+        qy = quat.!!2
+        qz = quat.!!3
+        phi = 0
         theta = 0
         psi = 0
+
+        -- ex = atan2(2.0f*(qw*qx+qy*qz), qw*qw-qx*qx-qy*qy+qz*qz);
+        -- ey = asin(2.0f*(qx*qz-qw*qy));
+        -- ez = atan2(2.0f*(qx*qy+qw*qz), qw*qw+qx*qx-qy*qy-qz*qz);
