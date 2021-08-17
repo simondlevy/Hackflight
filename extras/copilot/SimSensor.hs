fd97af7050f2daf1c078c8704b01c48e2202d0c7
simondlevy <simon.d.levy@gmail.com>
2021-08-17 16:25:55 -0400

    =More Haskell Copilot work

--- extras/copilot/SimSensor.hs
+++ extras/copilot/SimSensor.hs
@@ -24,4 +24,17 @@ simSensorDz = extern "simSensorDz" Nothing
 
 simSensorModifyState :: VehicleState
 
-simSensorModifyState = VehicleState simSensorZ simSensorDz
+simSensorModifyState =
+
+  VehicleState 0 -- x
+               0 -- dx
+               0 -- y
+               0 -- dy
+               simSensorZ
+               simSensorDz
+               0 -- phi
+               0 -- dphi
+               0 -- theta
+               0 -- dtheta
+               0 -- psi
+               0 -- dpsi
--- extras/copilot/VehicleState.hs
+++ extras/copilot/VehicleState.hs
@@ -12,17 +12,38 @@ module VehicleState where
 
 import Language.Copilot
 
-data VehicleState = VehicleState {   z :: Stream Double
-                                  , dz :: Stream Double
+data VehicleState = VehicleState {       x :: Stream Double
+                                  ,     dx :: Stream Double
+                                  ,      y :: Stream Double
+                                  ,     dy :: Stream Double
+                                  ,      z :: Stream Double
+                                  ,     dz :: Stream Double
+                                  ,    phi :: Stream Double
+                                  ,   dphi :: Stream Double
+                                  ,  theta :: Stream Double
+                                  , dtheta :: Stream Double
+                                  ,    psi :: Stream Double
+                                  ,   dpsi :: Stream Double
+
                                  } deriving (Show)
 
 initialVehicleState :: VehicleState
 
-initialVehicleState = VehicleState 0 0
+initialVehicleState = VehicleState 0 0 0 0 0 0 0 0 0 0 0 0
 
 
 addStates :: VehicleState -> VehicleState -> VehicleState
 
 addStates v1 v2 = 
-    VehicleState ((z v1) + (z v2))
-                 ((dz v1) + (dz v2))
+    VehicleState ((x v1)      + (x v2))
+                 ((dx v1)     + (dx v2))
+                 ((y v1)      + (y v2))
+                 ((dy v1)     + (dy v2))
+                 ((z v1)      + (z v2))
+                 ((dz v1)     + (dz v2))
+                 ((phi v1)    + (phi v2))
+                 ((dphi v1)   + (dphi v2))
+                 ((theta v1)  + (theta v2))
+                 ((dtheta v1) + (dtheta v2))
+                 ((psi v1)    + (psi v2))
+                 ((dpsi v1)   + (dpsi v2))
