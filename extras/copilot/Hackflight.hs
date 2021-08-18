7b4d298235e5ae0b67660401f4fbeec24d02633f
simondlevy <simon.d.levy@gmail.com>
2021-08-18 18:31:58 -0400

    =More Haskell Copilot work

--- extras/copilot/Hackflight.hs
+++ extras/copilot/Hackflight.hs
@@ -61,28 +61,12 @@ spec = do
   -- Sum over the list of demands to get the final demands
   let demands = foldr addDemands zeroDemands (map pidDemands pidControllers'')
 
-  let motor = if receiverThrottle < 0 then 0 else receiverThrottle
-
-  let m1 = motor
-  let m2 = motor
-  let m3 = motor
-  let m4 = motor
+  let m1 = getMotor1 mixer demands
+  let m2 = getMotor2 mixer demands
+  let m3 = getMotor3 mixer demands
+  let m4 = getMotor4 mixer demands
 
   trigger "runMotors" true [arg m1, arg m2, arg m3, arg m4]
-
-  trigger "showVehicleState" true [  arg (x      vehicleState)
-                                   , arg (dx     vehicleState)
-                                   , arg (y      vehicleState)
-                                   , arg (dy     vehicleState)
-                                   , arg (z      vehicleState)
-                                   , arg (dz     vehicleState)
-                                   , arg (phi    vehicleState)
-                                   , arg (dphi   vehicleState)
-                                   , arg (theta  vehicleState)
-                                   , arg (dtheta vehicleState)
-                                   , arg (psi    vehicleState)
-                                   , arg (dpsi   vehicleState)
-                                  ]
 
 -- Compile the spec
 main = reify spec >>= compile "hackflight"
