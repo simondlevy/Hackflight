2181b0f4109809b6d0c04d6cf2d3807f6ce689a9
simondlevy <simon.d.levy@gmail.com>
2021-08-17 20:45:20 -0400

    =More Haskell Copilot work

--- extras/copilot/Hackflight.hs
+++ extras/copilot/Hackflight.hs
@@ -36,7 +36,7 @@ receiverYaw  = extern "receiverYaw" Nothing
 
 spec = do
 
-  let sensors = [gyrometer, altimeter]
+  let sensors = [euler, gyrometer, altimeter]
 
   -- Get the vehicle state by running the sensors
   let vehicleState = compose sensors initialVehicleState
