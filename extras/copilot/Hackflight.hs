d0698d00c02f5c86f6b92de946097e4f556286d1
simondlevy <simon.d.levy@gmail.com>
2021-08-20 20:08:33 -0400

    Begin separate Main/Hackflight modules

---
+++ extras/copilot/Hackflight.hs
@@ -0,0 +1,16 @@
+{--
+  Hackflight core algorithm
+
+  Copyright(C) 2021 on D.Levy
+
+  MIT License
+--}
+
+{-# LANGUAGE RebindableSyntax #-}
+{-# LANGUAGE DataKinds        #-}
+
+module Hackflight where
+
+import Language.Copilot
+import Copilot.Compile.C99
+
