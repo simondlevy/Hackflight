9b69dfccb224fe5b189b7e6eabcaded8ce4f7ffe
simondlevy <simon.d.levy@gmail.com>
2021-08-28 19:21:32 -0400

    More Haskell Copilot work

--- examples/Copilot/Copilot.ino
+++ examples/Copilot/Copilot.ino
@@ -34,29 +34,6 @@ void serialEvent1(void)
     }
 }
 
-// Used by Copilot ---------------------------------
-
-float copilot_time = 0;
-
-float copilot_receiverThrottle = 0;
-float copilot_receiverRoll = 0;
-float copilot_receiverPitch = 0;
-float copilot_receiverYaw = 0;
-
-bool copilot_receiverLostSignal = false;
-bool copilot_receiverReady = true;
-bool copilot_receiverInArmedState = true;
-bool copilot_receiverInactive = false;
-
-float copilot_gyrometerX = 0;
-float copilot_gyrometerY = 0;
-float copilot_gyrometerZ = 0;
-
-float copilot_quaternionW = 0;
-float copilot_quaternionX = 0;
-float copilot_quaternionY = 0;
-float copilot_quaternionZ = 0;
-
 static void reportMotor(float value, uint8_t index)
 {
     Serial.print("m");
