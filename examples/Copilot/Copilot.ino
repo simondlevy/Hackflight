9637fc7e112047aba4d4710e15f04dab1873d554
simondlevy <simon.d.levy@gmail.com>
2021-08-27 22:11:06 -0400

    Begin Arduino sketch with Haskell Copilot

---
+++ examples/Copilot/Copilot.ino
@@ -0,0 +1,61 @@
+/*
+   Hackflight using Haskell Copilot
+
+   Copyright(C) 2021 Simon D.Levy
+
+   MIT License
+*/
+
+#include "hackflight.h"
+
+// Used by Copilot ---------------------------------
+
+float copilot_time = 0;
+
+float copilot_receiverThrottle = 0;
+float copilot_receiverRoll = 0;
+float copilot_receiverPitch = 0;
+float copilot_receiverYaw = 0;
+
+bool copilot_receiverLostSignal = false;
+bool copilot_receiverReady = true;
+bool copilot_receiverInArmedState = true;
+bool copilot_receiverInactive = false;
+
+float copilot_altimeterZ = 0;
+float copilot_altimeterDz = 0;
+
+float copilot_gyrometerX = 0;
+float copilot_gyrometerY = 0;
+float copilot_gyrometerZ = 0;
+
+float copilot_quaternionW = 0;
+float copilot_quaternionX = 0;
+float copilot_quaternionY = 0;
+float copilot_quaternionZ = 0;
+
+float copilot_flowX = 0;
+float copilot_flowY = 0;
+
+// Sent by Copilot to copilot_runMotors() -----------
+static float _m1;
+static float _m2;
+static float _m3;
+static float _m4;
+
+// Called by Copilot
+void copilot_runMotors(float m1, float m2, float m3, float m4)
+{
+    _m1 = m1;
+    _m2 = m2;
+    _m3 = m3;
+    _m4 = m4;
+}
+
+void setup(void)
+{
+}
+
+void loop(void)
+{
+}
