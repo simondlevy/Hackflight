66ff150b79eea7c5dc2d8d7835b67029cd530927
simondlevy <simon.d.levy@gmail.com>
2021-09-02 03:57:08 -0400

    =More Haskell Copilot work

--- extras/haskell/LED.hs
+++ extras/haskell/LED.hs
@@ -8,8 +8,9 @@
 
 module LED where
 
-import Prelude hiding((<))
+import Prelude hiding((<), (>), (&&), (||))
 import Language.Copilot
+
 import Time
 
 type LedState = Stream Bool
@@ -17,4 +18,15 @@ type LedState = Stream Bool
 ledState :: LedState
 
 -- Blink LED on startup
-ledState = time < 1.0
+ledState = time < 1.0 && 
+           (  time > 0.00 && time < 0.05
+           || time > 0.10 && time < 0.15 
+           || time > 0.20 && time < 0.25 
+           || time > 0.30 && time < 0.35 
+           || time > 0.40 && time < 0.45 
+           || time > 0.50 && time < 0.55 
+           || time > 0.60 && time < 0.65 
+           || time > 0.70 && time < 0.75 
+           || time > 0.80 && time < 0.85 
+           || time > 0.90 && time < 0.95 
+           )
