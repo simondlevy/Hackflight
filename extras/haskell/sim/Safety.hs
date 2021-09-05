c5528cd09ab082aa98e1c7cea11dd8324c7cc22d
simondlevy <simon.d.levy@gmail.com>
2021-09-05 16:41:33 -0400

    =More Haskell Copilot work

---
+++ extras/haskell/sim/Safety.hs
@@ -0,0 +1,38 @@
+{--
+  Hackflight safety checking
+
+  Copyright(C) 2021 on D.Levy
+
+  MIT License
+--}
+
+{-# LANGUAGE RebindableSyntax #-}
+
+module Safety where
+
+import Language.Copilot
+
+import Receiver
+import State
+
+data Safety = Safety { armed :: Stream Bool, failsafe :: Stream Bool }
+
+getSafety :: Stream Bool -> State -> Safety
+
+getSafety starting vehicleState = Safety armed failsafe
+
+  where
+
+    -- Use receiver data to trap failsafe
+    failsafe = receiverLostSignal || failsafe' where failsafe' = [False] ++ failsafe
+
+    -- Arm after safety checks
+    armed = if failsafe then false 
+
+            else if receiverAux1 > 0
+                 && receiverThrottleIsDown
+                 && safeToArm vehicleState then true 
+
+            else if receiverAux1 < 0 then false
+
+            else armed' where armed' = [False] ++ armed
