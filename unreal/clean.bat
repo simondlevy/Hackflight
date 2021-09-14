8a997cf65ccedce99b03a2be173712116f803ac2
simondlevy <simon.d.levy@gmail.com>
2021-09-13 21:48:50 -0400

    Removed unused OpenCV DLLs

--- unreal/clean.bat
+++ unreal/clean.bat
@@ -9,7 +9,4 @@
 
 del /f /q *.txt Makefile *.sln *.pri *.kdev4 *.pro *.*workspace > NUL
 
-del /f /q Binaries\Win64\*.target > NUL
-del /f /q Binaries\Win64\UE4* > NUL
-
-rmdir /q /s .vs Build DerivedDataCache Intermediate Saved > NUL:q
+rmdir /q /s .vs Binaries Build DerivedDataCache Intermediate Saved > NUL
