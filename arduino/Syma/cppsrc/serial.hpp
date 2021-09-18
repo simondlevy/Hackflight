b281c820871fbdd1e0883929a73b3914c0bd5c16
simondlevy <simon.d.levy@gmail.com>
2021-09-17 21:15:02 -0400

    Cleanup

--- arduino/Syma/cppsrc/serial.hpp
+++ arduino/Syma/cppsrc/serial.hpp
@@ -19,43 +19,120 @@ namespace hf {
 
         friend class Hackflight;
 
+        private:
+
+        void prepareSerialOutput(
+                State * vehicleState,
+                Mixer * mixer,
+                uint8_t type,
+                serial_t & serial)
+        {
+            serial.type = type;
+
+            switch (type) {
+
+                case 121:
+                    {
+                        serial.count = 6;
+                        serial.output01 = copilot_receiverThrottle;
+                        serial.output02 = copilot_receiverRoll;
+                        serial.output03 = copilot_receiverPitch;
+                        serial.output04 = copilot_receiverYaw;
+                        serial.output05 = copilot_receiverAux1;
+                        serial.output06 = 0; // XXX we should support aux2
+                    } break;
+
+                case 122:
+                    {
+                        serial.count = 12;
+                        float * x = vehicleState->x;
+                        serial.output01 = x[State::X];
+                        serial.output02 = x[State::DX];
+                        serial.output03 = x[State::Y];
+                        serial.output04 = x[State::DY];
+                        serial.output05 = x[State::Z];
+                        serial.output06 = x[State::DZ];
+                        serial.output07 = x[State::PHI];
+                        serial.output08 = x[State::DPHI];
+                        serial.output09 = x[State::THETA];
+                        serial.output10 = x[State::DTHETA];
+                        serial.output11 = x[State::PSI];
+                        serial.output12 = x[State::DPSI];
+                    } break;
+
+                case 123:
+                    {
+                        serial.count = 1;
+                        serial.output01 = mixer->getType();
+                    } break;
+
+            } // switch (type)
+
+        } // prepareSerialOutput
+
+        void handleSerialInput(Mixer * mixer, uint8_t type)
+        {
+            switch (type) {
+
+                case 215:
+                    {
+                        mixer->setMotorDisarmed(0, copilot_Input1);
+                        mixer->setMotorDisarmed(1, copilot_Input2);
+                        mixer->setMotorDisarmed(2, copilot_Input3);
+                        mixer->setMotorDisarmed(3, copilot_Input4);
+
+                    } break;
+
+            } // switch (type)
+
+        } // handleSerialInput
+
+        static uint8_t mtype2count(uint8_t mt) 
+        {
+            return mt == 121 ? 6
+                 : mt == 122 ? 12
+                 : mt == 123 ? 1
+                 : 0;
+        }
+
+
         protected:
 
-            void parse(Mixer * mixer, State * vehicleState, serial_t & serial)
-            {
-                enum {
-                    IDLE,
-                    GOT_START,
-                    GOT_M,
-                    GOT_ARROW,
-                    GOT_SIZE,
-                    IN_PAYLOAD
-                }; 
+        void parse(Mixer * mixer, State * vehicleState, serial_t & serial)
+        {
+            enum {
+                IDLE,
+                GOT_START,
+                GOT_M,
+                GOT_ARROW,
+                GOT_SIZE,
+                IN_PAYLOAD
+            }; 
 
-                static uint8_t parser_state;
+            static uint8_t parser_state;
 
-                static uint8_t type;
-                static uint8_t crc;
-                static uint8_t size;
-                static uint8_t index;
+            static uint8_t type;
+            static uint8_t crc;
+            static uint8_t size;
+            static uint8_t index;
 
-                uint8_t c = copilot_serialByte;
+            uint8_t c = copilot_serialByte;
 
-                // Payload functions
-                size = parser_state == GOT_ARROW ? c : size;
-                index = parser_state == IN_PAYLOAD ? index + 1 : 0;
-                bool incoming = type >= 200;
-                bool in_payload = incoming && parser_state == IN_PAYLOAD;
+            // Payload functions
+            size = parser_state == GOT_ARROW ? c : size;
+            index = parser_state == IN_PAYLOAD ? index + 1 : 0;
+            bool incoming = type >= 200;
+            bool in_payload = incoming && parser_state == IN_PAYLOAD;
 
-                // Command acquisition function
-                type = parser_state == GOT_SIZE ? c : type;
+            // Command acquisition function
+            type = parser_state == GOT_SIZE ? c : type;
 
-                // Checksum transition function
-                crc = parser_state == GOT_ARROW ? c
-                    : parser_state == IN_PAYLOAD  ?  crc ^ c 
-                    : 0;
+            // Checksum transition function
+            crc = parser_state == GOT_ARROW ? c
+                : parser_state == IN_PAYLOAD  ?  crc ^ c 
+                : 0;
 
-                // Parser state transition function
+            // Parser state transition function
                 parser_state
                     = parser_state == IDLE && c == '$' ? GOT_START
                     : parser_state == GOT_START && c == 'M' ? GOT_M
@@ -66,16 +143,16 @@ namespace hf {
                     : parser_state == IN_PAYLOAD ? IDLE
                     : parser_state;
 
-                serial.count = 0; // assume nothing
+                bool ready = parser_state == IDLE && crc == c;
 
-                // Payload accumulation
-                if (in_payload) {
-                    serial.count = -1; // incoming
-                    serial.input = c;
-                }
+                serial.count = in_payload ? -1
+                    : ready && size > 0 ? mtype2count(type)
+                    : 0;
+
+                serial.input = in_payload ? c : 0;
 
                 // Message dispatch
-                if (parser_state == IDLE && crc == c) {
+                if (ready) {
 
                     if (size > 0) {
                         handleSerialInput(mixer, type);
@@ -88,68 +165,6 @@ namespace hf {
                 }
 
             } // parse
-
-            void prepareSerialOutput(State * vehicleState, Mixer * mixer, uint8_t type, serial_t & serial)
-            {
-                serial.type = type;
-
-                switch (type) {
-
-                    case 121:
-                        {
-                            serial.count = 6;
-                            serial.output01 = copilot_receiverThrottle;
-                            serial.output02 = copilot_receiverRoll;
-                            serial.output03 = copilot_receiverPitch;
-                            serial.output04 = copilot_receiverYaw;
-                            serial.output05 = copilot_receiverAux1;
-                            serial.output06 = 0; // XXX we should support aux2
-                        } break;
-
-                    case 122:
-                        {
-                            serial.count = 12;
-                            float * x = vehicleState->x;
-                            serial.output01 = x[State::X];
-                            serial.output02 = x[State::DX];
-                            serial.output03 = x[State::Y];
-                            serial.output04 = x[State::DY];
-                            serial.output05 = x[State::Z];
-                            serial.output06 = x[State::DZ];
-                            serial.output07 = x[State::PHI];
-                            serial.output08 = x[State::DPHI];
-                            serial.output09 = x[State::THETA];
-                            serial.output10 = x[State::DTHETA];
-                            serial.output11 = x[State::PSI];
-                            serial.output12 = x[State::DPSI];
-                        } break;
-
-                    case 123:
-                        {
-                            serial.count = 1;
-                            serial.output01 = mixer->getType();
-                        } break;
-
-                } // switch (type)
-
-            } // prepareSerialOutput
-
-            void handleSerialInput(Mixer * mixer, uint8_t type)
-            {
-                switch (type) {
-
-                    case 215:
-                        {
-                            mixer->setMotorDisarmed(0, copilot_Input1);
-                            mixer->setMotorDisarmed(1, copilot_Input2);
-                            mixer->setMotorDisarmed(2, copilot_Input3);
-                            mixer->setMotorDisarmed(3, copilot_Input4);
-
-                        } break;
-
-                } // switch (type)
-
-            } // handleSerialInput
 
     }; // class SerialTask
 
