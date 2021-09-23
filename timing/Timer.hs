ef3564376df34a4165f2527e5fc009879db4d9d0
simondlevy <simon.d.levy@gmail.com>
2021-09-23 02:07:57 -0400

    =More Haskell Copilot work

--- timing/Timer.hs
+++ timing/Timer.hs
@@ -5,21 +5,21 @@ module Main where
 import Language.Copilot
 import Copilot.Compile.C99
 
-import Prelude hiding ((>), (-), (++), (==))
+import Prelude hiding ((>), (-), (++), (==), div)
 
 time_sec :: Stream Float
 time_sec  = extern "copilot_time_sec" Nothing
 
-ready :: Stream Bool
+ready :: Stream Float -> Stream Bool
 
-ready = (time_sec == time_sec_prev) where
+ready period = (time_sec == time_sec_prev) where
 
-  time_sec_prev = if (time_sec - time_sec_prev') > 1 then time_sec else time_sec_prev'
+  time_sec_prev = if (time_sec - time_sec_prev') > period then time_sec else time_sec_prev'
   time_sec_prev' = [0] ++ time_sec_prev
 
 spec = do
 
-  trigger "tick" ready []
+  trigger "tick" (ready 1) []
 
 -- Compile the spec
 main = reify spec >>= compile "timer"
