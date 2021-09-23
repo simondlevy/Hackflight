5318e2ad4ac387842205da1af6fe2c35e3689d64
simondlevy <simon.d.levy@gmail.com>
2021-09-23 02:04:05 -0400

    =More Haskell Copilot work

--- timing/Timer.hs
+++ timing/Timer.hs
@@ -12,23 +12,14 @@ time_sec  = extern "copilot_time_sec" Nothing
 
 ready :: Stream Bool
 
-ready = (time_sec - time_sec_prev)  > 1 where
-
-  time_sec_prev = if (time_sec - time_sec_prev') > 1 then time_sec else time_sec_prev'
-  time_sec_prev' = [0] ++ time_sec_prev
-
-times :: (Stream Float, Stream Float)
-
-times = (time_sec, time_sec_prev) where
+ready = (time_sec == time_sec_prev) where
 
   time_sec_prev = if (time_sec - time_sec_prev') > 1 then time_sec else time_sec_prev'
   time_sec_prev' = [0] ++ time_sec_prev
 
 spec = do
 
-  let (t, p) = times
-
-  trigger "tick" (t == p) []
+  trigger "tick" ready []
 
 -- Compile the spec
 main = reify spec >>= compile "timer"
