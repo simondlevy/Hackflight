a02ceb57d64d51830f97fb01a907107d5602540a
simondlevy <simon.d.levy@gmail.com>
2021-09-23 02:17:27 -0400

    =More Haskell Copilot work

--- source/Time.hs
+++ source/Time.hs
@@ -6,9 +6,19 @@
   MIT License
 --}
 
+{-# LANGUAGE RebindableSyntax #-}
+
 module Time where
 
 import Language.Copilot
+import Prelude hiding ((>), (-), (++), (==), (/))
+
+ready :: Stream Float -> Stream Bool
+
+ready freq = (time_sec == time_sec_prev) where
+
+  time_sec_prev = if (time_sec - time_sec_prev') > (1 / freq) then time_sec else time_sec_prev'
+  time_sec_prev' = [0] ++ time_sec_prev
 
 time_sec :: Stream Float
 time_sec  = extern "copilot_time_sec" Nothing
