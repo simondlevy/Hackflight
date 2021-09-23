dee987403c73395c118af105e57f4c111558cee6
simondlevy <simon.d.levy@gmail.com>
2021-09-23 01:11:05 -0400

    =More Haskell Copilot work

--- timing/Timer.hs
+++ timing/Timer.hs
@@ -5,9 +5,13 @@ import Copilot.Compile.C99
 
 import Prelude hiding ((>), (<), div)
 
+time_sec :: Stream Float
+time_sec  = extern "copilot_time_sec" Nothing
+
 spec = do
 
   trigger "tick"  true []
 
 -- Compile the spec
 main = reify spec >>= compile "timer"
+
