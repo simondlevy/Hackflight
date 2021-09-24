23d621cdde92461c3109b6b58a53f6b1754b53ee
simondlevy <simon.d.levy@gmail.com>
2021-09-24 09:50:43 -0400

    Moved setup code into Arduino sketch

---
+++ arduino/NewSyma/copilot_setup.h
@@ -0,0 +1,16 @@
+/*
+   Startup routine declarations
+
+   Copyright (C) 2021 Simon D. Levy
+
+   MIT License
+
+ */
+
+void copilot_startLed(uint8_t pin);
+
+void copilot_startReceiver(void);;
+
+void copilot_startImu(void);
+
+
