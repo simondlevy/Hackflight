f48c61f8db219b8099f63561eecafe8bf3d01b9a
simondlevy <simon.d.levy@gmail.com>
2021-10-07 22:12:36 -0400

    Cleanup

--- src/HF_timertask.hpp
+++ src/HF_timertask.hpp
@@ -15,21 +15,21 @@ namespace hf {
         private:
 
             uint32_t _period = 0;
-            uint32_t _time_prev = 0;
+            uint32_t _time_usec;
 
         protected:
 
             TimerTask(uint32_t freq)
             {
                 _period = 1000000 / freq;
-                _time_prev = 0;
+                _time_usec = 0;
             }
 
             bool ready(uint32_t time_usec)
             {
-                if ((time_usec - _time_prev) > _period)
+                if ((time_usec - _time_usec) > _period)
                 {
-                    _time_prev = time_usec;
+                    _time_usec = time_usec;
                     return true;
                 }
 
