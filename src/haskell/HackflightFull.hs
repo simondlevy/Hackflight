1da7d7e47b2a7a62e2795bd014c3b2b2b59964b3
simondlevy <simon.d.levy@gmail.com>
2021-10-17 15:19:57 -0400

    Cleanup

--- src/haskell/HackflightFull.hs
+++ src/haskell/HackflightFull.hs
@@ -35,7 +35,7 @@ hackflight receiver sensors pidfuns mixer = (state, mready, mcut , pdemands, mot
     rdemands = getDemands receiver
 
     -- Get the vehicle state by composing the sensor functions over the current state
-    state = compose sensors state'
+    state = compose sensors (state' state)
 
     -- Periodically get the demands by composing the PID controllers over the receiver
     -- demands
@@ -48,16 +48,3 @@ hackflight receiver sensors pidfuns mixer = (state, mready, mcut , pdemands, mot
 
     -- Blink LED during first couple of seconds; keep it solid when armed
     led = if micros < 2000000 then (mod (div micros 50000) 2 == 0) else armed
-
-    state' = State ([0] ++ (x state))
-                   ([0] ++ (dx state))
-                   ([0] ++ (y state))
-                   ([0] ++ (dy state))
-                   ([0] ++ (z state))
-                   ([0] ++ (dz state))
-                   ([0] ++ (phi state))
-                   ([0] ++ (dphi state))
-                   ([0] ++ (theta state))
-                   ([0] ++ (dtheta state))
-                   ([0] ++ (psi state))
-                   ([0] ++ (dpsi state))
--- src/haskell/State.hs
+++ src/haskell/State.hs
@@ -13,7 +13,7 @@
 module State where
 
 import Language.Copilot
-import Prelude hiding((&&), (<))
+import Prelude hiding((&&), (<), (++))
 
 import Utils
 
@@ -36,3 +36,19 @@ safeToArm :: State -> SBool
 
 safeToArm (State _ _ _ _ _ _ phi _ theta _ _ _) = safe phi && safe theta
   where safe angle = (abs (rad2deg angle)) < 25
+    
+
+state' :: State -> State
+
+state' state = State ([0] ++ (x state))
+                     ([0] ++ (dx state))
+                     ([0] ++ (y state))
+                     ([0] ++ (dy state))
+                     ([0] ++ (z state))
+                     ([0] ++ (dz state))
+                     ([0] ++ (phi state))
+                     ([0] ++ (dphi state))
+                     ([0] ++ (theta state))
+                     ([0] ++ (dtheta state))
+                     ([0] ++ (psi state))
+                     ([0] ++ (dpsi state))
