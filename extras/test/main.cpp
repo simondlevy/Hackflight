f57dfd05b3cd7a201854562fbf54db328d710483
simondlevy <simon.d.levy@gmail.com>
2021-10-21 21:18:34 -0400

    =More parser testing work

---
+++ extras/test/main.cpp
@@ -0,0 +1,19 @@
+/*
+   Parser tester
+
+   MIT License
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "parser.hpp"
+
+int main(int argc, char ** argv)
+{
+    parse(36);
+    parse(77);
+    parse(60);
+
+    return 0;
+}
