afb8a7bcee1f32a43a872848e5c061ceb91af42b
simondlevy <simon.d.levy@gmail.com>
2021-10-25 13:59:58 -0400

    Moved parser buffering code to sketch

--- examples/Syma/Syma.ino
+++ examples/Syma/Syma.ino
@@ -20,11 +20,36 @@
 #include "parser.hpp"
 #include "debugger.hpp"
 #include "stream_serial.h"
+#include "stream_receiver.h"
 #include "copilot.h"
 
 void stream_writeBrushedMotors(
         uint8_t m1_pin, uint8_t m2_pin, uint8_t m3_pin, uint8_t m4_pin,
         float m1_val, float m2_val, float m3_val, float m4_val);
+
+static void setOutBuf(uint8_t * buffer, uint8_t index, bool sending, uint8_t byte)
+{
+    buffer[index] = sending ? byte : buffer[index];
+}
+
+static void serialize(uint8_t * buffer, uint8_t & buffer_size, uint8_t & crc_out, bool sending, uint8_t byte)
+{
+    setOutBuf(buffer, buffer_size, sending, byte);
+    buffer_size = sending ? buffer_size + 1 : buffer_size;
+    crc_out = sending ? crc_out ^ byte : crc_out;
+}
+
+static void serializeFloat(uint8_t * buffer, 
+        uint8_t & buffer_size, uint8_t & crc_out, bool sending, float value)
+{
+    uint32_t uintval = 1000 * (value + 2);
+
+    serialize(buffer, buffer_size, crc_out, sending, uintval     & 0xFF);
+    serialize(buffer, buffer_size, crc_out, sending, uintval>>8  & 0xFF);
+    serialize(buffer, buffer_size, crc_out, sending, uintval>>16 & 0xFF);
+    serialize(buffer, buffer_size, crc_out, sending, uintval>>24 & 0xFF);
+}
+
 
 void setup(void)
 {
@@ -49,18 +74,68 @@ void stream_run(
         float m3_flying,
         float m4_flying)
 {
-    bool data_available = false;
-    uint8_t data_byte = 0;
-    uint8_t gcs_motor_index = 0;
-    uint8_t gcs_motor_percent = 0;
+    static uint8_t _buffer[128];
+    static uint8_t _buffer_size;
+    static uint8_t _buffer_index;
+    static uint8_t _crc_out;
 
-    parse(state_phi,
-          state_theta,
-          state_psi,
-          data_available,
-          data_byte,
-          gcs_motor_index,
-          gcs_motor_percent);
+    bool sending = false;
+    bool receiving = false;
+    uint8_t pindex = 0;
+    uint8_t msgtype = 0;
+
+    parse(stream_serialAvailable, stream_serialByte,
+          sending, receiving, pindex, msgtype);
+
+    _buffer[pindex] = receiving ? stream_serialByte : _buffer[pindex];
+
+    _buffer_index = sending ? 0 : _buffer_index;
+
+    setOutBuf(_buffer, 0, sending, '$');
+    setOutBuf(_buffer, 1, sending, 'M');
+    setOutBuf(_buffer, 2, sending, '>');
+
+    uint8_t outsize = 4 * (msgtype == 121 ? 6 : msgtype == 122 ? 3 : 0);
+
+    _buffer_size = sending ? 3 : _buffer_size;
+    _buffer[_buffer_size] = sending ? outsize : _buffer[_buffer_size];
+
+    _buffer_size = sending ? 4 : _buffer_size;
+    _buffer[_buffer_size] = sending ? msgtype : _buffer[_buffer_size];
+
+    _buffer_size = sending ? 5 : _buffer_size;
+
+    _crc_out = sending ? outsize ^ msgtype : 0;
+
+    serializeFloat(_buffer, _buffer_size, _crc_out, sending,
+            msgtype == 121 ? stream_receiverThrottle : msgtype == 122 ? state_phi : 0);
+
+    serializeFloat(_buffer, _buffer_size, _crc_out, sending,
+            msgtype == 121 ? stream_receiverRoll : msgtype == 122 ? state_theta : 0);
+
+    serializeFloat(_buffer, _buffer_size, _crc_out, sending,
+            msgtype == 121 ? stream_receiverPitch : msgtype == 122 ? state_psi : 0);
+
+    bool sendrc = sending && msgtype == 121;
+
+    serializeFloat(_buffer, _buffer_size, _crc_out, sendrc, stream_receiverYaw);
+
+    serializeFloat(_buffer, _buffer_size, _crc_out, sendrc, stream_receiverAux1);
+
+    serializeFloat(_buffer, _buffer_size, _crc_out, sendrc, stream_receiverAux2);
+
+    serialize(_buffer, _buffer_size, _crc_out, sending, _crc_out);
+
+    uint8_t motor_index = msgtype == 215 ? _buffer[0] : 0;
+    uint8_t motor_percent = msgtype == 215 ? _buffer[1] : 0;
+
+    uint8_t data_available = _buffer_size > 0;
+
+    _buffer_size = data_available ? _buffer_size - 1 : _buffer_size;
+
+    uint8_t data_byte = data_available ? _buffer[_buffer_index] : 0;
+
+    _buffer_index = data_available ? _buffer_index + 1 : _buffer_index;
 
     stream_serialUpdate();
 
@@ -72,10 +147,10 @@ void stream_run(
         stream_serialWrite(data_byte);
     }
 
-    float m1_val = armed ? m1_flying : gcs_motor_index == 1 ? gcs_motor_percent/100. : 0;
-    float m2_val = armed ? m2_flying : gcs_motor_index == 2 ? gcs_motor_percent/100. : 0;
-    float m3_val = armed ? m3_flying : gcs_motor_index == 3 ? gcs_motor_percent/100. : 0;
-    float m4_val = armed ? m4_flying : gcs_motor_index == 4 ? gcs_motor_percent/100. : 0;
+    float m1_val = armed ? m1_flying : motor_index == 1 ? motor_percent/100. : 0;
+    float m2_val = armed ? m2_flying : motor_index == 2 ? motor_percent/100. : 0;
+    float m3_val = armed ? m3_flying : motor_index == 3 ? motor_percent/100. : 0;
+    float m4_val = armed ? m4_flying : motor_index == 4 ? motor_percent/100. : 0;
 
     stream_writeBrushedMotors(m1_pin, m2_pin, m3_pin, m4_pin, m1_val, m2_val, m3_val, m4_val);
 }
--- src/parser.hpp
+++ src/parser.hpp
@@ -6,152 +6,48 @@
 
 #pragma once
 
-#include "copilot.h"
-
-#include "stream_receiver.h"
-#include "stream_serial.h"
-#include "buffer.h"
-
-#include "debugger.hpp"
-#ifndef stream_receiverRoll
-extern float stream_receiverRoll;
-extern float stream_receiverPitch;
-extern float stream_receiverYaw;
-#endif
-
-static void setOutBuf(uint8_t * buffer, buffer_t & buff, uint8_t index, bool sending, uint8_t byte)
-{
-    buffer[index] = sending ? byte : buffer[index];
-    setbuff(buff, sending, index, byte);
-}
-
-static void serialize(uint8_t * buffer, buffer_t & buff,
-        uint8_t & buffer_size, uint8_t & crc_out, bool sending, uint8_t byte)
-{
-    setOutBuf(buffer, buff, buffer_size, sending, byte);
-    buffer_size = sending ? buffer_size + 1 : buffer_size;
-    crc_out = sending ? crc_out ^ byte : crc_out;
-}
-
-static void serializeFloat(uint8_t * buffer, buffer_t & buff,
-        uint8_t & buffer_size, uint8_t & crc_out, bool sending, float value)
-{
-    uint32_t uintval = 1000 * (value + 2);
-
-    serialize(buffer, buff, buffer_size, crc_out, sending, uintval     & 0xFF);
-    serialize(buffer, buff, buffer_size, crc_out, sending, uintval>>8  & 0xFF);
-    serialize(buffer, buff, buffer_size, crc_out, sending, uintval>>16 & 0xFF);
-    serialize(buffer, buff, buffer_size, crc_out, sending, uintval>>24 & 0xFF);
-}
-
-void parse(
-        float state_phi,
-        float state_theta,
-        float state_psi,
-        bool & data_available,
-        uint8_t & data_byte,
-        uint8_t & motor_index,
-        uint8_t & motor_percent)
+void parse(bool avail, uint8_t byte,
+        bool & sending, bool & receiving, uint8_t & pindex, uint8_t & msgtype)
 {
     static uint8_t _parser_state;
-    static uint8_t _msgtype;
-    static uint8_t _crc_in;
     static uint8_t _input_size;
     static uint8_t _payload_index;
-    static uint8_t _crc_out;
-    static uint8_t _buffer[128];
-    static uint8_t _buffer_index;
-    static uint8_t _buffer_size;
-
-    static buffer_t _buff;
-
-    motor_index = 0;
-    motor_percent = 0;
+    static uint8_t _msgtype;
+    static uint8_t _crc_in;
 
     // Payload functions
-    _input_size = _parser_state == 3 ? stream_serialByte : _input_size;
+    _input_size = _parser_state == 3 ? byte : _input_size;
     _payload_index = _parser_state == 5 ? _payload_index + 1 : 0;
-    bool receiving = _msgtype >= 200 && _parser_state == 5 && _payload_index <= _input_size;
+    receiving = _msgtype >= 200
+             && _parser_state == 5
+             && _payload_index <= _input_size;
 
     // Command acquisition function
-    _msgtype = _parser_state == 4 ? stream_serialByte : _msgtype;
+    _msgtype = _parser_state == 4 ? byte : _msgtype;
 
     // Checksum transition function
-    _crc_in = _parser_state == 3 ? stream_serialByte
-        : _parser_state == 4  ?  _crc_in ^ stream_serialByte 
-        : receiving ?  _crc_in ^ stream_serialByte
+    _crc_in = _parser_state == 3 ? byte
+        : _parser_state == 4  ?  _crc_in ^ byte 
+        : receiving ?  _crc_in ^ byte
         : _parser_state == 5  ?  _crc_in
         : 0;
 
     // Parser state transition function
     _parser_state
-        = _parser_state == 0 && stream_serialByte == '$' ? 1
-        : _parser_state == 1 && stream_serialByte == 'M' ? 2
-        : _parser_state == 2 && (stream_serialByte == '<' || stream_serialByte == '>') ? 3
+        = _parser_state == 0 && byte == '$' ? 1
+        : _parser_state == 1 && byte == 'M' ? 2
+        : _parser_state == 2 && (byte == '<' || byte == '>') ? 3
         : _parser_state == 3 ? 4
         : _parser_state == 4 ? 5
         : _parser_state == 5 && receiving ? 5
         : _parser_state == 5 ? 0
         : _parser_state;
 
-    // Incoming payload accumulation
-    uint8_t pindex = receiving ? _payload_index - 1 : 0;
-    _buffer[pindex] = receiving ? stream_serialByte : _buffer[pindex];
-    setbuff(_buff, receiving, pindex, stream_serialByte);
+    msgtype = _msgtype;
 
-    // Message dispatch
-    bool sending = stream_serialAvailable
-              && _parser_state == 0
-              && _crc_in == stream_serialByte
-              && (_msgtype == 121 || _msgtype == 122);
+    uint8_t incoming = _msgtype >= 200;
 
-    _buffer_index = sending ? 0 : _buffer_index;
+    sending = avail && _parser_state == 0 && _crc_in == byte && !incoming;
 
-    uint8_t outsize = 4 * (_msgtype == 121 ? 6 : _msgtype == 122 ? 3 : 0);
-
-    setOutBuf(_buffer, _buff, 0, sending, '$');
-    setOutBuf(_buffer, _buff, 1, sending, 'M');
-    setOutBuf(_buffer, _buff, 2, sending, '>');
-
-    _buffer_size = sending ? 3 : _buffer_size;
-    _buffer[_buffer_size] = sending ? outsize : _buffer[_buffer_size];
-    setbuff(_buff, sending, _buffer_size, outsize);
-
-    _buffer_size = sending ? 4 : _buffer_size;
-    _buffer[_buffer_size] = sending ? _msgtype : _buffer[_buffer_size];
-    setbuff(_buff, sending, _buffer_size, _msgtype);
-
-    _buffer_size = sending ? 5 : _buffer_size;
-
-    _crc_out = sending ? outsize ^ _msgtype : 0;
-
-    serializeFloat(_buffer, _buff, _buffer_size, _crc_out, sending,
-            _msgtype == 121 ? stream_receiverThrottle : _msgtype == 122 ? state_phi : 0);
-
-    serializeFloat(_buffer, _buff, _buffer_size, _crc_out, sending,
-            _msgtype == 121 ? stream_receiverRoll : _msgtype == 122 ? state_theta : 0);
-
-    serializeFloat(_buffer, _buff, _buffer_size, _crc_out, sending,
-            _msgtype == 121 ? stream_receiverPitch : _msgtype == 122 ? state_psi : 0);
-
-    bool sendrc = sending && _msgtype == 121;
-
-    serializeFloat(_buffer, _buff, _buffer_size, _crc_out, sendrc, stream_receiverYaw);
-
-    serializeFloat(_buffer, _buff, _buffer_size, _crc_out, sendrc, stream_receiverAux1);
-
-    serializeFloat(_buffer, _buff, _buffer_size, _crc_out, sendrc, stream_receiverAux2);
-
-    serialize(_buffer, _buff, _buffer_size, _crc_out, sending, _crc_out);
-
-    motor_index = _msgtype == 215 ? _buffer[0] : 0;
-    motor_percent = _msgtype == 215 ? _buffer[1] : 0;
-
-    data_available = _buffer_size > 0;
-
-    _buffer_size = data_available ? _buffer_size - 1 : _buffer_size;
-
-    data_byte = data_available ? _buffer[_buffer_index] : 0;
-
-    _buffer_index = data_available ? _buffer_index + 1 : _buffer_index;
+    pindex = receiving ? _payload_index - 1 : 0;
 }
