d66941072b9eb160a469cba77f290a0330dd552f
simondlevy <simon.d.levy@gmail.com>
2021-10-25 16:34:37 -0400

    =More parser work

--- src/haskell/Parser.hs
+++ src/haskell/Parser.hs
@@ -30,6 +30,16 @@ parse avail byte = ParserState sending receiving index msgtype where
   index = 0
   msgtype = 0
 
+  -- Parser state transition function
+  state  = if state' == 0 && byte == 36 then 1
+      else if state' == 1 && byte == 77 then 2
+      else if state' == 2 && (byte == 60 || byte == 62) then 3
+      else if state' == 3 then 4
+      else if state' == 4 then 5
+      -- else if state' == 5 && receiving then 5
+      else if state' == 5 then 0
+      else state' :: SWord8
+
 {--
   -- Payload handling
   input_size = if state == 3 then byte else input_size'
@@ -47,27 +57,17 @@ parse avail byte = ParserState sending receiving index msgtype where
       else if state == 5  then crc_in'
       else 0
 
-  -- Parser state transition function
-  state  = if state' == 0 && byte == 36 then 1
-      else if state' == 1 && byte == 77 then 2
-      else if state' == 2 && (byte == 60 || byte == 62) then 3
-      else if state' == 3 then 4
-      else if state' == 4 then 5
-      else if state' == 5 && receiving then 5
-      else if state' == 5 then 0
-      else state' :: SWord8
-
   incoming = msgtype >= 200
 
   sending = avail && state == 0 && crc_in == byte && not incoming
 
   index = if receiving then payload_index - 1 else 0
 
-  -- State variables
   input_size'    = [0] ++ input_size
-  state'         = [0] ++ state
   payload_index' = [0] ++ payload_index
   crc_in'        = [0] ++ crc_in
   msgtype'       = [0] ++ msgtype
 --}
 
+  -- State variables
+  state'         = [0] ++ state
