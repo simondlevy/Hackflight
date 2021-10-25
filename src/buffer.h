3732f05d3e3dba6292bbddea05fc5b2da7067e79
simondlevy <simon.d.levy@gmail.com>
2021-10-24 23:45:00 -0400

    More parser work

--- src/buffer.h
+++ src/buffer.h
@@ -83,3 +83,40 @@ void setbuff(buffer_t & buff, bool ready, uint8_t index, uint8_t value)
     _set(buff.byte30, index, value, 30, ready);
     _set(buff.byte31, index, value, 31, ready);
 }
+
+uint8_t getbuff(buffer_t & buff, uint8_t index)
+{
+    return index == 0 ? buff.byte00
+        : index == 1  ? buff.byte01 
+        : index == 2  ? buff.byte02 
+        : index == 3  ? buff.byte03 
+        : index == 4  ? buff.byte04 
+        : index == 5  ? buff.byte05 
+        : index == 6  ? buff.byte06 
+        : index == 7  ? buff.byte07 
+        : index == 8  ? buff.byte08 
+        : index == 9  ? buff.byte09 
+        : index == 10 ? buff.byte10 
+        : index == 11 ? buff.byte11 
+        : index == 12 ? buff.byte12 
+        : index == 13 ? buff.byte13 
+        : index == 14 ? buff.byte14 
+        : index == 15 ? buff.byte15 
+        : index == 16 ? buff.byte16 
+        : index == 17 ? buff.byte17 
+        : index == 18 ? buff.byte18 
+        : index == 19 ? buff.byte19 
+        : index == 20 ? buff.byte20 
+        : index == 21 ? buff.byte21 
+        : index == 22 ? buff.byte22 
+        : index == 23 ? buff.byte23 
+        : index == 24 ? buff.byte24 
+        : index == 25 ? buff.byte25 
+        : index == 26 ? buff.byte26 
+        : index == 27 ? buff.byte27 
+        : index == 28 ? buff.byte28 
+        : index == 29 ? buff.byte29 
+        : index == 30 ? buff.byte30 
+        : index == 31 ? buff.byte31 
+        : 0;
+}
