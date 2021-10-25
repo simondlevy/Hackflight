db740328916de015289c0ac4428c6c6d41e35a33
simondlevy <simon.d.levy@gmail.com>
2021-10-25 15:35:51 -0400

    =More parser work

--- src/haskell/Serial.hs
+++ src/haskell/Serial.hs
@@ -17,17 +17,6 @@ import Prelude hiding((++), (==), take, drop)
 import Utils
 
 
-type SerialBuffer = [SWord8]
-
-buff :: SerialBuffer
-
-buff = [   0::SWord8
-         , 0::SWord8
-       ]
-
-insert :: SerialBuffer -> SWord8 -> SWord8 -> SerialBuffer
-insert buff index value = [0, 0]
-
 serialAvailable :: Stream Bool
 serialAvailable = extern "stream_serialAvailable" Nothing
 
