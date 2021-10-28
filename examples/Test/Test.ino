8b774d50533a84eca5936d3a2493f5985efcd7b9
simondlevy <simon.d.levy@gmail.com>
2021-10-27 22:22:06 -0400

    =More parser work

--- examples/Test/Test.ino
+++ examples/Test/Test.ino
@@ -41,12 +41,15 @@ void stream_run(
         handleSerialInput(msgtype, state_phi, state_theta, state_psi);
     }
 
-    else if (msgtype == 215 && payindex == 2) {
-        Debugger::printf(Serial1, "motor value: %d\n", parserbyte);
-    }
+    else if (msgtype == 215) {
 
-    else if (msgtype == 215 && payindex == 1) {
-        Debugger::printf(Serial1, "motor index: %d\n", parserbyte);
+        if (payindex == 1) {
+            Debugger::printf(Serial1, "motor index: %d\n", parserbyte);
+        }
+
+        if (payindex == 2) {
+            Debugger::printf(Serial1, "motor value: %d\n", parserbyte);
+        }
     }
 
     updateSerialOutput();
@@ -61,8 +64,8 @@ void stream_run(
     float m3_val = motor_index == 3 ? motor_percent/100. : 0;
     float m4_val = motor_index == 4 ? motor_percent/100. : 0;
 
-//    Debugger::printf(Serial1, "m1=%3.3f m2=%3.3f m3=%3.3f m4=%3.3f\n",
-//            m1_val, m2_val, m3_val, m4_val);
+    //    Debugger::printf(Serial1, "m1=%3.3f m2=%3.3f m3=%3.3f m4=%3.3f\n",
+    //            m1_val, m2_val, m3_val, m4_val);
 
 
     updateSerialOutput();
