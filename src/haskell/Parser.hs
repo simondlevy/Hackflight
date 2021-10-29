835eb82765847f9459cc1fc70c285d0cf324093c
simondlevy <simon.d.levy@gmail.com>
2021-10-28 20:25:27 -0400

    =More parser work

--- src/haskell/Parser.hs
+++ src/haskell/Parser.hs
@@ -25,15 +25,18 @@ parse avail byte = (msgtype, sending, index, state, size, crc) where
       else if state' == 2 && (byte == 60 || byte == 62) then 3
       else if state' == 3 then 4
       else if state' == 4 then 5
-      else if msgtype' < 200 && state' == 5 then 6
-      else if state' == 5 && size' > 0 then 5
+      else if state' == 5 && msgtype' < 200 then 6
+      else if state' == 5 && size' > 1 then 5
+      else if state' == 5 && size' == 1 then 6
       else 0
 
-  size = if state == 4 then byte + 3
+  size = if state == 4 then byte + 2
          else if size' > 0 then size' - 1
          else 0
 
-  msgtype = if state == 5 then byte else msgtype'
+  msgtype = if state == 5 && msgtype' == 0 then byte
+            else if state == 0 then 0
+            else msgtype'
 
   index = if state' < 5 then 0
           else if msgtype >= 200 then index' + 1
