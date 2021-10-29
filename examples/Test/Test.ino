f38d419d6c4b20890010d0f31ade85de584fc89e
simondlevy <simon.d.levy@gmail.com>
2021-10-28 22:39:54 -0400

    =More parser work

--- examples/Test/Test.ino
+++ examples/Test/Test.ino
@@ -26,5 +26,5 @@ void stream_debug(float m1, float m2, float m3, float m4)
 {
     Debugger::printf(Serial1, "m1=%3.3f m2=%3.3f m3=%3.3f m4=%3.3f\n", m1, m2, m3, m4);
 
-    delay(10);
+    delay(5);
 }
