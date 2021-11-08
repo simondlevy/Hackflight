ddf50639aef5a4e312e2a9e459aed8cb1f21d875
simondlevy <simon.d.levy@gmail.com>
2021-11-08 18:44:19 -0500

    More Nicla Sense ME work

---
+++ arduino/cpp/arduino_bhy2.cpp
@@ -0,0 +1,23 @@
+/*
+   Streaming support for BHY2 sensor
+
+   Copyright (C) 2021 Simon D. Levy
+
+   MIT License
+
+ */
+
+
+#define _EXTERN
+#include "hackflight.h"
+
+#include <Arduino.h>
+#include <Arduino_BHY2.h>
+
+void stream_startBhy2(void)
+{
+}
+
+void stream_updateBhy2(void)
+{
+}
