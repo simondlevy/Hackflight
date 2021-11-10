e889220843dcbc1f9a5d39ddc8e51a5494764dfa
simondlevy <simon.d.levy@gmail.com>
2021-11-10 16:50:03 -0500

    Cleanup

--- haskell/Hackflight.hs
+++ haskell/Hackflight.hs
@@ -36,17 +36,17 @@ hackflight receiver sensors pidfuns mixer = (msgbuff, motors, led)
     rdemands = getDemands receiver
 
     -- Get the vehicle state by composing the sensor functions over the previous state
-    vstate = compose sensors (state' vstate)
+    state = compose sensors (state' state)
 
     -- Check safety (arming / failsafe)
-    (armed, failsafe, mzero) = safety rdemands vstate
+    (armed, failsafe, cut) = safety rdemands state
 
     -- Periodically get the demands by composing the PID controllers over the receiver
     -- demands
-    (_, _, pdemands) = compose pidfuns (vstate, timerReady 300, rdemands)
+    (_, _, pdemands) = compose pidfuns (state, timerReady 300, rdemands)
 
     -- Run mixer on demands to get motor values
-    motors' = (mixerfun mixer) (\m -> if mzero then 0 else m) pdemands
+    motors' = (mixerfun mixer) (\m -> if cut then 0 else m) pdemands
 
     -- Blink LED during first couple of seconds; keep it solid when armed
     -- led = if micros < 2000000 then (mod (div micros 50000) 2 == 0) else armed
@@ -56,7 +56,7 @@ hackflight receiver sensors pidfuns mixer = (msgbuff, motors, led)
     (msgtyp, sending, payindex, _checked) = parse stream_serialAvailable stream_serialByte
 
     -- Convert the message into a buffer to send to GCS
-    msgbuff = message sending msgtyp vstate
+    msgbuff = message sending msgtyp state
 
     -- Check for incoming SET_MOTOR messages from GCS
     motor_index = if msgtyp == 215 && payindex == 1 then stream_serialByte
