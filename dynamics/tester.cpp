e7ac25ae294d80d063f923b48e9cfe1a17404190
simondlevy <simon.d.levy@gmail.com>
2021-11-16 23:31:05 -0500

    =More dynamics testing

--- dynamics/tester.cpp
+++ dynamics/tester.cpp
@@ -1,7 +1,30 @@
 #include <stdio.h>
 #include <Dynamics.hpp>
 
+Dynamics::vehicle_params_t vparams = {
+
+    // Estimated
+    2.E-06, // d drag cofficient [T=d*w^2]
+
+    // https://www.dji.com/phantom-4/info
+    1.380,  // m mass [kg]
+
+    // Estimated
+    2,      // Ix [kg*m^2] 
+    2,      // Iy [kg*m^2] 
+    3,      // Iz [kg*m^2] 
+    3.8E-03, // Jr prop inertial [kg*m^2] 
+    15000,   // maxrpm
+};
+
+Dynamics::fixed_pitch_params_t fpparams = {
+    5.E-06, // b thrust coefficient [F=b*w^2]
+    0.350   // l arm length [m]
+};
+
 int main(int argc, char ** argv)
 {
+    Dynamics dynamics = Dynamics(vparams, fpparams);
+
     return 0;
 }
