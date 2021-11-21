b3d9ef8af6ad87394e01a0cc861bbc218899ebb7
simondlevy <simon.d.levy@gmail.com>
2021-11-21 15:37:17 -0500

    More Unmixer work

--- haskell/dynamics/QuadXAP.hs
+++ haskell/dynamics/QuadXAP.hs
@@ -14,8 +14,14 @@ import Language.Copilot
 
 import Dynamics
 import Demands
+import Motors
 import Utils
 
 quadXapUnmixer :: SFloat -> Unmixer
 
-quadXapUnmixer l motors = Demands 0 0 0 0
+quadXapUnmixer l motors = Demands t r p y where
+
+  t = (m1 motors) + (m2 motors) + (m3 motors) + (m4 motors)
+  r = 0
+  p = 0
+  y = 0
