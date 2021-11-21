2b631e72797e685c0bfcbaa7c5b5f201da9c7738
simondlevy <simon.d.levy@gmail.com>
2021-11-20 19:38:39 -0500

    Cleanup

--- haskell/SimMixers.hs
+++ haskell/SimMixers.hs
@@ -24,15 +24,17 @@ data SimMixer =  SimMixer {  mixer :: Mixer
 rps :: SFloat -> SFloat -> SFloat
 rps motorval maxrpm = motorval * maxrpm * pi / 30
 
+thrust :: SFloat -> SFloat -> SFloat
+thrust rpsval rho = rho * rpsval**2
+
+-- QuadXAP -------------------------------------------------------------------------------
+
 getRPS :: Motors -> SimMixer -> SFloat -> Motors
 getRPS motors (SimMixer QuadXAP _) maxrpm = Quad r1 r2 r3 r4 where
   r1 = rps (m1 motors) maxrpm
   r2 = rps (m2 motors) maxrpm
   r3 = rps (m3 motors) maxrpm
   r4 = rps (m4 motors) maxrpm
-
-thrust :: SFloat -> SFloat -> SFloat
-thrust rpsval rho = rho * rpsval**2
 
 getThrusts :: Motors -> SimMixer -> SFloat -> Motors
 getThrusts motors (SimMixer QuadXAP _) rho = Quad t1 t2 t3 t4 where
@@ -42,16 +44,24 @@ getThrusts motors (SimMixer QuadXAP _) rho = Quad t1 t2 t3 t4 where
   t4 = thrust (m4 motors) rho
 
 getTorque :: Motors -> SimMixer -> SFloat
-getTorque motors (SimMixer QuadXAP _) = (m1 motors) + (m2 motors) - (m3 motors) - (m4 motors)
+getTorque motors (SimMixer QuadXAP _) =
+  (m1 motors) + (m2 motors) - (m3 motors) - (m4 motors)
 
 getThrust :: Motors -> SimMixer -> SFloat
-getThrust motors (SimMixer QuadXAP _) = (m1 motors) + (m2 motors) + (m3 motors) + (m4 motors)
+getThrust motors (SimMixer QuadXAP _) =
+  (m1 motors) + (m2 motors) + (m3 motors) + (m4 motors)
 
 getRoll :: Motors -> SimMixer -> SFloat
-getRoll motors (SimMixer QuadXAP l) = l * ((m2 motors) + (m3 motors) - (m1 motors) - (m4 motors))
+getRoll motors (SimMixer QuadXAP l) =
+  l * ((m2 motors) + (m3 motors) - (m1 motors) - (m4 motors))
 
 getPitch :: Motors -> SimMixer -> SFloat
-getPitch motors (SimMixer QuadXAP l) = l * ((m2 motors) + (m4 motors) - (m1 motors) - (m3 motors))
+getPitch motors (SimMixer QuadXAP l) =
+  l * ((m2 motors) + (m4 motors) - (m1 motors) - (m3 motors))
 
 getYaw :: Motors -> SimMixer -> SFloat
-getYaw motors (SimMixer QuadXAP _) = (m1 motors) + (m2 motors) - (m3 motors) - (m4 motors)
+getYaw motors (SimMixer QuadXAP _) =
+  (m1 motors) + (m2 motors) - (m3 motors) - (m4 motors)
+
+-- ThrustVector --------------------------------------------------------------------------
+
