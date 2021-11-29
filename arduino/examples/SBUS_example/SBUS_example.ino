cb35eddde0e3d0905f39268d9f7c38a85ed37ef3
simondlevy <simon.d.levy@gmail.com>
2021-11-29 01:00:23 -0500

    Added SBUS Arduino example

---
+++ arduino/examples/SBUS_example/SBUS_example.ino
@@ -0,0 +1,41 @@
+#include <sbus.h>
+
+SbusTx sbus_tx(&Serial1);
+
+void setup() {
+
+  sbus_tx.Begin();
+}
+
+void loop() {
+
+    std::array<uint16_t, 16> txvals;
+
+    static uint16_t val;
+    static int8_t dir;
+
+    if (!val) {
+        val = 172;
+        dir = +1;
+    }
+
+    for (uint8_t k=0; k<16; ++k) {
+        txvals[k] = val;
+    }
+
+    val += dir;
+
+    if (val == 1811) {
+        dir = -1;
+    }
+    
+    if (val == 172) {
+        dir = +1;
+    }
+     sbus_tx.tx_channels(txvals);
+
+    sbus_tx.Write();
+
+    delay(10);
+}
+
