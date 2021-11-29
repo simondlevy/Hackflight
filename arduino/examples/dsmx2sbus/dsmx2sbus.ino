44d4b1168f783f5a56a473524aad2a27743c7691
simondlevy <simon.d.levy@gmail.com>
2021-11-29 12:09:00 -0500

    More dsmx2sbus work

--- arduino/examples/dsmx2sbus/dsmx2sbus.ino
+++ arduino/examples/dsmx2sbus/dsmx2sbus.ino
@@ -2,6 +2,9 @@
 
 SbusTx sbus_tx(&Serial1);
 
+static uint16_t SBUS_MIN = 172;
+static uint16_t SBUS_MAX = 1811;
+
 void setup() {
 
   sbus_tx.Begin();
@@ -15,23 +18,23 @@ void loop() {
     static int8_t dir;
 
     if (!val) {
-        val = 172;
+        val = SBUS_MIN;
         dir = +1;
     }
 
     for (uint8_t k=0; k<16; ++k) {
-        txvals[k] = 172;
+        txvals[k] = SBUS_MIN;
     }
 
     txvals[4] = val;
 
     val += dir;
 
-    if (val == 1811) {
+    if (val == SBUS_MAX) {
         dir = -1;
     }
     
-    if (val == 172) {
+    if (val == SBUS_MIN) {
         dir = +1;
     }
      sbus_tx.tx_channels(txvals);
