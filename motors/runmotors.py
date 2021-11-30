7a16f27978ab7707cf1415e2994e16076bc53980
simondlevy <simon.d.levy@gmail.com>
2021-11-30 15:11:56 -0500

    =More motors work

--- motors/runmotors.py
+++ motors/runmotors.py
@@ -1,1 +1,30 @@
 from mspparser import MspParser
+from serial import Serial
+from time import sleep
+from sys import stdout
+
+class MyParser(MspParser):
+
+    def handle_ATTITUDE(self, angx, angy, heading):
+        print(angx, angy, heading)
+
+PORT = 'COM31'
+
+port = Serial(PORT, 115200, timeout=1)
+
+cmd = MspParser.serialize_ATTITUDE_Request()
+
+parser = MyParser()
+
+port.write(cmd)
+
+while True:
+
+    try:
+        byte = port.read(1)
+        print(byte)
+        stdout.flush()
+        sleep(.001)
+
+    except KeyboardInterrupt:
+        break
