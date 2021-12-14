92dd38d2cd258405e315509a1215e84a44881b21
simondlevy <simon.d.levy@gmail.com>
2021-12-13 21:16:33 -0500

    =More DSMX => Bluetooth work

--- dsmxblue/BlueRX/BlueRX.ino
+++ dsmxblue/BlueRX/BlueRX.ino
@@ -5,13 +5,67 @@
  */
 
 
-#include "BluetoothSerial.h"
+#include <BluetoothSerial.h>
+
+#include "parser.hpp"
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
-BluetoothSerial SerialBT;
+class MyParser : public Parser {
+
+    private:
+
+    uint8_t _payload[128] = {};
+
+        void handle_SET_NORMAL_RC(float  thr, float  rol, float  pit, float  yaw, float  aux1, float  aux2)
+        {
+            // XXX
+        }
+
+    protected:
+
+        virtual void collectPayload(uint8_t index, uint8_t value) override
+        {
+            _payload[index] = value;
+        }
+
+        virtual void dispatchMessage(uint8_t command) override
+        {
+            switch (command) {
+
+                case 204:
+                    {
+                        float thr = 0;
+                        memcpy(&thr,  &_payload[0], sizeof(float));
+
+                        float rol = 0;
+                        memcpy(&rol,  &_payload[4], sizeof(float));
+
+                        float pit = 0;
+                        memcpy(&pit,  &_payload[8], sizeof(float));
+
+                        float yaw = 0;
+                        memcpy(&yaw,  &_payload[12], sizeof(float));
+
+                        float aux1 = 0;
+                        memcpy(&aux1,  &_payload[16], sizeof(float));
+
+                        float aux2 = 0;
+                        memcpy(&aux2,  &_payload[20], sizeof(float));
+
+                        handle_SET_NORMAL_RC(thr, rol, pit, yaw, aux1, aux2);
+                    } break;
+
+            } // switch (_command)
+
+        } // dispatchMessage 
+
+}; // class MyParser
+
+
+static BluetoothSerial SerialBT;
 
 void setup()
 {
