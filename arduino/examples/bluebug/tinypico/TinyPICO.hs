ec9e12be12c3754a9b934a807338d54391804179
simondlevy <simon.d.levy@gmail.com>
2021-12-15 01:32:29 -0500

    Cleanup

---
+++ arduino/examples/bluebug/tinypico/TinyPICO.hs
@@ -0,0 +1,43 @@
+{--
+  TinyPICO Support for Bluebug flight controller
+
+  Copyright(C) 2021 on D.Levy
+
+  MIT License
+--}
+
+{-# LANGUAGE RebindableSyntax #-}
+
+module Bluebug where
+
+import Language.Copilot
+import Copilot.Compile.C99
+
+import Time
+
+-- Serial comms
+import Serial
+import Parser
+
+-- Misc
+import Utils
+
+------------------------------------------------------------
+
+spec = do
+
+  -- Get flags for startup, loop
+  let (running, starting) = runstate
+
+  -- Do some stuff at startup
+  trigger "stream_startSerial" starting []
+  trigger "stream_startLed" starting [arg led_pin]
+
+  -- Do some other stuff in loop
+  trigger "stream_updateTime" running []
+  trigger "stream_writeLed" running []
+  trigger "stream_serialUpdate" running []
+  trigger "stream_serial1Read" stream_serialAvailable []
+
+-- Compile the spec
+main = reify spec >>= compile "hackflight"
