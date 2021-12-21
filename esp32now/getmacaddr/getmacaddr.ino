9281bf621bffa187f25a0d183c9897cc43a86a18
simondlevy <simon.d.levy@gmail.com>
2021-12-21 15:44:22 -0500

    Begin ESP32 NOW work

---
+++ esp32now/getmacaddr/getmacaddr.ino
@@ -0,0 +1,14 @@
+// Complete Instructions to Get and Change ESP MAC Address: https://RandomNerdTutorials.com/get-change-esp32-esp8266-mac-address-arduino/
+
+#include "WiFi.h"
+ 
+void setup(){
+  Serial.begin(115200);
+  WiFi.mode(WIFI_MODE_STA);
+}
+ 
+void loop(){
+  Serial.println(WiFi.macAddress());
+  delay(500);
+
+}
