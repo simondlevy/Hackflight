c39f04d5d51c54192fa776db659ccc94a8cd0f2c
simondlevy <simon.d.levy@gmail.com>
2021-12-30 23:01:30 -0500

    More ESP32 transmitter work

---
+++ arduino/examples/esp32tx/esp32tx.ino
@@ -0,0 +1,38 @@
+/*
+   ESP32-based transmitter
+
+   Copyright (c) Simon D. Levy 2021
+
+   MIT License
+ */
+
+#include <TinyPICO.h>
+
+static const uint8_t THR_PIN = 25;
+static const uint8_t ROL_PIN = 26;
+static const uint8_t PIT_PIN = 27;
+static const uint8_t YAW_PIN = 15;
+static const uint8_t AU1_PIN = 14;
+static const uint8_t AU2_PIN =  4;
+
+static TinyPICO tp;
+
+void setup(void)
+{
+    pinMode(AU1_PIN, INPUT_PULLUP);
+    pinMode(AU2_PIN, INPUT_PULLUP);
+
+    Serial.begin(115200);
+
+    tp.DotStar_SetPixelColor(0, 255, 0);
+}
+
+void loop(void)
+{
+    printf("THR=%04d\n", analogRead(THR_PIN)); 
+  //Serial.println(analogRead(THR_PIN)); 
+  //Serial.println(digitalRead(AU1_PIN));
+  //Serial.println(digitalRead(AU2_PIN));
+
+  delay(100);
+}
