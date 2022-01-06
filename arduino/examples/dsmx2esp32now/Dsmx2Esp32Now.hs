1935ea7cc1ed4f83b377b2dd9b8d3d6e4682c904
simondlevy <simon.d.levy@gmail.com>
2022-01-05 21:41:44 -0500

    More Dsmx2Esp32Now work

--- arduino/examples/dsmx2esp32now/Dsmx2Esp32Now.hs
+++ arduino/examples/dsmx2esp32now/Dsmx2Esp32Now.hs
@@ -8,7 +8,7 @@
 
 {-# LANGUAGE RebindableSyntax #-}
 
-module TpDsmx2Sbus where
+module Dsmx2Esp32Now where
 
 import Language.Copilot
 import Copilot.Compile.C99
@@ -24,15 +24,6 @@ import Utils
 dsmx_in_rx_pin = 4  :: SWord8  
 dsmx_in_tx_pin = 14 :: SWord8 -- unused
 
-sbus_out_rx_pin = 15 :: SWord8 -- unused 
-sbus_out_tx_pin = 27 :: SWord8 
-
-sbus_min = 172  :: SFloat
-sbus_max = 1811 :: SFloat
-
-scale :: SFloat -> SFloat
-scale val = ((val + 1) / 2) * (sbus_max - sbus_min) + sbus_min
-
 ------------------------------------------------------------
 
 spec = do
@@ -41,33 +32,19 @@ spec = do
   let (running, starting) = runstate
 
   -- Do some stuff at startup
+  trigger "stream_serialStart" starting []
   trigger "stream_dsmrxStart" starting [ arg dsmx_in_rx_pin, arg dsmx_in_tx_pin]
-  trigger "stream_sbusOutStart" starting [ arg sbus_out_rx_pin, arg sbus_out_tx_pin]
 
   -- Do some other stuff in loop
   trigger "stream_dsmrxUpdate" running []
   trigger "stream_dsmrxGet" receiverGotNewFrame []
-  trigger "stream_sbusWrite" running [  arg $ scale receiverThrottle  
-                                      , arg $ scale receiverRoll  
-                                      , arg $ scale receiverPitch  
-                                      , arg $ scale receiverYaw  
-                                      , arg $ scale receiverAux1
-                                      , arg $ scale receiverAux2
-                                      , arg sbus_min
-                                      , arg sbus_min
-                                      , arg sbus_min
-                                      , arg sbus_min
-                                      , arg sbus_min
-                                      , arg sbus_min
-                                      , arg sbus_min
-                                      , arg sbus_min
-                                      , arg sbus_min
-                                      , arg sbus_min
-                                     ]
---}
-
-  -- XXX ignore these varaibles for now
-  -- trigger "stream_ignore" running [arg receiverTimedOut]
+  trigger "debug" running [  arg receiverThrottle
+                           , arg receiverRoll
+                           , arg receiverPitch
+                           , arg receiverYaw
+                           , arg receiverAux1
+                           , arg receiverAux2
+                          ]
 
 -- Compile the spec
 main = reify spec >>= compile "hackflight"
