9e4d06de44cb49e85164ad0346468f2493a7ea17
simondlevy <simon.d.levy@gmail.com>
2022-01-12 20:43:41 -0500

    =More old-school MSP migration

--- haskell/Messages.hs
+++ haskell/Messages.hs
@@ -32,7 +32,7 @@ payload msgtype vstate = (paysize, val00, val01, val02, val03, val04, val05) whe
           else 0
 
   val01 = if msgtype == 105 then c_receiverRoll
-          else if msgtype == 108 then (theta vstate)
+          else if msgtype == 108 then 10 * (rad2deg (theta vstate))
           else 0
 
   val02 = if msgtype == 105 then c_receiverPitch
