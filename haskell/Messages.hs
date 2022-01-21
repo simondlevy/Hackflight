8d1309646f7e6779b1ae4db1278058d8e8f6209e
simondlevy <simon.d.levy@gmail.com>
2022-01-20 21:17:26 -0500

    =More Exp32Now2Sbus work

--- haskell/Messages.hs
+++ haskell/Messages.hs
@@ -78,11 +78,12 @@ rxmessage c1 c2 c3 c4 c5 c6 = Message 0x3E
 
 ----------------------------------------------------------------------------
 
-makeval :: SWord8 -> SWord8 -> SWord16
+makeval :: SWord8 -> SWord8 -> SWord8 -> (SWord16, SBool)
 
-makeval byte  payindex = value
+makeval byte payindex index = (value, ready) where
 
-  -- Make a 16-bit motor value from two-byte pairs
+  ready = index == div payindex 2
+
   b = cast byte :: SWord16
   b' = [0] ++ b
   value = if mod payindex 2 == 0 then b' .|. b.<<.(8::SWord8)
@@ -94,18 +95,14 @@ getMotors :: SWord8 -> SWord8 -> SWord8 -> (SFloat, SFloat, SFloat, SFloat)
 
 getMotors msgtype payindex byte = (m1, m2, m3, m4) where
 
-  mindex = div payindex 2
-
   m1 = getmval 1 m1' where m1' = [0] ++ m1
   m2 = getmval 2 m2' where m2' = [0] ++ m2
   m3 = getmval 3 m3' where m3' = [0] ++ m3
   m4 = getmval 4 m4' where m4' = [0] ++ m4
 
-  getmval idx oldval = if mindex == idx
-                       then ((unsafeCast mvalue) - 1000) / 1000
-                       else oldval :: SFloat
-
-  mvalue = makeval byte payindex
+  getmval index oldval = let (value, ready) = makeval byte payindex index
+                         in if ready then ((unsafeCast value) - 1000) / 1000
+                            else oldval :: SFloat
 
 ----------------------------------------------------------------------------
 
@@ -114,8 +111,6 @@ getChannels :: SWord8 -> SWord8 -> SWord8
 
 getChannels msgtype payindex byte = (c1, c2, c3, c4, c5, c6) where
 
-  cindex = div payindex 2
-
   c1 = getcval 1 c1' where c1' = [0] ++ c1
   c2 = getcval 2 c2' where c2' = [0] ++ c2
   c3 = getcval 3 c3' where c3' = [0] ++ c3
@@ -123,10 +118,7 @@ getChannels msgtype payindex byte = (c1, c2, c3, c4, c5, c6) where
   c5 = getcval 5 c5' where c5' = [0] ++ c5
   c6 = getcval 6 c6' where c6' = [0] ++ c6
 
-  getcval idx oldval = if cindex == idx then cvalue else oldval
+  getcval index oldval = let (value, ready) = makeval byte payindex index
+                         in if ready then value else oldval
 
-  -- Make a 16-bit motor value from two-byte pairs
-  b = cast byte :: SWord16
-  b' = [0] ++ b
-  cvalue = if mod payindex 2 == 0 then b' .|. b.<<.(8::SWord8)
-           else  cvalue' where cvalue' = [0] ++ cvalue :: SWord16
+
