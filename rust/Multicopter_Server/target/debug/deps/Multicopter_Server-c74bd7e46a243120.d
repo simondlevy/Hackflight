8ff742430ebbb9415ed06f605aab98af651a2a55
Nicholas Nguyen <nicholasqnguyen8@gmail.com>
2022-05-27 13:14:28 -0400

    moved the rust code into the simulator

--- rust/Multicopter_Server/target/debug/deps/Multicopter_Server-c74bd7e46a243120.d
+++
@@ -1,6 +0,0 @@
-/home/hananguyen/Downloads/Hackflight/rust/Multicopter_Server/target/debug/deps/Multicopter_Server-c74bd7e46a243120: src/main.rs src/multicopter_server.rs
-
-/home/hananguyen/Downloads/Hackflight/rust/Multicopter_Server/target/debug/deps/Multicopter_Server-c74bd7e46a243120.d: src/main.rs src/multicopter_server.rs
-
-src/main.rs:
-src/multicopter_server.rs:
