d1912dafabda1ed16dee3ae24f0d3a9a7b1f3a60
simondlevy <simon.d.levy@gmail.com>
2022-06-18 14:51:35 -0400

    =More Rust work

--- rust/hackflight/src/main.rs
+++ rust/hackflight/src/main.rs
@@ -3,18 +3,21 @@
 
    This file is part of Hackflight.
 
-   Hackflight is free software: you can redistribute it and/or modify it under the
-   terms of the GNU General Public License as published by the Free Software
-   Foundation, either version 3 of the License, or (at your option) any later
-   version.
+   Hackflight is free software: you can redistribute it and/or modify it under
+   the terms of the GNU General Public License as published by the Free
+   Software Foundation, either version 3 of the License, or (at your option)
+   any later version.
 
-   Hackflight is distributed in the hope that it will be useful, but WITHOUT ANY
-   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-   PARTICULAR PURPOSE. See the GNU General Public License for more details.
+   Hackflight is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+   more details.
 
    You should have received a copy of the GNU General Public License along with
    Hackflight. If not, see <https://www.gnu.org/licenses/>.
  */
+
+// Vehicle state ------------------------------------------------------------------------
 
 struct VehicleState 
 {
@@ -31,6 +34,33 @@ struct VehicleState
     psi:    f32,
     dpsi:   f32
 }
+
+
+// Scheduling ------------------------------------------------------------------
+
+struct Scheduler
+{
+    loop_start_cycles: i32,
+    loop_start_min_cycles: i32,
+    loop_start_max_cycles: i32,
+    loop_start_delta_down_cycles: u32,
+    loop_start_delta_up_cycles: u32,
+
+    task_guard_cycles: i32,
+    task_guard_min_cycles: i32,
+    task_guard_max_cycles: i32,
+    task_guard_delta_down_cycles: u32,
+    task_guard_delta_up_cycles: u32,
+
+    desired_period_cycles: i32,
+    last_target_cycles: u32,
+
+    next_timing_cycles: u32,
+
+    guard_margin: i32,
+    clock_rate: u32
+}
+
 
 fn main() {
     println!("Hello, world!");
