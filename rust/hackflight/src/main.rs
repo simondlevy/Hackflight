5a5ad88f708817f0575398113b9f1cdbe8fae20b
simondlevy <simon.d.levy@gmail.com>
2022-06-22 14:04:17 -0400

    =More Rust work

--- rust/hackflight/src/main.rs
+++ rust/hackflight/src/main.rs
@@ -297,6 +297,22 @@ enum SerialPortIdentifier
     SerialPortLpUart1 = 40,
 }
 
+// Arming ---------------------------------------------------------------------
+
+#[allow(dead_code)]
+struct Arming
+{
+
+    acc_done_calibrating: bool,
+    angle_okay: bool,
+    arming_switch_okay: bool,
+    gyro_done_calibrating: bool,
+    dshot_bitbang_okay: bool,
+    is_armed: bool,
+    rx_failsafe_okay: bool,
+    throttle_is_down: bool
+}
+
 // Scheduling ------------------------------------------------------------------
 
 #[allow(dead_code)]
