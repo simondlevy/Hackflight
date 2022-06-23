0efd4ff5ab43725e5942492a58abdc2ded193a21
simondlevy <simon.d.levy@gmail.com>
2022-06-23 18:17:55 -0400

    =More Arduino motor work

--- examples/Ladymax/Ladymax.ino
+++ examples/Ladymax/Ladymax.ino
@@ -21,7 +21,6 @@
 
 #include <hackflight_full.h>
 #include <stm32_clock.h>
-#include <arduino/motor.h>
 
 static hackflight_t _hf;
 
@@ -30,10 +29,10 @@ void setup(void)
     Wire.begin();
     delay(100);
 
-    void * motorDevice = NULL;
+    static uint8_t motorPins[4] = {0, 0, 0, 0};
 
     // Always use Serial1 for receiver, no no need to specify
-    hackflightInitFull(&_hf, motorDevice, SERIAL_PORT_NONE, 5, A4);
+    hackflightInitFull(&_hf, (void *)&motorPins, SERIAL_PORT_NONE, 5, A4);
 
     stm32_startCycleCounter();
 }
