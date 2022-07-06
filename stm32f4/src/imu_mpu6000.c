123fa2a91b91f7d712c9876dcee05ca58e19ab1a
simondlevy <simon.d.levy@gmail.com>
2022-07-05 21:46:01 -0400

    Cleanup

--- stm32f4/src/imu_mpu6000.c
+++ stm32f4/src/imu_mpu6000.c
@@ -41,38 +41,13 @@ static const uint32_t MPU6000_MAX_SPI_CLK_HZ = 20000000;
 
 static uint32_t MPU6000_SHORT_THRESHOLD = 82;
 
-// Bits
-#define BIT_SLEEP                   0x40
-#define BIT_H_RESET                 0x80
-#define BITS_CLKSEL                 0x07
-#define MPU_CLK_SEL_PLLGYROX        0x01
-#define MPU_CLK_SEL_PLLGYROZ        0x03
-#define MPU_EXT_SYNC_GYROX          0x02
-#define BITS_FS_250DPS              0x00
-#define BITS_FS_500DPS              0x08
-#define BITS_FS_1000DPS             0x10
-#define BITS_FS_2000DPS             0x18
-#define BITS_FS_2G                  0x00
-#define BITS_FS_4G                  0x08
-#define BITS_FS_8G                  0x10
-#define BITS_FS_16G                 0x18
-#define BITS_FS_MASK                0x18
-#define BITS_DLPF_CFG_256HZ         0x00
-#define BITS_DLPF_CFG_188HZ         0x01
-#define BITS_DLPF_CFG_98HZ          0x02
-#define BITS_DLPF_CFG_42HZ          0x03
-#define BITS_DLPF_CFG_20HZ          0x04
-#define BITS_DLPF_CFG_10HZ          0x05
-#define BITS_DLPF_CFG_5HZ           0x06
-#define BITS_DLPF_CFG_2100HZ_NOLPF  0x07
-#define BITS_DLPF_CFG_MASK          0x07
-#define BIT_INT_ANYRD_2CLEAR        0x10
-#define BIT_RAW_RDY_EN              0x01
-#define BIT_I2C_IF_DIS              0x10
-#define BIT_INT_STATUS_DATA         0x01
-#define BIT_GYRO                    0x04
-#define BIT_ACC                     0x02
-#define BIT_TEMP                    0x01
+static const uint8_t MPU_CLK_SEL_PLLGYROZ = 0x03;
+
+static const uint8_t BIT_TEMP       = 0x01;
+static const uint8_t BIT_ACC        = 0x02;
+static const uint8_t BIT_GYRO       = 0x04;
+static const uint8_t BIT_I2C_IF_DIS = 0x10;
+static const uint8_t BIT_H_RESET    = 0x80;
 
 // Product ID Description for MPU6000
 // high 4 bits low 4 bits
