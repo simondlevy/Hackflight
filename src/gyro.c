64f6c29fa2ffb0e3697ec3da857abe7248b3fe51
simondlevy <simon.d.levy@gmail.com>
2022-07-14 17:46:33 -0400

    =More gyro cleanup

--- src/gyro.c
+++ src/gyro.c
@@ -47,36 +47,14 @@ static float nullFilterApply(filter_t *filter, float input)
     return input;
 }
 
-static void initLowpassFilter( gyro_t * gyro, int slot, uint16_t lpfHz)
+static void initLowpassFilter(
+        gyroLowpassFilter_t * lowpassFilter,
+        uint16_t lpfHz)
 {
-    filterApplyFnPtr *lowpassFilterApplyFn;
-    gyroLowpassFilter_t *lowpassFilter = NULL;
-
-    switch (slot) {
-        case FILTER_LPF1:
-            lowpassFilterApplyFn = &gyro->lowpassFilterApplyFn;
-            lowpassFilter = gyro->lowpassFilter;
-            break;
-
-        case FILTER_LPF2:
-            lowpassFilterApplyFn = &gyro->lowpass2FilterApplyFn;
-            lowpassFilter = gyro->lowpass2Filter;
-            break;
-
-        default:
-            return;
-    }
-
-    const float gain = pt1FilterGain(lpfHz, CORE_DT());
-
-    // Dereference the pointer to null before checking valid cutoff and filter
-    // type. It will be overridden for positive cases.
-    *lowpassFilterApplyFn = nullFilterApply;
-
-    // If lowpass cutoff has been specified
-    *lowpassFilterApplyFn = (filterApplyFnPtr) pt1FilterApply;
     for (int axis = 0; axis < 3; axis++) {
-        pt1FilterInit(&lowpassFilter[axis].pt1FilterState, gain);
+        pt1FilterInit(
+                &lowpassFilter[axis].pt1FilterState, 
+                pt1FilterGain(lpfHz, CORE_DT()));
     }
 }
 
@@ -130,11 +108,12 @@ void gyroInit(hackflight_t * hf)
 {
     gyro_t * gyro = &hf->gyro;
 
-    initLowpassFilter(gyro, FILTER_LPF1, LPF1_DYN_MIN_HZ);
+    initLowpassFilter(gyro->lowpassFilter, LPF1_DYN_MIN_HZ);
 
-    initLowpassFilter( gyro, FILTER_LPF2, LPF2_STATIC_HZ);
+    initLowpassFilter(gyro->lowpass2Filter, LPF2_STATIC_HZ);
 
-    setCalibrationCycles(gyro); // start calibrating
+    // Start calibrating
+    setCalibrationCycles(gyro);
 }
 
 void gyroReadScaled(hackflight_t * hf, vehicleState_t * vstate)
@@ -165,12 +144,12 @@ void gyroReadScaled(hackflight_t * hf, vehicleState_t * vstate)
     }
 
     // using gyro lowpass 2 filter for downsampling
-    gyro->sampleSum[0] = gyro->lowpass2FilterApplyFn(
-            (filter_t *)&gyro->lowpass2Filter[0], gyro->dps[0]);
-    gyro->sampleSum[1] = gyro->lowpass2FilterApplyFn(
-            (filter_t *)&gyro->lowpass2Filter[1], gyro->dps[1]);
-    gyro->sampleSum[2] = gyro->lowpass2FilterApplyFn(
-            (filter_t *)&gyro->lowpass2Filter[2], gyro->dps[2]);
+    gyro->sampleSum[0] =
+        pt1FilterApply((pt1Filter_t *)&gyro->lowpass2Filter[0], gyro->dps[0]);
+    gyro->sampleSum[1] =
+        pt1FilterApply((pt1Filter_t *)&gyro->lowpass2Filter[1], gyro->dps[1]);
+    gyro->sampleSum[2] =
+        pt1FilterApply((pt1Filter_t *)&gyro->lowpass2Filter[2], gyro->dps[2]);
 
     for (int axis = 0; axis < 3; axis++) {
 
@@ -179,8 +158,8 @@ void gyroReadScaled(hackflight_t * hf, vehicleState_t * vstate)
 
         // apply static notch filters and software lowpass filters
         dps_filtered =
-            gyro->lowpassFilterApplyFn((filter_t *)&gyro->lowpassFilter[axis],
-                    dps_filtered);
+            //gyro->lowpassFilterApplyFn((filter_t *)&gyro->lowpassFilter[axis],
+            pt1FilterApply((pt1Filter_t *)&gyro->lowpassFilter[axis], dps_filtered);
 
         gyro->dps_filtered[axis] = dps_filtered;
     }
