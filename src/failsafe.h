363e5d92e3fd14456fcb41bc49abc8eda857aa41
simondlevy <simon.d.levy@gmail.com>
2022-08-24 18:00:49 -0400

    Made all Failsafe private

--- src/failsafe.h
+++ src/failsafe.h
@@ -26,109 +26,110 @@
 
 class Failsafe {
 
-    private:
+    friend class AnglePidController;
+    friend class Hackflight;
+    friend class Receiver;
+    friend class Task;
 
-        static const uint32_t MILLIS_PER_TENTH_SECOND    =   100;
-        static const uint32_t MILLIS_PER_SECOND          =  1000;
+    static const uint32_t MILLIS_PER_TENTH_SECOND    =   100;
+    static const uint32_t MILLIS_PER_SECOND          =  1000;
 
-        static uint32_t PERIOD_OF_1_SECONDS() { return 1 * MILLIS_PER_SECOND; }
-        static uint32_t PERIOD_OF_3_SECONDS() { return 3 * MILLIS_PER_SECOND; }
+    static uint32_t PERIOD_OF_1_SECONDS() { return 1 * MILLIS_PER_SECOND; }
+    static uint32_t PERIOD_OF_3_SECONDS() { return 3 * MILLIS_PER_SECOND; }
 
-        static const uint32_t PERIOD_RXDATA_FAILURE      =   200;    // millis
-        static const uint32_t PERIOD_RXDATA_RECOVERY     =   200;    // millis
+    static const uint32_t PERIOD_RXDATA_FAILURE      =   200;    // millis
+    static const uint32_t PERIOD_RXDATA_RECOVERY     =   200;    // millis
 
-        typedef enum {
-            IDLE = 0,
-            RX_LOSS_DETECTED,
-            LANDING,
-            LANDED,
-            RX_LOSS_MONITORING,
-            RX_LOSS_RECOVERED
-        } failsafePhase_e;
+    typedef enum {
+        IDLE = 0,
+        RX_LOSS_DETECTED,
+        LANDING,
+        LANDED,
+        RX_LOSS_MONITORING,
+        RX_LOSS_RECOVERED
+    } failsafePhase_e;
 
-        typedef enum {
-            RXLINK_DOWN = 0,
-            RXLINK_UP
-        } failsafeRxLinkState_e;
+    typedef enum {
+        RXLINK_DOWN = 0,
+        RXLINK_UP
+    } failsafeRxLinkState_e;
 
-        typedef enum {
-            PROCEDURE_AUTO_LANDING = 0,
-            PROCEDURE_DROP_IT,
-            PROCEDURE_COUNT   // must be last
-        } failsafeProcedure_e;
+    typedef enum {
+        PROCEDURE_AUTO_LANDING = 0,
+        PROCEDURE_DROP_IT,
+        PROCEDURE_COUNT   // must be last
+    } failsafeProcedure_e;
 
-        typedef enum {
-            SWITCH_MODE_STAGE1 = 0,
-            SWITCH_MODE_KILL,
-            SWITCH_MODE_STAGE2
-        } failsafeSwitchMode_e;
+    typedef enum {
+        SWITCH_MODE_STAGE1 = 0,
+        SWITCH_MODE_KILL,
+        SWITCH_MODE_STAGE2
+    } failsafeSwitchMode_e;
 
-        bool                   m_active;
-        int16_t                m_events;
-        uint32_t               m_landingShouldBeFinishedAt;
-        bool                   m_monitoring;
-        failsafePhase_e        m_phase;
-        uint32_t               m_receivingRxDataPeriod;        
-        uint32_t               m_receivingRxDataPeriodPreset; 
-        uint32_t               m_rxDataFailurePeriod;
-        failsafeRxLinkState_e  m_rxLinkState;
-        uint32_t               m_validRxDataReceivedAt;
-        uint32_t               m_rxDataRecoveryPeriod;
-        uint32_t               m_throttleLowPeriod;             
-        uint32_t               m_validRxDataFailedAt;
+    bool                   m_active;
+    int16_t                m_events;
+    uint32_t               m_landingShouldBeFinishedAt;
+    bool                   m_monitoring;
+    failsafePhase_e        m_phase;
+    uint32_t               m_receivingRxDataPeriod;        
+    uint32_t               m_receivingRxDataPeriodPreset; 
+    uint32_t               m_rxDataFailurePeriod;
+    failsafeRxLinkState_e  m_rxLinkState;
+    uint32_t               m_validRxDataReceivedAt;
+    uint32_t               m_rxDataRecoveryPeriod;
+    uint32_t               m_throttleLowPeriod;             
+    uint32_t               m_validRxDataFailedAt;
 
-        void activate(void)
-        {
-            m_active = true;
+    void activate(void)
+    {
+        m_active = true;
 
-            m_phase = LANDING;
+        m_phase = LANDING;
 
-            m_landingShouldBeFinishedAt =
-                timeMillis() + 10 * MILLIS_PER_TENTH_SECOND;
+        m_landingShouldBeFinishedAt =
+            timeMillis() + 10 * MILLIS_PER_TENTH_SECOND;
 
-            m_events++;
+        m_events++;
+    }
+
+
+    bool isReceivingRxData(void)
+    {
+        return (m_rxLinkState == RXLINK_UP);
+    }
+
+    Failsafe()
+    {
+        m_events = 0;
+        m_monitoring = false;
+
+        reset();
+    }
+
+    bool isMonitoring(void)
+    {
+        return m_monitoring;
+    }
+
+    bool isActive(void)
+    {
+        return m_active;
+    }
+
+    void onValidDataFailed(Arming * arming)
+    {
+        (void)arming;
+        arming->setRxFailsafe(false);
+        m_validRxDataFailedAt = timeMillis();
+        if ((m_validRxDataFailedAt - m_validRxDataReceivedAt) >
+                m_rxDataFailurePeriod) {
+            m_rxLinkState = RXLINK_DOWN;
         }
+    }
 
-
-        bool isReceivingRxData(void)
-        {
-            return (m_rxLinkState == RXLINK_UP);
-        }
-
-    public:
-
-        Failsafe()
-        {
-            m_events = 0;
-            m_monitoring = false;
-
-            reset();
-        }
-
-        bool isMonitoring(void)
-        {
-            return m_monitoring;
-        }
-
-        bool isActive(void)
-        {
-            return m_active;
-        }
-
-        void onValidDataFailed(Arming * arming)
-        {
-            (void)arming;
-            arming->setRxFailsafe(false);
-            m_validRxDataFailedAt = timeMillis();
-            if ((m_validRxDataFailedAt - m_validRxDataReceivedAt) >
-                    m_rxDataFailurePeriod) {
-                m_rxLinkState = RXLINK_DOWN;
-            }
-        }
-
-        void onValidDataReceived(Arming * arming)
-        {
-            m_validRxDataReceivedAt = timeMillis();
+    void onValidDataReceived(Arming * arming)
+    {
+        m_validRxDataReceivedAt = timeMillis();
             if ((m_validRxDataReceivedAt - m_validRxDataFailedAt) >
                     m_rxDataRecoveryPeriod) {
                 m_rxLinkState = RXLINK_UP;
@@ -260,4 +261,5 @@ class Failsafe {
                 }
             } while (reprocessState);        
         }
-};
+
+}; // class Failsafe
