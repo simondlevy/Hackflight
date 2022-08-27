91742a88d7c5891c16cfe46e989ff04350e4b947
simondlevy <simon.d.levy@gmail.com>
2022-08-27 18:42:57 -0400

    =More MpuImu class work

--- src/imus/fusion/mpu/6000.cpp
+++ src/imus/fusion/mpu/6000.cpp
@@ -18,84 +18,15 @@
 #include <stdint.h>
 
 #include <constrain.h>
+#include <imus/fusion/mpu/6000.h>
 #include <system.h>
 #include <time.h>
 
-#include "platform.h"
-#include "../mpu.h"
-#include "bus_spi.h"
-#include "exti.h"
-#include "io.h"
-#include "systemdev.h"
-
-class Mpu6000Imu {
-
-    public:
-
-        // RF = Register Flag
-        static const uint8_t RF_DATA_RDY_EN = 1 << 0;
-
-        static const uint8_t CONFIG = 0x1A;
-
-        // 1 MHz max SPI frequency for initialisation
-        static const uint32_t MAX_SPI_INIT_CLK_HZ = 1000000;
-
-        // 20 MHz max SPI frequency
-        static const uint32_t MAX_SPI_CLK_HZ = 20000000;
-
-        // Any interrupt interval less than this will be recognised as the
-        // short interval of ~79us
-        static const uint8_t SHORT_THRESHOLD = 82 ;
-
-        // Bits
-        static const uint8_t BIT_SLEEP                   = 0x40;
-        static const uint8_t BIT_H_RESET                 = 0x80;
-        static const uint8_t BITS_CLKSEL                 = 0x07;
-        static const uint8_t CLK_SEL_PLLGYROX            = 0x01;
-        static const uint8_t CLK_SEL_PLLGYROZ            = 0x03;
-        static const uint8_t EXT_SYNC_GYROX              = 0x02;
-        static const uint8_t BITS_FS_250DPS              = 0x00;
-        static const uint8_t BITS_FS_500DPS              = 0x08;
-        static const uint8_t BITS_FS_1000DPS             = 0x10;
-        static const uint8_t BITS_FS_2000DPS             = 0x18;
-        static const uint8_t BITS_FS_2G                  = 0x00;
-        static const uint8_t BITS_FS_4G                  = 0x08;
-        static const uint8_t BITS_FS_8G                  = 0x10;
-        static const uint8_t BITS_FS_16G                 = 0x18;
-        static const uint8_t BITS_FS_MASK                = 0x18;
-        static const uint8_t BITS_DLPF_CFG_256HZ         = 0x00;
-        static const uint8_t BITS_DLPF_CFG_188HZ         = 0x01;
-        static const uint8_t BITS_DLPF_CFG_98HZ          = 0x02;
-        static const uint8_t BITS_DLPF_CFG_42HZ          = 0x03;
-        static const uint8_t BITS_DLPF_CFG_20HZ          = 0x04;
-        static const uint8_t BITS_DLPF_CFG_10HZ          = 0x05;
-        static const uint8_t BITS_DLPF_CFG_5HZ           = 0x06;
-        static const uint8_t BITS_DLPF_CFG_2100HZ_NOLPF  = 0x07;
-        static const uint8_t BITS_DLPF_CFG_MASK          = 0x07;
-        static const uint8_t BIT_INT_ANYRD_2CLEAR        = 0x10;
-        static const uint8_t BIT_RAW_RDY_EN              = 0x01;
-        static const uint8_t BIT_I2C_IF_DIS              = 0x10;
-        static const uint8_t BIT_INT_STATUS_DATA         = 0x01;
-        static const uint8_t BIT_GYRO                    = 0x04;
-        static const uint8_t BIT_ACC                     = 0x02;
-        static const uint8_t BIT_TEMP                    = 0x01;
-
-        // Product ID Description for MPU6000 high 4 bits low 4 bits Product
-        // Name Product Revision
-        static const uint8_t ES_REV_C4 = 0x14;
-        static const uint8_t ES_REV_C5 = 0x15;
-        static const uint8_t ES_REV_D6 = 0x16;
-        static const uint8_t ES_REV_D7 = 0x17;
-        static const uint8_t ES_REV_D8 = 0x18;
-        static const uint8_t REV_C4   = 0x54;
-        static const uint8_t REV_C5   = 0x55;
-        static const uint8_t REV_D6   = 0x56;
-        static const uint8_t REV_D7   = 0x57;
-        static const uint8_t REV_D8   = 0x58;
-        static const uint8_t REV_D9   = 0x59;
-        static const uint8_t REV_D10  = 0x5A;
-
-}; // class Mpu6000Imu
+#include <platform.h>
+#include <bus_spi.h>
+#include <exti.h>
+#include <io.h>
+#include <systemdev.h>
 
 static void mpu6000SpiGyroInit(gyroDev_t *gyro)
 {
