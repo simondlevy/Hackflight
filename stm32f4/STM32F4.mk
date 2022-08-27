e69246ded08bd4375ced481e002fd9c11ba81ca3
simondlevy <simon.d.levy@gmail.com>
2022-08-27 02:21:30 -0400

    =Moved imu_mpu stuff from STM32F4.mk to Makefile

--- stm32f4/STM32F4.mk
+++ stm32f4/STM32F4.mk
@@ -104,7 +104,6 @@ DEVICE_FLAGS    += -DHSE_VALUE=$(HSE_VALUE)
 
 MCU_COMMON_SRC = \
             startup/system_stm32f4xx.c \
-            imu_mpu.c \
             bus_spi_stdperiph.c \
             dma_stm32f4xx.c \
             dshot_bitbang.c \
