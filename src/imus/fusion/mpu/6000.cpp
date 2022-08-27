fe4cd2ce5d5ef12558fdfe0face37e5e2cf8b0cc
simondlevy <simon.d.levy@gmail.com>
2022-08-27 17:18:03 -0400

    More #define => static const

--- src/imus/fusion/mpu/6000.cpp
+++ src/imus/fusion/mpu/6000.cpp
@@ -28,77 +28,68 @@ Hackflight. If not, see <https://www.gnu.org/licenses/>.
 #include "io.h"
 #include "systemdev.h"
 
-// 16.384 dps/lsb scalefactor for 2000dps sensors
-#define GYRO_SCALE_2000DPS (2000.0f / (1 << 15))   
-
-// 8.192 dps/lsb scalefactor for 4000dps sensors
-#define GYRO_SCALE_4000DPS (4000.0f / (1 << 15))   
-
 // RF = Register Flag
-#define MPU_RF_DATA_RDY_EN (1 << 0)
+static const uint8_t MPU_RF_DATA_RDY_EN = 1 << 0;
 
-#define MPU6000_CONFIG              0x1A
-
-#define BITS_DLPF_CFG_256HZ         0x00
-#define BITS_DLPF_CFG_188HZ         0x01
-#define BITS_DLPF_CFG_98HZ          0x02
-#define BITS_DLPF_CFG_42HZ          0x03
+static const uint8_t MPU6000_CONFIG = 0x1A;
 
 // 1 MHz max SPI frequency for initialisation
-#define MPU6000_MAX_SPI_INIT_CLK_HZ 1000000
-// 20 MHz max SPI frequency
-#define MPU6000_MAX_SPI_CLK_HZ 20000000
+static const uint32_t MPU6000_MAX_SPI_INIT_CLK_HZ = 1000000;
 
-#define MPU6000_SHORT_THRESHOLD         82  // Any interrupt interval less than this will be recognised as the short interval of ~79us
+// 20 MHz max SPI frequency
+static const uint32_t MPU6000_MAX_SPI_CLK_HZ = 20000000;
+
+// Any interrupt interval less than this will be recognised as the short interval of ~79us
+static const uint8_t MPU6000_SHORT_THRESHOLD = 82 ;
 
 // Bits
-#define BIT_SLEEP                   0x40
-#define BIT_H_RESET                 0x80
-#define BITS_CLKSEL                 0x07
-#define MPU_CLK_SEL_PLLGYROX        0x01
-#define MPU_CLK_SEL_PLLGYROZ        0x03
-#define MPU_EXT_SYNC_GYROX          0x02
-#define BITS_FS_250DPS              0x00
-#define BITS_FS_500DPS              0x08
-#define BITS_FS_1000DPS             0x10
-#define BITS_FS_2000DPS             0x18
-#define BITS_FS_2G                  0x00
-#define BITS_FS_4G                  0x08
-#define BITS_FS_8G                  0x10
-#define BITS_FS_16G                 0x18
-#define BITS_FS_MASK                0x18
-#define BITS_DLPF_CFG_256HZ         0x00
-#define BITS_DLPF_CFG_188HZ         0x01
-#define BITS_DLPF_CFG_98HZ          0x02
-#define BITS_DLPF_CFG_42HZ          0x03
-#define BITS_DLPF_CFG_20HZ          0x04
-#define BITS_DLPF_CFG_10HZ          0x05
-#define BITS_DLPF_CFG_5HZ           0x06
-#define BITS_DLPF_CFG_2100HZ_NOLPF  0x07
-#define BITS_DLPF_CFG_MASK          0x07
-#define BIT_INT_ANYRD_2CLEAR        0x10
-#define BIT_RAW_RDY_EN              0x01
-#define BIT_I2C_IF_DIS              0x10
-#define BIT_INT_STATUS_DATA         0x01
-#define BIT_GYRO                    0x04
-#define BIT_ACC                     0x02
-#define BIT_TEMP                    0x01
+static const uint8_t BIT_SLEEP                   = 0x40;
+static const uint8_t BIT_H_RESET                 = 0x80;
+static const uint8_t BITS_CLKSEL                 = 0x07;
+static const uint8_t MPU_CLK_SEL_PLLGYROX        = 0x01;
+static const uint8_t MPU_CLK_SEL_PLLGYROZ        = 0x03;
+static const uint8_t MPU_EXT_SYNC_GYROX          = 0x02;
+static const uint8_t BITS_FS_250DPS              = 0x00;
+static const uint8_t BITS_FS_500DPS              = 0x08;
+static const uint8_t BITS_FS_1000DPS             = 0x10;
+static const uint8_t BITS_FS_2000DPS             = 0x18;
+static const uint8_t BITS_FS_2G                  = 0x00;
+static const uint8_t BITS_FS_4G                  = 0x08;
+static const uint8_t BITS_FS_8G                  = 0x10;
+static const uint8_t BITS_FS_16G                 = 0x18;
+static const uint8_t BITS_FS_MASK                = 0x18;
+static const uint8_t BITS_DLPF_CFG_256HZ         = 0x00;
+static const uint8_t BITS_DLPF_CFG_188HZ         = 0x01;
+static const uint8_t BITS_DLPF_CFG_98HZ          = 0x02;
+static const uint8_t BITS_DLPF_CFG_42HZ          = 0x03;
+static const uint8_t BITS_DLPF_CFG_20HZ          = 0x04;
+static const uint8_t BITS_DLPF_CFG_10HZ          = 0x05;
+static const uint8_t BITS_DLPF_CFG_5HZ           = 0x06;
+static const uint8_t BITS_DLPF_CFG_2100HZ_NOLPF  = 0x07;
+static const uint8_t BITS_DLPF_CFG_MASK          = 0x07;
+static const uint8_t BIT_INT_ANYRD_2CLEAR        = 0x10;
+static const uint8_t BIT_RAW_RDY_EN              = 0x01;
+static const uint8_t BIT_I2C_IF_DIS              = 0x10;
+static const uint8_t BIT_INT_STATUS_DATA         = 0x01;
+static const uint8_t BIT_GYRO                    = 0x04;
+static const uint8_t BIT_ACC                     = 0x02;
+static const uint8_t BIT_TEMP                    = 0x01;
 
 // Product ID Description for MPU6000
 // high 4 bits low 4 bits
 // Product Name Product Revision
-#define MPU6000ES_REV_C4 0x14
-#define MPU6000ES_REV_C5 0x15
-#define MPU6000ES_REV_D6 0x16
-#define MPU6000ES_REV_D7 0x17
-#define MPU6000ES_REV_D8 0x18
-#define MPU6000_REV_C4 0x54
-#define MPU6000_REV_C5 0x55
-#define MPU6000_REV_D6 0x56
-#define MPU6000_REV_D7 0x57
-#define MPU6000_REV_D8 0x58
-#define MPU6000_REV_D9 0x59
-#define MPU6000_REV_D10 0x5A
+static const uint8_t MPU6000ES_REV_C4 = 0x14;
+static const uint8_t MPU6000ES_REV_C5 = 0x15;
+static const uint8_t MPU6000ES_REV_D6 = 0x16;
+static const uint8_t MPU6000ES_REV_D7 = 0x17;
+static const uint8_t MPU6000ES_REV_D8 = 0x18;
+static const uint8_t MPU6000_REV_C4 = 0x54;
+static const uint8_t MPU6000_REV_C5 = 0x55;
+static const uint8_t MPU6000_REV_D6 = 0x56;
+static const uint8_t MPU6000_REV_D7 = 0x57;
+static const uint8_t MPU6000_REV_D8 = 0x58;
+static const uint8_t MPU6000_REV_D9 = 0x59;
+static const uint8_t MPU6000_REV_D10 = 0x5A;
 
 static void mpu6000SpiGyroInit(gyroDev_t *gyro)
 {
