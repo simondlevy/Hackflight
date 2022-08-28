9f92f6f90a9d6bb2ab373b751dd75a5c6bc624ca
simondlevy <simon.d.levy@gmail.com>
2022-08-27 21:35:56 -0400

    =Cleanup

--- src/imus/fusion/mpudev.h
+++ src/imus/fusion/mpudev.h
@@ -24,16 +24,16 @@ struct gyroDev_s;
 
 typedef struct gyroDev_s {
 
-    int16_t                   adcRaw[3];                          
-    uint32_t                  detectedEXTI;
-    volatile bool             dataReady;
-    extDevice_t               dev;
-    extiCallbackRec_t         exti;
-    int32_t                   gyroDmaMaxDuration;
-    uint32_t                  gyroLastEXTI;
-    int32_t                   gyroShortPeriod;
-    uint32_t                  gyroSyncEXTI;
-    ioTag_t                   mpuIntExtiTag;
+    int16_t           adcRaw[3];                          
+    uint32_t          detectedEXTI;
+    volatile bool     dataReady;
+    extDevice_t       dev;
+    extiCallbackRec_t exti;
+    int32_t           gyroDmaMaxDuration;
+    uint32_t          gyroLastEXTI;
+    int32_t           gyroShortPeriod;
+    uint32_t          gyroSyncEXTI;
+    ioTag_t           mpuIntExtiTag;
 
 } gyroDev_t;
 
