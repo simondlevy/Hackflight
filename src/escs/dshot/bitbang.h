0ba9fc67f72255ca219500b3875af5fc128c2fff
simondlevy <simon.d.levy@gmail.com>
2022-08-31 13:37:15 -0400

    =More DshotBitbangEsc work

--- src/escs/dshot/bitbang.h
+++ src/escs/dshot/bitbang.h
@@ -28,14 +28,65 @@ class DshotBitbangEsc : public DshotEsc {
         virtual escDevice_t * deviceInit(void) override
         {
             return dshotBitbangDevInit(m_motorCount);
-        }
 
-    public:
+            /*
+            bbDevice.vTable = bbVTable;
+            motorCount = count;
+            bbStatus = DSHOT_BITBANG_STATUS_OK;
 
-        DshotBitbangEsc(uint8_t count) 
-            : DshotEsc(count)
-        {
-        }
+            memset(bbOutputBuffer, 0, sizeof(bbOutputBuffer));
+
+            for (int motorIndex = 0; motorIndex < MAX_SUPPORTED_MOTORS &&
+                    motorIndex < motorCount; motorIndex++) {
+                const timerHardware_t *timerHardware =
+                    timerGetConfiguredByTag(ESC_IO_TAGS[motorIndex]);
+                const IO_t io = IOGetByTag(ESC_IO_TAGS[motorIndex]);
+
+                uint8_t output = timerHardware->output;
+                bbPuPdMode = (output & TIMER_OUTPUT_INVERTED) ?
+                    BB_GPIO_PULLDOWN :
+                    BB_GPIO_PULLUP;
+
+                if (!IOIsFreeOrPreinit(io)) {
+                    // not enough motors initialised for the mixer or a break
+                    // in the motors
+                    bbDevice.vTable.write = escDevWriteNull;
+                    bbDevice.vTable.updateStart = escUpdateStartNull;
+                    bbDevice.vTable.updateComplete = escUpdateCompleteNull;
+                    bbStatus = DSHOT_BITBANG_STATUS_MOTOR_PIN_CONFLICT;
+                    return NULL;
+                }
+
+                int pinIndex = IO_GPIOPinIdx(io);
+
+                bbMotors[motorIndex].pinIndex = pinIndex;
+                bbMotors[motorIndex].io = io;
+                bbMotors[motorIndex].output = output;
+                bbMotors[motorIndex].iocfg = IO_CONFIG(GPIO_Mode_OUT, GPIO_Speed_50MHz,
+                        GPIO_OType_PP, bbPuPdMode);
+
+                IOInit(io, OWNER_MOTOR, RESOURCE_INDEX(motorIndex));
+                IOConfigGPIO(io, bbMotors[motorIndex].iocfg);
+                if (output & TIMER_OUTPUT_INVERTED) {
+                    IOLo(io);
+                } else {
+                    IOHi(io);
+                }
+
+                // Fill in motors structure for 4way access 
+                motors[motorIndex].io = bbMotors[motorIndex].io;
+            }
+
+            return &bbDevice;
+            */
+        }        
+
+public:
+
+    DshotBitbangEsc(uint8_t count) 
+: DshotEsc(count)
+{
+}
 
 
 }; // class DshotBitbangEsc
