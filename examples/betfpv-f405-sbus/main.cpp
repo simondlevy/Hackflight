eca66fd8792b3987dd160210b6a703e2420a7d15
simondlevy <simon.d.levy@gmail.com>
2022-09-03 04:23:49 -0400

    Renamed bitbang_class.h => bitbang.h

--- examples/betfpv-f405-sbus/main.cpp
+++ examples/betfpv-f405-sbus/main.cpp
@@ -20,7 +20,7 @@
 #include <alignment/rotate270.h>
 #include <core/clock.h>
 #include <core/mixers/fixedpitch/quadxbf.h>
-#include <escs/dshot/bitbang_class.h>
+#include <escs/dshot/bitbang.h>
 #include <hackflight.h>
 #include <receivers/sbus.h>
 #include <serial.h>
