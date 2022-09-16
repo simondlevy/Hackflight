9abd207d8e0548f0e5752fdb8206aa910f28da16
simondlevy <simon.d.levy@gmail.com>
2022-09-16 02:16:53 -0400

    Merged timer_stm32f4xx.c into timer_stm32f411.c

--- stm32f4/src/timer_stm32f411.c
+++ stm32f4/src/timer_stm32f411.c
@@ -730,3 +730,103 @@ uint16_t timerGetPrescalerByDesiredHertz(TIM_TypeDef *tim, uint32_t hz)
     }
     return (uint16_t)((timerClock(tim) + hz / 2 ) / hz) - 1;
 }
+
+const timerDef_t timerDefinitions[HARDWARE_TIMER_DEFINITION_COUNT] = {
+    { .TIMx = TIM1,  .rcc = RCC_APB2(TIM1),  .inputIrq = TIM1_CC_IRQn},
+    { .TIMx = TIM2,  .rcc = RCC_APB1(TIM2),  .inputIrq = TIM2_IRQn},
+    { .TIMx = TIM3,  .rcc = RCC_APB1(TIM3),  .inputIrq = TIM3_IRQn},
+    { .TIMx = TIM4,  .rcc = RCC_APB1(TIM4),  .inputIrq = TIM4_IRQn},
+    { .TIMx = TIM5,  .rcc = RCC_APB1(TIM5),  .inputIrq = TIM5_IRQn},
+    { .TIMx = TIM6,  .rcc = RCC_APB1(TIM6),  .inputIrq = 0},
+    { .TIMx = TIM7,  .rcc = RCC_APB1(TIM7),  .inputIrq = 0},
+    { .TIMx = TIM9,  .rcc = RCC_APB2(TIM9),  .inputIrq = TIM1_BRK_TIM9_IRQn},
+    { .TIMx = TIM10, .rcc = RCC_APB2(TIM10), .inputIrq = TIM1_UP_TIM10_IRQn},
+    { .TIMx = TIM11, .rcc = RCC_APB2(TIM11), .inputIrq = TIM1_TRG_COM_TIM11_IRQn},
+};
+
+const timerHardware_t fullTimerHardware[FULL_TIMER_CHANNEL_COUNT] = {
+    // Auto-generated from 'timer_def.h'
+//PORTA
+    DEF_TIM(TIM2, CH1, PA0, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM2, CH2, PA1, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM2, CH3, PA2, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM2, CH4, PA3, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM2, CH1, PA5, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM1, CH1N, PA7, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM1, CH1, PA8, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM1, CH2, PA9, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM1, CH3, PA10, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM1, CH1N, PA11, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM2, CH1, PA15, TIM_USE_ANY, 0, 0),
+
+    DEF_TIM(TIM5, CH1, PA0, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM5, CH2, PA1, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM5, CH3, PA2, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM5, CH4, PA3, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM3, CH1, PA6, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM3, CH2, PA7, TIM_USE_ANY, 0, 0),
+
+    DEF_TIM(TIM9, CH1, PA2, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM9, CH2, PA3, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM8, CH1N, PA5, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM8, CH1N, PA7, TIM_USE_ANY, 0, 0),
+
+    DEF_TIM(TIM13, CH1, PA6, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM14, CH1, PA7, TIM_USE_ANY, 0, 0),
+
+//PORTB
+    DEF_TIM(TIM1, CH2N, PB0, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM1, CH3N, PB1, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM2, CH2, PB3, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM2, CH3, PB10, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM2, CH4, PB11, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM1, CH1N, PB13, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM1, CH2N, PB14, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM1, CH3N, PB15, TIM_USE_ANY, 0, 0),
+
+    DEF_TIM(TIM3, CH3, PB0, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM3, CH4, PB1, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM3, CH1, PB4, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM3, CH2, PB5, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM4, CH1, PB6, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM4, CH2, PB7, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM4, CH3, PB8, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM4, CH4, PB9, TIM_USE_ANY, 0, 0),
+
+    DEF_TIM(TIM10, CH1, PB8, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM11, CH1, PB9, TIM_USE_ANY, 0, 0),
+
+//PORTC
+    DEF_TIM(TIM3, CH1, PC6, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM3, CH2, PC7, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM3, CH3, PC8, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM3, CH4, PC9, TIM_USE_ANY, 0, 0),
+
+//PORTD
+    DEF_TIM(TIM4, CH1, PD12, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM4, CH2, PD13, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM4, CH3, PD14, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM4, CH4, PD15, TIM_USE_ANY, 0, 0),
+
+//PORTE
+    DEF_TIM(TIM1, CH1N, PE8, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM1, CH1, PE9, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM1, CH2N, PE10, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM1, CH2, PE11, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM1, CH3N, PE12, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM1, CH3, PE13, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM1, CH4, PE14, TIM_USE_ANY, 0, 0),
+
+    DEF_TIM(TIM9, CH1, PE5, TIM_USE_ANY, 0, 0),
+    DEF_TIM(TIM9, CH2, PE6, TIM_USE_ANY, 0, 0),
+
+//PORTF
+
+};
+
+uint32_t timerClock(TIM_TypeDef *tim)
+{
+    UNUSED(tim);
+    return SystemCoreClock;
+}
+
