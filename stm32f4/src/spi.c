148c3b5b6f16fc850675e46c0c425a090e8f2226
simondlevy <simon.d.levy@gmail.com>
2022-09-16 19:05:46 -0400

    More SPI consolidation

--- stm32f4/src/spi.c
+++ stm32f4/src/spi.c
@@ -740,3 +740,103 @@ void spiPreinitByTag(ioTag_t tag)
     spiPreinitByIO(IOGetByTag(tag));
 }
 
+const spiHardware_t spiHardware[] = {
+    {
+        .device = SPIDEV_1,
+        .reg = SPI1,
+        .sckPins = {
+            { DEFIO_TAG_E(PA5) },
+            { DEFIO_TAG_E(PB3) },
+        },
+        .misoPins = {
+            { DEFIO_TAG_E(PA6) },
+            { DEFIO_TAG_E(PB4) },
+        },
+        .mosiPins = {
+            { DEFIO_TAG_E(PA7) },
+            { DEFIO_TAG_E(PB5) },
+        },
+        .af = GPIO_AF_SPI1,
+        .rcc = RCC_APB2(SPI1),
+    },
+    {
+        .device = SPIDEV_2,
+        .reg = SPI2,
+        .sckPins = {
+            { DEFIO_TAG_E(PB10) },
+            { DEFIO_TAG_E(PB13) },
+        },
+        .misoPins = {
+            { DEFIO_TAG_E(PB14) },
+            { DEFIO_TAG_E(PC2) },
+        },
+        .mosiPins = {
+            { DEFIO_TAG_E(PB15) },
+            { DEFIO_TAG_E(PC3) },
+        },
+        .af = GPIO_AF_SPI2,
+        .rcc = RCC_APB1(SPI2),
+    },
+    {
+        .device = SPIDEV_3,
+        .reg = SPI3,
+        .sckPins = {
+            { DEFIO_TAG_E(PB3) },
+            { DEFIO_TAG_E(PC10) },
+        },
+        .misoPins = {
+            { DEFIO_TAG_E(PB4) },
+            { DEFIO_TAG_E(PC11) },
+        },
+        .mosiPins = {
+            { DEFIO_TAG_E(PB5) },
+            { DEFIO_TAG_E(PC12) },
+        },
+        .af = GPIO_AF_SPI3,
+        .rcc = RCC_APB1(SPI3),
+    },
+};
+
+void spiPinConfigure(void)
+{
+    spiPinConfig_t spiPinConfig;
+
+    spiPinConfig.ioTagSck = 21;   // 21 & 0x07 = 5; so, PA5
+    spiPinConfig.ioTagMiso = 22;  //                    PA6
+    spiPinConfig.ioTagMosi = 23;  //                    PA7
+    spiPinConfig.txDmaopt = -1;
+    spiPinConfig.rxDmaopt = -1;
+
+    spiPinConfig_t * pConfig = &spiPinConfig;
+
+    for (size_t hwindex = 0 ; hwindex < ARRAYLEN(spiHardware) ; hwindex++) {
+        const spiHardware_t *hw = &spiHardware[hwindex];
+
+        if (!hw->reg) {
+            continue;
+        }
+
+        SPIDevice device = hw->device;
+        spiDevice_t *pDev = &spiDevice[device];
+
+        for (int pindex = 0 ; pindex < MAX_SPI_PIN_SEL ; pindex++) {
+            if (pConfig[device].ioTagSck == hw->sckPins[pindex].pin) {
+                pDev->sck = hw->sckPins[pindex].pin;
+            }
+            if (pConfig[device].ioTagMiso == hw->misoPins[pindex].pin) {
+                pDev->miso = hw->misoPins[pindex].pin;
+            }
+            if (pConfig[device].ioTagMosi == hw->mosiPins[pindex].pin) {
+                pDev->mosi = hw->mosiPins[pindex].pin;
+            }
+        }
+
+        if (pDev->sck && pDev->miso && pDev->mosi) {
+            pDev->dev = hw->reg;
+            pDev->af = hw->af;
+            pDev->rcc = hw->rcc;
+            pDev->leadingEdge = false; // XXX Should be part of transfer context
+            pDev->dmaIrqHandler = hw->dmaIrqHandler;
+        }
+    }
+}
