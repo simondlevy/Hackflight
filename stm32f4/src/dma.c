e0e85732155a4008583f348940288eda7f5339f6
simondlevy <simon.d.levy@gmail.com>
2022-10-11 16:23:46 -0400

    =More DMA macro elimination

--- stm32f4/src/dma.c
+++ stm32f4/src/dma.c
@@ -486,38 +486,7 @@ dmaoptValue_t dmaGetOptionByTimer(const timerHardware_t *timer)
     return DMA_OPT_UNUSED;
 }
 
-#define DEFINE_DMA_CHANNEL(d, s, f) { \
-    .dma = d, \
-    .ref = (dmaResource_t *)d ## _Stream ## s, \
-    .stream = s, \
-    .irqHandlerCallback = NULL, \
-    .flagsShift = f, \
-    .irqN = d ## _Stream ## s ## _IRQn, \
-    .userParam = 0, \
-    .owner.owner = 0, \
-    .owner.resourceIndex = 0 \
-    }
-
-
-static dmaChannelDescriptor_t dmaDescriptors[DMA_LAST_HANDLER] = {
-    DEFINE_DMA_CHANNEL(DMA1, 0,  0),
-    DEFINE_DMA_CHANNEL(DMA1, 1,  6),
-    DEFINE_DMA_CHANNEL(DMA1, 2, 16),
-    DEFINE_DMA_CHANNEL(DMA1, 3, 22),
-    DEFINE_DMA_CHANNEL(DMA1, 4, 32),
-    DEFINE_DMA_CHANNEL(DMA1, 5, 38),
-    DEFINE_DMA_CHANNEL(DMA1, 6, 48),
-    DEFINE_DMA_CHANNEL(DMA1, 7, 54),
-
-    DEFINE_DMA_CHANNEL(DMA2, 0,  0),
-    DEFINE_DMA_CHANNEL(DMA2, 1,  6),
-    DEFINE_DMA_CHANNEL(DMA2, 2, 16),
-    DEFINE_DMA_CHANNEL(DMA2, 3, 22),
-    DEFINE_DMA_CHANNEL(DMA2, 4, 32),
-    DEFINE_DMA_CHANNEL(DMA2, 5, 38),
-    DEFINE_DMA_CHANNEL(DMA2, 6, 48),
-    DEFINE_DMA_CHANNEL(DMA2, 7, 54),
-};
+static dmaChannelDescriptor_t dmaDescriptors[DMA_LAST_HANDLER];
 
 /*
  * DMA IRQ Handlers
