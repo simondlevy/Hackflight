876b716b8b088fee90eade157295889bf28689dc
simondlevy <simon.d.levy@gmail.com>
2022-11-05 18:09:48 -0400

    =Cleanup

--- src/escs/dshot/stm32f405.h
+++ src/escs/dshot/stm32f405.h
@@ -380,21 +380,9 @@ class Stm32F405DshotEsc : public DshotEsc {
             return NULL;
         }
 
-        const dmaChannelSpec_t *findDmaChannelSpec(uint8_t portIndex, uint8_t channel, int8_t dmaOpt)
+        const dmaChannelSpec_t *findDmaChannelSpec(uint8_t portIndex, int8_t option)
         {
-            uint8_t i = portIndex;
-
-            //for (uint8_t i=0 ; i<DMA_TIMER_MAPPING_COUNT; i++) {
-
-                const dmaTimerMapping_t *timerMapping = &m_dmaTimerMapping[i];
-
-                //if (timerMapping->channel == channel && timerMapping->channelSpec[dmaOpt].ref) {
-
-                    return &timerMapping->channelSpec[dmaOpt];
-                //}
-            //}
-
-            //return NULL;
+            return &m_dmaTimerMapping[portIndex].channelSpec[option];
         }
 
         port_t *allocatePort(IO_t io, uint8_t option, int32_t portIndex)
@@ -403,8 +391,7 @@ class Stm32F405DshotEsc : public DshotEsc {
             port->index = portIndex;
             ++m_usedMotorPorts;
 
-            const dmaChannelSpec_t * dmaChannelSpec =
-                findDmaChannelSpec(portIndex, port->channel, option); 
+            const dmaChannelSpec_t * dmaChannelSpec = findDmaChannelSpec(portIndex, option); 
 
             port->dmaResource = dmaChannelSpec->ref;
 
