7a3cb8b0fc258283c71369cb4a9d5462aa43482f
simondlevy <simon.d.levy@gmail.com>
2022-11-26 14:59:46 -0500

    =More BeatFpvF405SbusSR work

--- src/board/stm32/stm32f4/stm32f405.h
+++ src/board/stm32/stm32f4/stm32f405.h
@@ -17,6 +17,7 @@
 #pragma once
 
 #include "board/stm32/stm32f4.h"
+#include "sr.h"
 
 #include <stm32f4xx.h>
 
@@ -40,6 +41,18 @@ class Stm32F405Board : public Stm32F4Board {
         Stm32F405Board(
                 Receiver & receiver,
                 Imu & imu,
+                vector<PidController *> & pids,
+                Mixer & mixer,
+                Esc & esc,
+                const uint8_t ledPin) 
+            : Stm32F4Board(2, receiver, imu, pids, mixer, esc, ledPin)
+        {
+        }
+
+        Stm32F405Board(
+                Receiver & receiver,
+                Imu & imu,
+                SR & sr,
                 vector<PidController *> & pids,
                 Mixer & mixer,
                 Esc & esc,
