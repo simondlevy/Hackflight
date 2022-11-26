7b2acdf03e671fb09ffe59bda2bfc50068644763
simondlevy <simon.d.levy@gmail.com>
2022-11-26 01:54:27 -0500

    Cleanup

--- examples/GepRcF411Sbus/GepRcF411Sbus.ino
+++ examples/GepRcF411Sbus/GepRcF411Sbus.ino
@@ -21,15 +21,16 @@
 #include <board/stm32/stm32f4/stm32f411.h>
 #include <core/mixers/fixedpitch/quadxbf.h>
 #include <esc/mock.h>
-#include <imu/real/softquat/bmi270.h>
+//#include <imu/real/softquat/bmi270.h>
+#include <imu/mock.h>
 #include <task/receiver/sbus.h>
 
 // IMU
-static const uint8_t MOSI_PIN = PA7; // PB15
-static const uint8_t MISO_PIN = PA6; // PB14
-static const uint8_t SCLK_PIN = PA5; // PB13
-static const uint8_t CS_PIN   = PA4;
-static const uint8_t EXTI_PIN = PA1;
+//static const uint8_t MOSI_PIN = PA7; // PB15
+//static const uint8_t MISO_PIN = PA6; // PB14
+//static const uint8_t SCLK_PIN = PA5; // PB13
+//static const uint8_t CS_PIN   = PA4;
+//static const uint8_t EXTI_PIN = PA1;
 
 #include <vector>
 using namespace std;
@@ -37,7 +38,7 @@ using namespace std;
 //static const uint8_t LED_PIN  = PC13; // orange
 static const uint8_t LED_PIN  = PC14; // blue
 
-static SPIClass _spi(MOSI_PIN, MISO_PIN, SCLK_PIN);
+//static SPIClass _spi(MOSI_PIN, MISO_PIN, SCLK_PIN);
 
 static AnglePidController _anglePid(
         1.441305,     // Rate Kp
@@ -49,7 +50,7 @@ static AnglePidController _anglePid(
 static vector<PidController *> _pids = {&_anglePid};
 
 static Stm32F411Board * _board;
-static Bmi270 * _imu;
+//static Bmi270 * _imu;
 
 static SbusReceiver _rx;
 
@@ -62,14 +63,16 @@ void serialEvent2(void)
 
 void setup(void)
 {
-    static Bmi270 imu(RealImu::rotate270, _spi, CS_PIN);
+    //static Bmi270 imu(RealImu::rotate270, _spi, CS_PIN);
+
+    static MockImu imu;
 
     static MockEsc esc;
 
     static Stm32F411Board board(_rx, imu, _pids, _mixer, esc, LED_PIN);
 
     _board = &board;
-    _imu = &imu;
+    //_imu = &imu;
 
     Serial2.begin(100000, SERIAL_8E2);
 
@@ -80,5 +83,5 @@ void loop(void)
 {
     _board->step();
 
-    Serial.println(_imu->id, HEX);
+    //Serial.println(_imu->id, HEX);
 }
