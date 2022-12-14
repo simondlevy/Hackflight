5014f5ff080b2f94f64661be433e91f1a851fc68
simondlevy <simon.d.levy@gmail.com>
2022-12-14 03:09:32 -0500

    =More Rust

--- rust/examples/hello.rs
+++ rust/examples/hello.rs
@@ -2,5 +2,6 @@ extern crate hackflight;
 
 fn main() {
 
-    let bar = hackflight::tests::foo();
+    // let _bar = hackflight::datatypes::foo();
+    let _motors = hackflight::datatypes::Motors {m1:0.0, m2:0.0, m3:0.0, m4:0.0};
 }
