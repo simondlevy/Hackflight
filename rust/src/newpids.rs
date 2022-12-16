5e4ff906a4a9d7bd0f0d475d766139c262e36719
simondlevy <simon.d.levy@gmail.com>
2022-12-16 15:56:49 -0500

    =More new Rust PID controller work

--- rust/src/newpids.rs
+++ rust/src/newpids.rs
@@ -16,14 +16,19 @@ pub mod newpids {
     enum PidController {
 
         AnglePid {
+            k_rate_p: f32,
+            k_rate_i: f32,
+            k_rate_d: f32,
+            k_rate_f: f32,
+            k_level_p: f32,
             dyn_lpf_previous_quantized_throttle: i32,  
             feedforward_lpf_initialized: bool,
             sum: f32,
         },
 
         AltitudePid {
-            kp: f32,
-            ki: f32,
+            k_p: f32,
+            k_i: f32,
             in_band_prev: bool,
             error_integral: f32,
             altitude_target: f32 
@@ -35,26 +40,36 @@ pub mod newpids {
         match pid {
 
             PidController::AnglePid {
+                k_rate_p,
+                k_rate_i,
+                k_rate_d,
+                k_rate_f,
+                k_level_p,
                 dyn_lpf_previous_quantized_throttle,  
                 feedforward_lpf_initialized,
                 sum,
             } => { 
                 get_angle_demands(
+                    k_rate_p,
+                    k_rate_i,
+                    k_rate_d,
+                    k_rate_f,
+                    k_level_p,
                     dyn_lpf_previous_quantized_throttle,  
                     feedforward_lpf_initialized,
                     sum) 
             },
 
             PidController::AltitudePid {
-                kp,
-                ki,
+                k_p,
+                k_i,
                 in_band_prev,  
                 error_integral,
                 altitude_target,
             } => { 
                 get_alt_hold_demands(
-                    kp,
-                    ki,
+                    k_p,
+                    k_i,
                     in_band_prev,  
                     error_integral,
                     altitude_target) 
@@ -64,6 +79,11 @@ pub mod newpids {
     }
 
     fn get_angle_demands(
+                    k_rate_p: &f32,
+                    k_rate_i: &f32,
+                    k_rate_d: &f32,
+                    k_rate_f: &f32,
+                    k_level_p: &f32,
                     dyn_lpf_previous_quantized_throttle: &i32,  
                     feedforward_lpf_initialized: &bool,
                     sum: &f32) -> Demands  {
@@ -76,8 +96,8 @@ pub mod newpids {
     }
 
     fn get_alt_hold_demands(
-            kp: &f32,
-            ki: &f32,
+            k_p: &f32,
+            k_i: &f32,
             in_band_prev: &bool,
             error_integral: &f32,
             altitude_target: &f32) -> Demands  {
