c816142550cb4d4032c78230c63bd81340a361f8
simondlevy <simon.d.levy@gmail.com>
2022-12-16 12:50:50 -0500

    =More new Rust PID controller work

--- rust/src/newpids.rs
+++ rust/src/newpids.rs
@@ -11,5 +11,22 @@ pub mod newpids {
 
     use crate::datatypes::Demands;
     use crate::datatypes::VehicleState;
+
+    #[derive(Debug,Clone)]
+    enum PidController {
+
+        AnglePid {
+            x: f32,
+            y: f32,
+            s: f32 
+        },
+
+        AltitudePid {
+            x: f32,
+            y: f32,
+            r: f32 
+        },
+    }
+
 }
 
