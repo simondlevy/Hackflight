bc4aa063270c6c032114f7e509e1163d4d16611e
simondlevy <simon.d.levy@gmail.com>
2022-12-16 22:44:21 -0500

    =More new Rust PID controller work

--- rust/src/filters.rs
+++ rust/src/filters.rs
@@ -33,30 +33,30 @@ pub mod filters {
         dt / (rc + dt)
     }
 
+    #[derive(Clone,Copy)]
     pub struct Pt2 {
 
         state: f32,
         state1: f32,
     }
 
-    /*
-    pub fn applyPt2(filter: Pt2, f_cut: f32, input: f32) -> (f32, Pt2) {
+    pub fn applyPt2(filter: Pt2, f_cut: f32, input: f32, dt: f32) -> (f32, Pt2) {
 
-        let k = computeGainPt2(filter, f_cut);
+        let k = computeGainPt2(filter, f_cut, dt);
 
         let state1 = filter.state1 + k * (input - filter.state1);
         let state = filter.state + k * (state1 - filter.state);
 
-        (state, Pt2 {state: state, state1: state1, dt: filter.dt, k: k})
+        (state, Pt2 {state: state, state1: state1})
     }
 
-    fn computeGainPt2(filter: Pt2, f_cut: f32) -> f32 {
+    fn computeGainPt2(filter: Pt2, f_cut: f32, dt: f32) -> f32 {
 
         let order: f32 = 2.0;
-        let order_cutoff_correction = 1 / sqrt(pow(2.0, 1.0 / order) - 1);
-        let rc = 1.0 / (2.0 * order_cutoff_correction * std::f32::consts::PI * f_cut);
+        let order_cutoff_correction: f32 = 1.0; //1 / sqrt(pow(2.0, 1.0 / order) - 1);
+        let rc = 1.0 / (2.0 * order_cutoff_correction * PI * f_cut);
 
-        filter.dt / (rc + filter.dt)
-    }*/
+        dt / (rc + dt)
+    }
 
 } // pub mod filters
