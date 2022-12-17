2bcccd09bacb2ec2cb283e4395c1d6e924430a4b
simondlevy <simon.d.levy@gmail.com>
2022-12-16 22:40:57 -0500

    =More new Rust PID controller work

--- rust/src/filters.rs
+++ rust/src/filters.rs
@@ -15,45 +15,48 @@ pub mod filters {
     pub struct Pt1 {
 
         state: f32,
-        dt: f32,
-        k: f32
     }
 
-    pub fn applyPt1(filter: Pt1, f_cut: f32, input: f32) -> (f32, Pt1) {
+    pub fn applyPt1(filter: Pt1, f_cut: f32, input: f32, dt: f32) -> (f32, Pt1) {
 
-        let k = computeGainPt1(filter, f_cut);
+        let k = computeGainPt1(filter, f_cut, dt);
 
         let state = filter.state + k * (input - filter.state);
 
-        (state, Pt1 {state: state, dt: filter.dt, k: k})
+        (state, Pt1 {state: state})
     }
 
-    fn computeGainPt1(filter: Pt1, f_cut: f32) -> f32 {
+    fn computeGainPt1(filter: Pt1, f_cut: f32, dt: f32) -> f32 {
 
         let rc = 1.0 / (2.0 * PI * f_cut);
 
-        filter.dt / (rc + filter.dt)
+        dt / (rc + dt)
     }
 
-    /*
     pub struct Pt2 {
 
         state: f32,
         state1: f32,
-        dt: f32,
-        k: f32
     }
 
+    /*
     pub fn applyPt2(filter: Pt2, f_cut: f32, input: f32) -> (f32, Pt2) {
 
+        let k = computeGainPt2(filter, f_cut);
+
+        let state1 = filter.state1 + k * (input - filter.state1);
+        let state = filter.state + k * (state1 - filter.state);
+
+        (state, Pt2 {state: state, state1: state1, dt: filter.dt, k: k})
+    }
+
+    fn computeGainPt2(filter: Pt2, f_cut: f32) -> f32 {
+
         let order: f32 = 2.0;
-        let order_cutoff_correction = 1 / sqrtf(powf(2, 1.0f / order) - 1);
+        let order_cutoff_correction = 1 / sqrt(pow(2.0, 1.0 / order) - 1);
         let rc = 1.0 / (2.0 * order_cutoff_correction * std::f32::consts::PI * f_cut);
-        let k = filter.dt / (rc + filter.dt);
 
-        let state1 = filter.state1 + k * (input - let state1);
-        let state = let state + let k * (let state1 - let state);
-
-        (state, Pt2 {state: state, dt: filter.dt, k: k})
+        filter.dt / (rc + filter.dt)
     }*/
-}
+
+} // pub mod filters
