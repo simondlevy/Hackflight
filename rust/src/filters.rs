7358b93cd6d0124980b20346dd88ddccff62f73a
simondlevy <simon.d.levy@gmail.com>
2022-12-16 21:39:31 -0500

    =More new Rust PID controller work

--- rust/src/filters.rs
+++ rust/src/filters.rs
@@ -8,11 +8,16 @@
 
 pub mod filters {
 
-    struct Pt1 {
+    pub struct Pt1 {
 
         state: f32,
         dt: f32,
         k: f32
+    }
+
+    pub fn applyPt1(input: f32) -> (f32, Pt1) {
+
+        (0.0, Pt1 {state: 0.0, dt: 0.0, k: 0.0})
     }
 
     pub fn foo() -> f32 {
