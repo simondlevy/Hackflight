8e0040790e44ed59e0d9eeef83c0fbb4e6971027
simondlevy <simon.d.levy@gmail.com>
2022-12-17 03:13:45 -0500

    =More Rust PIDs

--- rust/src/utils.rs
+++ rust/src/utils.rs
@@ -8,12 +8,16 @@
 
 pub const DT: f32 = 100.0;
 
-pub fn constrain(value: f32, lo: f32, hi: f32) -> f32 {
-    if value  < lo {lo} else if value > hi {hi} else {value}
+pub fn constrain_f(val: f32, lo: f32, hi: f32) -> f32 {
+    if val < lo {lo} else if val > hi {hi} else {val }
 }
 
-pub fn constrain_abs(value : f32, limit : f32) -> f32 {
-    constrain(value, -limit, limit)
+pub fn constrain(val: f32, lo: f32, hi: f32) -> f32 {
+    if val  < lo {lo} else if val > hi {hi} else {val}
+}
+
+pub fn constrain_abs(val : f32, limit : f32) -> f32 {
+    constrain(val, -limit, limit)
 }
 
 pub fn deg2rad(x: f32) -> f32 {
