ece6661146d44afce88cd372bfdfd6f62b0df0a4
simondlevy <simon.d.levy@gmail.com>
2022-12-20 15:14:11 -0500

    =More Rust

--- newrust/src/pids/angle.rs
+++ newrust/src/pids/angle.rs
@@ -9,6 +9,44 @@
 use crate::Demands;
 use crate::VehicleState;
 
+const DTERM_LPF1_DYN_MIN_HZ: f32 = 75.0;
+const DTERM_LPF1_DYN_MAX_HZ: f32 = 150.0;
+const DTERM_LPF2_HZ: f32 = 150.0;
+const D_MIN_LOWPASS_HZ: f32 = 35.0;  
+const ITERM_RELAX_CUTOFF: f32 = 15.0;
+const D_MIN_RANGE_HZ: f32 = 85.0;  
+
+// minimum of 5ms between updates
+const DYN_LPF_THROTTLE_UPDATE_DELAY_US: u32 = 5000; 
+
+const DYN_LPF_THROTTLE_STEPS: f32 = 100.0;
+
+const ITERM_LIMIT: f32 = 400.0;
+
+const ITERM_WINDUP_POINT_PERCENT: f32 = 85.0;        
+
+// Full iterm suppression in setpoint mode at high-passed setpoint rate > 40deg/sec
+const ITERM_RELAX_SETPOINT_THRESHOLD: f32 = 40.0;
+
+const D_MIN: f32 = 30.0;
+const D_MIN_GAIN: f32 = 37.0;
+const D_MIN_ADVANCE: f32 = 20.0;
+
+const FEEDFORWARD_MAX_RATE_LIMIT: f32 = 900.0;
+
+const DYN_LPF_CURVE_EXPO: f32 = 5.0;
+
+// PT2 lowpass cutoff to smooth the boost effect
+const D_MIN_GAIN_FACTOR: f32  = 0.00008;
+const D_MIN_SETPOINT_GAIN_FACTOR: f32 = 0.00008;
+
+const RATE_ACCEL_LIMIT: f32 = 0.0;
+const YAW_RATE_ACCEL_LIMIT: f32 = 0.0;
+
+const OUTPUT_SCALING: f32 = 1000.0;
+const  LIMIT_CYCLIC: f32 = 500.0; 
+const  LIMIT_YAW: f32 = 400.0;
+
 #[derive(Debug,Clone)]
 pub struct Pid { 
     k_rate_p: f32,
@@ -30,17 +68,17 @@ pub fn make(
     k_rate_f: f32,
     k_level_p: f32) -> Pid {
 
-    Pid {
-        k_rate_p: k_rate_p,
-        k_rate_i: k_rate_i,
-        k_rate_d: k_rate_d,
-        k_rate_f: k_rate_f,
-        k_level_p: k_level_p,
-        //roll : CyclicAxis,
-        //pitch : CyclicAxis,
-        //yaw: Axis,
-        dyn_lpf_previous_quantized_throttle: 0, 
-    }
+        Pid {
+            k_rate_p: k_rate_p,
+            k_rate_i: k_rate_i,
+            k_rate_d: k_rate_d,
+            k_rate_f: k_rate_f,
+            k_level_p: k_level_p,
+            //roll : CyclicAxis,
+            //pitch : CyclicAxis,
+            //yaw: Axis,
+            dyn_lpf_previous_quantized_throttle: 0, 
+        }
 } 
 
 pub fn get_demands(
@@ -49,7 +87,7 @@ pub fn get_demands(
     vstate: &VehicleState,
     reset: &bool) -> Demands {
 
-    pid.dyn_lpf_previous_quantized_throttle = 0;
+        pid.dyn_lpf_previous_quantized_throttle = 0;
 
-    Demands {throttle: 0.0, roll:0.0, pitch: 0.0, yaw: 0.0}
+        Demands {throttle: 0.0, roll:0.0, pitch: 0.0, yaw: 0.0}
 }
