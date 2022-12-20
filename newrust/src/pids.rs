1321e2beebeaa4259af03c4d0c4b1d5730c10c4a
simondlevy <simon.d.levy@gmail.com>
2022-12-20 02:38:05 -0500

    =More Rust

--- newrust/src/pids.rs
+++ newrust/src/pids.rs
@@ -10,7 +10,7 @@ use crate::Demands;
 use crate::VehicleState;
 
 #[derive(Debug,Clone)]
-pub struct NewAnglePid { 
+pub struct AnglePid { 
     k_rate_p: f32,
     k_rate_i: f32,
     k_rate_d: f32,
@@ -20,13 +20,32 @@ pub struct NewAnglePid {
     //pitch : CyclicAxis,
     //yaw: Axis,
     dyn_lpf_previous_quantized_throttle: i32,  
-    foo: bool,
     //pterm_yaw_lpf: filters::Pt1
 }
 
-pub fn angpid_get_demands(angpid: &mut NewAnglePid) -> Demands {
+pub fn make_angle_pid(
+    k_rate_p: f32,
+    k_rate_i: f32,
+    k_rate_d: f32,
+    k_rate_f: f32,
+    k_level_p: f32) -> AnglePid {
 
-    angpid.foo = true;
+    AnglePid {
+        k_rate_p: k_rate_p,
+        k_rate_i: k_rate_i,
+        k_rate_d: k_rate_d,
+        k_rate_f: k_rate_f,
+        k_level_p: k_level_p,
+        //roll : CyclicAxis,
+        //pitch : CyclicAxis,
+        //yaw: Axis,
+        dyn_lpf_previous_quantized_throttle: 0, 
+    }
+} 
+
+pub fn angpid_get_demands(angpid: &mut AnglePid) -> Demands {
+
+    angpid.dyn_lpf_previous_quantized_throttle = 0;
 
     Demands {throttle: 0.0, roll:0.0, pitch: 0.0, yaw: 0.0}
 }
@@ -34,21 +53,8 @@ pub fn angpid_get_demands(angpid: &mut NewAnglePid) -> Demands {
 #[derive(Debug,Clone)]
 pub enum PidController {
 
-    New {
-        angpid: NewAnglePid
-    },
-
-    AnglePid { 
-        k_rate_p: f32,
-        k_rate_i: f32,
-        k_rate_d: f32,
-        k_rate_f: f32,
-        k_level_p: f32,
-        //roll : CyclicAxis,
-        //pitch : CyclicAxis,
-        //yaw: Axis,
-        dyn_lpf_previous_quantized_throttle: i32,  
-        //pterm_yaw_lpf: filters::Pt1
+    Angle {
+        angpid: AnglePid
     },
 
     AltHoldPid { 
@@ -67,13 +73,14 @@ pub fn make_angle(
     k_rate_f: f32,
     k_level_p: f32 ) -> PidController {
 
-    PidController::AnglePid {
-        k_rate_p: k_rate_p,
-        k_rate_i: k_rate_i,
-        k_rate_d: k_rate_d,
-        k_rate_f: k_rate_f,
-        k_level_p: k_level_p,
-        dyn_lpf_previous_quantized_throttle: 0  
+    PidController::Angle {
+        angpid: make_angle_pid(
+                    k_rate_p,
+                    k_rate_i,
+                    k_rate_d,
+                    k_rate_f,
+                    k_level_p,
+                    )
     }
 }
 
@@ -92,27 +99,12 @@ pub fn get_demands(t: &mut PidController, vstate: VehicleState) -> Demands {
 
     match *t {
 
-        PidController::New {
+        PidController::Angle {
             ref mut angpid
         } => {
 
             angpid_get_demands(angpid)
         }
-
-
-        PidController::AnglePid {
-            k_rate_p: _,
-            k_rate_i: _,
-            k_rate_d: _,
-            k_rate_f: _,
-            k_level_p: _,
-            ref mut dyn_lpf_previous_quantized_throttle
-        } => {
-
-            *dyn_lpf_previous_quantized_throttle = 0;
-
-            Demands {throttle: 0.0, roll:0.0, pitch: 0.0, yaw: 0.0}
-        },
 
         PidController::AltHoldPid {
             k_p: _,
