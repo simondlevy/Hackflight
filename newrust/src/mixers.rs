6df669094d4c12cc801da4cbfec35f1e99a257f4
simondlevy <simon.d.levy@gmail.com>
2022-12-20 02:15:36 -0500

    =More Rust

--- newrust/src/mixers.rs
+++ newrust/src/mixers.rs
@@ -4,41 +4,38 @@
    Copyright (C) 2022 Simon D. Levy
 
    MIT License
-*/
+ */
 
-pub mod mixers {
+use crate::Demands;
+use crate::Motors;
 
-    use crate::Demands;
-    use crate::Motors;
+/*
+   Mixer values for quad-X frames using Betaflight motor layout:
 
-    /*
-       Mixer values for quad-X frames using Betaflight motor layout:
+   4cw   2ccw
+   \ /
+   ^
+   / \
+   3ccw  1cw
 
-       4cw   2ccw
-       \ /
-       ^
-       / \
-       3ccw  1cw
+   Copyright (C) 2022 Simon D. Levy
 
-       Copyright (C) 2022 Simon D. Levy
+   MIT License
+ */
+pub fn run_quad_xbf(demands:Demands) -> Motors {
 
-       MIT License
-     */
-    pub fn run_quad_xbf(demands:Demands) -> Motors {
+    Motors {
 
-        Motors {
+        // right rear
+        m1: demands.throttle - demands.roll + demands.pitch + demands.yaw, 
 
-            // right rear
-            m1: demands.throttle - demands.roll + demands.pitch + demands.yaw, 
+        // right front
+        m2: demands.throttle - demands.roll - demands.pitch - demands.yaw, 
 
-                // right front
-                m2: demands.throttle - demands.roll - demands.pitch - demands.yaw, 
+        // left rear
+        m3: demands.throttle + demands.roll + demands.pitch - demands.yaw, 
 
-                // left rear
-                m3: demands.throttle + demands.roll + demands.pitch - demands.yaw, 
-
-                // left front
-                m4: demands.throttle + demands.roll - demands.pitch + demands.yaw  
-        }
+        // left front
+        m4: demands.throttle + demands.roll - demands.pitch + demands.yaw  
     }
 }
