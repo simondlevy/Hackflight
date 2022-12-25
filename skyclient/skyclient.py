96ed7d1a27bdf43a9422bba901888201d34f39bf
simondlevy <simon.d.levy@gmail.com>
2022-12-25 17:45:59 -0500

    More skyclient work

--- skyclient/skyclient.py
+++ skyclient/skyclient.py
@@ -5,29 +5,39 @@ from serial import Serial
 
 from mspparser import MspParser
 
+
 class SkyParser(MspParser):
+
+    def __init__(self, port):
+
+        MspParser.__init__(self)
+        self.port = port
+        self.msg = self.serialize_PAA3905_Request()
+        self._send()
+
+    def _send(self):
+
+        self.port.write(self.msg)
 
     def handle_PAA3905(self, x, y):
 
         print(x, y)
+        self._send()
+
 
 cmdparser = ArgumentParser()
 cmdparser.add_argument('-p', '--port', default='/dev/ttyUSB0', help='COM port')
 args = cmdparser.parse_args()
 
-msp = SkyParser()
-
-msg = SkyParser.serialize_PAA3905_Request()
-
 port = Serial(args.port, 115200)
 
-port.write(msg)
+skyparser = SkyParser(port)
 
 while True:
 
     try:
 
-        msp.parse(port.read(1))
+        skyparser.parse(port.read(1))
 
     except KeyboardInterrupt:
 
