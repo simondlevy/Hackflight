86b5e88434596a5912d42cd85f2cfab58753e484
simondlevy <simon.d.levy@gmail.com>
2022-12-25 15:44:19 -0500

    More SkyMock work

--- examples/SkyMock/SkyMock.ino
+++ examples/SkyMock/SkyMock.ino
@@ -9,9 +9,23 @@ void setup(void)
 void loop(void)
 {
     static MspParser _parser;
+    static MspSerializer _serializer;
 
     while (Serial.available()) {
 
         auto messageType = _parser.parse(Serial.read());
+
+        int16_t x = -1;
+        int16_t y = +1;
+
+        switch (messageType) {
+
+            case 121: // PAA3905
+                _serializer.prepareToSerializeShorts(messageType, 2);
+                _serializer.serializeShort(x);
+                _serializer.serializeShort(y);
+                _serializer.completeSerialize();
+                break;
+        }
     }
 }
