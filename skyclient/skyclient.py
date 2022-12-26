9d06a3fdd6eb503e89e39a6dbf7bcf3511a8f795
simondlevy <simon.d.levy@gmail.com>
2022-12-25 23:47:18 -0500

    More VL53L5 support

--- skyclient/skyclient.py
+++ skyclient/skyclient.py
@@ -12,22 +12,29 @@ class SkyParser(MspParser):
 
         MspParser.__init__(self)
         self.port = port
+
         self.msgVL53L5 = self.serialize_VL53L5_Request()
+        self._sendVL53L5()
+
         self.msgPAA3905 = self.serialize_PAA3905_Request()
         self._sendPAA3905()
-
-    def _sendPAA3905(self):
-
-        self.port.write(self.msgPAA3905)
 
     def handle_VL53L5(self, p11, p12, p13, p14, p21, p22, p23, p24, p31, p32, p33, p34, p41, p42, p43, p44):
 
         pass
 
+    def _sendVL53L5(self):
+
+        self.port.write(self.msgVL53L5)
+
     def handle_PAA3905(self, x, y):
 
         print(x, y)
         self._sendPAA3905()
+
+    def _sendPAA3905(self):
+
+        self.port.write(self.msgPAA3905)
 
 
 cmdparser = ArgumentParser()
