a88e0d584666bb06ee94bc8d17b459679ad6daa1
simondlevy <simon.d.levy@gmail.com>
2022-12-31 02:02:16 -0500

    More ArduinoMsp

--- src/board/stm32/ladybug.h
+++ src/board/stm32/ladybug.h
@@ -16,6 +16,7 @@
 
 #pragma once
 
+#include "msp/arduino.h"
 #include "board/stm32.h"
 #include "esc/brushed.h"
 #include "imu/real/usfs.h"
@@ -24,6 +25,8 @@
 class LadybugBoard : public Stm32Board {
 
     private:
+
+        ArduinoMsp msp;
 
         vector<uint8_t> motorPins = {0x0D, 0x10, 0x03, 0x0B};
 
@@ -36,7 +39,7 @@ class LadybugBoard : public Stm32Board {
         static const uint8_t LED_PIN = 0x12;
 
         LadybugBoard(Receiver & rx, vector<PidController *> & pids, Mixer & mixer)
-            : Stm32Board(rx, imu, pids, mixer, esc, -LED_PIN)
+            : Stm32Board(msp, rx, imu, pids, mixer, esc, -LED_PIN)
         {
         }
 
