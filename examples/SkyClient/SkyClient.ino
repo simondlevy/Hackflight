ec3e9777596167b5c8e5033c233a33e8eec7da5f
simondlevy <simon.d.levy@gmail.com>
2023-01-01 00:19:23 -0500

    SkyClient working with real IMU

--- examples/SkyClient/SkyClient.ino
+++ examples/SkyClient/SkyClient.ino
@@ -22,7 +22,7 @@
 #include <board/stm32/stm32f4/stm32f405.h>
 #include <core/mixers/fixedpitch/quadxbf.h>
 #include <esc/mock.h>
-#include <imu/mock.h>
+#include <imu/real/softquat/mpu6x00/arduino.h>
 #include <receiver/mock.h>
 
 #include <vector>
@@ -39,7 +39,7 @@ static vector<uint8_t> MOTOR_PINS = {PB_0, PB_1, PA_3, PA_2};
 
 static const uint8_t LED_PIN = PB5;
 
-static SPIClass _spi(MOSI_PIN, MISO_PIN, SCLK_PIN);
+static SPIClass spi(MOSI_PIN, MISO_PIN, SCLK_PIN);
 
 static AnglePidController _anglePid(
         1.441305,     // Rate Kp
@@ -48,32 +48,45 @@ static AnglePidController _anglePid(
         0.0165048,    // Rate Kf
         0.0); // 3.0; // Level Kp
 
-static Mixer _mixer = QuadXbfMixer::make();
+static Mixer mixer = QuadXbfMixer::make();
 
 static ArduinoMsp msp;
 
-static MockImu imu;
 static MockReceiver rx;
+
+static ArduinoMpu6x00 imu(spi, RealImu::rotate270, CS_PIN);
+
+static vector<PidController *> pids = {&_anglePid};
+
 static MockEsc esc;
 
-static Stm32F405Board board(msp, rx, imu, _pids, _mixer, esc, LED_PIN);
+static Stm32F405Board board(msp, rx, imu, pids, mixer, esc, LED_PIN);
 
+static void handleImuInterrupt(void)
+{
+    imu.handleInterrupt();
+}
+
+// Incoming data from Skyranger
 void serialEvent4(void)
 {
     while (Serial4.available()) {
-        _board.parseSkyRanger(Serial4.read());
+        board.parseSkyRanger(Serial4.read());
     }
 }
 
 void setup(void)
 {
+    pinMode(EXTI_PIN, INPUT);
+    attachInterrupt(EXTI_PIN, handleImuInterrupt, RISING);  
+
     // Skyranger connection
     Serial4.begin(115200);
 
-    _board.begin();
+    board.begin();
 }
 
 void loop(void)
 {
-    _board.step();
+    board.step();
 }
