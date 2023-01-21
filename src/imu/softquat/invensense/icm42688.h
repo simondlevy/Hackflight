70f266364a1753cdf06b942d29a87fd09987c9bb
simondlevy <simon.d.levy@gmail.com>
2023-01-20 20:02:13 -0500

    Removed SPI calls from ICM42688

--- src/imu/softquat/invensense/icm42688.h
+++ src/imu/softquat/invensense/icm42688.h
@@ -18,8 +18,6 @@
    Hackflight. If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include <SPI.h>
-
 #include <stdint.h>
 
 #include "imu/softquat/invensense.h"
@@ -103,11 +101,6 @@ class Icm42688 : public InvenSenseImu {
                 16;
         }
 
-        static float scale(const uint16_t n)
-        {
-            return n / 32768.0;
-        }
-
         // 1 MHz max SPI frequency for initialisation
         static const uint32_t MAX_SPI_INIT_CLK_HZ = 1000000;
 
@@ -121,76 +114,58 @@ class Icm42688 : public InvenSenseImu {
 
         virtual void begin(uint32_t clockSpeed) override
         {
-            InvenSenseImu::begin(clockSpeed);
+            InvenSenseImu::begin(clockSpeed, MAX_SPI_CLOCK_RATE, MAX_SPI_CLOCK_RATE);
+        }
 
-            m_spi.setClockDivider(calculateSpiDivisor(clockSpeed, MAX_SPI_CLOCK_RATE));
+        virtual void getRegisterSettings(
+                std::vector<registerSetting_t> & settings) override
+        {
+            settings.push_back({REG_BANK_SEL, 0});
 
-            writeRegister(REG_BANK_SEL, 0);
+            settings.push_back({UB0_REG_DEVICE_CONFIG, 0x01});
 
-            writeRegister(UB0_REG_DEVICE_CONFIG, 0x01);
-
-            delay(15);
-
-            writeRegister(REG_PWR_MGMT0,
+            settings.push_back({REG_PWR_MGMT0,
                     PWR_MGMT0_TEMP_DISABLE_OFF |
                     PWR_MGMT0_ACCEL_MODE_LN |
-                    PWR_MGMT0_GYRO_MODE_LN);
-            delay(15);
+                    PWR_MGMT0_GYRO_MODE_LN});
 
-            writeRegister(REG_GYRO_CONFIG0, (3 - GYRO_2000DPS) << 5 | (m_odr & 0x0F));
-            delay(15);
+            settings.push_back({REG_GYRO_CONFIG0,
+                    (3 - GYRO_2000DPS) << 5 | (m_odr & 0x0F)});
 
-            writeRegister(REG_ACCEL_CONFIG0, (3 - ACCEL_16G) << 5 | (m_odr & 0x0F));
-            delay(15);
+            settings.push_back({REG_ACCEL_CONFIG0,
+                    (3 - ACCEL_16G) << 5 | (m_odr & 0x0F)});
 
-            // Configure gyro Anti-Alias Filter (see section 5.3 "ANTI-ALIAS FILTER")
-            writeRegister(REG_GYRO_CONFIG_STATIC3, m_antiAliasDelta);
-            delay(15);
+            settings.push_back({REG_GYRO_CONFIG_STATIC3, m_antiAliasDelta});
 
             uint16_t deltSqr = m_antiAliasDelta * m_antiAliasDelta;
-            writeRegister(REG_GYRO_CONFIG_STATIC4, deltSqr & 0xFF);
-            delay(15);
+            settings.push_back({REG_GYRO_CONFIG_STATIC4, deltSqr & 0xFF});
 
-            writeRegister(REG_GYRO_CONFIG_STATIC5,
-                    (deltSqr >> 8) | (m_antiAliasBitshift << 4));
-            delay(15);
+            settings.push_back({REG_GYRO_CONFIG_STATIC5,
+                    (deltSqr >> 8) | (m_antiAliasBitshift << 4)});
 
-            // Configure acc Anti-Alias Filter for 1kHz sample rate (see tasks.c)
-            writeRegister(REG_ACCEL_CONFIG_STATIC2, m_antiAliasDelta << 1);
-            delay(15);
+            settings.push_back({REG_ACCEL_CONFIG_STATIC2, m_antiAliasDelta << 1});
 
-            writeRegister(REG_ACCEL_CONFIG_STATIC3, deltSqr & 0xFF);
-            delay(15);
+            settings.push_back({REG_ACCEL_CONFIG_STATIC3, deltSqr & 0xFF});
 
-            writeRegister(REG_ACCEL_CONFIG_STATIC4,
-                    (deltSqr >> 8) | (m_antiAliasBitshift << 4));
-            delay(15);
+            settings.push_back({REG_ACCEL_CONFIG_STATIC4,
+                    (deltSqr >> 8) | (m_antiAliasBitshift << 4)});
 
-            // Configure gyro and acc UI Filters
-            writeRegister(REG_GYRO_ACCEL_CONFIG0,
-                    ACCEL_UI_FILT_BW_LOW_LATENCY | GYRO_UI_FILT_BW_LOW_LATENCY);
-            delay(15);
+            settings.push_back({REG_GYRO_ACCEL_CONFIG0,
+                    ACCEL_UI_FILT_BW_LOW_LATENCY | GYRO_UI_FILT_BW_LOW_LATENCY});
 
-            writeRegister(REG_INT_CONFIG,
-                    INT1_MODE_PULSED | INT1_DRIVE_CIRCUIT_PP | INT1_POLARITY_ACTIVE_HIGH);
-            delay(15);
+            settings.push_back({REG_INT_CONFIG,
+                    INT1_MODE_PULSED | INT1_DRIVE_CIRCUIT_PP | INT1_POLARITY_ACTIVE_HIGH});
 
-            writeRegister(REG_INT_CONFIG0, UI_DRDY_INT_CLEAR_ON_SBR);
-            delay(15);
+            settings.push_back({REG_INT_CONFIG0, UI_DRDY_INT_CLEAR_ON_SBR});
 
-            writeRegister(REG_INT_SOURCE0, UI_DRDY_INT1_EN_ENABLED);
-            delay(15);
+            settings.push_back({REG_INT_SOURCE0, UI_DRDY_INT1_EN_ENABLED});
 
             // Datasheet says: "User should change setting to 0 from default
             // setting of 1, for proper INT1 and INT2 pin operation"
             uint8_t intConfig1Value = readRegister(REG_INT_CONFIG1);
             intConfig1Value &= ~(1 << INT_ASYNC_RESET_BIT);
-
             intConfig1Value |= (INT_TPULSE_DURATION_8 | INT_TDEASSERT_DISABLED);
-            writeRegister(REG_INT_CONFIG1, intConfig1Value);
-            delay(15);
-
-            m_spi.setClockDivider(calculateSpiDivisor(clockSpeed, MAX_SPI_CLOCK_RATE));
+            settings.push_back({REG_INT_CONFIG1, intConfig1Value});
         }
 
         virtual int16_t readRawGyro(uint8_t k) override
