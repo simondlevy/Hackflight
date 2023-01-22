1a66174e5a36dcc42af47e23c4b9a17591bd3afa
simondlevy <simon.d.levy@gmail.com>
2023-01-22 16:18:48 -0500

    Restored FlywooF411Dsmx sketch

---
+++ examples/incomplete/FlywooF411Dsmx/FlywooF411Dsmx.ino
@@ -0,0 +1,69 @@
+/*
+   Copyright (c) 2022 Simon D. Levy
+
+   This file is part of Hackflight.
+
+   Hackflight is free software: you can redistribute it and/or modify it under
+   the terms of the GNU General Public License as published by the Free
+   Software Foundation, either version 3 of the License, or (at your option)
+   any later version.
+
+   Hackflight is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+   more details.
+
+   You should have received a copy of the GNU General Public License along with
+   Hackflight. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <hackflight.h>
+#include <board/stm32/f/4/stm32f411.h>
+#include <core/mixers/fixedpitch/quadxbf.h>
+#include <receiver/dsmx.h>
+#include <imu/softquat/invensense/mpu6x00.h>
+#include <esc/mock.h>
+
+#include <vector>
+
+static const uint8_t CS_PIN   = PA4;
+static const uint8_t EXTI_PIN = PB2;
+static const uint8_t LED_PIN  = PC13;
+
+static Mixer mixer = QuadXbfMixer::make();
+
+static DsmxReceiver rx;
+
+static Mpu6x00 imu(CS_PIN, Imu::rotate180);
+
+static std::vector<PidController *> pids = {};
+
+static MockEsc esc;
+
+static Stm32F411Board board(rx, imu, pids, mixer, esc, LED_PIN);
+
+// IMU interrupt
+static void handleImuInterrupt(void)
+{
+    board.handleImuInterrupt();
+}
+
+// Receiver interrupt
+void serialEvent2(void)
+{
+    Board::handleReceiverSerialEvent(rx, Serial2);
+}
+
+void setup(void)
+{
+    Board::setInterrupt(EXTI_PIN, handleImuInterrupt, RISING);  
+
+    Serial2.begin(115200);
+
+    board.begin();
+}
+
+void loop(void)
+{
+    board.step();
+}
