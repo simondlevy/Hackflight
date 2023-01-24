2a0f689bbbee3d6db5633a7c384194b8e879252a
simondlevy <simon.d.levy@gmail.com>
2023-01-23 19:19:04 -0500

    Working on ICM42688 interrupt reset

--- src/board/stm32/stm32f.h
+++ src/board/stm32/stm32f.h
@@ -139,8 +139,21 @@ class Stm32FBoard : public Stm32Board {
 
                 for (auto r : registerSettings) {
                     writeRegister(r.address, r.value);
-                    delay(100);
+                    delay(100); // arbitrary; should be long enough for any delays
                 }
+
+                // ICM42688 requires register reset for interrupt
+                const uint8_t ICM426XX_RA_INT_CONFIG1 = 0x64;
+                const uint8_t ICM426XX_INT_ASYNC_RESET_BIT = 4;
+                const uint8_t ICM426XX_INT_TDEASSERT_DISABLE_BIT  = 5;
+                const uint8_t ICM426XX_INT_TPULSE_DURATION_BIT = 6;
+                const uint8_t ICM426XX_INT_TPULSE_DURATION_8 = (1 << ICM426XX_INT_TPULSE_DURATION_BIT);
+                const uint8_t ICM426XX_INT_TDEASSERT_DISABLED  = (1 << ICM426XX_INT_TDEASSERT_DISABLE_BIT);
+                uint8_t intConfig1Value = readRegister(ICM426XX_RA_INT_CONFIG1);
+                intConfig1Value &= ~(1 << ICM426XX_INT_ASYNC_RESET_BIT);
+                intConfig1Value |= (ICM426XX_INT_TPULSE_DURATION_8 | ICM426XX_INT_TDEASSERT_DISABLED);
+                writeRegister(ICM426XX_RA_INT_CONFIG1, intConfig1Value);
+                delay(100);
 
                 m_spi.setClockDivider(
                         InvenSenseImu::calculateSpiDivisor(clockSpeed, m_maxSpiFreq));
