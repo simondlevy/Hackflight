01cea4a313d83c9d689db7584156b63a65d5599c
simondlevy <simon.d.levy@gmail.com>
2023-01-28 00:27:01 -0500

    =Begin DSMRX work

--- src/board/stm32/f/4/stm32f411.h
+++ src/board/stm32/f/4/stm32f411.h
@@ -36,13 +36,12 @@ class Stm32F411Board : public Stm32F4Board {
     public:
 
         Stm32F411Board(
-                Receiver & receiver,
                 InvenSenseImu & imu,
                 std::vector<PidController *> & pids,
                 Mixer & mixer,
                 Esc & esc,
                 const uint8_t ledPin)
-            : Stm32F4Board(1, receiver, imu, pids, mixer, esc, ledPin)
+            : Stm32F4Board(1, imu, pids, mixer, esc, ledPin)
         {
         }
 };
