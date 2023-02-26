2e8ab9bbcb14936f321553cd4d770c49972313fa
simondlevy <simon.d.levy@gmail.com>
2023-02-26 13:56:47 -0500

    Fixed license headers in Rust code

--- rust/src/utils.rs
+++ rust/src/utils.rs
@@ -1,9 +1,19 @@
 /*
-   Utility functions
+   Copyright (c) 2022 Simon D. Levy
 
-   Copyright (C) 2022 Simon D. Levy
+   This file is part of Hackflight.
 
-   MIT License
+   Hackflight is free software: you can redistribute it and/or modify it under the
+   terms of the GNU General Public License as published by the Free Software
+   Foundation, either version 3 of the License, or (at your option) any later
+   version.
+
+   Hackflight is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+   PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along with
+   Hackflight. If not, see <https://www.gnu.org/licenses/>.
  */
 
 pub fn constrain_f(val: f32, lo: f32, hi: f32) -> f32 {
