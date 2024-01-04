8d988bfeb7f9ede21ff11839ba7f11ece92f266b
simondlevy <simon.d.levy@gmail.com>
2024-01-04 16:13:06 -0500

    Added "Make unbrick"

--- examples/Crazyflie21/Crazyflie21.ino
+++ examples/Crazyflie21/Crazyflie21.ino
@@ -1,9 +1,16 @@
+static const uint8_t LED_RED_L_PIN = 8;
+
 void setup(void)
 {
     Serial.begin(115200);
+
+    pinMode(LED_RED_L_PIN, OUTPUT);
 }
 
 void loop(void)
 {
-    Serial.println(micros());
+    digitalWrite(LED_RED_L_PIN, HIGH);
+    delay(500);
+    digitalWrite(LED_RED_L_PIN, LOW);
+    delay(500);
 }
