6228933315fef3e83e40e4d1c726358054145123
simondlevy <simon.d.levy@gmail.com>
2024-01-04 17:02:13 -0500

    new file:   free_rtos.h

---
+++ src/free_rtos.h
@@ -0,0 +1,39 @@
+/**
+ *
+ * Copyright (C) 2011-2022 Bitcraze AB, 2024 Simon D. Levy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, in version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#if defined(TEENSYDUINO)
+#include <FreeRTOS_TEENSY4.h>
+#else
+#include <FreeRTOS.h>
+#endif
+
+#define M2T(X) ((unsigned int)(X))
+#define F2T(X) ((unsigned int)((configTICK_RATE_HZ/(X))))
+#define T2M(X) ((unsigned int)(X))
+
+#define TASK_LED_ID_NBR         1
+#define TASK_RADIO_ID_NBR       2
+#define TASK_CORE_ID_NBR        3
+#define TASK_ADC_ID_NBR         4
+#define TASK_PM_ID_NBR          5
+#define TASK_PROXIMITY_ID_NBR   6
+
+
+
+
