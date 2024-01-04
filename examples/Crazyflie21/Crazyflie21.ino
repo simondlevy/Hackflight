512863f1ffa5db2e7d2eef4d33bb5c67fa948d11
simondlevy <simon.d.levy@gmail.com>
2024-01-04 16:18:23 -0500

    "Make upload" working

--- examples/Crazyflie21/Crazyflie21.ino
+++ examples/Crazyflie21/Crazyflie21.ino
@@ -1,5 +1,23 @@
 static const uint8_t LED_RED_L_PIN = 8;
 
+static void reboot(void)
+{
+    __enable_irq();
+    HAL_RCC_DeInit();
+    HAL_DeInit();
+    SysTick->CTRL = SysTick->LOAD = SysTick->VAL = 0;
+    __HAL_SYSCFG_REMAPMEMORY_SYSTEMFLASH();
+
+    const uint32_t p = (*((uint32_t *) 0x1FFF0000));
+    __set_MSP( p );
+
+    void (*SysMemBootJump)(void);
+    SysMemBootJump = (void (*)(void)) (*((uint32_t *) 0x1FFF0004));
+    SysMemBootJump();
+
+    NVIC_SystemReset();
+}
+
 void setup(void)
 {
     Serial.begin(115200);
@@ -9,8 +27,16 @@ void setup(void)
 
 void loop(void)
 {
-    digitalWrite(LED_RED_L_PIN, HIGH);
-    delay(500);
-    digitalWrite(LED_RED_L_PIN, LOW);
-    delay(500);
+    /*
+       digitalWrite(LED_RED_L_PIN, HIGH);
+       delay(500);
+       digitalWrite(LED_RED_L_PIN, LOW);
+       delay(500);*/
+
+    while (Serial.available()) {
+
+        if (Serial.read() == 'R') {
+            reboot();
+        }
+    }
 }
