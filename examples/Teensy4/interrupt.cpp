f306f7468d91fa0c4005b0909e681d1575114c17
simondlevy <simon.d.levy@gmail.com>
2024-01-11 22:14:45 -0500

    modified:   interrupt.cpp

--- examples/Teensy4/interrupt.cpp
+++ examples/Teensy4/interrupt.cpp
@@ -1,6 +1,12 @@
 #include <stdbool.h>
 
+#include <imxrt.h>
+
+// https://web.eece.maine.edu/~zhu/book/Core/core_cm3_constant.s
+#define SCB_ICSR_VECTACTIVE_Pos  0                                   
+#define SCB_ICSR_VECTACTIVE_Msk  (0x1FF << SCB_ICSR_VECTACTIVE_Pos)
+
 bool hal_isInInterrupt(void)
 {
-    return false;
+    return (SCB_ICSR  & SCB_ICSR_VECTACTIVE_Msk) != 0;
 }
