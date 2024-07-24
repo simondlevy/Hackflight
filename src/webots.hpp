007f14633707dd9b7132e32cbffbe74309e3a7e1
simondlevy <simon.d.levy@gmail.com>
2024-07-23 20:30:52 -0400

    Added headers

--- src/webots.hpp
+++ src/webots.hpp
@@ -1,20 +1,21 @@
-/**
- * Webots support
- *
- * Copyright (C) 2024 Simon D. Levy
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, in version 3.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
+/*
+  Webots-based flight simulator support for Hackflight
+ 
+  Copyright (C) 2024 Simon D. Levy
+ 
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, in version 3.
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http:--www.gnu.org/licenses/>.
+*/
+
 
 #pragma once
 
@@ -416,5 +417,4 @@ class Simulator {
             yprev = y;
             zprev = state.z;
         }
-
 };
