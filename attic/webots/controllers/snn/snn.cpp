dfad1f23bd9f672cf131b020d3b4cc91224f2103
simondlevy <simon.d.levy@gmail.com>
2024-08-08 23:51:11 -0400

    Renamed takeoff => snn

---
+++ attic/webots/controllers/snn/snn.cpp
@@ -0,0 +1,83 @@
+/*
+   C++ flight simulator spiking-neural net controller for Hackflight
+
+   Copyright (C) 2024 Simon D. Levy
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, in version 3.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program. If not, see <http:--www.gnu.org/licenses/>.
+ */
+
+#include <webots/motor.h>
+#include <webots/robot.h>
+#include <webots/gps.h>
+
+#include <webots.hpp>
+#include <snn.hpp>
+#include <utils.hpp>
+
+static const float THRUST_TAKEOFF = 56;
+
+static const float THRUST_BASE = 55.385;
+
+static const float K_CLIMBRATE = 25;
+
+int main(int argc, char ** argv)
+{
+    (void)argc;
+    (void)argv;
+
+    hf::Simulator sim = {};
+
+    sim.init();
+
+    SNN * snn = NULL;
+
+    try {
+        snn = new SNN("networks/hover_risp.txt", "risp");
+    } catch (const SRE &e) {
+        fprintf(stderr, "Couldn't set up SNN:\n%s\n", e.what());
+        exit(1);
+    }
+
+    while (true) {
+
+        hf::state_t state = {};
+        hf::demands_t stickDemands = {};
+        bool hitTakeoffButton = false;
+        bool completedTakeoff = false;
+
+        if (!sim.step(stickDemands, state, hitTakeoffButton, completedTakeoff)) {
+            break;
+        }
+
+        double thrust = 0;
+
+        if (completedTakeoff) {
+            vector<double> o = {stickDemands.thrust, state.dz};
+            vector <double> a;
+            snn->getActions(o, a);
+            printf("%+f %+f => %+f\n", o[0], o[1], a[0]);
+            thrust = THRUST_BASE + a[0];
+
+        }
+
+        else if (hitTakeoffButton) {
+            thrust = THRUST_TAKEOFF;
+        }
+
+        sim.setMotors(thrust, thrust, thrust, thrust);
+    }
+
+    wb_robot_cleanup();
+
+    return 0;
+}
--- webots/controllers/snn/snn.cpp
+++ webots/controllers/snn/snn.cpp
@@ -1,80 +1,91 @@
 /*
-   C++ flight simulator spiking-neural net controller for Hackflight
-
-   Copyright (C) 2024 Simon D. Levy
-
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, in version 3.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program. If not, see <http:--www.gnu.org/licenses/>.
- */
+  C++ flight simulator takeoff example for Hackflight
+ 
+  Copyright (C) 2024 Simon D. Levy
+ 
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, in version 3.
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http:--www.gnu.org/licenses/>.
+*/
 
 #include <webots/motor.h>
 #include <webots/robot.h>
 #include <webots/gps.h>
 
-#include <webots.hpp>
 #include <snn.hpp>
-#include <utils.hpp>
 
-static const float THRUST_TAKEOFF = 56;
+// Time constant for computing climb rate
+static const float DT = 0.01;
 
-static const float THRUST_BASE = 55.385;
+// Motors
+static WbDeviceTag _motor1;
+static WbDeviceTag _motor2;
+static WbDeviceTag _motor3;
+static WbDeviceTag _motor4;
 
-static const float K_CLIMBRATE = 25;
+static WbDeviceTag _makeMotor(const char * name, const float direction)
+{
+    auto motor = wb_robot_get_device(name);
+
+    wb_motor_set_position(motor, INFINITY);
+    wb_motor_set_velocity(motor, direction);
+
+    return motor;
+}
 
 int main(int argc, char ** argv)
 {
     (void)argc;
     (void)argv;
 
-    hf::Simulator sim = {};
-
-    sim.init();
-
     SNN * snn = NULL;
 
     try {
-        snn = new SNN("networks/hover_risp.txt", "risp");
+        snn = new SNN("networks/takeoff_risp.txt", "risp");
     } catch (const SRE &e) {
         fprintf(stderr, "Couldn't set up SNN:\n%s\n", e.what());
         exit(1);
     }
 
-    while (true) {
+    wb_robot_init();
 
-        hf::state_t state = {};
-        hf::demands_t stickDemands = {};
-        bool hitTakeoffButton = false;
-        bool completedTakeoff = false;
+    _motor1 = _makeMotor("motor1", +1);
+    _motor2 = _makeMotor("motor2", -1);
+    _motor3 = _makeMotor("motor3", +1);
+    _motor4 = _makeMotor("motor4", -1);
 
-        if (!sim.step(stickDemands, state, hitTakeoffButton, completedTakeoff)) {
-            break;
-        }
+    const int timestep = (int)wb_robot_get_basic_time_step();
 
-        double thrust = 0;
+    auto gps = wb_robot_get_device("gps");
+    wb_gps_enable(gps, timestep);
 
-        if (completedTakeoff) {
-            vector<double> o = {stickDemands.thrust, state.dz};
-            vector <double> a;
-            snn->getActions(o, a);
-            printf("%+f %+f => %+f\n", o[0], o[1], a[0]);
-            thrust = THRUST_BASE + a[0];
+    float zprev = 0;
 
-        }
+    while (wb_robot_step(timestep) != -1) {
 
-        else if (hitTakeoffButton) {
-            thrust = THRUST_TAKEOFF;
-        }
+        // Get current altitude and climb rate observations
+        const auto z = wb_gps_get_values(gps)[2];
+        const auto dz = (z - zprev) / DT;
+        zprev = z;
 
-        sim.setMotors(thrust, thrust, thrust, thrust);
+        vector<double> o = {z, dz};
+        vector <double> a;
+        snn->getActions(o, a);
+        const auto motor = a[0];
+
+        // Run the motors
+        wb_motor_set_velocity(_motor1, +motor);
+        wb_motor_set_velocity(_motor2, -motor);
+        wb_motor_set_velocity(_motor3, +motor);
+        wb_motor_set_velocity(_motor4, -motor);
     }
 
     wb_robot_cleanup();
