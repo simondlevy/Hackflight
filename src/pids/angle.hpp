28c351263ac3d07db34e3e89b2ee6d043c4e088a
simondlevy <simon.d.levy@gmail.com>
2024-08-19 17:41:44 -0400

    =Cleanup

--- src/pids/angle.hpp
+++ src/pids/angle.hpp
@@ -27,6 +27,25 @@ namespace hf {
 
     class AnglePid {
 
+        //DESCRIPTION: Computes control commands based on state error (angle)
+        /*
+         * Basic PID control to stablize on angle setpoint based on desired
+         * states roll_des, pitch_des, and yaw_des computed in getDesState().
+         * Error is simply the desired state minus the actual state (ex.
+         * roll_des - roll_IMU). Two safety features are implimented here
+         * regarding the I terms. The I terms are saturated within specified
+         * limits on startup to prevent excessive buildup. This can be seen by
+         * holding the vehicle at an angle and seeing the motors ramp up on one
+         * side until they've maxed out throttle...saturating I to a specified
+         * limit fixes this. The second feature defaults the I terms to 0 if
+         * the throttle is at the minimum setting. This means the motors will
+         * not start spooling up on the ground, and the I terms will always
+         * start from 0 on takeoff. This function updates the variables
+         * roll_PID, pitch_PID, and yaw_PID which can be thought of as 1-D
+         * stablized signals. They are mixed to the configuration of the
+         * vehicle in the mixer.
+         */
+
         private:
 
             static constexpr float I_LIMIT = 25.0;     
@@ -41,18 +60,17 @@ namespace hf {
 
             static constexpr float SCALE = 0.01;
 
-            class PidController {
+            static const uint32_t THROTTLE_DOWN = 1060;
+
+            class PitchRollPidController {
 
                 public:
 
                     void run(
-                            const float kp,
-                            const float ki,
-                            const float kd,
                             const float dt,
                             const float des,
                             const float imu,
-                            const uint32_t channel_1_pwm,
+                            const bool reset,
                             const float gyro,
                             float & pid)
                     {
@@ -60,7 +78,7 @@ namespace hf {
 
                         _integral = _integral_prev + error * dt;
 
-                        if (channel_1_pwm < 1060) {
+                        if (reset) {
                             _integral = 0;
                         }
 
@@ -68,7 +86,8 @@ namespace hf {
 
                         const auto derivative = gyro;
 
-                        pid = SCALE * (kp * error + ki * _integral - kd * derivative); 
+                        pid = SCALE * (KP_PITCH_ROLL * error +
+                                KI_PITCH_ROLL * _integral - KD_PITCH_ROLL * derivative); 
 
                         _integral_prev = _integral;
                     }
@@ -80,8 +99,48 @@ namespace hf {
                     float _integral_prev;
             };
 
-            PidController _rollPid;
-            PidController _pitchPid;
+            class YawPidController {
+
+                public:
+
+                    void run(
+                            const float dt,
+                            const float des,
+                            const bool reset,
+                            const float gyro,
+                            float & pid)
+                    {
+                        const auto error = des - gyro;
+
+                        _integral = _integral_prev + error * dt;
+
+                        if (reset) {
+                            _integral = 0;
+                        }
+
+                        _integral = hf::Utils::fconstrain(_integral, -I_LIMIT, I_LIMIT); 
+
+                        const auto derivative = (error - _error_prev) / dt;
+
+                        pid = SCALE * (KP_YAW * error + KI_YAW * _integral - KD_YAW * derivative); 
+
+                        _integral_prev = _integral;
+                        _error_prev = error;
+                    }
+
+
+                private:
+
+                    float _integral;
+                    float _integral_prev;
+                    float _error_prev;
+            };
+
+            PitchRollPidController _rollPid;
+
+            PitchRollPidController _pitchPid;
+
+            YawPidController _yawPid;
 
         public:
 
@@ -92,7 +151,7 @@ namespace hf {
                     const float yaw_des, 
                     const float roll_IMU,
                     const float pitch_IMU,
-                    const uint32_t channel_1_pwm,
+                    const uint32_t throttle,
                     const float GyroX,
                     const float GyroY,
                     const float GyroZ,
@@ -100,48 +159,13 @@ namespace hf {
                     float & pitch_PID,
                     float & yaw_PID) 
             {
-                //DESCRIPTION: Computes control commands based on state error (angle)
-                /*
-                 * Basic PID control to stablize on angle setpoint based on desired states
-                 * roll_des, pitch_des, and yaw_des computed in getDesState(). Error is
-                 * simply the desired state minus the actual state (ex. roll_des -
-                 * roll_IMU). Two safety features are implimented here regarding the I
-                 * terms. The I terms are saturated within specified limits on startup to
-                 * prevent excessive buildup. This can be seen by holding the vehicle at an
-                 * angle and seeing the motors ramp up on one side until they've maxed out
-                 * throttle...saturating I to a specified limit fixes this. The second
-                 * feature defaults the I terms to 0 if the throttle is at the minimum
-                 * setting. This means the motors will not start spooling up on the ground,
-                 * and the I terms will always start from 0 on takeoff. This function
-                 * updates the variables roll_PID, pitch_PID, and yaw_PID which can be
-                 * thought of as 1-D stablized signals. They are mixed to the configuration
-                 * of the vehicle in the mixer.
-                 */
+                const auto reset = throttle < THROTTLE_DOWN;
 
-                static float _integral_yaw;
-                static float _integral_yaw_prev;
+                _rollPid.run(dt, roll_des, roll_IMU, reset, GyroX, roll_PID);
 
-                static float _error_yaw_prev;
+                _pitchPid.run(dt, pitch_des, pitch_IMU, reset, GyroY, pitch_PID);
 
-                _rollPid.run(KP_PITCH_ROLL, KI_PITCH_ROLL, KD_PITCH_ROLL, dt,
-                        roll_des, roll_IMU, channel_1_pwm, GyroX, roll_PID);
-
-                _pitchPid.run(KP_PITCH_ROLL, KI_PITCH_ROLL, KD_PITCH_ROLL, dt,
-                        pitch_des, pitch_IMU, channel_1_pwm, GyroY, pitch_PID);
-
-                //Yaw, stablize on rate from GyroZ
-                const auto error_yaw = yaw_des - GyroZ;
-                _integral_yaw = _integral_yaw_prev + error_yaw*dt;
-                if (channel_1_pwm < 1060) {   
-                    _integral_yaw = 0;
-                }
-                _integral_yaw = hf::Utils::fconstrain(_integral_yaw, -I_LIMIT, I_LIMIT); 
-                const auto derivative_yaw = (error_yaw - _error_yaw_prev)/dt; 
-                yaw_PID = .01*(KP_YAW*error_yaw + KI_YAW*_integral_yaw + KD_YAW*derivative_yaw); 
-
-                //Update roll variables
-                _error_yaw_prev = error_yaw;
-                _integral_yaw_prev = _integral_yaw;
+                _yawPid.run(dt, yaw_des, reset, GyroZ, yaw_PID);
             }    
 
     };
