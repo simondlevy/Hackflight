f6456e78b5e85aebb4e18188a2c60b399a88cdd0
simondlevy <simon.d.levy@gmail.com>
2024-08-20 19:31:43 -0400

    =More sim

--- src/pids/newangle.hpp
+++ src/pids/newangle.hpp
@@ -49,9 +49,9 @@ namespace hf {
 
             static constexpr float I_LIMIT = 25.0;     
 
-            static constexpr float KP_PITCH_ROLL = 0.075;    
-            static constexpr float KI_PITCH_ROLL = 0.3;    
-            static constexpr float KD_PITCH_ROLL = 0.0125;   
+            static constexpr float KP_PITCH_ROLL = 7.5;    
+            static constexpr float KI_PITCH_ROLL = 0.0;    
+            static constexpr float KD_PITCH_ROLL = 1.25;   
 
             static constexpr float KP_YAW = 0.3;           
             static constexpr float KI_YAW = 0.05;          
@@ -83,8 +83,8 @@ namespace hf {
 
                         _integral = hf::Utils::fconstrain(_integral, -I_LIMIT, I_LIMIT); 
 
-                        const auto output =
-                             (KP_PITCH_ROLL * error - KD_PITCH_ROLL * dangle);
+                        const auto output = SCALE * (KP_PITCH_ROLL * error -
+                                KD_PITCH_ROLL * dangle);
 
                         _integral_prev = _integral;
 
