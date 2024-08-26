391fcaa88c77eef0e19c3fe8364d79b3f2ba6505
simondlevy <simon.d.levy@gmail.com>
2024-08-26 01:17:48 -0400

    new file:   controllers/snn-roll/snn-roll.cpp

--- webots/controllers/snn-roll/snn-roll.cpp
+++ webots/controllers/snn-roll/snn-roll.cpp
@@ -1,17 +1,20 @@
-/* C++ flight simulator support for Hackflight Copyright (C) 2024 Simon D. Levy
-
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, in version 3.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program. If not, see <http:--www.gnu.org/licenses/>.
- */
+/*
+  C++ flight simulator takeoff example for Hackflight
+ 
+  Copyright (C) 2024 Simon D. Levy
+ 
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, in version 3.
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http:--www.gnu.org/licenses/>.
+*/
 
 #include <hackflight.hpp>
 #include <mixers.hpp>
@@ -21,6 +24,16 @@
 #include <pids/climbrate.hpp>
 #include <pids/position.hpp>
 
+#include <pids/position.hpp>
+
+//#include <snn.hpp>
+
+static const int VIZ_PORT = 8100;
+
+static const float THRUST_TAKEOFF = 56;
+
+static const float THRUST_BASE = 55.385;
+
 static const float DT = 0.01;
 
 static const float YAW_ANGLE_MAX = 200;
@@ -29,24 +42,43 @@ static const float PITCH_ROLL_DEMAND_POST_SCALE = 30; // deg
 
 static const float YAW_DEMAND_PRE_SCALE = 160; // deg/sec
 
+
 int main(int argc, char ** argv)
 {
+    // Create a simulator object for Webots functionality 
+
     hf::Simulator sim = {};
 
     sim.init();
 
     hf::AnglePid _anglePid = {};
 
-    FILE * logfp = fopen("snufa.csv", "w");
-    fprintf(logfp, "time,setpoint,dy,phi,dphi,output\n");
+    // Load up the network specified in the command line
+
+    /*
+    SNN * snn = NULL;
+
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s RISP_NETWORK\n", argv[0]);
+        exit(1);
+    }
+
+    try {
+        snn = new SNN(argv[1], "risp");
+    } catch (const SRE &e) {
+        fprintf(stderr, "Couldn't set up SNN:\n%s\n", e.what());
+        exit(1);
+    }
+
+    snn->serve_visualizer(VIZ_PORT);
+    */
 
     while (true) {
 
         if (!sim.step()) {
             break;
         }
-
-        hf::quad_motors_t motors = {};
 
         const auto thrust_demand = hf::ClimbRatePid::run(
                 sim.hitTakeoffButton(),
@@ -71,12 +103,6 @@ int main(int argc, char ** argv)
 
         roll_demand *= PITCH_ROLL_DEMAND_POST_SCALE;
 
-        fprintf(logfp, "%f,%f,%f,%f,%f,%f\n",
-                sim.time(), sim.roll(), sim.dy(), sim.phi(), sim.dphi(),
-                roll_demand);
-
-        fflush(logfp);
-
         pitch_demand *= PITCH_ROLL_DEMAND_POST_SCALE;
 
         float m1=0, m2=0, m3=0, m4=0;
@@ -84,10 +110,14 @@ int main(int argc, char ** argv)
                 thrust_demand, roll_demand, pitch_demand, yaw_demand,
                 m1, m2, m3, m4);
 
+ 
         sim.setMotors(m1, m2, m3, m4);
+
+        // snn->send_counts_to_visualizer();
+
     }
 
-    sim.close();
+    wb_robot_cleanup();
 
     return 0;
 }
