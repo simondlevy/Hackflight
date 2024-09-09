047de030cff0f02307f43511564235ec04b9514e
simondlevy <simon.d.levy@gmail.com>
2024-09-09 19:32:17 -0400

    modified:   usfs-dsmx.ino

--- examples/usfs-dsmx/usfs-dsmx.ino
+++ examples/usfs-dsmx/usfs-dsmx.ino
@@ -77,7 +77,7 @@ static hf::PitchRollRatePid _pitchRollRatePid;
 // Das Blinkenlights ---------------------------------------------------------
 
 static const float BLINK_RATE_HZ = 1.5;
-static const uint8_t LED_PIN = 13;
+static const uint8_t LED_PIN = 0;
 static hf::BlinkTask _blinkTask;
 
 // Motors --------------------------------------------------------------------
