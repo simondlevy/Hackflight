87becb92260259725b4841c8b7fdf0d880a5a787
simondlevy <simon.d.levy@gmail.com>
2024-09-22 02:56:52 -0400

    renamed:    snnmerge.py -> snnconcat.py

--- webots/controllers/snn/networks/snnconcat.py
+++ webots/controllers/snn/networks/snnconcat.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
 
+'''
+Copyright (C) 2024 Simon D. Levy
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, in version 3.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http:--www.gnu.org/licenses/>.
+'''
+
 import json
 import argparse
 
@@ -45,10 +61,14 @@ def main():
                 'from':snn1map[edge['from']], 
                 'to':snn1map[edge['to']], 
                 'values':edge['values'] 
-            }
+                }
             for edge in snn1['Edges'] ]
 
-    print(edges1_renamed)
+    # print(edges1_renamed)
+
+    print(snn1.keys())
+
+    exit(0)
 
     json_out = json.dumps(snn_out, sort_keys=True, indent=4)
 
