da1bc1b41d36f253f2094c6c3d5d92021c6505db
simondlevy <simon.d.levy@gmail.com>
2024-09-21 23:32:48 -0400

    new file:   stitcher.py

---
+++ webots/controllers/snn/networks/stitcher.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import json
+import argparse
+
+
+def main():
+
+    fmtr = argparse.ArgumentDefaultsHelpFormatter
+
+    parser = argparse.ArgumentParser(formatter_class=fmtr)
+
+main()
+
