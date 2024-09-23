a24d8980e7243c8405991d67b528ad722568481f
simondlevy <simon.d.levy@gmail.com>
2024-09-23 18:17:23 -0400

    modified:   viz.sh

--- webots/controllers/snn/networks/viz.sh
+++ webots/controllers/snn/networks/viz.sh
@@ -1,2 +1,3 @@
 cd ${HOME}/Desktop/framework/viz; \
-    love . -n ${HOME}/Desktop/hackflight/webots/controllers/snn/networks/$1
+    love . -n ${HOME}/Desktop/hackflight/webots/controllers/snn/networks/$1 \
+		--use_name_neuron '{"0":"0","1":"1","2":"2","3":"3","4":"4","5":"5","6":"6","7":"7","8":"8"}'
