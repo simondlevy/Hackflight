e638bbc9616652b4f06d21532679d6ff0560ce67
simondlevy <simon.d.levy@gmail.com>
2024-10-08 16:46:43 -0400

    modified:   ../../../src/oldsim.hpp

--- src/oldsim.hpp
+++ src/oldsim.hpp
@@ -82,54 +82,7 @@ namespace hf {
                         demands.yaw,
                         button);
 
-                // Track previous time and position for calculating motion
-                static float tprev;
-                static float xprev;
-                static float yprev;
-                static float zprev;
-
-                const auto tcurr = wb_robot_get_time();
-                const auto dt =  tcurr - tprev;
-                tprev = tcurr;
-
-                auto psi = wb_inertial_unit_get_roll_pitch_yaw(_imu)[2];
-
-                state.z = wb_gps_get_values(_gps)[2];
-
-                state.phi = Utils::RAD2DEG*(
-                        wb_inertial_unit_get_roll_pitch_yaw(_imu)[0]);
-
-                state.dphi = Utils::RAD2DEG*(
-                        wb_gyro_get_values(_gyro)[0]);
-
-                state.theta = Utils::RAD2DEG*(
-                        wb_inertial_unit_get_roll_pitch_yaw(_imu)[1]);
-
-                state.dtheta =  Utils::RAD2DEG*(wb_gyro_get_values(_gyro)[1]); 
-
-                state.psi  =  -Utils::RAD2DEG*(psi); 
-
-                state.dpsi =  -Utils::RAD2DEG*(wb_gyro_get_values(_gyro)[2]);
-
-                // Use temporal first difference to get world-cooredinate
-                // velocities
-                auto x = wb_gps_get_values(_gps)[0];
-                auto y = wb_gps_get_values(_gps)[1];
-                auto dx = (x - xprev) / dt;
-                auto dy = (y - yprev) / dt;
-                state.dz = (state.z - zprev) / dt;
-
-                // Rotate X,Y world velocities into body frame to simulate
-                // optical-flow sensor
-                auto cospsi = cos(psi);
-                auto sinpsi = sin(psi);
-                state.dx = dx * cospsi + dy * sinpsi;
-                state.dy = dx * sinpsi - dy * cospsi;
-
-                // Save past time and position for next time step
-                xprev = x;
-                yprev = y;
-                zprev = state.z;
+                getState(state);
 
                 if (button) {
                     _button_was_hit = true;
@@ -192,6 +145,58 @@ namespace hf {
 
             // Handles bogus nonzero throttle stick values at startup
             bool ready;
+            
+            void getState(state_t & state)
+            {
+                // Track previous time and position for calculating motion
+                static float tprev;
+                static float xprev;
+                static float yprev;
+                static float zprev;
+
+                const auto tcurr = wb_robot_get_time();
+                const auto dt =  tcurr - tprev;
+                tprev = tcurr;
+
+                auto psi = wb_inertial_unit_get_roll_pitch_yaw(_imu)[2];
+
+                state.z = wb_gps_get_values(_gps)[2];
+
+                state.phi = Utils::RAD2DEG*(
+                        wb_inertial_unit_get_roll_pitch_yaw(_imu)[0]);
+
+                state.dphi = Utils::RAD2DEG*(
+                        wb_gyro_get_values(_gyro)[0]);
+
+                state.theta = Utils::RAD2DEG*(
+                        wb_inertial_unit_get_roll_pitch_yaw(_imu)[1]);
+
+                state.dtheta =  Utils::RAD2DEG*(wb_gyro_get_values(_gyro)[1]); 
+
+                state.psi  =  -Utils::RAD2DEG*(psi); 
+
+                state.dpsi =  -Utils::RAD2DEG*(wb_gyro_get_values(_gyro)[2]);
+
+                // Use temporal first difference to get world-cooredinate
+                // velocities
+                auto x = wb_gps_get_values(_gps)[0];
+                auto y = wb_gps_get_values(_gps)[1];
+                auto dx = (x - xprev) / dt;
+                auto dy = (y - yprev) / dt;
+                state.dz = (state.z - zprev) / dt;
+
+                // Rotate X,Y world velocities into body frame to simulate
+                // optical-flow sensor
+                auto cospsi = cos(psi);
+                auto sinpsi = sin(psi);
+                state.dx = dx * cospsi + dy * sinpsi;
+                state.dy = dx * sinpsi - dy * cospsi;
+
+                // Save past time and position for next time step
+                xprev = x;
+                yprev = y;
+                zprev = state.z;
+            }
 
             static bool button_stick(const uint8_t axis)
             {
