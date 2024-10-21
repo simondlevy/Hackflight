0466684b7e5803ea2ebc2eb8cfd1113d93b8949b
simondlevy <simon.d.levy@gmail.com>
2024-10-21 12:38:29 -0400

    =More two-way ESP32

--- sketches/esp32/onboard/onboard.ino
+++ sketches/esp32/onboard/onboard.ino
@@ -3,23 +3,6 @@
 
 static uint8_t broadcastAddress[] = {0xD4, 0xD4, 0xDA, 0x83, 0x9B, 0xA4};
 
-static float temperature;
-static float humidity;
-static float pressure;
-
-static String success;
-
-typedef struct struct_message 
-{
-    float temp;
-    float hum;
-    float pres;
-} struct_message;
-
-static struct_message BME280Readings;
-
-static struct_message incomingReadings;
-
 static esp_now_peer_info_t peerInfo;
 
 static void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) 
@@ -28,13 +11,6 @@ static void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len
   Serial.println(len);
 }
  
-static void getReadings()
-{
-  temperature = 99;
-  humidity = 100;
-  pressure = 101;
-}
-
 void setup() 
 {
   Serial.begin(115200);
@@ -60,13 +36,10 @@ void setup()
  
 void loop() 
 {
-  getReadings();
- 
-  BME280Readings.temp = temperature;
-  BME280Readings.hum = humidity;
-  BME280Readings.pres = pressure;
+  const uint8_t bytes[20] =
+    {0x24,0x4D,0x3C,0x0C,0xC8,0x00,0x00,0x00,0x04,0x00,0x04,0xFA,0x03,0x00,0x00,0x00,0x00,0x00,0x00,0x3D};
 
-  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &BME280Readings, sizeof(BME280Readings));
+  esp_err_t result = esp_now_send(broadcastAddress, bytes, 20);
    
   if (result != ESP_OK) {
     Serial.println("Error sending the data");
