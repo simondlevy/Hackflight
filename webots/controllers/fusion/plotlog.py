4e686f6225abc933f64fb0cb82c7580230967c87
simondlevy <simon.d.levy@gmail.com>
2024-11-07 17:49:21 -0500

    Pass constant IMU values to Madgwick

---
+++ webots/controllers/fusion/plotlog.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt('log.csv', delimiter=',')
+
+plt.subplot(2, 1, 1)
+plt.plot(data[:,0])
+
+plt.subplot(2, 1, 2)
+plt.plot(data[:,1])
+
+plt.show()
+
