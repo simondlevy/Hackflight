372cc8301cafc7790024e946fe1deb172a45afac
simondlevy <simon.d.levy@gmail.com>
2024-11-11 23:01:26 -0500

    =More sim EKF

--- webots/controllers/ekf/plotlog.py
+++ webots/controllers/ekf/plotlog.py
@@ -7,19 +7,19 @@ data = np.loadtxt('log.csv', delimiter=',')
 
 time = data[:,0]
 
-def plot2(j, k1, k2, label, lo, hi):
+def plot2(j, k, label, lo, hi):
     plt.subplot(7, 1, j)
-    plt.plot(time, data[:, k1])
-    plt.plot(time, data[:, k2])
+    plt.plot(time, data[:, k])
+    plt.plot(time, data[:, k+1])
     plt.ylim((lo, hi))
     plt.ylabel(label)
     plt.legend(('true', 'ekf'))
 
-def plot3(j, k1, k2, k3, label, lo, hi):
+def plot3(j, k, label, lo, hi):
     plt.subplot(7, 1, j)
-    plt.plot(time, data[:, k1])
-    plt.plot(time, data[:, k2])
-    plt.plot(time, data[:, k3])
+    plt.plot(time, data[:, k])
+    plt.plot(time, data[:, k+1], '-x')
+    plt.plot(time, data[:, k+2])
     plt.ylim((lo, hi))
     plt.ylabel(label)
     plt.legend(('true', 'comp', 'ekf'))
@@ -28,13 +28,13 @@ VEL_LIM = 1.5
 Z_LIM = 1
 ANG_LIM = 10
 
-plot3(1, 1, 2, 3, 'dx/dt (m/s)', -VEL_LIM, +VEL_LIM)
-plot3(2, 4, 5, 6, 'dy/dt (m/s)', -VEL_LIM, +VEL_LIM)
-plot3(3, 7, 8, 9, 'z (m)', 0, Z_LIM);
-plot3(4, 10, 11, 12, 'dz/dt (m/s)', -VEL_LIM, +VEL_LIM)
-plot2(5, 13, 14, 'phi (deg)', -ANG_LIM, +ANG_LIM)
-plot2(6, 15, 16, 'theta (deg)', -ANG_LIM, +ANG_LIM)
-plot2(7, 17, 18, 'psi (deg)', -180, +180)
+plot3(1, 1, 'dx/dt (m/s)', -VEL_LIM, +VEL_LIM)
+plot3(2, 4, 'dy/dt (m/s)', -VEL_LIM, +VEL_LIM)
+plot3(3, 7, 'z (m)', 0, Z_LIM);
+plot3(4, 10, 'dz/dt (m/s)', -VEL_LIM, +VEL_LIM)
+plot2(5, 13, 'phi (deg)', -ANG_LIM, +ANG_LIM)
+plot2(6, 15, 'theta (deg)', -ANG_LIM, +ANG_LIM)
+plot2(7, 17, 'psi (deg)', -180, +180)
 
 plt.xlabel('time (s)')
 
