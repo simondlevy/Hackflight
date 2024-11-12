57a652a28bfaac6625025f43da283673d571e388
simondlevy <simon.d.levy@gmail.com>
2024-11-12 17:19:49 -0500

    More newsim

--- newsim/main.cpp
+++ newsim/main.cpp
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <sys/time.h>
 
 #include "dynamics/fixedpitch/QuadXBF.hpp"
 
-// Time constant
-static const double DT = 2.0e-5;
+static const double DYNAMICS_FREQ = 5.0e6;
 
 // Vehicle constants
 
@@ -36,24 +36,37 @@ int main(int argc, char ** argv)
     double rotation[3] = {0,0,0};
     dynamics.init(rotation);
 
+    time_t sec_prev = 0;
+
+    const auto dynamics_dt = 1 / DYNAMICS_FREQ;
+
     // Loop forever, communicating with server
     for (uint64_t k=0; ; ++k) {
 
-        const double time = k * DT;
+        const double time = k * dynamics_dt;
 
         const float MOTOR = 55.385; // rad/sec
         float motorvals[4] = {MOTOR, MOTOR, MOTOR, MOTOR};
 
-        printf("t=%05f   m=%f %f %f %f  z=%+3.3f\n", 
-                time,
-                motorvals[0],
-                motorvals[1],
-                motorvals[2],
-                motorvals[3],
-                dynamics.state.z);
+        struct timeval tv = {};
+        gettimeofday(&tv, NULL);
+        time_t sec_curr = tv.tv_sec;
+
+        if (sec_curr - sec_prev > 0) {
+
+            sec_prev = sec_curr;
+
+            printf("t=%05f   m=%f %f %f %f  z=%+3.3f\n", 
+                    time,
+                    motorvals[0],
+                    motorvals[1],
+                    motorvals[2],
+                    motorvals[3],
+                    dynamics.state.z);
+        }
 
         // Update dynamics with motor values
-        dynamics.update(motorvals, DT);
+        dynamics.update(motorvals, dynamics_dt);
 
         // Set AGL to arbitrary positive value to avoid kinematic trick
         dynamics.setAgl(1);
