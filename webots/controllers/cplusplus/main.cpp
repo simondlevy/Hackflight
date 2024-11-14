ba65bc658d58ebbd280a48d1f53ee1fa607a1b0d
simondlevy <simon.d.levy@gmail.com>
2024-11-14 11:15:34 -0500

    =Cleanup

--- webots/controllers/cplusplus/main.cpp
+++ webots/controllers/cplusplus/main.cpp
@@ -1,5 +1,7 @@
 /* 
-   C++ flight simulator support for Hackflight Copyright (C) 2024 Simon D. Levy
+   C++ flight simulator support for Hackflight
+   
+   Copyright (C) 2024 Simon D. Levy
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
--- webots/controllers/custom/main.cpp
+++ webots/controllers/custom/main.cpp
@@ -1,4 +1,22 @@
+/* 
+   C++ flight simulator support for Hackflight with custom physics plugin
+   
+   Copyright (C) 2024 Simon D. Levy
 
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, in version 3.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program. If not, see <http:--www.gnu.org/licenses/>.
+ */
+
+#
 #include <webots/camera.h>
 #include <webots/emitter.h>
 #include <webots/keyboard.h>
@@ -9,40 +27,51 @@
 #define TIMESTEP 32  // ms
 #define KEY_INCREMENT 0.05
 
-int main() {
+int main() 
+{
   double command[3] = {0.0, 0.0, 0.0};
-  WbDeviceTag gEmitter = 0;
 
   wb_robot_init();
+
   WbDeviceTag camera = wb_robot_get_device("camera");
+
   wb_camera_enable(camera, TIMESTEP * 2);
+
   // Handle joystick
   jsJoystick *gJoystick = new jsJoystick();
+
   if (gJoystick->notWorking()) {
     delete gJoystick, gJoystick = NULL;
-    printf("No joystick available, using keyboard control (click in the 3D windows before pressing keys).\n");
-    printf("Available control keys: up, down, right, left, page up, page down and space (reset).\n");
+    printf("No joystick available, ...\n");
+    printf("Available control keys: ...\n");
     wb_keyboard_enable(TIMESTEP);
   }
 
   // Handle emitter
-  gEmitter = wb_robot_get_device("emitter");
-  if (!gEmitter)
+  const auto gEmitter = wb_robot_get_device("emitter");
+
+  if (!gEmitter) {
     printf("!!! joystick :: reset :: emitter is not available.\n");
+  }
 
   while (wb_robot_step(TIMESTEP) != -1) {
 
     // Send joystick value.
     if (gEmitter) {
+
       // read joystick.
       if (gJoystick) {
+
         float axes[12];
         int buttons[12];
         gJoystick->read(buttons, axes);
         command[0] = (double)-axes[1];
         command[1] = (double)axes[3];
         command[2] = (double)axes[2];
-      } else {
+      } 
+      
+      else {
+
         switch (wb_keyboard_get_key()) {
           case WB_KEYBOARD_DOWN:
             command[0] -= KEY_INCREMENT;
@@ -69,12 +98,14 @@ int main() {
         }
       }
       // setup emitter buffer
-      if (command[0] || command[1] || command[2])
+      if (command[0] || command[1] || command[2]) {
         printf("command = ( %g , %g , %g )\n", command[0], command[1], command[2]);
+      }
       wb_emitter_send(gEmitter, command, sizeof(command));
     }
   }
 
   wb_robot_cleanup();
+
   return 0;
 }
