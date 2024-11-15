1af0e5db135f4512b306eddfe1111f84dede16da
simondlevy <simon.d.levy@gmail.com>
2024-11-14 22:02:24 -0500

    renamed:    ../../../../src/sim/dynamics.hpp -> dynamics.hpp

--- webots/plugins/physics/quadrotor_physics/quadrotor_physics.cpp
+++ webots/plugins/physics/quadrotor_physics/quadrotor_physics.cpp
@@ -1,6 +1,6 @@
 /* 
    Custom physics plugin for Hackflight simulator
-   
+
    Copyright (C) 2024 Simon D. Levy
 
    This program is free software: you can redistribute it and/or modify
@@ -18,9 +18,7 @@
 
 #include <plugins/physics.h>
 
-#include "quadrotor2b.h"
-#include "quadrotormodel.h"
-#include "utils.h"
+#include "dynamics.hpp"
 
 // constants
 const char kRobotName[] = "quadrotor";
@@ -28,38 +26,36 @@ const char kRobotName[] = "quadrotor";
 // globals
 static dBodyID gRobotBody = NULL;
 
-static dReal x = 0;
-static dReal y = 0;
 static dReal z = 0.015;
 static dReal inc = 1;
 
 DLLEXPORT void webots_physics_init() 
 {
-  // init global variables
-  gRobotBody = dWebotsGetBodyFromDEF(kRobotName);
+    // init global variables
+    gRobotBody = dWebotsGetBodyFromDEF(kRobotName);
 
-  if (gRobotBody == NULL) {
+    if (gRobotBody == NULL) {
 
-    dWebotsConsolePrintf("!!! quadrotor_physics :: webots_physics_init :: ");
-    dWebotsConsolePrintf("error : could not get body of robot.\r\n");
-  }
-  else {
+        dWebotsConsolePrintf("!!! quadrotor_physics :: webots_physics_init :: ");
+        dWebotsConsolePrintf("error : could not get body of robot.\r\n");
+    }
+    else {
 
-    dBodySetGravityMode(gRobotBody, 0);
-  }
+        dBodySetGravityMode(gRobotBody, 0);
+    }
 }
 
 DLLEXPORT void webots_physics_step() 
 {
-  // we return if we have no robot to actuate.
-  if (gRobotBody == NULL) {
-    return;
+    // we return if we have no robot to actuate.
+    if (gRobotBody == NULL) {
+        return;
     }
-    
-    dBodySetPosition(gRobotBody, x, y, z);
-    
+
+    dBodySetPosition(gRobotBody, 0, 0, z);
+
     z += inc * .005;
-    
+
     if (z > 0.2) { 
         inc = -1;
     }
@@ -71,7 +67,9 @@ DLLEXPORT void webots_physics_step()
 
 DLLEXPORT int webots_physics_collide(dGeomID g1, dGeomID g2) 
 {
-  return 0;
+    (void)g1;
+    (void)g2;
+    return 0;
 }
 
 DLLEXPORT void webots_physics_cleanup() 
