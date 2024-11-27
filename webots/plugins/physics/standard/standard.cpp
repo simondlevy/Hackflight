82fef2856109326f447531f0a72e4da8139205b7
simondlevy <simon.d.levy@gmail.com>
2024-11-27 15:26:42 -0500

    new file:   src/sim/standard_controllers.hpp

--- webots/plugins/physics/standard/standard.cpp
+++ webots/plugins/physics/standard/standard.cpp
@@ -16,66 +16,6 @@
  * along with this program. If not, see <http:--www.gnu.org/licenses/>.
  */
 
-#include <stdio.h>
 
-#include <hackflight.hpp>
-#include <sim/dynamics.hpp>
+#include <sim/standard_controllers.hpp>
 #include <sim/state_from_dynamics.hpp>
-#include <pids/altitude.hpp>
-#include <pids/position.hpp>
-#include <pids/pitch_roll_angle.hpp>
-#include <pids/pitch_roll_rate.hpp>
-#include <pids/yaw_rate.hpp>
-
-static const float THROTTLE_DOWN = 0.06;
-static const float PITCH_ROLL_POST_SCALE = 50;
-static const float MOTOR_HOVER = 74.565; // rad/sec
-
-static hf::AltitudePid _altitudePid;
-static hf::PitchRollAnglePid _pitchRollAnglePid;
-static hf::PitchRollRatePid _pitchRollRatePid;
-static hf::YawRatePid _yawRatePid;
-
-// Called by webots_physics_init(); unneeded here
-void setup_controllers()
-{
-}
-
-hf::demands_t run_controllers(
-        const float pid_dt,
-        const hf::siminfo_t & siminfo,
-        const hf::state_t & state)
-{
-    const auto open_loop_demands = siminfo.demands;
-
-    // Throttle-down should reset pids
-    const auto resetPids = open_loop_demands.thrust < THROTTLE_DOWN;
-
-    // Start with open-loop demands
-    hf::demands_t demands = {
-        open_loop_demands.thrust,
-        open_loop_demands.roll,
-        open_loop_demands.pitch,
-        open_loop_demands.yaw
-    };
-
-    if (siminfo.requested_takeoff) {
-
-        _altitudePid.run(siminfo.is_springy, pid_dt, state, demands);
-
-        demands.thrust += MOTOR_HOVER;
-    }
-
-    hf::PositionPid::run(state, demands);
-
-    _pitchRollAnglePid.run(pid_dt, resetPids, state, demands);
-
-    _pitchRollRatePid.run(pid_dt, resetPids, state, demands,
-            PITCH_ROLL_POST_SCALE);
-
-    _yawRatePid.run(pid_dt, resetPids, state, demands);
-
-    return demands;
-}
-
-
