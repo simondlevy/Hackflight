a05e671a06ad38b9efb77b6441a5f20ac2751c24
simondlevy <simon.d.levy@gmail.com>
2025-01-18 17:02:03 -0500

    new file:   rpi/main.cpp

---
+++ rpi/main.cpp
@@ -0,0 +1,26 @@
+/* 
+ * Custom physics plugin for Hackflight simulator using ground-truth state 
+ * and Spiking Neural Net controllers
+ *
+ *  Copyright (C) 2024 Simon D. Levy
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, in version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http:--www.gnu.org/licenses/>.
+ */
+
+int main(int argc, char ** argv)
+{
+    (void)argc;
+    (void)argv;
+
+    return 0;
+}
