da266c5a120f705bed386f5c06d6793b4b57866d
simondlevy <simon.d.levy@gmail.com>
2025-01-18 17:26:53 -0500

    =More RPi SNN

--- rpi/main.cpp
+++ rpi/main.cpp
@@ -14,10 +14,41 @@
  * along with this program. If not, see <http:--www.gnu.org/licenses/>.
  */
 
+#include <cstdlib>
+
+#include <levy_snn_util.hpp>
+
+static const char * NETWORK = "/home/levys/Desktop/framework/networks/difference_risp_train.txt";
+
+static const size_t NITER = 100;
+static const double divisor = 50;
+static const double offset = 0;
+
+static double randval()
+{
+
+    return 2 * (double)rand() / INT_MAX - 1;
+}
+
 int main(int argc, char ** argv)
 {
     (void)argc;
     (void)argv;
 
-    return 0;
+    static SNN * snn = new SNN(NETWORK, "risp");
+
+    for (size_t k=0; k<100; ++k) {
+
+        vector<double> observations = { randval(), randval() };
+
+        vector <int> counts = {};
+
+        snn->step(observations, counts);
+
+        const double action = counts[0] / divisor - offset;
+
+        printf("%+3.3f - %+3.3f = %+3.3f\n", 
+                observations[0], observations[1], action);
+    }
+
 }
