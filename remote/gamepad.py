659d8aa9db3ff7ea1835e1b7ee2b9217778ea0da
simondlevy <simon.d.levy@gmail.com>
2025-03-07 13:27:26 -0500

    modified:   gamepad.py

--- remote/gamepad.py
+++ remote/gamepad.py
@@ -12,41 +12,48 @@ def gamepad_threadfun(vals, running):
 
         sleep(0)  # yield
 
+
+def main():
     
-AXIS_MAP = {'X': 0, 'Y': 1, 'Z': 2, 'RX': 3, 'RY': 4, 'RZ': 5}
-    
-gamepad_vals = [0, 1024, 1024, 1024, 0, 0]
+    AXIS_MAP = {'X': 0, 'Y': 1, 'Z': 2, 'RX': 3, 'RY': 4, 'RZ': 5}
+        
+    gamepad_vals = [0, 1024, 1024, 1024, 0, 0]
 
-running = [True]
+    running = [True]
 
-Thread(target=gamepad_threadfun, args=(gamepad_vals, running)).start()
+    #Thread(target=gamepad_threadfun, args=(gamepad_vals, running)).start()
 
-while running[0]:
+    while running[0]:
 
-    print(gamepad_vals)
+        try:
 
-    try:
+            for event in inputs.get_gamepad():
 
-        for event in inputs.get_gamepad():
+                code = str(event.code)
 
-            code = str(event.code)
+                if 'ABS' in code:
 
-            if 'ABS' in code:
+                    axis = AXIS_MAP[code[4:]]
 
-                axis = AXIS_MAP[code[4:]]
+                    gamepad_vals[axis] = event.state
 
-                gamepad_vals[axis] = event.state
+                elif code in {'BTN_TR', 'BTN_PINKIE'} :
 
-    except inputs.UnpluggedError:
-        print('No gamepad detected')
-        running[0] = False
+                    print(event.state)
 
-    except KeyboardInterrupt:
-        running[0] = False
+        except inputs.UnpluggedError:
+            print('No gamepad detected')
+            running[0] = False
 
-    except OSError:
-        print('Gamepad unplugged')
-        running[0] = False
+        except KeyboardInterrupt:
+            running[0] = False
+
+        except OSError:
+            print('Gamepad unplugged')
+            running[0] = False
+
+
+main()
 
 
 
