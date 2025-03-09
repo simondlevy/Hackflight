5dd041b2cfd835309c7baa0aca7dc29ff681ba06
simondlevy <simon.d.levy@gmail.com>
2025-03-08 19:30:21 -0500

    modified:   gcs.py

--- remote/gcs.py
+++ remote/gcs.py
@@ -28,6 +28,7 @@ from sys import stdout
 SUPPORTED_GAMEPADS = {'Microsoft X-Box 360 pad',  'Logitech Gamepad F310'}
 
 RPI_ADDRESS = 'B8:27:EB:3F:AB:47'
+RPI_RADIO_PORT = 1
 RPI_LOGGING_PORT = 2
 
 
@@ -67,12 +68,9 @@ def logging_threadfun(client, status):
         sleep(0)  # yield
 
 
-def main():
+def connect_to_server(port, status):
 
-    GAMEPAD_AXIS_MAP = {'X': 3, 'Y': 0, 'Z': 1, 'RX': 1, 'RY': 2, 'RZ': 2}
-
-    print('Connecting to server %s:%d ... ' %
-          (RPI_ADDRESS, RPI_LOGGING_PORT), end='')
+    print('Connecting to server %s:%d ... ' % (RPI_ADDRESS, port), end='')
     stdout.flush()
 
     # Create a Bluetooth or IP socket depending on address format
@@ -82,17 +80,24 @@ def main():
                       else socket.socket(socket.AF_INET, socket.SOCK_STREAM))
 
     try:
-        logging_client.connect((RPI_ADDRESS, RPI_LOGGING_PORT))
+        logging_client.connect((RPI_ADDRESS, port))
 
     except Exception as e:
         print(str(e) + ': is server running?')
         exit(0)
 
+    Thread(target=logging_threadfun, args=(logging_client, status)).start()
+
     print(' connected')
+
+
+def main():
+
+    GAMEPAD_AXIS_MAP = {'X': 3, 'Y': 0, 'Z': 1, 'RX': 1, 'RY': 2, 'RZ': 2}
 
     status = {'running': True, 'armed': False}
 
-    Thread(target=logging_threadfun, args=(logging_client, status)).start()
+    connect_to_server(RPI_LOGGING_PORT, status)
 
     gamepads = inputs.devices.gamepads
 
@@ -112,6 +117,7 @@ def main():
 
     Thread(target=gamepad_threadfun, args=(gamepad_vals, status)).start()
 
+    # Gamepad reading blocks, so run it on main thread
     while status['running']:
 
         try:
