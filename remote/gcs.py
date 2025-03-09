4d6891ef3a40aeea9632d3c702b2187a4a494fb4
simondlevy <simon.d.levy@gmail.com>
2025-03-08 19:18:00 -0500

    modified:   gcs.py

--- remote/gcs.py
+++ remote/gcs.py
@@ -19,15 +19,16 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 '''
 
 import socket
-import argparse
 import inputs
 from threading import Thread
 from struct import unpack
 from time import sleep
 from sys import stdout
-import inputs
 
-SUPPORTED_GAMEPADS = { 'Microsoft X-Box 360 pad',  'Logitech Gamepad F310' }
+SUPPORTED_GAMEPADS = {'Microsoft X-Box 360 pad',  'Logitech Gamepad F310'}
+
+RPI_ADDRESS = 'B8:27:EB:3F:AB:47'
+RPI_LOGGING_PORT = 2
 
 
 def gamepad_threadfun(vals, status):
@@ -36,8 +37,8 @@ def gamepad_threadfun(vals, status):
 
     while status['running']:
 
-        scaled = tuple(map(lambda x : x/32768,
-                          (-vals[0], vals[1], -vals[2], vals[3])))
+        # scaled = tuple(map(lambda x: x/32768,
+        #                (-vals[0], vals[1], -vals[2], vals[3])))
 
         # print('t=%+3.3f  r=%+3.3f  p=%+3.3f  y=%+3.3f' % scaled, end=' ')
         # print('Armed: %d' % status['armed'])
@@ -45,10 +46,10 @@ def gamepad_threadfun(vals, status):
         armed = status['armed']
 
         if armed and not was_armed:
-            print('*********************************** Armed ***********************************')
+            print('************************* Armed *************************')
 
         if not armed and was_armed:
-            print('********************************* Disarmed **********************************')
+            print('*********************** Disarmed ************************')
 
         was_armed = armed
 
@@ -70,45 +71,38 @@ def main():
 
     GAMEPAD_AXIS_MAP = {'X': 3, 'Y': 0, 'Z': 1, 'RX': 1, 'RY': 2, 'RZ': 2}
 
-    # Parse command-line arguments
-    parser = argparse.ArgumentParser(
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-a', '--address', default='B8:27:EB:3F:AB:47',
-                        help='server address (IP or MAC)')
-    parser.add_argument('-p', '--port', help='port', type=int, default=1)
-    args = parser.parse_args()
-
-    print('Connecting to server %s:%d ...' % (args.address, args.port), end='')
+    print('Connecting to server %s:%d ... ' %
+          (RPI_ADDRESS, RPI_LOGGING_PORT), end='')
     stdout.flush()
 
     # Create a Bluetooth or IP socket depending on address format
-    client = (socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM,
-                    socket.BTPROTO_RFCOMM)
-              if ':' in args.address 
-              else socket.socket(socket.AF_INET, socket.SOCK_STREAM))
+    logging_client = (socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM,
+                      socket.BTPROTO_RFCOMM)
+                      if ':' in RPI_ADDRESS
+                      else socket.socket(socket.AF_INET, socket.SOCK_STREAM))
 
     try:
-        client.connect((args.address, args.port))
+        logging_client.connect((RPI_ADDRESS, RPI_LOGGING_PORT))
 
-    except:
-        print('Connection failed; make sure server is running')
+    except Exception as e:
+        print(str(e) + ': is server running?')
         exit(0)
 
     print(' connected')
 
     status = {'running': True, 'armed': False}
 
-    Thread(target=logging_threadfun, args=(client, status)).start()
+    Thread(target=logging_threadfun, args=(logging_client, status)).start()
 
     gamepads = inputs.devices.gamepads
 
     if len(gamepads) == 0:
         print('No gamepad detected')
         exit(0)
-        
-    devname = inputs.devices.gamepads[0].name 
 
-    if not devname in SUPPORTED_GAMEPADS:
+    devname = inputs.devices.gamepads[0].name
+
+    if devname not in SUPPORTED_GAMEPADS:
         print(devname + ' not supported')
         exit(0)
 
@@ -136,7 +130,7 @@ def main():
 
                         gamepad_vals[axis] = event.state
 
-                elif code in {'BTN_TR', 'BTN_PINKIE'} :
+                elif code in {'BTN_TR', 'BTN_PINKIE'}:
 
                     if not event.state and button_state_prev:
 
