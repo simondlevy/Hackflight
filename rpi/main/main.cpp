5ee81af05a845a94c83b6773c7ebc3b21e01613c
simondlevy <simon.d.levy@gmail.com>
2025-03-10 19:27:29 -0400

    modified:   main.cpp

--- rpi/main/main.cpp
+++ rpi/main/main.cpp
@@ -103,12 +103,6 @@ int main(int argc, char ** argv)
             serializer.serializeShorts(hf::MSP_SET_RC, chanvals, 5);
         
             write(serialfd, serializer.payload, serializer.payloadSize);
-
-            /*
-            const auto armed = (bool)chanvals[4];
-            printf("t=%+06d  r=%+06d  p=%+06d  y=%+06d | armed=%1d\n", 
-                    chanvals[0], chanvals[1], chanvals[2], chanvals[3], armed);
-                    */
         }
 
 
