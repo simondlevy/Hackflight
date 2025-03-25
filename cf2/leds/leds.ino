be5daa1bee7bbf14a487951c275cebf551fb16cf
simondlevy <simon.d.levy@gmail.com>
2025-03-25 00:56:01 -0400

    new file:   cf2/leds/leds.ino

---
+++ cf2/leds/leds.ino
@@ -0,0 +1,28 @@
+static const uint8_t BLUE_L  = PD2;
+static const uint8_t GREEN_L = PC1;
+static const uint8_t RED_L   = PC0;
+static const uint8_t GREEN_R = PC2;
+static const uint8_t RED_R   = PC3;
+
+static const uint8_t pins[5] = {BLUE_L, GREEN_L, RED_L, GREEN_R, RED_R};
+
+static uint8_t active;
+
+void setup() 
+{
+    for (uint8_t k=0; k<5; ++k) {
+        pinMode(pins[k], OUTPUT);
+    }
+
+    active = 0;
+}
+
+void loop() 
+{
+    digitalWrite(pins[active], HIGH);
+    delay(500);
+    digitalWrite(pins[active], LOW);
+    delay(500);
+
+    active = (active + 1) % 5;
+}
