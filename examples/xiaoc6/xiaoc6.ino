cfe911fcab689763c30781474d6dc196f0e33c17
simondlevy <simon.d.levy@gmail.com>
2025-03-25 17:38:32 -0400

    modified:   xiaoc6.ino

--- examples/xiaoc6/xiaoc6.ino
+++ examples/xiaoc6/xiaoc6.ino
@@ -3,8 +3,8 @@
 #include <BLEUtils.h>
 #include <BLEServer.h>
 
-#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+static const char * SERVICE_UUID        = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
+static const char * CHARACTERISTIC_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
 
 void setup() 
 {
