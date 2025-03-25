eead8c4cbe159e35b35ad9ffc67cb2a7e7a26342
simondlevy <simon.d.levy@gmail.com>
2025-03-25 00:59:02 -0400

    modified:   cf2/leds/leds.ino

--- cf2/leds/leds.ino
+++ cf2/leds/leds.ino
@@ -5,6 +5,7 @@ static const uint8_t GREEN_R = PC2;
 static const uint8_t RED_R   = PC3;
 
 static const uint8_t pins[5] = {BLUE_L, GREEN_L, RED_L, GREEN_R, RED_R};
+static const uint8_t polarities[5] = {1, 0, 0, 0, 0};
 
 static uint8_t active;
 
@@ -19,9 +20,9 @@ void setup()
 
 void loop() 
 {
-    digitalWrite(pins[active], HIGH);
+    digitalWrite(pins[active], polarities[active]);
     delay(500);
-    digitalWrite(pins[active], LOW);
+    digitalWrite(pins[active], 1-polarities[active]);
     delay(500);
 
     active = (active + 1) % 5;
