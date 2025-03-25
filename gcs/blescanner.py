58f6104db483ebc4a0c6083675c66a196044e8ea
simondlevy <simon.d.levy@gmail.com>
2025-03-25 17:34:23 -0400

    modified:   blescanner.py

--- gcs/blescanner.py
+++ gcs/blescanner.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
 
+import asyncio
 from bleak import BleakScanner
 
-def main():
+async def main():
+    stop_event = asyncio.Event()
 
-    scanner = BleakScanner()
+    # TODO: add something that calls stop_event.set()
 
-main()
+    def callback(device, advertising_data):
+
+        print(advertising_data)
+
+    async with BleakScanner(callback) as scanner:
+
+        # Important! Wait for an event to trigger stop, otherwise scanner
+        # will stop immediately.
+        await stop_event.wait()
+
+    # scanner stops when block exits
+    ...
+
+asyncio.run(main())
