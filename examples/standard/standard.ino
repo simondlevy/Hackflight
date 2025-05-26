136bad7b0cc4da459b1f710185d92367eb3e96b2
simondlevy <simon.d.levy@gmail.com>
2025-05-26 15:31:28 -0400

    modified:   standard.ino

--- examples/standard/standard.ino
+++ examples/standard/standard.ino
@@ -15,11 +15,35 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+// External libraries
 #include <arduino_freertos.h>
+#include <BMI088.h>
 
+// Hackflight
 #include <system.h>
+#include <tasks/imu.hpp>
 
 static const uint8_t FLOWDECK_CS_PIN = 13;
+
+static const uint8_t GYRO_INTERRUPT_PIN = 4;
+
+static Bmi088Accel _accel(Wire, 0x19);
+static Bmi088Gyro _gyro(Wire, 0x69);
+
+static ImuTask * _imuTask;
+
+static volatile bool got_gyro_interrupt;
+
+static void gyro_interrupt_handler()
+{
+    got_gyro_interrupt = true;
+}
+
+static void report_forever(const char * msg)
+{
+    printf("%s\n", msg);
+    delay(500);
+}
 
 FLASHMEM __attribute__((noinline)) void setup()
 {
@@ -31,8 +55,40 @@ FLASHMEM __attribute__((noinline)) void setup()
         Serial.println();
         Serial.flush();
     }
+
+    pinMode(GYRO_INTERRUPT_PIN, arduino::INPUT);
+    attachInterrupt(GYRO_INTERRUPT_PIN, gyro_interrupt_handler,
+            arduino::RISING);  
 }
 
 void loop()
 {
 }
+
+void ImuTask::readGyroRaw(Axis3i16* dataOut)
+{
+    (void)dataOut;
+}
+
+void ImuTask::readAccelRaw(Axis3i16 * dataOut)
+{
+    (void)dataOut;
+}
+
+bool uartReadByte(uint8_t * byte)
+{
+    (void)byte;
+    return false;
+}
+
+void uartWriteByte(const uint8_t byte)
+{
+    (void)byte;
+}
+ 
+void ImuTask::deviceInit(void)
+{
+    _imuTask = this;
+}
+
+
