f85758925514bd2bca9f27f2c599f3ab02190a3d
simondlevy <simon.d.levy@gmail.com>
2025-05-26 23:42:12 -0400

    deleted:    bolt/src/imu_bmi088.cpp

--- bolt/src/main.cpp
+++ bolt/src/main.cpp
@@ -267,6 +267,21 @@ void ImuTask::deviceInit(void)
     portENABLE_INTERRUPTS();
 }
 
+float ImuTask::gyroRaw2Dps(const int16_t raw)
+{
+    return (float)raw * 2 * 2000 / 65536.f;
+}
+
+float ImuTask::accelRaw2Gs(const int16_t raw)
+{
+    return (float)raw * 2 * 24 / 65536.f;
+}
+
+const float ImuTask::rawGyroVarianceBase()
+{
+    return 100;
+}
+
 extern "C" {
 
     VL53L1_Error VL53L1_WaitUs(VL53L1_Dev_t *pdev, int32_t usec)
