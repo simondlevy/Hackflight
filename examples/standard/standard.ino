8f52ffcd436f212e110cb4e096af7ee7da17d5ec
simondlevy <simon.d.levy@gmail.com>
2025-05-28 01:13:02 -0400

    modified:   standard.ino

--- examples/standard/standard.ino
+++ examples/standard/standard.ino
@@ -1,26 +1,18 @@
-/*
- * This file is part of the FreeRTOS port to Teensy boards.
- * Copyright (c) 2020-2024 Timo Sandmann
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library. If not, see <http://www.gnu.org/licenses/>.
- */
-
 /**
- * @file    main.cpp
- * @brief   FreeRTOS example for Teensy boards
- * @author  Timo Sandmann
- * @date    17.05.2020
+ *
+ * Copyright (C) 2025 Simon D. Levy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, in version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "arduino_freertos.h"
@@ -54,9 +46,11 @@ static void task2(void*) {
     }
 }
 
-FLASHMEM __attribute__((noinline)) void setup() {
+FLASHMEM __attribute__((noinline)) void setup() 
+{
     Serial.begin(0);
-    delay(2'000);
+
+    //delay(2'000);
 
     if (CrashReport) {
         Serial.print(CrashReport);
