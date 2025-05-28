cefdfe60d57d8724fed09621b1d8da5886b496ea
simondlevy <simon.d.levy@gmail.com>
2025-05-28 01:14:34 -0400

    modified:   standard/standard.ino

--- examples/standard/standard.ino
+++ examples/standard/standard.ino
@@ -52,8 +52,6 @@ FLASHMEM __attribute__((noinline)) void setup()
 {
     Serial.begin(0);
 
-    //delay(2'000);
-
     if (CrashReport) {
         Serial.print(CrashReport);
         Serial.println();
