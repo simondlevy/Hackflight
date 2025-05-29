4e52e5c9734f53aba9cc4e4a61a629cc6144dcc0
simondlevy <simon.d.levy@gmail.com>
2025-05-28 22:36:06 -0400

    Appear to be reading from VL53l1X

--- examples/standard/standard.ino
+++ examples/standard/standard.ino
@@ -15,8 +15,12 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "arduino_freertos.h"
-#include "avr/pgmspace.h"
+#include <Wire.h>
+
+#include <arduino_freertos.h>
+#include <avr/pgmspace.h>
+
+#include <VL53L1X.h>
 
 #include <system.h>
 #include "tasks/zranger2.hpp"
@@ -24,9 +28,23 @@
 static const uint8_t LED_PIN = 5;
 static const uint8_t FLOWDECK_CS_PIN = 10;
 
+static VL53L1X _vl53l1x;
+
+static void reportForever(const char *msg)
+{
+    while (true) {
+        printf("%s\n", msg);
+        delay(500);
+    }
+}
+
 FLASHMEM __attribute__((noinline)) void setup() 
 {
     Serial.begin(0);
+
+    Wire1.begin();
+    Wire1.setClock(400000);
+    _vl53l1x.setBus(&Wire1);
 
     if (CrashReport) {
         Serial.print(CrashReport);
@@ -45,12 +63,19 @@ void debug(const char * msg)
 }
 
 void ZRangerTask::hardware_init()
-{
+{ 
+    if (!_vl53l1x.init()) {
+        reportForever("VL53L1X::init() failed");
+    }
+
+    _vl53l1x.setDistanceMode(VL53L1X::Long);
+    _vl53l1x.setMeasurementTimingBudget(50000);
+    _vl53l1x.startContinuous(50);
 }
 
 float ZRangerTask::hardware_read()
 {
-    return 0;
+    return (float)_vl53l1x.read();
 }
 
 
