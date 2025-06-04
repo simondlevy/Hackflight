b5e6e99a9ddd5d5e7b764a29b49154624043dff8
simondlevy <simon.d.levy@gmail.com>
2025-06-04 15:53:00 -0400

    modified:   spikes.sh

--- clients/spikes.sh
+++ clients/spikes.sh
@@ -1,4 +1,1 @@
-~/Desktop/spikeplotter/spikeplot.py \
-    -f ~/Desktop/framework/networks/difference_risp_train.txt  \
-    -a B8:27:EB:3F:AB:47  \
-    -p 3
+~/Desktop/spikeplotter/spikeplot.py -n 2 -a B8:27:EB:3F:AB:47 -p 3
