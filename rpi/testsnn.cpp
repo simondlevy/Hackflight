0dfb6723c01cd8a008c94ce43ea233e55572eb2f
simondlevy <simon.d.levy@gmail.com>
2025-06-09 18:41:35 -0400

    modified:   testsnn.cpp

--- rpi/testsnn.cpp
+++ rpi/testsnn.cpp
@@ -24,6 +24,10 @@
 
 #include <snn_util.hpp>
 
+static const double SCALE = 0.1;
+
+static const double OFFSET = 0;
+
 int main(int argc, char ** argv)
 {
     if (argc < 2) {
@@ -33,9 +37,9 @@ int main(int argc, char ** argv)
 
     auto snn = SNN(argv[1], "risp");
 
-    for (int j=-10; j<=10; ++j) {
+    for (int j=-9; j<=9; ++j) {
 
-        for (int k=-10; k<=10; ++k) {
+        for (int k=-9; k<=9; ++k) {
 
             vector<double> observations = { (double)j/10, (double)k/10 };
 
@@ -43,7 +47,7 @@ int main(int argc, char ** argv)
 
             snn.step(observations, counts);
 
-            //const double action = counts[0] * scale + offset;
+            //const double action = counts[0] * SCALE + OFFSET;
 
             printf("%+3.3f - %+3.3f = %d\n",
                     observations[0], observations[1], counts[0]);
