e7c67fa6ac2c351183181832f07d51e38e0c8f00
simondlevy <simon.d.levy@gmail.com>
2025-06-09 20:17:17 -0400

    modified:   newspikes.sh

--- clients/newspikes.sh
+++ clients/newspikes.sh
@@ -1,3 +1,3 @@
 BTADDR=B8:27:EB:3F:AB:47
 # BTADDR=B8:27:EB:E0:1D:07
-~/Desktop/spikeplotter/spikeplot.py -n 1 -a $BTADDR -p 3
+~/Desktop/spikeplotter/spikeplot.py -f ~/Desktop/framework/networks/difference_risp_train.txt -a $BTADDR -p 3
