f25a03baec94f487489b1b715d7dc93dcdb6445b
simondlevy <simon.d.levy@gmail.com>
2025-06-11 21:06:17 -0400

    Tested with 100 values

--- webots/plugins/physics/newsnn/difference_risp.cpp
+++ webots/plugins/physics/newsnn/difference_risp.cpp
@@ -136,7 +136,7 @@ static double get_spike_time(const float inp, const double max)
     return round(max * (1 - inp) / 2);
 }
 
-int main(int argc, char **argv) 
+int main() 
 {
     static const char * NETWORK_FILENAME = "/home/levys/Desktop/diffnet/difference_risp_plank.txt";
 
@@ -170,43 +170,39 @@ int main(int argc, char **argv)
         }
     }
 
-    // Apply spikes ----------------------------------------------------------
+    for (int j=-10; j<=+10; ++j) {
 
-    if (argc < 3) {
-        fprintf(stderr, "Usage: %s INP1 INP2\n", argv[0]);
-        exit(1);
+        for (int k=-10; k<+10; ++k) {
+
+            const double inp1 = (double)j / 10;
+            const double inp2 = (double)k / 10;
+
+            const double spike_time_1 = get_spike_time(inp1, MAX);
+            const double spike_time_2 = get_spike_time(inp2, MAX);
+
+            vector <Spike> spikes_array = {};
+
+            apply_spike(net, proc, 0, spike_time_1, spikes_array);
+            apply_spike(net, proc, 1, spike_time_2, spikes_array);
+            apply_spike(net, proc, 2, 0, spikes_array);
+
+            if (network_processor_validation(net, proc)) {
+
+                const auto sim_time = 3 * MAX + 2;
+
+                proc->run(sim_time);
+
+                spikes_array.clear();
+            }
+
+            const auto out = proc->output_vectors()[0][0];
+
+            const auto time = out == MAX + 1 ? -2 : out;
+
+            const auto diff = (time-(MAX))*4/(2*MAX)-2;
+
+            printf("%+3.3f - %+3.3f = %+3.3f (%+3.3f)\n",
+                    inp1, inp2, diff, inp1-inp2);
+        }
     }
-
-    const double inp1 = atof(argv[1]);
-    const double inp2 = atof(argv[2]);
-
-    const double spike_time_1 = get_spike_time(inp1, MAX);
-    const double spike_time_2 = get_spike_time(inp2, MAX);
-
-    vector <Spike> spikes_array = {};
-
-    apply_spike(net, proc, 0, spike_time_1, spikes_array);
-    apply_spike(net, proc, 1, spike_time_2, spikes_array);
-    apply_spike(net, proc, 2, 0, spikes_array);
-
-    // Run -------------------------------------------------------------------
-
-    if (network_processor_validation(net, proc)) {
-
-        const auto sim_time = 3 * MAX + 2;
-
-        proc->run(sim_time);
-
-        spikes_array.clear();
-    }
-
-    // Output -----------------------------------------------------------------
-
-    const auto out = proc->output_vectors()[0][0];
-
-    const auto time = out == MAX + 1 ? -2 : out;
-
-    const auto diff = (time-(MAX))*4/(2*MAX)-2;
-
-    printf("%3.3f -%3.3f = %3.3f\n", inp1, inp2, diff);
 }
