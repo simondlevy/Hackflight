7a041f77fe8de7a47623b80b7b4b4752ec1b344c
simondlevy <simon.d.levy@gmail.com>
2025-06-24 00:12:27 -0400

    =More newsnn

--- webots/plugins/physics/newsnn/vector_sizes.h
+++ webots/plugins/physics/newsnn/vector_sizes.h
@@ -1,9 +1,8 @@
 #pragma once
 
-static const size_t N_NODES = 8;
-static const size_t NEW_N_NODES = 7;
+static const size_t N_NODES = 7;
 static const size_t N_EDGES = 20;
-static const size_t N_INPUTS = 20;
+static const size_t N_INPUTS = 3;
 static const size_t N_OUTPUTS = 20;
 
 
