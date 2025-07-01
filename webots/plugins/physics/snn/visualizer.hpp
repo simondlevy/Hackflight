bd571409638be18f0e37ba9e3d54c6ee98071fa1
simondlevy <simon.d.levy@gmail.com>
2025-07-01 18:00:06 -0400

    modified:   visualizer.hpp

--- webots/plugins/physics/snn/visualizer.hpp
+++ webots/plugins/physics/snn/visualizer.hpp
@@ -16,9 +16,10 @@
 
 #pragma once
 
+#include <vector>
+
 #include <posix-utils/server.hpp>
 
-#if 0
 class Visualizer {
 
     private:
@@ -30,82 +31,53 @@ class Visualizer {
         const char * NETWORK_PATH =
             "%s/Desktop/2025-diff-network/levy/max_%d.txt";
 
-        // For visualization
-        static constexpr double I_SCALE = 50 / MAX_SPIKE_TIME;
-        static constexpr double D_SCALE = 250 / MAX_SPIKE_TIME;
-        static constexpr double O_SCALE = 25 / MAX_SPIKE_TIME;
-        static constexpr double S_BIAS = 0.8 * MAX_SPIKE_TIME;
-        static constexpr double S_SCALE = 125 / MAX_SPIKE_TIME;
-
-        Framework _framework = Framework(MAX_SPIKE_TIME);
+        float _max_spike_time;
 
         ServerSocket _spikeServer;
 
-        void send_spikes_to_visualizer(
-        {
-            static uint32_t _count;
-
-            if (_count++ == VIZ_SEND_PERIOD) {
-                const vector<int> tmp = _framework.get_neuron_counts();
-                const vector<int> counts = {
-                    (int)(spike_time_1 * I_SCALE),
-                    (int)(spike_time_2 * I_SCALE),
-                    1,
-                    (int)(tmp[3] * D_SCAE),
-                    (int)(tmp[4] * D_SCALE),
-                    (int)((out - MAX_SPIKE_TIME) * O_SCALE),
-                    (int)((tmp[6] - S_BIAS) * S_SCALE)
-                };
-
-                const string msg = _framework.make_viz_message(counts);
-                _spikeServer.sendData((uint8_t *)msg.c_str(), msg.length());
-                _count = 0;
-            }
-        }
-
     public:
 
-        void init(const bool visualize=false)
+        void init(const float max_spike_time)
         {
-            // Load the network
-            char path[256] = {};
-            sprintf(path, NETWORK_PATH, getenv("HOME"), (int)MAX_SPIKE_TIME);
-            _framework.load(path);
+            _max_spike_time = max_spike_time;
 
-            // Listen for and accept connections from vizualization client
-            if (visualize) {
-                _spikeServer.open(VIZ_PORT);
-                _spikeServer.acceptClient();
-            }
-
-            _visualize = visualize;
+            _spikeServer.open(VIZ_PORT);
+            _spikeServer.acceptClient();
         }
 
-        double run(const float inp1, const float inp2)
+        void send_spikes(
+                const int i1_spike_time,
+                const int i2_spike_time,
+                const int d1_spike_time,
+                const int d2_spike_time,
+                const int s_spike_time,
+                const int o_spike_time)
         {
-            // Turn the inputs into spikes
-            const double spike_time_1 = _framework.value_to_spike_time(inp1);
-            const double spike_time_2 = _framework.value_to_spike_time(inp2);
+            const float i_scale = 50 / _max_spike_time;
+            const float d_scale = 250 / _max_spike_time;
+            const float o_scale = 25 / _max_spike_time;
+            const float s_bias = 0.8 * _max_spike_time;
+            const float s_scale = 125 / _max_spike_time;
 
-            // Apply the spikes to the network
-            _framework.apply_spike(0, spike_time_1);
-            _framework.apply_spike(1, spike_time_2);
-            _framework.apply_spike(2, 0);
+            static uint32_t _tick;
 
-            // Run the network
-            _framework.run(3 * MAX_SPIKE_TIME + 2);
+            if (_tick++ == VIZ_SEND_PERIOD) {
 
-            // Get the output network's firing time
-            const double out = _framework.get_output_vector()[0];
-            const double time = out == MAX_SPIKE_TIME + 1 ? -2 : out;
+                const std::vector<int> counts = {
+                    (int)(i1_spike_time * i_scale),
+                    (int)(i2_spike_time * i_scale),
+                    1,
+                    (int)(d1_spike_time * d_scale),
+                    (int)(d2_spike_time * d_scale),
+                    (int)((o_spike_time - _max_spike_time) * o_scale),
+                    (int)((s_spike_time - s_bias) * s_scale)
+                };
 
-            // Send spikes to the visualizer if indicated
-            if (_visualize) {
-                send_spikes_to_visualizer(spike_time_1, spike_time_2, out);
+                //const string msg = _framework.make_viz_message(counts);
+
+                //_spikeServer.sendData((uint8_t *)msg.c_str(), msg.length());
+
+                _tick = 0;
             }
-
-            // Convert the firing time to a difference in [-2,+2]
-            return (time-MAX_SPIKE_TIME)*2 / MAX_SPIKE_TIME - 2;
         }
 };
-#endif
