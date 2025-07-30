8d3e06df2a27f800c67d8c1eaa800a5c3d85f19f
simondlevy <simon.d.levy@gmail.com>
2025-07-30 18:01:01 -0400

    modified:   motortest.ino

--- examples/motortest/motortest.ino
+++ examples/motortest/motortest.ino
@@ -1,20 +1,20 @@
 /*
-   Test ESCs.  Make sure to run Calibrate sketch first
+   Test ESCs
 
-   This file is part of Teensy-OneShot125.
+   This file is part of Hackflight.
 
-   Teensy-OneShot125 is free software: you can redistribute it and/or modify it
+   Hackflight is free software: you can redistribute it and/or modify it
    under the terms of the GNU General Public License as published by the Free
    Software Foundation, either version 3 of the License, or (at your option)
    any later version.
 
-   Teensy-OneShot125 is distributed in the hope that it will be useful, but
+   Hackflight is distributed in the hope that it will be useful, but
    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
    or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
    more details.
 
    You should have received a copy of the GNU General Public License along with
-   Teensy-OneShot125. If not, see <https://www.gnu.org/licenses/>.
+   Hackflight. If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include <oneshot125.hpp>
@@ -30,30 +30,57 @@ static auto motors = OneShot125(PINS);
 static float val;
 static float dir;
 
+static volatile bool running;
+
+void serialEvent()
+{
+  while (Serial.available()) {
+    Serial.read();
+  }
+
+  running = !running;
+}
+
 void setup() 
 {
     Serial.begin(115200);
 
     motors.arm(); 
 
-    val = 0;
     dir = +1;
 
 }
 
 static float inputGet()
 {
-    val += INC * dir;
+    if (running) {
 
-    if (val >= MAX) {
-        dir = -1;
+        val += INC * dir;
+
+        if (val >= MAX) {
+            dir = -1;
+        }
+
+        if (val <= 0) {
+            dir = +1;
+        }
+    }
+    else {
+        val = 0;
     }
 
-    if (val <= 0) {
-        dir = +1;
-    }
-    
     return val;
+}
+
+static void prompt()
+{
+    static uint32_t tprev;
+    const uint32_t tcurr = millis();
+    if (tcurr - tprev > 1000) {
+      tprev = tcurr;
+      Serial.print("Hit Enter to ");
+      Serial.println(running ? "stop" : "start");
+    }
 }
 
 void loop() 
@@ -68,4 +95,6 @@ void loop()
     motors.set(3, pulseWidth);
 
     motors.run();
+
+    prompt();
 }
