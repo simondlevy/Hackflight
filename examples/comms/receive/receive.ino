274751af24722aa262201b46f60765062943194d
simondlevy <simon.d.levy@gmail.com>
2025-08-25 18:39:40 -0400

    modified:   receive.ino

--- examples/comms/receive/receive.ino
+++ examples/comms/receive/receive.ino
@@ -1,15 +1,17 @@
+HardwareSerial Serial2(USART2);
+
 void setup() 
 {
-    Serial5.begin(115200);
+    Serial2.begin(115200);
 }
 
 void loop() 
 {
     static uint8_t k;
 
-    Serial5.write(65 + k);
+    Serial2.write(65 + k);
 
     k = (k + 1) % 26;
 
-    delay(250);
+    delay(50);
 }
