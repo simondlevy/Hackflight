289625d1a09bdf5ef86ccdf6809f8a3fd1d414c9
simondlevy <simon.d.levy@gmail.com>
2025-08-27 21:00:38 -0400

    modified:   teensy.ino

--- teensy/teensy.ino
+++ teensy/teensy.ino
@@ -11,31 +11,12 @@ using namespace arduino;
 #include <safety.hpp>
 #include <tasks/led.hpp>
 
-static SemaphoreHandle_t canStartMutex;
-static StaticSemaphore_t canStartMutexBuffer;
+//static SemaphoreHandle_t canStartMutex;
+//static StaticSemaphore_t canStartMutexBuffer;
 
 static Safety safety;
 
 static LedTask ledTask;
-
-/*
-static void systemTask(void*) 
-{
-    canStartMutex = xSemaphoreCreateMutexStatic(&canStartMutexBuffer);
-    xSemaphoreTake(canStartMutex, portMAX_DELAY);
-
-    pinMode(LED_BUILTIN, OUTPUT);
-
-    while (true) {
-        digitalWriteFast(LED_BUILTIN, LOW);
-        vTaskDelay(pdMS_TO_TICKS(500));
-
-        digitalWriteFast(LED_BUILTIN, HIGH);
-        vTaskDelay(pdMS_TO_TICKS(500));
-    }
-
-    vTaskDelete(nullptr);
-}*/
 
 void setup() 
 {
@@ -46,8 +27,6 @@ void setup()
         Serial.println();
         Serial.flush();
     }
-
-    //xTaskCreate(systemTask, "systemTask", 128, nullptr, 2, nullptr);
 
     ledTask.begin(&safety, LED_BUILTIN, false);
 
