b904172a0bc027c0f393c782027e3ce3473fda32
simondlevy <simon.d.levy@gmail.com>
2025-09-01 15:52:49 -0400

    modified:   motors.ino Use override pin

--- newbolt/motors/motors.ino
+++ newbolt/motors/motors.ino
@@ -1,72 +1,50 @@
-#include <dshot_stm32f4.h>
+/*
+   Test ESCs -- remove propellers first!
+
+   This file is part of Hackflight.
+
+   Hackflight is free software: you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the Free
+   Software Foundation, either version 3 of the License, or (at your option)
+   any later version.
+
+   Hackflight is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+   more details.
+
+   You should have received a copy of the GNU General Public License along with
+   Hackflight. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <oneshot125.hpp>
+#include <input_keyboard.hpp>
 
 #include <vector>
 
-static const uint8_t PIN = PB11;
+static const std::vector<uint8_t> MOTOR_PINS = {PA1};
+static const std::vector<uint8_t> OVERRIDE_PINS = {PA0};
 
-static std::vector<uint8_t> pins = {PIN};
+static auto motors = OneShot125(MOTOR_PINS);
 
-static const uint32_t FREQUENCY = 8000;
-
-static Stm32F4Dshot dshot;
-
-
-extern "C" void DMA2_Stream1_IRQHandler(void) 
-{
-    dshot.handleDmaIrqStream1();
-}
-
-extern "C" void DMA2_Stream2_IRQHandler(void) 
-{
-    dshot.handleDmaIrqStream2();
-}
-
-static float motorval;
-
-void serialEvent(void)
-{
-    if (Serial.available()) {
-        Serial.read();
-        motorval = motorval == 0 ? 0.1 : 0;
-    }
-}
-
-static void run(const uint32_t usec)
-{
-    static uint32_t prev;
-
-    if (usec-prev > 1000000/FREQUENCY) {
-        prev = usec;
-        dshot.write(&motorval);
-    }
-}
-
-static void prompt(const uint32_t usec)
-{
-    static uint32_t prev;
-
-    if (usec-prev > 1000000) {
-        prev = usec;
-        Serial.println(
-                motorval == 0 ?
-                "After removing propellers, hit Enter to start motor" :
-                "Hit Enter to stop motor"
-                );
-    }
-}
-
-void setup(void)
+void setup() 
 {
     Serial.begin(115200);
 
-    dshot.begin(pins);
+    inputInit();
+
+    pinMode(OVERRIDE_PINS[0], OUTPUT);
+    digitalWrite(OVERRIDE_PINS[0], HIGH);
+
+    motors.arm(); 
 }
 
-void loop(void)
+void loop() 
 {
-    const auto usec = micros();
+    auto pulseWidth = (uint8_t)(125 * (inputGet() + 1));
 
-    prompt(usec);
 
-    run(usec);
+    motors.set(0, pulseWidth);
+
+    motors.run();
 }
