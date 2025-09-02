7b10b3611282fa168f003d957325d8f94b689c41
simondlevy <simon.d.levy@gmail.com>
2025-09-01 21:00:21 -0400

    modified:   rtos.ino

--- newbolt/tests/rtos/rtos.ino
+++ newbolt/tests/rtos/rtos.ino
@@ -4,52 +4,56 @@ static const uint8_t LED_PIN = PC0;
 
 SemaphoreHandle_t sem;
 
-static void Thread1(void* arg) {
-  UNUSED(arg);
-  while (1) {
+static void Thread1(void* arg) 
+{
+    (void)arg;
 
-    xSemaphoreTake(sem, portMAX_DELAY);
+    while (true) {
 
-    digitalWrite(LED_PIN, LOW);
-  }
+        xSemaphoreTake(sem, portMAX_DELAY);
+
+        digitalWrite(LED_PIN, LOW);
+    }
 }
 
 static void Thread2(void* arg) {
-  UNUSED(arg);
-  pinMode(LED_PIN, OUTPUT);
+    
+    (void)arg;
 
-  while (1) {
+    pinMode(LED_PIN, OUTPUT);
 
-    digitalWrite(LED_PIN, HIGH);
+    while (true) {
 
-    vTaskDelay((200L * configTICK_RATE_HZ) / 1000L);
+        digitalWrite(LED_PIN, HIGH);
 
-    xSemaphoreGive(sem);
+        vTaskDelay((200L * configTICK_RATE_HZ) / 1000L);
 
-    vTaskDelay((200L * configTICK_RATE_HZ) / 1000L);
-  }
+        xSemaphoreGive(sem);
+
+        vTaskDelay((200L * configTICK_RATE_HZ) / 1000L);
+    }
 }
 
 void setup() 
 {
-  portBASE_TYPE s1, s2;
+    portBASE_TYPE s1, s2;
 
-  Serial.begin(115200);
+    Serial.begin(115200);
 
-  sem = xSemaphoreCreateCounting(1, 0);
+    sem = xSemaphoreCreateCounting(1, 0);
 
-  s1 = xTaskCreate(Thread1, NULL, configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+    s1 = xTaskCreate(Thread1, NULL, configMINIMAL_STACK_SIZE, NULL, 2, NULL);
 
-  s2 = xTaskCreate(Thread2, NULL, configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+    s2 = xTaskCreate(Thread2, NULL, configMINIMAL_STACK_SIZE, NULL, 1, NULL);
 
-  if (sem== NULL || s1 != pdPASS || s2 != pdPASS ) {
-    Serial.println(F("Creation problem"));
-    while(1);
-  }
+    if (sem== NULL || s1 != pdPASS || s2 != pdPASS ) {
+        Serial.println(F("Creation problem"));
+        while (true);
+    }
 
-  vTaskStartScheduler();
-  Serial.println("Insufficient RAM");
-  while(1);
+    vTaskStartScheduler();
+    Serial.println("Insufficient RAM");
+    while (true);
 }
 
 void loop() 
