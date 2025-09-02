2fd841efdbf6a5beb59583b97beb93251eca61da
simondlevy <simon.d.levy@gmail.com>
2025-09-01 21:34:54 -0400

    modified:   newbolt/main/main.ino

--- newbolt/main/main.ino
+++ newbolt/main/main.ino
@@ -1,60 +1,44 @@
 #include <STM32FreeRTOS.h>
 
-static const uint8_t LED_PIN = PC0;
-
-SemaphoreHandle_t sem;
-
-static void Thread1(void* arg) {
-  UNUSED(arg);
-  while (1) {
-
-    xSemaphoreTake(sem, portMAX_DELAY);
-
-    digitalWrite(LED_PIN, LOW);
-  }
+static uint32_t m2t(const uint32_t msec)
+{
+    return (200L * configTICK_RATE_HZ) / 1000L;
 }
 
-static void Thread2(void* arg) {
-  UNUSED(arg);
-  pinMode(LED_PIN, OUTPUT);
+static const uint8_t LED_PIN = PC0;
 
-  while (1) {
+static void Task2(void* arg) {
     
-    digitalWrite(LED_PIN, HIGH);
+    (void)arg;
 
-    vTaskDelay((200L * configTICK_RATE_HZ) / 1000L);
+    pinMode(LED_PIN, OUTPUT);
 
-    xSemaphoreGive(sem);
+    while (true) {
 
-    vTaskDelay((200L * configTICK_RATE_HZ) / 1000L);
-  }
+        digitalWrite(LED_PIN, HIGH);
+
+        vTaskDelay(m2t(200));
+
+        digitalWrite(LED_PIN, LOW);
+
+        vTaskDelay(m2t(200));
+
+    }
 }
 
 void setup() 
 {
-  portBASE_TYPE s1, s2;
+    Serial.begin(115200);
 
-  Serial.begin(115200);
+    xTaskCreate(Task2, NULL, configMINIMAL_STACK_SIZE, NULL, 1, NULL);
 
-  sem = xSemaphoreCreateCounting(1, 0);
+    vTaskStartScheduler();
 
-  s1 = xTaskCreate(Thread1, NULL, configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+    Serial.println("Insufficient RAM");
 
-  s2 = xTaskCreate(Thread2, NULL, configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-
-  if (sem== NULL || s1 != pdPASS || s2 != pdPASS ) {
-    Serial.println(F("Creation problem"));
-    while(1);
-  }
-
-  
-  vTaskStartScheduler();
-
-  Serial.println("Insufficient RAM");
-
-  while(1);
+    while (true);
 }
 
-void loop()
+void loop() 
 {
 }
