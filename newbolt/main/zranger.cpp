7ca0d972970942888f06401fbc26f8ae6e64799b
simondlevy <simon.d.levy@gmail.com>
2025-09-01 22:57:35 -0400

    modified:   zranger.cpp

--- newbolt/main/zranger.cpp
+++ newbolt/main/zranger.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2025 Simon D. Levy
+ * Copyright (C) 2011-2018 Bitcraze AB, 2025 Simon D. Levy
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,16 +14,32 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#pragma once
+#include <Wire.h>
+#include <VL53L1X.h>
 
-#include <stdbool.h>
+#include <zranger_api.h>
+
+VL53L1X _vl53l1x;
 
 bool zranger_deviceInit()
 {
+    Wire.begin();
+    Wire.setClock(400000);
+
+    _vl53l1x.setBus(&Wire);
+
+    if (!_vl53l1x.init()) {
+        return false;
+    }
+
+    _vl53l1x.setDistanceMode(VL53L1X::Medium);
+    _vl53l1x.setMeasurementTimingBudget(25000); // usec
+    _vl53l1x.startContinuous(25); // msec
+
     return true;
 }
 
 float zranger_deviceRead()
 {
-    return 0;
+    return (float)_vl53l1x.read();
 }
