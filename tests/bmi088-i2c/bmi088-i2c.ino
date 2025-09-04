20fa004e31787287ce23901c75f8ea04bc58b928
simondlevy <simon.d.levy@gmail.com>
2025-09-04 15:46:26 -0400

    BMI-088 working on TinyPICO

--- tests/bmi088-i2c/bmi088-i2c.ino
+++ tests/bmi088-i2c/bmi088-i2c.ino
@@ -2,7 +2,7 @@
 
 #include <BMI088.h>
 
-static const uint8_t GYRO_INTERRUPT_PIN = 6;
+static const uint8_t GYRO_INTERRUPT_PIN = 32;
 
 static const uint8_t ACCEL_ADDRESS = 0x19;
 static const uint8_t GYRO_ADDRESS = 0x69;
@@ -48,6 +48,7 @@ void setup()
 
 void loop() 
 {
+
     if (gyro_flag) {
 
         gyro_flag = false;
@@ -56,15 +57,12 @@ void loop()
 
         gyro.readSensor();
 
-        printf("%+05d\n", accel.getAccelX_raw());
-
-        /*
         printf("ax=%+3.3f ay=%+3.3f az=%+3.3f m/s^2 | gx=%+3.3f gy=%+3.3f gz=%+3.3f rad/sec\n", 
                 accel.getAccelX_mss(),
                 accel.getAccelY_mss(),
                 accel.getAccelZ_mss(),
                 gyro.getGyroX_rads(),
                 gyro.getGyroY_rads(),
-                gyro.getGyroZ_rads());*/
+                gyro.getGyroZ_rads());
     }
 }
