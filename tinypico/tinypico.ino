c545ba19bc53bb67784bfccbccf704de882b4965
simondlevy <simon.d.levy@gmail.com>
2025-09-04 21:38:47 -0400

    modified:   tinypico.ino

--- tinypico/tinypico.ino
+++ tinypico/tinypico.ino
@@ -3,20 +3,29 @@
 
 static const uint8_t LED_SIGNAL_PIN = 22;
 
+static const uint8_t UART_RX_PIN = 21;
+static const uint8_t UART_TX_PIN = 32;
+
 static TinyPICO tp = TinyPICO(); 
 
 static BluetoothSerial serialBt;
+
+static HardwareSerial serialUart(2);
 
 static void setLed(const bool on)
 {
     tp.DotStar_SetPixelColor(on ? 255 : 0, 0, 0); 
 }
 
+HardwareSerial gpsSerial(2);
+
 void setup() 
 {
+    Serial.begin(115200);
+
     serialBt.begin("Esp32-BT");
 
-    Serial.begin(115200);
+    serialUart.begin(115200, SERIAL_8N1, UART_RX_PIN, UART_TX_PIN);
 
     setLed(false);
 
@@ -40,6 +49,10 @@ void loop()
             setLed(true);
         }
     }*/
+
+    while (serialUart.available()) {
+        Serial.print((char)serialUart.read());
+    }
 
     setLed(digitalRead(LED_SIGNAL_PIN));
 }
