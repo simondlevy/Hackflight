0cb2c18e6440a97c2caa746671ddd523ed8080d8
simondlevy <simon.d.levy@gmail.com>
2025-09-11 14:10:15 -0400

    modified:   blink.ino

--- darwinfpv/blink/blink.ino
+++ darwinfpv/blink/blink.ino
@@ -1,19 +1,16 @@
-//static const uint8_t LED_PIN = PC8; // XXX
+static const uint8_t LED_PIN = PC13; // XXX
 
 void setup() 
 {
-    //pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(LED_PIN, OUTPUT);
 
     Serial.begin(115200);
 }
 
 void loop() 
 {
-    /*
-       digitalWrite(LED_BUILTIN, HIGH);  
-       delay(1000);                     
-       digitalWrite(LED_BUILTIN, LOW); 
-       delay(1000);*/
-
-    Serial.println(micros());
+    digitalWrite(LED_PIN, HIGH);  
+    delay(1000);                     
+    digitalWrite(LED_PIN, LOW); 
+    delay(1000);
 }
