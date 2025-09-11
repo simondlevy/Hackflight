67b41e0fa7eb94356d4f71b1f671724d2817895a
simondlevy <simon.d.levy@gmail.com>
2025-09-11 18:09:06 -0400

    modified:   mpu6500.ino

--- darwinfpv/mpu6500/mpu6500.ino
+++ darwinfpv/mpu6500/mpu6500.ino
@@ -1,4 +1,20 @@
-#include <mpu6x00.h>
+/**
+ * Copyright (C) 2025 Simon D. Levy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, in version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <mpu6x00.hpp>
 
 static const uint8_t SCLK_PIN = PA5;
 static const uint8_t MISO_PIN = PA6;
@@ -46,10 +62,11 @@ void loop(void)
 
         imu.readSensor();
 
+        Serial.print("gx=");
         Serial.print(imu.getRawGyroX());
-        Serial.print("  ");
+        Serial.print("  gy=");
         Serial.print(imu.getRawGyroY());
-        Serial.print("  ");
+        Serial.print("  gz=");
         Serial.print(imu.getRawGyroZ());
         Serial.println(); 
 
