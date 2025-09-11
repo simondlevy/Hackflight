d1399022d82b211cd407ffe20212cff1aba9a181
simondlevy <simon.d.levy@gmail.com>
2025-09-11 18:29:20 -0400

    new file:   darwinfpv/led/led.ino

---
+++ darwinfpv/led/led.ino
@@ -0,0 +1,16 @@
+static const uint8_t LED_PIN = PC13;
+
+void setup() 
+{
+    pinMode(LED_PIN, OUTPUT);
+
+    Serial.begin(115200);
+}
+
+void loop() 
+{
+    digitalWrite(LED_PIN, HIGH);  
+    delay(1000);                     
+    digitalWrite(LED_PIN, LOW); 
+    delay(1000);
+}
