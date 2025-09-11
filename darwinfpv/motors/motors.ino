ac0e96cc26387bb68fceb9183cfb8c6494d0d54d
simondlevy <simon.d.levy@gmail.com>
2025-09-11 17:29:26 -0400

    Spinning all four motors on BabyApe

--- darwinfpv/motors/motors.ino
+++ darwinfpv/motors/motors.ino
@@ -15,47 +15,28 @@
  */
 
 #include <Arduino.h>
-
 #include <oneshot125.hpp>
+#include <input_keyboard.hpp>
 
-#include <tasks/motors.hpp>
-
-static const std::vector<uint8_t> MOTOR_PINS = {PA1, PB11, PA15, PB10};
-
-static const std::vector<uint8_t> POWER_SWITCH_PINS = {PA0, PB12, PC8, PC15};
+static const std::vector<uint8_t> MOTOR_PINS = {PB5, PB4, PB6, PB7};
 
 static auto motors = OneShot125(MOTOR_PINS);
 
-static void enableMotor(const uint8_t id)
+void setup()
 {
-    pinMode(POWER_SWITCH_PINS[id], OUTPUT);
-    digitalWrite(POWER_SWITCH_PINS[id], HIGH);
-}
-
-void MotorsTask::device_init()
-{
-    enableMotor(0);
-    enableMotor(1);
-    enableMotor(2);
-    enableMotor(3);
+    Serial.begin(115200);
 
     motors.arm(); }
 
-void MotorsTask::device_setRatio(const uint8_t id, const uint16_t ratio)
+void loop()
 {
-    const uint8_t pulse_usec = map(ratio, 0, 65535, 125, 250);
+    auto pulseWidth = (uint8_t)(125 * (inputGet() + 1));
 
-    motors.set(id, pulse_usec);
+    motors.set(0, pulseWidth);
+    motors.set(1, pulseWidth);
+    motors.set(2, pulseWidth);
+    motors.set(3, pulseWidth);
 
-    motors.run();
-}
-
-void MotorsTask::device_stop()
-{
-    motors.set(0, 125);
-    motors.set(1, 125);
-    motors.set(2, 125);
-    motors.set(3, 125);
 
     motors.run();
 }
