fbb5713b391b78513ca70b9e097dd73c8bb13ae4
simondlevy <simon.d.levy@gmail.com>
2025-09-16 11:00:57 -0400

    modified:   client/client.py

--- client/client.py
+++ client/client.py
@@ -34,8 +34,10 @@ except Exception as e:
 
 
 BLUETOOTH_ADDRESSES = {
-        'bolt': '64:B7:08:94:2A:32',
-        'teensy': '64:B7:08:93:71:1E'
+    'bolt': '64:B7:08:93:71:1E',
+    'teensy': '64:B7:08:86:F2:AE',
+    'tinypico': 'D4:D4:DA:AA:2E:F2',
+    'darwinfpv': '64:B7:08:87:AD:76'
 }
 
 BLUETOOTH_PORT = 1
