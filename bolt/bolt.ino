43163b2adee3230e9c9f3786fbbabf30052aadb1
simondlevy <simon.d.levy@gmail.com>
2025-09-18 17:17:35 -0400

    new file:   semaphore_example.cpp

--- bolt/bolt.ino
+++ bolt/bolt.ino
@@ -17,40 +17,6 @@ static Safety safety = Safety(&motors);
 static DebugTask debugTask;
 static LedTask ledTask;
 
-/*
-static xSemaphoreHandle sem;
-
-static void Thread1(void* arg) 
-{
-    (void)arg;
-
-    while (true) {
-
-        // Wait for signal from thread 2.
-        xSemaphoreTake(sem, portMAX_DELAY);
-
-        digitalWrite(LED_PIN, LOW);
-    }
-}
-
-static void Thread2(void* arg) 
-{
-    (void)arg;
-
-    pinMode(LED_PIN, OUTPUT);
-
-    while (true) {
-
-        digitalWrite(LED_PIN, HIGH);
-
-        vTaskDelay((200L * configTICK_RATE_HZ) / 1000L);
-
-        xSemaphoreGive(sem);
-
-        vTaskDelay((200L * configTICK_RATE_HZ) / 1000L);
-    }
-}*/
-
 static void systemTask(void *arg)
 {
 	debugTask.begin();
@@ -93,22 +59,6 @@ static void systemTask(void *arg)
 void setup() 
 {
     Serial.begin(115200);
-
-    /*
-    sem = xSemaphoreCreateCounting(1, 0);
-
-    portBASE_TYPE s1 =
-        xTaskCreate(Thread1, NULL, configMINIMAL_STACK_SIZE, NULL, 2, NULL);
-
-    portBASE_TYPE s2 =
-        xTaskCreate(Thread2, NULL, configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-
-    if (sem== NULL || s1 != pdPASS || s2 != pdPASS ) {
-        while (true) {
-            Serial.println("Problem creating tasks");
-            delay(500);
-        }
-    }*/
 
     xTaskCreate(
             systemTask, 
