147229a45886f446caff73802caa234c8f2bde52
simondlevy <simon.d.levy@gmail.com>
2025-09-18 16:42:08 -0400

    renamed:    newbolt/newbolt.ino -> bolt/bolt.ino

--- bolt/src/bosch/bstdr_comm_support.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "bstdr_types.h"
-
-bstdr_ret_t bstdr_burst_read(uint8_t dev_id, uint8_t reg_addr, 
-        uint8_t *reg_data, uint32_t len);
-
-bstdr_ret_t bstdr_burst_write(uint8_t dev_id, uint8_t reg_addr, 
-        uint8_t *reg_data, uint32_t len);
