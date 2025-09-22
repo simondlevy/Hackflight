7bb43847c2a621b3e10188f77c908c8f44796c39
simondlevy <simon.d.levy@gmail.com>
2025-09-21 20:01:21 -0400

    Cleanup

--- cf2/cf2.ino
+++ cf2/cf2.ino
@@ -1,6 +1,33 @@
+/**
+ *
+ * Copyright (C) 2025 Simon D. Levy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, in version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <Wire.h>   
 
-static TwoWire wire = TwoWire(PC9, PA8);
+#include <BMI088.h>
+
+static const uint8_t SDA_PIN = PC9;
+static const uint8_t SCL_PIN = PA8;
+static const uint8_t GYRO_INT_PIN = PC14;
+
+static TwoWire wire = TwoWire(SDA_PIN, SCL_PIN);
+
+static Bmi088Accel accel(spi, ACCEL_CS_PIN);
+
+static Bmi088Gyro gyro(spi, GYRO_CS_PIN);
 
 void setup()
 {
@@ -11,48 +38,6 @@ void setup()
     delay(100);
 }
 
-static void scan(TwoWire & wire, const char * name)
-{
-
-    Serial.print("Scanning ");
-    Serial.print(name);
-    Serial.println (" ...");
-
-    int nDevices = 0;
-
-    for(byte address = 1; address < 127; address++ ) 
-    {
-
-        wire.beginTransmission(address);
-
-        byte error = wire.endTransmission();
-
-        if (error == 0)
-        {
-            Serial.print("I2C device found at address 0x");
-            if (address<16) 
-                Serial.print("0");
-            Serial.println(address,HEX);
-
-            nDevices++;
-        }
-        else if (error==4) 
-        {
-            Serial.print("Unknown error at address 0x");
-            if (address<16) 
-                Serial.print("0");
-            Serial.println(address,HEX);
-        }    
-    }
-    if (nDevices == 0)
-        Serial.println("No I2C devices found\n");
-    else
-        Serial.println("done\n"); 
-
-    delay(1000);
-}
-
 void loop()
 {  
-    scan(wire, "Wire");
 }
--- cfbolt/imu.cpp
+++ cfbolt/imu.cpp
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright (C) 2011-2022 Bitcraze AB, 2025 Simon D. Levy
+ * Copyright (C) 2025 Simon D. Levy
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
