77ce3e12bda556928f060f19236b680d55945fc4
simondlevy <simon.d.levy@gmail.com>
2025-09-24 18:16:37 -0400

    modified:   tinypico/free_rtos_include.h

--- tinypico/free_rtos_include.h
+++ tinypico/free_rtos_include.h
@@ -15,4 +15,9 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-// FreeRTOS is built into ESP32
+#pragma once
+
+static bool xPortIsInsideInterrupt()
+{
+    return xPortInIsrContext();
+}
