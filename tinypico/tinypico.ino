ec3da1b945f11ae2486447f8942a3d582d6b24bc
simondlevy <simon.d.levy@gmail.com>
2025-09-25 17:16:14 -0400

    =More tinypico.ino

--- tinypico/tinypico.ino
+++ tinypico/tinypico.ino
@@ -1,8 +1,17 @@
+#include <free_rtos_include.h>
+
 #include <TinyPICO.h>
+
+static const auto STACKSIZE = 3 * configMINIMAL_STACK_SIZE; // arbitrary
 
 static TaskHandle_t Task1;
 
 static TinyPICO tinypico = TinyPICO();
+
+static StackType_t  taskStackBuffer[STACKSIZE]; 
+
+static StaticTask_t taskTaskBuffer;
+
 
 static void device_set(const bool on)
 {
@@ -22,14 +31,13 @@ static void fun(void * arg)
 
 void setup() 
 {
-    xTaskCreatePinnedToCore(
+    xTaskCreate(
             fun,   
             "Task1",     
             10000,       // stack size 
             NULL,        // parameter
             1,           // priority
-            &Task1,      
-            0);          // core
+            &Task1);      
 }
 
 void loop() 
