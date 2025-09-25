eaaf84c84f660c1eebabed75708c8eb4920cb4c6
simondlevy <simon.d.levy@gmail.com>
2025-09-25 17:39:04 -0400

    =More tinypico.ino

--- tinypico/tinypico.ino
+++ tinypico/tinypico.ino
@@ -4,6 +4,10 @@
 
 static const auto STACKSIZE = 3 * configMINIMAL_STACK_SIZE; // arbitrary
 
+static constexpr float HEARTBEAT_HZ = 1;
+static constexpr uint32_t PULSE_MSEC = 50;
+
+
 StaticTask_t xStaticTaskBuffer;
 
 StackType_t uxStaticTaskStack[STACKSIZE];
@@ -15,14 +19,28 @@ static void device_set(const bool on)
     tinypico.DotStar_SetPixelColor(on ? 255 : 0, 0, 0 );
 }
 
+void blink(const float rate)
+{
+    device_set(true);
+    vTaskDelay(PULSE_MSEC);
+    device_set(false);
+    vTaskDelay(1000/rate);
+}
+
+
 static void fun(void *) 
 {
+    TickType_t lastWakeTime = xTaskGetTickCount();
+
     while (true) {
 
+        blink(HEARTBEAT_HZ);
+
+        /*
         device_set(true);
         vTaskDelay(20);
         device_set(false);
-        vTaskDelay(1000);
+        vTaskDelay(1000);*/
     }
 }
 
@@ -30,14 +48,14 @@ void setup()
 {
 
     xTaskCreateStatic(
-        fun,      
-        "StaticTask",              
-        STACKSIZE,    
-        NULL, // argument
-        1,    // priority 
-        uxStaticTaskStack,         
-        &xStaticTaskBuffer         
-    );
+            fun,      
+            "StaticTask",              
+            STACKSIZE,    
+            NULL, // argument
+            1,    // priority 
+            uxStaticTaskStack,         
+            &xStaticTaskBuffer         
+            );
 }
 
 void loop()
