e2bb1ec5e3eb53a12ea1e4cb1b4eb263a98f16b4
simondlevy <simon.d.levy@gmail.com>
2025-09-25 17:08:41 -0400

    modified:   tinypico.ino

--- tinypico/tinypico.ino
+++ tinypico/tinypico.ino
@@ -1,58 +1,17 @@
-/*********
-  Rui Santos
-  Complete project details at http://randomnerdtutorials.com  
- *********/
-
 #include <TinyPICO.h>
 
-TaskHandle_t Task1;
-TaskHandle_t Task2;
+static TaskHandle_t Task1;
 
 static TinyPICO tinypico = TinyPICO();
-
-// LED pins
-const int led1 = 2;
-const int led2 = 4;
-
-void setup() {
-    Serial.begin(115200); 
-    pinMode(led1, OUTPUT);
-    pinMode(led2, OUTPUT);
-
-    //create a task that will be executed in the Task1code() function, with priority 1 and executed on core 0
-    xTaskCreatePinnedToCore(
-            Task1code,   /* Task function. */
-            "Task1",     /* name of task. */
-            10000,       /* Stack size of task */
-            NULL,        /* parameter of the task */
-            1,           /* priority of the task */
-            &Task1,      /* Task handle to keep track of created task */
-            0);          /* pin task to core 0 */                  
-    delay(500); 
-
-    //create a task that will be executed in the Task2code() function, with priority 1 and executed on core 1
-    xTaskCreatePinnedToCore(
-            Task2code,   /* Task function. */
-            "Task2",     /* name of task. */
-            10000,       /* Stack size of task */
-            NULL,        /* parameter of the task */
-            1,           /* priority of the task */
-            &Task2,      /* Task handle to keep track of created task */
-            1);          /* pin task to core 1 */
-    delay(500); 
-}
 
 static void device_set(const bool on)
 {
     tinypico.DotStar_SetPixelColor(on ? 255 : 0, 0, 0 );
 }
 
-//Task1code: blinks an LED every 1000 ms
-void Task1code( void * pvParameters ){
-    Serial.print("Task1 running on core ");
-    Serial.println(xPortGetCoreID());
-
-    for(;;){
+static void Task1code( void * pvParameters )
+{
+    while (true) {
         device_set(true);
         delay(1000);
         device_set(false);
@@ -60,20 +19,18 @@ void Task1code( void * pvParameters ){
     } 
 }
 
-//Task2code: blinks an LED every 700 ms
-void Task2code( void * pvParameters ){
-    Serial.print("Task2 running on core ");
-    Serial.println(xPortGetCoreID());
-
-    for(;;){
-        /*
-        digitalWrite(led2, HIGH);
-        delay(700);
-        digitalWrite(led2, LOW);
-        delay(700);*/
-    }
+void setup() 
+{
+    xTaskCreatePinnedToCore(
+            Task1code,   
+            "Task1",     
+            10000,       // stack size 
+            NULL,        // parameter
+            1,           // priority
+            &Task1,      
+            0);          // core
 }
 
-void loop() {
-
+void loop() 
+{
 }
