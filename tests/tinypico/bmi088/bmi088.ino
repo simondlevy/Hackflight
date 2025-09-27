14a40026f5a1bf8c1ca6a480f916b50e1fe738e5
simondlevy <simon.d.levy@gmail.com>
2025-09-26 20:39:02 -0400

    modified:   bmi088.ino

--- tests/tinypico/bmi088/bmi088.ino
+++ tests/tinypico/bmi088/bmi088.ino
@@ -1,8 +1,19 @@
 #include <BMI088.h>
 
+static const uint8_t ACCEL_ADDR = 0x19;
+static const uint8_t GYRO_ADDR = 0x69;
+static const uint8_t GYRO_INT_PIN = 4;
+
 Bmi088Accel accel(Wire, 0x19);
 
 Bmi088Gyro gyro(Wire, 0x69);
+
+static volatile bool got_gyro_interrupt;
+
+static void handle_gyro_interrupt()
+{
+    got_gyro_interrupt = true;
+}
 
 static void checkStatus(const int status, const char * sensorname)
 {
@@ -12,7 +23,6 @@ static void checkStatus(const int status, const char * sensorname)
         Serial.print(" initialization failed with status ");
         Serial.println(status);
         delay(500);
-
     }
 }
 
@@ -28,29 +38,42 @@ void setup()
 
     checkStatus(accel.begin(), "accel");
 
+    accel.setOdr(Bmi088Accel::ODR_100HZ_BW_19HZ);
+
     checkStatus(gyro.begin(), "gyro");
+
+    gyro.setOdr(Bmi088Gyro::ODR_100HZ_BW_12HZ);
+    gyro.pinModeInt3(Bmi088Gyro::PIN_MODE_PUSH_PULL,
+            Bmi088Gyro::PIN_LEVEL_ACTIVE_HIGH);
+    gyro.mapDrdyInt3(true);
+
+    pinMode(GYRO_INT_PIN, INPUT);
+    attachInterrupt(GYRO_INT_PIN, handle_gyro_interrupt, RISING);
 }
 
 void loop() 
 {
-    accel.readSensor();
+    if (got_gyro_interrupt) {
 
-    gyro.readSensor();
+        got_gyro_interrupt = false;
 
-    Serial.print(accel.getAccelX_mss());
-    Serial.print("\t");
-    Serial.print(accel.getAccelY_mss());
-    Serial.print("\t");
-    Serial.print(accel.getAccelZ_mss());
-    Serial.print("\t");
-    Serial.print(gyro.getGyroX_rads());
-    Serial.print("\t");
-    Serial.print(gyro.getGyroY_rads());
-    Serial.print("\t");
-    Serial.print(gyro.getGyroZ_rads());
-    Serial.print("\t");
-    Serial.print(accel.getTemperature_C());
-    Serial.print("\n");
+        accel.readSensor();
 
-    delay(20);
+        gyro.readSensor();
+
+        Serial.print(accel.getAccelX_mss());
+        Serial.print("\t");
+        Serial.print(accel.getAccelY_mss());
+        Serial.print("\t");
+        Serial.print(accel.getAccelZ_mss());
+        Serial.print("\t");
+        Serial.print(gyro.getGyroX_rads());
+        Serial.print("\t");
+        Serial.print(gyro.getGyroY_rads());
+        Serial.print("\t");
+        Serial.print(gyro.getGyroZ_rads());
+        Serial.print("\t");
+        Serial.print(accel.getTemperature_C());
+        Serial.print("\n");
+    }
 }
