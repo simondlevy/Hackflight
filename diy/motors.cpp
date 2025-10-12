5ab34e222220dc850c271355694628d37d81aed9
simondlevy <simon.d.levy@gmail.com>
2025-10-12 02:39:29 -0400

    modified:   motors.cpp

--- diy/motors.cpp
+++ diy/motors.cpp
@@ -21,22 +21,10 @@
 
 static const std::vector<uint8_t> MOTOR_PINS = {PA1, PB11, PA15, PB10};
 
-static const std::vector<uint8_t> POWER_SWITCH_PINS = {PA0, PB12, PC8, PC15};
-
 static auto motors = OneShot125(MOTOR_PINS);
-
-static void enableMotor(const uint8_t id)
-{
-    pinMode(POWER_SWITCH_PINS[id], OUTPUT);
-    digitalWrite(POWER_SWITCH_PINS[id], HIGH);
-}
 
 void CoreTask::motors_init()
 {
-    enableMotor(0);
-    enableMotor(1);
-    enableMotor(2);
-    enableMotor(3);
 
     motors.arm();
 }
