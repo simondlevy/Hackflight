5be945d7426fe3566e1262296f94749e9de70aaa
simondlevy <simon.d.levy@gmail.com>
2025-10-13 21:09:21 -0400

    new file:   i2cscanner/i2cscanner.ino

---
+++ tests/i2cscanner/i2cscanner.ino
@@ -0,0 +1,70 @@
+/**
+ *
+ * Copyright (C) 2025 Simon D. Levy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, in version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <Wire.h>   
+
+static const uint8_t SDA_PIN = PB7;
+static const uint8_t SCL_PIN = PB6;
+
+TwoWire wire(SDA_PIN, SCL_PIN);
+
+void setup()
+{
+    Serial.begin(115200);
+
+    wire.begin();
+
+    delay(100);
+}
+
+void loop()
+{  
+    Serial.print("Scanning ");
+
+    int nDevices = 0;
+
+    for(byte address = 1; address < 127; address++ ) 
+    {
+
+        wire.beginTransmission(address);
+
+        byte error = wire.endTransmission();
+
+        if (error == 0)
+        {
+            Serial.print("I2C device found at address 0x");
+            if (address<16) 
+                Serial.print("0");
+            Serial.println(address,HEX);
+
+            nDevices++;
+        }
+        else if (error==4) 
+        {
+            Serial.print("Unknown error at address 0x");
+            if (address<16) 
+                Serial.print("0");
+            Serial.println(address,HEX);
+        }    
+    }
+    if (nDevices == 0)
+        Serial.println("No I2C devices found\n");
+    else
+        Serial.println("done\n"); 
+
+    delay(1000);
+}
