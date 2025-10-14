cd78801fc0462839ea12d2fcb2c4dec85091ccf9
simondlevy <simon.d.levy@gmail.com>
2025-10-13 23:56:05 -0400

    new file:   bootloader.hpp

--- tests/i2cscanner/i2cscanner.ino
+++ tests/i2cscanner/i2cscanner.ino
@@ -17,78 +17,12 @@
 
 #include <Wire.h>   
 
-//static const uint8_t SDA_PIN = PC9;
-//static const uint8_t SCL_PIN = PA8;
+#include "bootloader.hpp"
 
-//static TwoWire wire = Wire; //TwoWire(SDA_PIN, SCL_PIN);
+static const uint8_t SDA_PIN = 10;
+static const uint8_t SCL_PIN = 9;
 
-class BootloaderJumper {
-
-    public:
-
-        static void jump(void) 
-        {
-            // Deinit HAL and Clocks
-            HAL_DeInit();
-            HAL_RCC_DeInit();
-
-            // Disable all interrupts
-            __disable_irq();
-
-            // Disable Systick
-            SysTick->CTRL = 0;
-            SysTick->LOAD = 0;
-            SysTick->VAL = 0;
-
-            // Disable interrupts and clear pending ones
-            for (size_t i = 0; i < sizeof(NVIC->ICER)/sizeof(NVIC->ICER[0]); i++) {
-                NVIC->ICER[i]=0xFFFFFFFF;
-                NVIC->ICPR[i]=0xFFFFFFFF;
-            }
-
-            // Re-enable interrupts
-            __enable_irq();
-
-            // Map Bootloader (system flash) memory to 0x00000000. This is STM32 family dependant.
-            __HAL_SYSCFG_REMAPMEMORY_SYSTEMFLASH();
-
-            // Set embedded bootloader vector table base offset
-            WRITE_REG(SCB->VTOR, SCB_VTOR_TBLOFF_Msk & 0x00000000);
-
-            // Switch to Main Stack Pointer (in case it was using the Process Stack Pointer)
-            __set_CONTROL(0);
-
-            // Instruction synchronization barrier
-            __ISB();
-
-            static const struct bootloader_vectable__t * BOOTLOADER_VECTOR_TABLE = 
-                ((struct bootloader_vectable__t *)BOOTLOADER_ADDR);
-
-            // Set Main Stack Pointer to the Bootloader defined value.
-            __set_MSP(BOOTLOADER_VECTOR_TABLE->stack_pointer);
-
-            __DSB(); // Data synchronization barrier
-            __ISB(); // Instruction synchronization barrier
-
-            // Jump to Bootloader Reset Handler
-            BOOTLOADER_VECTOR_TABLE->reset_handler();
-
-            // The next instructions will not be reached
-            while (1){}
-        }
-
-    private:
-
-        // Bootloader start address (refer to AN2606). STM32 family-dependent.
-        static const uint32_t BOOTLOADER_ADDR = 0x1FFF0000 ;
-
-        struct bootloader_vectable__t {
-            uint32_t stack_pointer;
-            void (*reset_handler)(void);
-        };
-
-
-};
+static TwoWire Wire1 = TwoWire(SDA_PIN, SCL_PIN);
 
 void serialEvent()
 {
@@ -103,21 +37,26 @@ void setup()
     Serial.begin(115200);
 
     Wire.begin();
+    //Wire1.begin();
 
     delay(100);
 }
 
-void loop()
-{  
-    Serial.println("Scanning ...");
+static void scan(TwoWire & wire, const char * name)
+{
+
+    Serial.print("Scanning ");
+    Serial.print(name);
+    Serial.println (" ...");
 
     int nDevices = 0;
 
-    for(byte address = 1; address < 127; address++ ) {
+    for(byte address = 1; address < 127; address++ ) 
+    {
 
-        Wire.beginTransmission(address);
+        wire.beginTransmission(address);
 
-        byte error = Wire.endTransmission();
+        byte error = wire.endTransmission();
 
         if (error == 0)
         {
@@ -142,4 +81,11 @@ void loop()
         Serial.println("done\n"); 
 
     delay(1000);
+}
+
+
+void loop()
+{  
+    scan(Wire, "Wire");
+    //scan(Wire1, "Wire1");
 }
