168fa4d4f83347aab4b0545cc59489544b3226c2
simondlevy <simon.d.levy@gmail.com>
2025-11-06 16:32:20 -0500

    Cleanup

--- client/client.py
+++ client/client.py
@@ -34,8 +34,7 @@ except Exception as e:
 
 BLUETOOTH_ADDRESSES = {
     'bench': '64:B7:08:94:28:76',
-    'cf2': '64:B7:08:87:AD:76',
-    'generic': '64:B7:08:87:AD:AA',
+    'cf2': '64:B7:08:87:AD:AA',
     'tinypico': 'D4:D4:DA:AA:2E:F2'
 }
 
