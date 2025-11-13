4e2cf0693dd01526ad14d8829c0cccafe51af80b
simondlevy <simon.d.levy@gmail.com>
2025-11-13 16:21:40 -0500

    modified:   betafpv/imu.cpp

--- betafpv/imu.cpp
+++ betafpv/imu.cpp
@@ -15,10 +15,26 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <mpu6x00.hpp>
+
 #include <tasks/imu.hpp>
+
+static const uint8_t MOSI_PIN = PA7;
+static const uint8_t MISO_PIN = PA6;
+static const uint8_t SCLK_PIN = PA5;
+
+static const uint8_t CS_PIN  = PA4;
+
+static SPIClass spi = SPIClass(MOSI_PIN, MISO_PIN, SCLK_PIN);
+
+static Mpu6000 imu = Mpu6000(spi, CS_PIN);
 
 bool ImuTask::device_init()
 {
+    spi.begin();
+
+    if (!imu.begin()) return false;
+
     return true;
 }
 
@@ -26,6 +42,15 @@ void ImuTask::device_readRaw(
         int16_t & gx, int16_t & gy, int16_t & gz, 
         int16_t & ax, int16_t & ay, int16_t & az)
 {
+        imu.readSensor();
+
+        gx = imu.getRawGyroX();
+        gy = imu.getRawGyroY();
+        gz = imu.getRawGyroZ();
+
+        ax = imu.getRawAccelX();
+        ay = imu.getRawAccelY();
+        az = imu.getRawAccelZ();
 }
 
 float ImuTask::device_gyroRaw2Dps(const int16_t raw)
@@ -35,5 +60,5 @@ float ImuTask::device_gyroRaw2Dps(const int16_t raw)
 
 float ImuTask::device_accelRaw2Gs(const int16_t raw)
 {
-    return (float)raw * 2 * 24 / 65536.f;
+    return (float)raw * 2 * 16 / 65536.f;
 }
