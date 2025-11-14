23cdc9fe2e671e5af945b45aaae37d05433bec6a
simondlevy <simon.d.levy@gmail.com>
2025-11-14 02:28:37 -0500

    More setpoint cleanup

--- src/tasks/setpoint.hpp
+++ src/tasks/setpoint.hpp
@@ -33,62 +33,26 @@ class SetpointTask {
         {
             _debugTask = debugTask;
 
-            setpointQueue = xQueueCreateStatic(
-                    QUEUE_LENGTH,
-                    SETPOINT_ITEM_SIZE,
-                    setpointQueueStorage,
-                    &setpointQueueBuffer);
-
-            xQueueSend(setpointQueue, &nullSetpoint, 0);
-
-            priorityQueue = xQueueCreateStatic(
-                    QUEUE_LENGTH,
-                    PRIORITY_ITEM_SIZE,
-                    priorityQueueStorage,
-                    &priorityQueueBuffer);
-
-            xQueueSend(priorityQueue, &priorityDisable, 0);
-
             _task.init(runSetpointTask, "setpoint", this, 3);
         }
 
         void getSetpoint(setpoint_t & setpoint)
         {
-            xQueuePeek(setpointQueue, &setpoint, 0);
+            memcpy(&setpoint, &_setpoint, sizeof(setpoint_t));
         }
 
     private:
 
-        static const uint8_t PRIORITY_LOW  = 1;
-        static const uint8_t PRIORITY_HIGH = 2;
-
-        static const uint16_t MIN_THRUST = 1000;
-        static const uint16_t MAX_THRUST = 60000;
- 
         static void runSetpointTask(void * obj)
         {
             ((SetpointTask *)obj)->run();
         }
 
-        const setpoint_t nullSetpoint = {};
-
-        const int priorityDisable = 0;
-
-        static const size_t QUEUE_LENGTH = 1;
-
-        static const auto SETPOINT_ITEM_SIZE = sizeof(setpoint_t);
-        uint8_t setpointQueueStorage[QUEUE_LENGTH * SETPOINT_ITEM_SIZE];
-        StaticQueue_t setpointQueueBuffer;
-        QueueHandle_t setpointQueue;
-
-        static const auto PRIORITY_ITEM_SIZE = sizeof(int);
-        uint8_t priorityQueueStorage[QUEUE_LENGTH * PRIORITY_ITEM_SIZE];
-        StaticQueue_t priorityQueueBuffer;
-        QueueHandle_t priorityQueue;
-
         FreeRtosTask _task;
 
         DebugTask * _debugTask;
+
+        setpoint_t _setpoint;
 
         void run(void)
         {
@@ -109,21 +73,19 @@ class SetpointTask {
                     switch (parser.parse(byte)) {
 
                         case MSP_SET_ARMING:
-                            setpoint.armed = !setpoint.armed;
+                            _setpoint.armed = !_setpoint.armed;
                             break;
 
                         case MSP_SET_IDLE:
-                            setpoint.hovering = false;
-                            setSetpoint(&setpoint);
+                            _setpoint.hovering = false;
                             break;
 
                         case MSP_SET_SETPOINT:
-                            setpoint.hovering = true;
-                            setpoint.demands.pitch = parser.getFloat(0);
-                            setpoint.demands.roll = parser.getFloat(1);
-                            setpoint.demands.yaw = parser.getFloat(2);
-                            setpoint.demands.thrust = parser.getFloat(3);
-                            setSetpoint(&setpoint);
+                            _setpoint.hovering = true;
+                            _setpoint.demands.pitch = parser.getFloat(0);
+                            _setpoint.demands.roll = parser.getFloat(1);
+                            _setpoint.demands.yaw = parser.getFloat(2);
+                            _setpoint.demands.thrust = parser.getFloat(3);
                             break;
 
                         default:
@@ -132,33 +94,5 @@ class SetpointTask {
                 }
 
             }
-        }
-
-        int getActivePriority(void)
-        {
-            int priority = 0;
-
-            const BaseType_t peekResult = xQueuePeek(priorityQueue, &priority, 0);
-
-            return peekResult == pdTRUE ? priority : 0;
-        }
-
-        void setSetpoint(setpoint_t *setpoint)
-        {
-            const int priority = PRIORITY_HIGH;
-            int currentPriority = 0;
-
-            const BaseType_t peekResult =
-                xQueuePeek(priorityQueue, &currentPriority, 0);
-
-            if (peekResult != pdTRUE) {
-                return;
-            }
-
-            if (priority >= currentPriority) {
-                setpoint->timestamp = xTaskGetTickCount();
-                xQueueOverwrite(setpointQueue, setpoint);
-                xQueueOverwrite(priorityQueue, &priority);
-            }        
         }
 };
