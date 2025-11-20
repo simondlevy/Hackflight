944c694d130a93c63cb1cf9ee22940899c4fdd69
simondlevy <simon.d.levy@gmail.com>
2025-11-20 17:31:14 -0500

    modified:   goku.ino

--- goku/goku.ino
+++ goku/goku.ino
@@ -1,61 +1,18 @@
-#include <ICM42688.h>
-
 #include "bootloader.hpp"
 
 #include <imu.hpp>
 
-/*
-static const uint8_t LED_PIN = PC14;
-
-static const uint8_t SCLK_PIN = PA5;
-static const uint8_t MISO_PIN = PA6;
-static const uint8_t MOSI_PIN = PA7;
-
-static SPIClass spi = SPIClass(MOSI_PIN, MISO_PIN, SCLK_PIN);
-
-static ICM42688 IMU(spi, PB12);
-*/
-
 static Imu imu;
-
-/*
-static void blinkLed()
-{
-    digitalWrite(LED_PIN, HIGH);  
-    delay(1000);            
-    digitalWrite(LED_PIN, LOW); 
-    delay(1000);   
-}*/
 
 void setup()
 {
     Serial.begin(115200);
 
     imu.init();
-
-    /*
-    pinMode(LED_PIN, OUTPUT);
-
-	int status = IMU.begin();
-
-	while (status < 0) {
-		Serial.println("IMU initialization unsuccessful");
-        delay(500);
-	}*/
 }
 
 void loop()
 {  
-    /*
-    //blinkLed();
-
-	IMU.getAGT();
-	
-Serial.print("\t");
-    Serial.println(IMU.temp(), 6);
-     */
-
-
     int16_t gx=0, gy=0, gz=0, ax=0, ay=0, az=0;
     imu.device_read(gx, gy, gz, ax, ay, az);
 
