ec5d0aa699366a9462bd07c0110047c7ebbdfa81
simondlevy <simon.d.levy@gmail.com>
2025-11-20 15:01:09 -0500

    new file:   goku/goku.ino

---
+++ goku/goku.ino
@@ -0,0 +1,15 @@
+static const uint8_t LED_PIN = PC14;
+
+void setup()
+{
+    Serial.begin(115200);
+
+    pinMode(LED_PIN, OUTPUT);
+}
+
+void loop()
+{  
+    digitalWrite(LED_PIN, HIGH);  
+    delay(1000);            
+    digitalWrite(LED_PIN, LOW); 
+    delay(1000);   }
