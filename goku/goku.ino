80d73064c2718258a7c4753b35ec57365ad71ed2
simondlevy <simon.d.levy@gmail.com>
2025-11-20 15:14:08 -0500

    modified:   Makefile

--- goku/goku.ino
+++ goku/goku.ino
@@ -2,6 +2,14 @@
 
 static const uint8_t LED_PIN = PC14;
 
+static void blinkLed()
+{
+    digitalWrite(LED_PIN, HIGH);  
+    delay(1000);            
+    digitalWrite(LED_PIN, LOW); 
+    delay(1000);   
+}
+
 void setup()
 {
     Serial.begin(115200);
@@ -11,10 +19,7 @@ void setup()
 
 void loop()
 {  
-    digitalWrite(LED_PIN, HIGH);  
-    delay(1000);            
-    digitalWrite(LED_PIN, LOW); 
-    delay(1000);   
+    blinkLed();
 
     if (Serial.available() && Serial.read() == 'R') {
         Bootloader::jump();
