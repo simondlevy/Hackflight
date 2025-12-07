1709439679a13cd240d409ccbf9704a0cfa0a240
simondlevy <simon.d.levy@gmail.com>
2025-12-07 03:14:10 -0500

    Moved all webots physics to webots/physics/physics

--- src/control/haskell.cpp
+++ src/control/haskell.cpp
@@ -14,17 +14,13 @@
  * along with this program. If not, see <http:--www.gnu.org/licenses/>.
  */
 
-#pragma once
-
 #include <stdio.h>
 #include <string.h>
 
-#include <datatypes.h>
-#include <serializer.hpp>
+#include <control.hpp>
 
 static demands_t _demands;
 
-#ifdef _MAIN
 void setDemands(float t, float r, float p, float y)
 {
     _demands.thrust = t;
@@ -32,78 +28,69 @@ void setDemands(float t, float r, float p, float y)
     _demands.pitch = p;
     _demands.yaw = y;
 }
-#endif
 
 void copilot_step_core();
 
-// Common closed-loop control API
 
-class ClosedLoopControl {
+void ClosedLoopControl::run(
+        const float dt,
+        const flightMode_t flightMode,
+        const vehicleState_t & vehicleState,
+        const demands_t & setpointDemands,
+        demands_t & demands)
+{
+    extern float stream_dt;
 
-    public:
+    extern bool stream_airborne;
 
-        void run(
-                const float dt,
-                const flightMode_t flightMode,
-                const vehicleState_t & vehicleState,
-                const demands_t & setpointDemands,
-                demands_t & demands)
-        {
-            extern float stream_dt;
+    extern float stream_thrust;
+    extern float stream_roll;
+    extern float stream_pitch;
+    extern float stream_yaw;
 
-            extern bool stream_airborne;
+    extern float stream_dx;
+    extern float stream_dy;
+    extern float stream_z;
+    extern float stream_dz;
+    extern float stream_phi;
+    extern float stream_dphi;
+    extern float stream_theta;
+    extern float stream_dtheta;
+    extern float stream_psi;
+    extern float stream_dpsi;
 
-            extern float stream_thrust;
-            extern float stream_roll;
-            extern float stream_pitch;
-            extern float stream_yaw;
+    stream_dt = dt;
 
-            extern float stream_dx;
-            extern float stream_dy;
-            extern float stream_z;
-            extern float stream_dz;
-            extern float stream_phi;
-            extern float stream_dphi;
-            extern float stream_theta;
-            extern float stream_dtheta;
-            extern float stream_psi;
-            extern float stream_dpsi;
+    stream_airborne = flightMode == MODE_HOVERING || 
+        flightMode == MODE_AUTONOMOUS;
 
-            stream_dt = dt;
+    stream_thrust = setpointDemands.thrust;
+    stream_roll = setpointDemands.roll;
+    stream_pitch = setpointDemands.pitch;
+    stream_yaw = setpointDemands.yaw;
 
-            stream_airborne = flightMode == MODE_HOVERING || 
-                flightMode == MODE_AUTONOMOUS;
+    stream_dx = vehicleState.dx;
+    stream_dy = vehicleState.dy;
+    stream_z = vehicleState.z;
+    stream_dz = vehicleState.dz;
+    stream_phi = vehicleState.phi;
+    stream_dphi = vehicleState.dphi;
+    stream_theta = vehicleState.theta;
+    stream_dtheta = vehicleState.dtheta;
+    stream_psi = vehicleState.psi;
+    stream_dpsi = vehicleState.dpsi;
 
-            stream_thrust = setpointDemands.thrust;
-            stream_roll = setpointDemands.roll;
-            stream_pitch = setpointDemands.pitch;
-            stream_yaw = setpointDemands.yaw;
+    // This will call setDemands() defined above
+    copilot_step_core();
 
-            stream_dx = vehicleState.dx;
-            stream_dy = vehicleState.dy;
-            stream_z = vehicleState.z;
-            stream_dz = vehicleState.dz;
-            stream_phi = vehicleState.phi;
-            stream_dphi = vehicleState.dphi;
-            stream_theta = vehicleState.theta;
-            stream_dtheta = vehicleState.dtheta;
-            stream_psi = vehicleState.psi;
-            stream_dpsi = vehicleState.dpsi;
+    memcpy(&demands, &_demands, sizeof(demands_t));
+}
 
-            // This will call setDemands() defined above
-            copilot_step_core();
+void ClosedLoopControl::serializeMessage(MspSerializer & serializer)
+{
+    (void)serializer;
+}
 
-            memcpy(&demands, &_demands, sizeof(demands_t));
-        }
-
-        void serializeMessage(MspSerializer & serializer)
-        {
-            (void)serializer;
-        }
-
-        // unused; needed for sim API
-        void init()
-        {
-        }
-};
-
+void ClosedLoopControl::init()
+{
+}
