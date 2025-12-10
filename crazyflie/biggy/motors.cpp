14f9587f11423594aa838ced9e5ba58b86a2de7e
Simon D. Levy <simon.d.levy@gmail.com>
2025-12-10 18:33:54 -0500

    new file:   crazyflie/biggy/motors.cpp

---
+++ crazyflie/biggy/motors.cpp
@@ -0,0 +1,43 @@
+/**
+ *
+ * Copyright (C) 2025 Simon D. Levy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, in version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <oneshot125.hpp>
+
+#include <hackflight.hpp>
+
+static const std::vector<uint8_t> MOTOR_PINS = {PA2, PA3, PC12, PB5};
+
+static auto motors = OneShot125(MOTOR_PINS);
+
+void Hackflight::motors_init()
+{
+    motors.arm();
+}
+
+void Hackflight::motors_setSpeed(uint32_t id, float speed)
+{
+    const uint8_t pulse_width = 125 * (speed + 1);
+
+    motors.set(id, pulse_width);
+}
+
+void Hackflight::motors_run()
+{
+    motors.run();
+}
+
+
