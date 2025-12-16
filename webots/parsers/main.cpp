39d877cc6a5bb11849a86629cc8cef8cba08774f
simondlevy <simon.d.levy@gmail.com>
2025-12-16 17:22:24 -0500

    new file:   parsers/main.cpp

---
+++ webots/parsers/main.cpp
@@ -0,0 +1,37 @@
+/* 
+   Webots world-parsing tester
+
+   Copyright (C) 2025 Simon D. Levy
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, in version 3.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program. If not, see <http:--www.gnu.org/licenses/>.
+ */
+
+#include <sstream>
+
+// Obstacles
+#include "world_parser.hpp"
+
+int main(int argc, char ** argv) 
+{
+    (void)argc;
+
+    const std::string world =  argv[1];
+
+    static WorldParser _worldParser;
+
+    _worldParser.parse("../worlds/" + world + ".wbt");
+
+    _worldParser.report();
+
+    return 0;
+}
