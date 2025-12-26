803e4b44da0be06b0dca47c2a55525b26aa9c472
Simon D. Levy <simon.d.levy@gmail.com>
2025-12-26 18:27:23 -0500

    modified:   client.py

--- client/client.py
+++ client/client.py
@@ -35,6 +35,7 @@ except Exception as e:
 BLUETOOTH_ADDRESSES = {
     'bolt': '64:B7:08:86:F2:86',
     'cf2': '64:B7:08:87:AD:AA',
+    'diy': '64:B7:08:93:E5:3A',
     'teensy': '64:B7:08:87:AD:2E',
     'tinypico': 'D4:D4:DA:AA:2E:F2'
 }
