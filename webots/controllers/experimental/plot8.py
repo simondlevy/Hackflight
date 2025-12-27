e10e1d82e696d35f9f63a5267929e13bb8cc073b
simondlevy <simon.d.levy@gmail.com>
2025-12-27 04:07:55 -0500

    modified:   plot8.py

--- webots/controllers/experimental/plot8.py
+++ webots/controllers/experimental/plot8.py
@@ -3,15 +3,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def clean(a):
+    a[a==-1] = np.nan
+
+def plot(a, k):
+    plt.subplot(8,1,k+1)
+    plt.plot(a[:,k])
+
 gt = np.loadtxt('groundtruth.csv', delimiter=',')
 #ss = np.loadtxt('simsens.csv')
 
-print(gt.shape)
+clean(gt)
 
-#plt.plot(gt)
+plot(gt, 0)
+
+plt.ylim((0, 4000))
+plt.xlabel('timestep')
+plt.ylabel('distance (mm)')
+plt.show()
+
 #plt.plot(ss)
-#plt.xlabel('timestep')
-#plt.ylabel('distance (mm)')
 #plt.legend(('groundtruth', 'simsens'))
-#plt.show()
 
