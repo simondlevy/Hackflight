2056151b5a7e390832a0b363762abdd99f198d5b
simondlevy <simon.d.levy@gmail.com>
2025-12-27 04:14:37 -0500

    modified:   plot8.py

--- webots/controllers/experimental/plot8.py
+++ webots/controllers/experimental/plot8.py
@@ -9,6 +9,7 @@ def clean(a):
 def plot(a, k):
     plt.subplot(8,1,k+1)
     plt.plot(a[:,k])
+    plt.ylim((0, 2000))
 
 gt = np.loadtxt('groundtruth.csv', delimiter=',')
 #ss = np.loadtxt('simsens.csv')
@@ -16,10 +17,16 @@ gt = np.loadtxt('groundtruth.csv', delimiter=',')
 clean(gt)
 
 plot(gt, 0)
+plot(gt, 1)
+plot(gt, 2)
+plot(gt, 3)
+plot(gt, 4)
+plot(gt, 5)
+plot(gt, 6)
+plot(gt, 7)
 
-plt.ylim((0, 4000))
 plt.xlabel('timestep')
-plt.ylabel('distance (mm)')
+#plt.ylabel('distance (mm)')
 plt.show()
 
 #plt.plot(ss)
