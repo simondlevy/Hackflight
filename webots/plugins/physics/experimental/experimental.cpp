179dbc624d8288542b3773b2ffedcad68b1d5d95
Simon D. Levy <simon.d.levy@gmail.com>
2025-12-29 00:08:46 -0500

    modified:   ../../plugins/physics/experimental/experimental.cpp

--- webots/plugins/physics/experimental/experimental.cpp
+++ webots/plugins/physics/experimental/experimental.cpp
@@ -34,66 +34,63 @@ static bool run_normal()
     siminfo_t siminfo = {};
     SimInnerLoop::pose_t pose = {};
 
-    _step(siminfo, pose);
+    if (_step(siminfo, pose)) {
 
-    return true;
+        static simsens::SimRangefinder * _simRangefinder;
+        static simsens::RangefinderVisualizer * _rangefinderVisualizer;
+        static simsens::RobotParser _robotParser;
+        static simsens::WorldParser _worldParser;
+        static FILE * _logfp;
 
-    /*
+        // Load world and robot info first time around
+        if (!_simRangefinder) {
 
-    static simsens::SimRangefinder * _simRangefinder;
-    static simsens::RangefinderVisualizer * _rangefinderVisualizer;
-    static simsens::RobotParser _robotParser;
-    static simsens::WorldParser _worldParser;
-    static FILE * _logfp;
+            char path[1000];
 
-    // Load world and robot info first time around
-    if (!_simRangefinder) {
+            sprintf(path, "%s/../../worlds/%s.wbt", siminfo.path, siminfo.worldname);
+            _worldParser.parse(path);
 
-        char path[1000];
+            sprintf(path, "%s/../../protos/DiyQuad.proto", siminfo.path);
+            _robotParser.parse(path);
 
-        sprintf(path, "%s/../../worlds/%s.wbt", siminfo.path, siminfo.worldname);
-        _worldParser.parse(path);
+            _simRangefinder = _robotParser.rangefinders[0];
 
-        sprintf(path, "%s/../../protos/DiyQuad.proto", siminfo.path);
-        _robotParser.parse(path);
+            _rangefinderVisualizer = new simsens::RangefinderVisualizer(_simRangefinder);
 
-        _simRangefinder = _robotParser.rangefinders[0];
+            _logfp = fopen(LOGFILE_NAME, "w");
+        }
 
-        _rangefinderVisualizer = new simsens::RangefinderVisualizer(_simRangefinder);
+        // Get simulated rangefinder distances
+        int rangefinder_distances_mm[1000] = {}; // arbitrary max size
+        int rangefinder_width=0, rangefinder_height=0;
+        _simRangefinder->read(
+                simsens::pose_t{pose.x, pose.y, pose.z,
+                pose.phi, pose.theta, pose.psi},
+                _worldParser.walls,
+                rangefinder_distances_mm,
+                rangefinder_width,
+                rangefinder_height);
 
-        _logfp = fopen(LOGFILE_NAME, "w");
-    }
-
-    // Get simulated rangefinder distances
-    int rangefinder_distances_mm[1000] = {}; // arbitrary max size
-    int rangefinder_width=0, rangefinder_height=0;
-    _simRangefinder->read(
-            simsens::pose_t{robot_x, robot_y, robot_z,
-            pose.phi, pose.theta, pose.psi},
-            _worldParser.walls,
-            rangefinder_distances_mm,
-            rangefinder_width,
-            rangefinder_height);
-
-    for (int k=0; k<rangefinder_width; ++k) {
-        fprintf(_logfp, "%d%c", rangefinder_distances_mm[k],
-                (k==rangefinder_width-1)?'\n':',');
-    }
-    fflush(_logfp);
+        for (int k=0; k<rangefinder_width; ++k) {
+            fprintf(_logfp, "%d%c", rangefinder_distances_mm[k],
+                    (k==rangefinder_width-1)?'\n':',');
+        }
+        fflush(_logfp);
 
 
-    _rangefinderVisualizer->show(rangefinder_distances_mm, RANGEFINDER_DISPLAY_SCALEUP);
+        _rangefinderVisualizer->show(rangefinder_distances_mm, RANGEFINDER_DISPLAY_SCALEUP);
 
-    // Stop if we detected a collision
-    const bool debug = true;
-    if (simsens::CollisionDetector::detect(
-                simsens::vec3_t{robot_x, robot_y, robot_x},
-                _worldParser.walls, debug)) {
-        return false;
+        // Stop if we detected a collision
+        const bool debug = true;
+        if (simsens::CollisionDetector::detect(
+                    simsens::vec3_t{pose.x, pose.y, pose.x},
+                    _worldParser.walls, debug)) {
+            return false;
+        }
+
     }
 
     return true;
-    */
 }
 
 // This is called by Webots in the outer (display, kinematics) loop
