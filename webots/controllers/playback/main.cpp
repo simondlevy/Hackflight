ba9e32c2b53c9e5529177f69eabfe9549fd47c53
Simon D. Levy <simon.d.levy@gmail.com>
2026-01-05 01:42:35 -0500

    modified:   webots/controllers/playback/main.cpp

--- webots/controllers/playback/main.cpp
+++ webots/controllers/playback/main.cpp
@@ -1,5 +1,5 @@
 /* 
-   C++ flight simulator playback
+   Hackflight simulator playback
 
    Copyright (C) 2026 Simon D. Levy
 
@@ -23,7 +23,25 @@ int main(int argc, char ** argv)
 {
     (void)argc;
 
-    const string logfilename =  argv[1];
+    const char * logfilename =  argv[1];
+
+    FILE * logfp = fopen(logfilename, "r");
+
+    if (!logfp) {
+        fprintf(stderr, "Unable to open file %s for input\n", logfilename);
+        exit(1);
+    }
+
+    while (true) {
+
+        char line[1000] = {};
+
+        if (!fgets(line, sizeof(line), logfp) != NULL) {
+            break;
+        }
+
+        printf("%s", line);
+    }
 
     return 0;
 }
