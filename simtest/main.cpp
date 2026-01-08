d225172c86f44f733dd7d7448d5aa5a4755ffae2
simondlevy <simon.d.levy@gmail.com>
2026-01-08 02:55:20 -0500

    modified:   simtest/main.cpp

--- simtest/main.cpp
+++ simtest/main.cpp
@@ -1,4 +1,13 @@
-int main()
+#include <stdio.h>
+
+#include <datatypes.h>
+
+int main(int argc, char ** argv)
 {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s WORLDFILE\n", argv[0]);
+        return 1;
+    }
+
     return 0;
 }
