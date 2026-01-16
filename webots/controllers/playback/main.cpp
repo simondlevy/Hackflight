4474737582c6b55a16dd28331b269cc55068360d
simondlevy <simon.d.levy@gmail.com>
2026-01-16 04:37:14 -0500

    Fixed sim playback

--- webots/controllers/playback/main.cpp
+++ webots/controllers/playback/main.cpp
@@ -111,12 +111,12 @@ int main(int argc, char ** argv)
             break;
         }
 
-        pose_t pose = {};
+        simsens::pose_t pose = {};
 
         int rangefinder_distances_mm[8] = {};
         int * d = rangefinder_distances_mm;
 
-        if (sscanf(line, "%f,%f,%f,%f,%f,%f,%d,%d,%d,%d,%d,%d,%d,%d", 
+        if (sscanf(line, "%lf,%lf,%lf,%lf,%lf,%lf,%d,%d,%d,%d,%d,%d,%d,%d", 
                 &pose.x, &pose.y, &pose.z,
                 &pose.phi, &pose.theta, &pose.psi,
                 &d[0], &d[1], &d[2], &d[3], &d[4], &d[5], &d[6], &d[7]) == 14) {
