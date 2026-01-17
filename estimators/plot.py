8c3c75ea72e3d92b802f2a017b6ea230c20183a2
Simon D. Levy <simon.d.levy@gmail.com>
2026-01-16 22:58:35 -0500

    modified:   plot.py

--- estimators/plot.py
+++ estimators/plot.py
@@ -5,5 +5,26 @@ import matplotlib.pyplot as plt
 
 data = np.loadtxt('log.csv', delimiter=',')
 
-print(data.shape)
+mphi = data[0,:]
+mdphi = data[1,:]
+mtheta = data[2,:]
+mdtheta = data[3,:]
+mpsi = data[4,:]
+mdpsi = data[5,:]
+
+ephi = data[6,:]
+edphi = data[7,:]
+etheta = data[8,:]
+edtheta = data[9,:]
+epsi = data[10,:]
+edpsi = data[11,:]
+
+# plt.subplot(6,1,1)
+plt.plot(mphi)
+plt.plot(ephi)
+plt.ylabel('phi')
+plt.legend(('Madg', 'EKF'))
+
+plt.show()
+
 
