621acbb06b575a25fc993b0215ce6bdb4b69d02b
simondlevy <simon.d.levy@gmail.com>
2026-01-19 02:31:34 -0500

    Cleanup

--- webots/controllers/playback/main.cpp
+++ webots/controllers/playback/main.cpp
@@ -123,9 +123,6 @@ int main(int argc, char ** argv)
 
             rangefinderVisualizer.show(rangefinder_distances_mm,
                     RANGEFINDER_DISPLAY_SCALEUP);
-
-            printf("%d,%d,%d,%d,%d,%d,%d,%d\n",
-                    d[0], d[1], d[2], d[3], d[4], d[5], d[6], d[7]);
         }
 
         if (wb_robot_step(timestep) == -1) {
