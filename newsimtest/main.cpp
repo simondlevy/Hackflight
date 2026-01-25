397573866b2d5d0122451dafc29cf9bc3c3ca456
simondlevy <simon.d.levy@gmail.com>
2026-01-25 03:01:24 -0500

    new file:   newsimtest/main.cpp

--- newsimtest/main.cpp
+++ newsimtest/main.cpp
@@ -26,6 +26,7 @@
 #include <simsensors/src/parsers/webots/robot.hpp>
 #include <simsensors/src/sensors/rangefinder.hpp>
 
+static const uint32_t MAXTIME = 10;
 static const float FRAMERATE = 32;
 
 static FILE * openlog(const char * filename, const char * mode)
@@ -41,9 +42,8 @@ static FILE * openlog(const char * filename, const char * mode)
 
 int main(int argc, char ** argv)
 {
-    if (argc < 4) {
-        fprintf(stderr, "Usage: %s WORLDFILE ROBOTFILE SETPOINTFILE\n",
-                argv[0]);
+    if (argc < 3) {
+        fprintf(stderr, "Usage: %s WORLDFILE ROBOTFILE\n", argv[0]);
         return 1;
     }
 
@@ -61,34 +61,20 @@ int main(int argc, char ** argv)
             {pose.x, pose.y, pose.z, pose.phi, pose.theta, pose.psi}, 
             FRAMERATE);
 
-    auto inputfp = openlog(argv[3], "r");
-
     auto outputfp = openlog("poselog.csv", "w");
 
-    while (true) {
-
-        char line[1000] = {};
-
-        if (!fgets(line, sizeof(line), inputfp)) {
-            break;
-        }
+    for (uint32_t t=0; t<MAXTIME*FRAMERATE; ++t) {
 
         mode_e mode = MODE_IDLE;
         demands_t setpoint = {};
 
-        if (sscanf(line, "%d,%f,%f,%f,%f",
-                    (int *)&mode, &setpoint.thrust,&setpoint.roll,
-                    &setpoint.pitch, &setpoint.yaw) == 5) {
+        const auto pose = simulator.step(mode, setpoint);
 
-            const auto pose = simulator.step(mode, setpoint);
-
-            fprintf(outputfp, "%f,%f,%f,%f,%f,%f\n",
-                    pose.x, pose.y, pose.z, pose.phi, pose.theta, pose.psi);
-        }
+        fprintf(outputfp, "%f,%f,%f,%f,%f,%f\n",
+                pose.x, pose.y, pose.z, pose.phi, pose.theta, pose.psi);
     }
 
     fclose(outputfp);
-    fclose(inputfp);
 
     return 0;
 }
