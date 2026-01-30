d6a049605b67b8bdfd7f19a232ea2c9b76f4955c
Simon D. Levy <simon.d.levy@gmail.com>
2026-01-30 11:23:12 -0500

    modified:   simtest/main.cpp

--- simtest/main.cpp
+++ simtest/main.cpp
@@ -16,9 +16,131 @@
 
 #include <stdio.h>
 
-#include "twoexit.hpp"
+// C/C++
+#include <stdio.h>
+
+// Hackflight
+#include <autopilot/rangefinder.hpp>
+#include <datatypes.h>
+#include <simulator/simulator.hpp>
+#include <vehicles/diyquad.hpp>
+
+// SimSensors
+#include <simsensors/src/parsers/webots/world.hpp>
+#include <simsensors/src/parsers/webots/robot.hpp>
+#include <simsensors/src/sensors/rangefinder.hpp>
+
+static constexpr float TAKEOFF_TIME_SEC = 2;
+static constexpr float FRAME_RATE_HZ = 32;
 
 static const char * LOGNAME = "log.csv";
+
+static void write_to_log(
+        FILE * logfile,
+        const pose_t pose,
+        const int * rangefinder_distances_mm,
+        const int rangefinder_width)
+{
+    fprintf(logfile, "%+3.3f,%+3.3f,%+3.3f,%+3.3f,%+3.3f,%+3.3f", 
+            pose.x, pose.y, pose.z, pose.phi, pose.theta, pose.psi);
+    for (int k=0; k<rangefinder_width; ++k) {
+        fprintf(logfile, ",%d", rangefinder_distances_mm[k]);
+    }
+    fprintf(logfile, "\n");
+}
+
+class SimTest {
+
+    private:
+
+        static constexpr float TRAVEL_AFTER_CLEAR_SEC = 1;
+
+    public:
+
+        static constexpr float MAX_TIME_SEC = 10;
+
+        static bool cleared_room(
+                const int frame, 
+                const int * rangefinder_distances_mm, 
+                const int rangefinder_size)
+        {
+            for (int i=0; i<rangefinder_size; ++i) {
+                if (rangefinder_distances_mm[i] != -1) {
+                    return false;
+                }
+            }
+
+            static int _cleared_at_frame;
+
+            if (_cleared_at_frame == 0) {
+                _cleared_at_frame = frame;
+            }
+
+            else if ((frame - _cleared_at_frame)/FRAME_RATE_HZ > TRAVEL_AFTER_CLEAR_SEC) {
+                return true;
+            }
+
+            return false;
+        }
+};
+
+static bool run(
+        const char * robot_path,
+        const char * world_path,
+        FILE * logfile=nullptr)
+{
+    simsens::Robot robot = {};
+    simsens::RobotParser::parse(robot_path, robot);
+
+    simsens::World world = {};
+    simsens::WorldParser::parse(world_path, world, robot_path);
+
+    simsens::Rangefinder rangefinder =
+        simsens::Rangefinder(*robot.rangefinders[0]);
+
+    const auto pose = world.getRobotPose();
+
+    hf::Simulator simulator = {};
+
+    simulator.init(
+            {pose.x, pose.y, pose.z, pose.phi, pose.theta, pose.psi}, 
+            FRAME_RATE_HZ);
+
+    int rangefinder_distances_mm[1000] = {}; // arbitrary max size
+
+    for (uint32_t frame=0; frame<SimTest::MAX_TIME_SEC*FRAME_RATE_HZ; ++frame) {
+
+        const auto mode = frame < TAKEOFF_TIME_SEC*FRAME_RATE_HZ ?
+            hf::MODE_HOVERING :
+            hf::MODE_AUTONOMOUS;
+
+        hf::demands_t setpoint = {};
+
+        hf::RangefinderSetpoint::run(rangefinder_distances_mm, setpoint);
+
+        const auto pose = simulator.step(mode, setpoint);
+
+        if (world.collided({pose.x, pose.y, pose.x})) {
+            return false;
+        }
+
+        if (SimTest::cleared_room(frame, rangefinder_distances_mm,
+                    rangefinder.getWidth())) {
+            return true;
+        }
+
+        rangefinder.read(
+                {pose.x, pose.y, pose.z, pose.phi, pose.theta, pose.psi},
+                world, rangefinder_distances_mm);
+
+        if (logfile) {
+            write_to_log(logfile, pose,
+                    rangefinder_distances_mm, rangefinder.getWidth());
+        }
+    }
+
+    return false; // timed out
+}
 
 int main(int argc, char ** argv)
 {
@@ -33,7 +155,7 @@ int main(int argc, char ** argv)
         exit(1);
     }
 
-    SimTest::run(argv[1], argv[2], logfile);
+    run(argv[1], argv[2], logfile);
 
     fclose(logfile);
 
