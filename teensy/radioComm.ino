7727ae95f84a42bf8683c8a86d05292290a22f3f
Simon D. Levy <simon.d.levy@gmail.com>
2026-01-30 22:49:26 -0500

    Cleanup

--- teensy/radioComm.ino
+++ teensy/radioComm.ino
@@ -4,47 +4,14 @@
 //Last Updated: 7/29/2022
 //Version: Beta 1.3
 
-//========================================================================================================================//
-
-//This file contains all necessary functions and code used for radio communication to avoid cluttering the main code
-
-unsigned long rising_edge_start_1, rising_edge_start_2, rising_edge_start_3, rising_edge_start_4, rising_edge_start_5, rising_edge_start_6; 
-unsigned long channel_1_raw, channel_2_raw, channel_3_raw, channel_4_raw, channel_5_raw, channel_6_raw;
-int ppm_counter = 0;
-unsigned long time_ms = 0;
+static unsigned long rising_edge_start_1, rising_edge_start_2, rising_edge_start_3, rising_edge_start_4, rising_edge_start_5, rising_edge_start_6; 
+static unsigned long channel_1_raw, channel_2_raw, channel_3_raw, channel_4_raw, channel_5_raw, channel_6_raw;
 
 void radioSetup()
 {
     Serial1.begin(115000);
 }
 
-unsigned long getRadioPWM(int ch_num) {
-    //DESCRIPTION: Get current radio commands from interrupt routines 
-    unsigned long returnPWM = 0;
-
-    if (ch_num == 1) {
-        returnPWM = channel_1_raw;
-    }
-    else if (ch_num == 2) {
-        returnPWM = channel_2_raw;
-    }
-    else if (ch_num == 3) {
-        returnPWM = channel_3_raw;
-    }
-    else if (ch_num == 4) {
-        returnPWM = channel_4_raw;
-    }
-    else if (ch_num == 5) {
-        returnPWM = channel_5_raw;
-    }
-    else if (ch_num == 6) {
-        returnPWM = channel_6_raw;
-    }
-
-    return returnPWM;
-}
-
-//For DSM type receivers
 void serialEvent1(void)
 {
     while (Serial1.available()) {
@@ -52,53 +19,6 @@ void serialEvent1(void)
     }
 }
 
-
-
-//========================================================================================================================//
-
-
-
-//INTERRUPT SERVICE ROUTINES (for reading PWM and PPM)
-
-void getPPM() {
-    unsigned long dt_ppm;
-    int trig = digitalRead(PPM_Pin);
-    if (trig==1) { //Only care about rising edge
-        dt_ppm = micros() - time_ms;
-        time_ms = micros();
-
-
-        if (dt_ppm > 5000) { //Waiting for long pulse to indicate a new pulse train has arrived
-            ppm_counter = 0;
-        }
-
-        if (ppm_counter == 1) { //First pulse
-            channel_1_raw = dt_ppm;
-        }
-
-        if (ppm_counter == 2) { //Second pulse
-            channel_2_raw = dt_ppm;
-        }
-
-        if (ppm_counter == 3) { //Third pulse
-            channel_3_raw = dt_ppm;
-        }
-
-        if (ppm_counter == 4) { //Fourth pulse
-            channel_4_raw = dt_ppm;
-        }
-
-        if (ppm_counter == 5) { //Fifth pulse
-            channel_5_raw = dt_ppm;
-        }
-
-        if (ppm_counter == 6) { //Sixth pulse
-            channel_6_raw = dt_ppm;
-        }
-
-        ppm_counter = ppm_counter + 1;
-    }
-}
 
 void getCh1() {
     int trigger = digitalRead(ch1Pin);
