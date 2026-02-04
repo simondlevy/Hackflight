e9b205001295c72e1129d47a69363e59349e1fa2
simondlevy <simon.d.levy@gmail.com>
2026-02-04 00:47:40 -0500

    modified:   playlog.py

--- simtest/playlog.py
+++ simtest/playlog.py
@@ -17,53 +17,17 @@ You should have received a copy of the GNU Lesser General Public License
 along with this code.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-# Map size, scale
-MAP_SIZE_PIXELS = 800
-MAP_SIZE_METERS = 32
 
-from roboviz import MapVisualizer
+from roboviz import Visualizer # MapVisualizer
 
 import numpy as np
 from sys import argv, exit
 from time import sleep
 
-'''
-def threadfunc(robot, slam, timestamps, lidars, odometries, mapbytes, pose):
-    Threaded function runs SLAM, setting the map bytes and robot pose for display
-    on the main thread.
+MAP_SIZE_PIXELS = 800
+MAP_SIZE_METERS = 32
+FRAMES_PER_SECOND = 30
 
-    # Initialize time for delay
-    prevtime = 0
-
-    # Loop over scans    
-    for scanno in range(len(lidars)):
-
-        if odometries is None:
-                  
-             # Update SLAM with lidar alone
-            slam.update(lidars[scanno])
-
-        else:
-        
-            # Convert odometry to velocities
-            velocities = robot.computePoseChange(odometries[scanno])
-
-            # Update SLAM with lidar and velocities
-            slam.update(lidars[scanno], velocities)
-
-        # Get new position
-        pose[0],pose[1],pose[2] = slam.getpos()    
-
-        # Get new map
-        slam.getmap(mapbytes)
-
-        # Add delay to yield to main thread
-        currtime = timestamps[scanno] / 1.e6 # Convert usec to sec
-        if prevtime > 0:
-            sleep(currtime-prevtime)
-        prevtime = currtime
-'''
-    
 def main():
 	    
     if len(argv) < 2:
@@ -72,21 +36,17 @@ def main():
     
     data = np.loadtxt(argv[1], delimiter=',')
 
-    viz = MapVisualizer(MAP_SIZE_PIXELS, MAP_SIZE_METERS)
+    # viz = MapVisualizer(MAP_SIZE_PIXELS, MAP_SIZE_METERS)
+    viz = Visualizer(MAP_SIZE_PIXELS, MAP_SIZE_METERS)
 
     for row in data:
 
-        pose = row[0], row[1], row[5]
+        x, y, psi = row[0], row[1], row[5] * 180 / np.pi
 
-        print(pose)
+        lidar = row[6:]
 
-    '''
-    # Loop forever,displaying current map and pose
-    while True:
+        viz.display(x, y, psi)
 
-        # Display map and robot pose, exiting gracefully if user closes it
-        if not viz.display(pose[0]/1000., pose[1]/1000., pose[2], mapbytes):
-            exit(0)
-    '''
+        sleep(1 / FRAMES_PER_SECOND)
                     
 main()
