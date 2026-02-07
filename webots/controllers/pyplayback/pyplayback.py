ed79e09b5b52707e907cf8613edee1e3db5f5ee5
simondlevy <simon.d.levy@gmail.com>
2026-02-07 00:44:21 -0500

    modified:   ../pyplayback/pyplayback.py

--- webots/controllers/pyplayback/pyplayback.py
+++ webots/controllers/pyplayback/pyplayback.py
@@ -16,7 +16,10 @@
    along with this program. If not, see <http:--www.gnu.org/licenses/>.
 '''
 
+from math import cos, sin, acos
+
 from controller import Supervisor
+
 
 class DiyQuad(Supervisor):
 
@@ -30,11 +33,35 @@ class DiyQuad(Supervisor):
 
         return Supervisor.getFromDef(self, self.ROBOT_NAME)
 
+
+def euler_to_rotation(euler):
+    '''
+    https://www.euclideanspace.com/maths/geometry/rotations/conversions/
+       eulerToAngle/index.htm
+    '''
+
+    phi = euler[0]
+    theta = euler[1]
+    psi = euler[2]
+
+    c1 = cos(theta/2)
+    c2 = cos(psi/2)
+    c3 = cos(phi/2)
+    s1 = sin(theta/2)
+    s2 = sin(psi/2)
+    s3 = sin(phi/2)
+
+    return [s1*s2*c3 + c1*c2*s3,
+            s1*c2*c3 + c1*s2*s3,
+            1 if phi==0 and theta==0 and psi==0 else c1*s2*c3 - s1*c2*s3,
+            2 * acos(c1*c2*c3 - s1*s2*s3)]
+
+
 def main():
 
     quad = DiyQuad()
-    
-    timestep = quad.getBasicTimeStep();
+
+    timestep = quad.getBasicTimeStep()
 
     robot_node = quad.getFromDef()
 
@@ -55,7 +82,9 @@ def main():
 
         translation_field.setSFVec3f(xyz)
 
-        rotation = vals[3:6]
+        euler = vals[3:6]
+
+        rotation_field.setSFRotation(euler_to_rotation(euler))
 
         rangefinder_distances = vals[6:]
 
