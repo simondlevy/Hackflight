ed75b15dc9f8f3212b802b7235c54ed4fa56ed9b
simondlevy <simon.d.levy@gmail.com>
2026-02-15 15:34:47 -0500

    modified:   src/autopilot/twoexit.hpp Added missing comma in log output

--- src/autopilot/twoexit.hpp
+++ src/autopilot/twoexit.hpp
@@ -88,7 +88,7 @@ namespace hf {
                 const auto d = rangefinder_distances_mm;
 
                 fprintf(logfile,
-                        "%+3.3f,%+3.3f,%+3.3f,%+3.3f,%+3.3f,%+3.3f" 
+                        "%+3.3f,%+3.3f,%+3.3f,%+3.3f,%+3.3f,%+3.3f," 
                         "%d,%d,%d,%d,%d,%d,%d,%d\n",
                         pose.x, pose.y, pose.z, pose.phi, pose.theta, pose.psi,
                         d[0], d[1], d[2], d[3], d[4], d[5], d[6], d[7]);
