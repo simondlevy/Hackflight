0903febffae0fe900d7c450517ab5b299fdebf68
simondlevy <simon.d.levy@gmail.com>
2026-02-25 16:40:40 -0500

    More sim cleanup

--- webots/plugins/physics/pingpong/pingpong.cpp
+++ webots/plugins/physics/pingpong/pingpong.cpp
@@ -30,6 +30,9 @@
 #include <simsensors/src/robot.hpp>
 #include <simsensors/src/sensors/rangefinder.hpp>
 
+static constexpr int DISTANCE_DIFFERENCE_THRESHOLD = 1800;
+static constexpr float SPEED = 0.5;
+
 static AutopilotHelper * _helper;
 
 static int readRangefinder(
@@ -47,52 +50,28 @@ static int readRangefinder(
     return distance_mm;
 }
 
-static constexpr int DISTANCE_DIFFERENCE_THRESHOLD = 1800;
-static constexpr float SPEED = 0.5;
+static auto getSetpoint(
+        const int distance_forward_mm,
+        const int distance_backward_mm,
+        const float dydt) -> hf::Setpoint
+{
+    const auto diff = distance_forward_mm - distance_backward_mm;
 
-class PingPongAutopilot {
+    const int8_t direction = 
 
-    public:
+        // (Nearly) motionless on startup; move in a random
+        // direction (forward or backward)
+        fabs(dydt) < 1e-6 ? 2 * (rand() % 2) - 1 :
 
-        int distance_forward_mm;
-        int distance_backward_mm;
+        // Too close to either wall; switch direction
+        diff > DISTANCE_DIFFERENCE_THRESHOLD ? +1 :
+        diff < -DISTANCE_DIFFERENCE_THRESHOLD ? - 1 :
 
-        static auto getSetpoint(const PingPongAutopilot & autopilot,
-                const float dydt) -> hf::Setpoint
-        {
-            const auto diff = autopilot.distance_forward_mm -
-                autopilot.distance_backward_mm;
+        // Otherwise, continue in same direction
+        dydt > 0 ? -1 : +1;
 
-            const int8_t direction = 
-
-                // (Nearly) motionless on startup; move in a random
-                // direction (forward or backward)
-                fabs(dydt) < 1e-6 ? 2 * (rand() % 2) - 1 :
-
-                // Too close to either wall; switch direction
-                diff > DISTANCE_DIFFERENCE_THRESHOLD ? +1 :
-                diff < -DISTANCE_DIFFERENCE_THRESHOLD ? - 1 :
-
-                // Otherwise, continue in same direction
-                dydt > 0 ? -1 : +1;
-
-            return hf::Setpoint(0, 0, direction * SPEED, 0);
-        }
-
-        static void readSensors(PingPongAutopilot & autopilot,
-                simsens::Robot & robot,
-                simsens::World & world,
-                const simsens::Pose & pose)
-        {
-            autopilot.distance_forward_mm = readRangefinder("VL53L1-forward", robot,
-                    world, pose);
-            autopilot.distance_backward_mm = readRangefinder("VL53L1-backward", robot,
-                    world, pose);
-        }
-
-};
-
-static PingPongAutopilot _autopilot;
+    return hf::Setpoint(0, 0, direction * SPEED, 0);
+}
 
 // Returns false on collision, true otherwise
 // This is called by Webots in the outer (display, kinematics) loop
@@ -102,25 +81,29 @@ DLLEXPORT void webots_physics_step()
 
     if (_helper->get_siminfo(siminfo)) {
 
+        static int distance_forward_mm;
+        static int distance_backward_mm;
+
         // Get current vehicle state
         const auto state = _helper->get_state_from_siminfo(siminfo);
 
         // Replace open-loop setpoint with setpoint from autopilot if
         // available
         siminfo.setpoint = siminfo.mode == hf::MODE_AUTONOMOUS ?
-            PingPongAutopilot::getSetpoint(_autopilot, state.dy) :
+            getSetpoint(distance_forward_mm, distance_backward_mm, state.dy) :
             siminfo.setpoint;
 
         // Get vehicle pose based on setpoint
         const auto pose = _helper->get_pose(siminfo);
 
         // Grab rangefinder readings for next iteration
-        PingPongAutopilot::readSensors(_autopilot, _helper->robot, _helper->world, pose);
+        distance_forward_mm = readRangefinder("VL53L1-forward", _helper->robot,
+                _helper->world, pose);
+        distance_backward_mm = readRangefinder("VL53L1-backward", _helper->robot,
+                _helper->world, pose);
 
         // Log data to file
-        const int distances[] = {
-            _autopilot.distance_forward_mm, 
-            _autopilot.distance_backward_mm};
+        const int distances[] = { distance_forward_mm, distance_backward_mm};
         _helper->write_to_log(pose, distances, 2);
     }
 }
