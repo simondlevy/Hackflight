cfa77d920d28c9ae84dbb1ad8977fee8099fc53e
Simon D. Levy <simon.d.levy@gmail.com>
2026-03-06 11:02:41 -0500

    =Cleanup

--- src/firmware/newimu.hpp
+++ src/firmware/newimu.hpp
@@ -36,9 +36,37 @@ namespace hf {
 
                     float output;
 
-                    LPF() : _delay1(0), _delay2(0) {}
+                    LPF() : output(0), _delay1(0), _delay2(0) {}
+
+                    LPF(const float delay1, const float delay2, const float output)
+                        : output(output), _delay1(0), _delay2(0) {}
 
                     LPF& operator=(const LPF& other) = default;
+
+                    auto apply(
+                            const LPF & lpf,
+                            const float sample,
+                            const float cutoff_freq,
+                            const float sample_freq=1000) -> LPF
+                    {
+                        const auto fr = sample_freq/cutoff_freq;
+                        const auto ohm = tanf(M_PI/fr);
+                        const auto c = 1+2*cosf(M_PI/4)*ohm+ohm*ohm;
+
+                        const auto b0 = ohm*ohm/c;
+                        const auto b1 = 2*b0;
+                        const auto a1 = 2*(ohm*ohm-1)/c;
+                        const auto a2 = (1-2*cosf(M_PI/4)*ohm+ohm*ohm)/c;
+
+                        const auto try_delay0 = sample - lpf._delay1 * a1 - lpf._delay2 * a2;
+
+                        // don't allow bad values to propigate through the filter
+                        const auto delay0 = isfinite(try_delay0) ? try_delay0 : sample;
+
+                        const auto output = delay0 * b0 + lpf._delay1 * b1 + lpf._delay2 * b0;
+
+                        return LPF(delay0, lpf._delay1, output);
+                    }
 
                     void apply(
                             const float sample,
