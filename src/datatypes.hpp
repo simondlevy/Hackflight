c7a1aaf31c35c96bd4f4daf3ca3d4225a3f19e9d
Simon D. Levy <simon.d.levy@gmail.com>
2026-03-07 16:07:56 -0500

    More cleanup

--- src/datatypes.hpp
+++ src/datatypes.hpp
@@ -99,15 +99,6 @@ namespace hf {
 
     } mode_e;
 
-    // XXX delendum
-    typedef struct {
-
-        float x;
-        float y;
-        float z;
-
-    } axis3_t;
-
     typedef struct {
         int16_t x;
         int16_t y;
