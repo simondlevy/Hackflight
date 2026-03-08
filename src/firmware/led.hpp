58b8217bf1d857ee61deb183b6d689fc66b7d6df
Simon D. Levy <simon.d.levy@gmail.com>
2026-03-08 17:18:08 -0400

    modified:   src/firmware/led.hpp

--- src/firmware/led.hpp
+++ src/firmware/led.hpp
@@ -14,49 +14,45 @@
    along with this program. If not, see <http:--www.gnu.org/licenses/>.
  */
 
+#include <Arduino.h>
+
 class LED {
 
     public:
 
         LED(const uint8_t pin) : _pin(pin) {}
 
+        void blinkInLoop(const uint32_t usec_curr)
+        {
+            static uint32_t blink_counter, blink_delay;
+            static bool blinkAlternate;
+
+            if (usec_curr - blink_counter > blink_delay) {
+                blink_counter = micros();
+                digitalWrite(_pin, blinkAlternate); 
+
+                if (blinkAlternate == 1) {
+                    blinkAlternate = 0;
+                    blink_delay = 100000;
+                }
+                else if (blinkAlternate == 0) {
+                    blinkAlternate = 1;
+                    blink_delay = 2000000;
+                }
+            }
+        }
+
+        void blinkOnStartup()
+        {
+            for (int j = 1; j<= 3; j++) {
+                digitalWrite(_pin, LOW);
+                delay(70);
+                digitalWrite(_pin, HIGH);
+                delay(360);
+            }
+        }
     private:
 
         uint8_t _pin;
 };
 
-#if 0
-
-// static const uint8_t LED_PIN = 14; // external 
-static const uint8_t LED_PIN = 13; // built-in
-
-static void blinkInLoop(const uint32_t usec_curr)
-{
-    static uint32_t blink_counter, blink_delay;
-    static bool blinkAlternate;
-
-    if (usec_curr - blink_counter > blink_delay) {
-        blink_counter = micros();
-        digitalWrite(LED_PIN, blinkAlternate); 
-
-        if (blinkAlternate == 1) {
-            blinkAlternate = 0;
-            blink_delay = 100000;
-        }
-        else if (blinkAlternate == 0) {
-            blinkAlternate = 1;
-            blink_delay = 2000000;
-        }
-    }
-}
-
-static void blinkOnStartup()
-{
-    for (int j = 1; j<= 3; j++) {
-        digitalWrite(LED_PIN, LOW);
-        delay(70);
-        digitalWrite(LED_PIN, HIGH);
-        delay(360);
-    }
-}
-#endif
