2cd282ab36abd7e9d3a3b78832c0ca9893980f3a
Simon D. Levy <simon.d.levy@gmail.com>
2026-03-18 14:09:31 -0400

    modified:   ../../src/firmware/filters/imufilter.hpp

--- src/firmware/filters/imufilter.hpp
+++ src/firmware/filters/imufilter.hpp
@@ -139,7 +139,7 @@ namespace hf {
                 const auto gyroSampleCount = isBufferFilled ? 0 : newBufferIndex;
 
                 const auto gyroUnbiased =
-                    scale(Vec3(imuraw.gyro) - gyroBias, gyro_range_dps);
+                    scale(Vec3(gyroraw) - gyroBias, gyro_range_dps);
 
                 const auto gyroAligned = alignToAirframe(gyroUnbiased);
 
