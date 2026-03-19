6794f95d1d0e49872c656508d0d61236a8272ccc
Simon D. Levy <simon.d.levy@gmail.com>
2026-03-18 21:57:50 -0400

    modified:   lsm6dso.ino

--- examples/lsm6dso/lsm6dso.ino
+++ examples/lsm6dso/lsm6dso.ino
@@ -1,44 +1,32 @@
 #include <LSM6DSOSensor.h>
 
-// Components
-LSM6DSOSensor AccGyr(&Wire);
+#include <hackflight.h>
+#include <firmware/debugging.hpp>
+
+static LSM6DSOSensor AccGyr(&Wire);
 
 void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(0);
 
-  Serial.begin(0);
-  
-  Wire.begin();
-  
-  AccGyr.begin();
-  AccGyr.Enable_X();
-  AccGyr.Enable_G();
+    Wire.begin();
+
+    AccGyr.begin();
+    AccGyr.Enable_X();
+    AccGyr.Enable_G();
 }
 
 void loop() 
 {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(250);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(250);
+    int32_t accel[3] = {};
+    AccGyr.Get_X_Axes(accel);
 
-  int32_t accelerometer[3];
-  int32_t gyroscope[3];
-  AccGyr.Get_X_Axes(accelerometer);
-  AccGyr.Get_G_Axes(gyroscope);
+    int32_t gyro[3] = {};
+    AccGyr.Get_G_Axes(gyro);
 
-  Serial.print("| Acc[mg]: ");
-  Serial.print(accelerometer[0]);
-  Serial.print(" ");
-  Serial.print(accelerometer[1]);
-  Serial.print(" ");
-  Serial.print(accelerometer[2]);
-  Serial.print(" | Gyr[mdps]: ");
-  Serial.print(gyroscope[0]);
-  Serial.print(" ");
-  Serial.print(gyroscope[1]);
-  Serial.print(" ");
-  Serial.print(gyroscope[2]);
-  Serial.println(" |");
+    const auto imuraw = hf::ImuRaw(
+            hf::Vec3Raw(gyro[0], gyro[1], gyro[2]),
+            hf::Vec3Raw(accel[0], accel[1], accel[2]));
+
+    hf::Debugger::report(imuraw);
 }
