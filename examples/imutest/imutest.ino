d7fb8bd3da4d7f4a57948e0e08c9e64fafb86172
Simon D. Levy <simon.d.levy@gmail.com>
2026-03-22 17:13:15 -0400

    modified:   imutest.ino

--- examples/imutest/imutest.ino
+++ examples/imutest/imutest.ino
@@ -1,38 +1,15 @@
 #include <LSM6DSOSensor.h>
 
-static const uint8_t INT_1_PIN = 4;
+static const uint8_t INT_PIN = 4;
+
+static const int16_t GRANGE = 2000;
+static const int16_t ARANGE = 16; 
 
 static LSM6DSOSensor _lsm6dso(&Wire);
-
-static volatile bool got_interrupt;
-
-static void INT1Event_cb()
-{
-    got_interrupt = true;
-}
 
 static bool bad(const LSM6DSOStatusTypeDef status)
 {
     return status != LSM6DSO_OK;
-}
-
-static void sendOrientation()
-{
-    uint8_t xl = 0;
-    uint8_t xh = 0;
-    uint8_t yl = 0;
-    uint8_t yh = 0;
-    uint8_t zl = 0;
-    uint8_t zh = 0;
-
-    _lsm6dso.Get_6D_Orientation_XL(&xl);
-    _lsm6dso.Get_6D_Orientation_XH(&xh);
-    _lsm6dso.Get_6D_Orientation_YL(&yl);
-    _lsm6dso.Get_6D_Orientation_YH(&yh);
-    _lsm6dso.Get_6D_Orientation_ZL(&zl);
-    _lsm6dso.Get_6D_Orientation_ZH(&zh);
-
-    printf("xl=%d xh=%d yl=%d yh=%d zl=%d zh=%d\n", xl, xh, yl, yh, zl, zh);
 }
 
 void setup()
@@ -43,12 +20,12 @@ void setup()
 
     Wire.begin();
 
-    attachInterrupt(INT_1_PIN, INT1Event_cb, RISING);
-
     if (
             bad(_lsm6dso.begin()) ||
+            bad(_lsm6dso.Enable_G())  ||
             bad(_lsm6dso.Enable_X()) ||
-            bad(_lsm6dso.Enable_6D_Orientation(LSM6DSO_INT1_PIN))) {
+            bad(_lsm6dso.Set_X_FS(ARANGE)) ||
+            bad(_lsm6dso.Set_G_FS(GRANGE))) {
 
         while (true) {
             printf("Initialization failed\n");
@@ -59,23 +36,19 @@ void setup()
 
 void loop()
 {
+    uint8_t status = 0;
 
-    if (got_interrupt) {
+    _lsm6dso.Get_G_DRDY_Status(&status);
 
-        got_interrupt = false;
+    if (status) {
 
-        LSM6DSO_Event_Status_t status;
+        int16_t g[3] = {};
+        _lsm6dso.Get_G_AxesRaw(g);
 
-        _lsm6dso.Get_X_Event_Status(&status);
+        int16_t a[3] = {};
+        _lsm6dso.Get_X_AxesRaw(a);
 
-        if (status.D6DOrientationStatus) {
-
-            sendOrientation();
-
-            digitalWrite(LED_BUILTIN, HIGH);
-            delay(100);
-            digitalWrite(LED_BUILTIN, LOW);  
-        }
+        printf("gx=%d gy=%d gz=%d ax=%d ay=%d az=%d\n",
+                g[0], g[1], g[2], a[0], a[1], a[2]);
     }
 }
-
