e817036f290abde68f54bacb67b5a99ea86a7694
Simon D. Levy <simon.d.levy@gmail.com>
2026-03-30 17:05:21 -0400

    modified:   src/firmware/zranger/filter.hpp

--- src/firmware/zranger/filter.hpp
+++ src/firmware/zranger/filter.hpp
@@ -24,47 +24,6 @@ namespace hf {
 
     class ZRangerFilter {
 
-        public:
-
-            typedef struct {
-                uint32_t timestamp;
-                float distance;
-                float stdDev;
-            } measurement_t;
-
-            void init()
-            {
-                // pre-compute constant in the measurement noise model for kalman
-                _expCoeff =
-                    logf(EXP_STD_B / EXP_STD_A) / (EXP_POINT_B - EXP_POINT_A);
-
-                device_init();
-            }
-
-            bool read(measurement_t & tofData, const uint32_t tick)
-            {
-                float range = device_read();
-
-                // check if range is feasible and push into the estimator the
-                // sensor should not be able to measure >5 [m], and outliers
-                // typically occur as >8 [m] measurements
-                if (range < OUTLIER_LIMIT_MM) {
-
-                    float distance = range / 1000; // Scale from [mm] to [m]
-
-                    float stdDev = EXP_STD_A * (
-                            1 + expf(_expCoeff * (distance - EXP_POINT_A)));
-
-                    tofData.timestamp = tick;
-                    tofData.distance = distance;
-                    tofData.stdDev = stdDev;
-
-                    return true;
-                }
-
-                return false;
-            }
-
         private:
 
             static const uint16_t OUTLIER_LIMIT_MM = 5000;
@@ -75,11 +34,52 @@ namespace hf {
             static constexpr float EXP_POINT_B = 4.0;
             static constexpr float EXP_STD_B = 0.2;   
 
-            float _expCoeff;
+        public:
 
-            bool device_init();
+            ZRangerFilter() = default;
 
-            float device_read();
+            ZRangerFilter& operator=(const ZRangerFilter& other) = default;
+ 
+            /*
+            static auto read(
+                    const ZRangerFilter & filter,
+                    const uint16_t range_mm,
+                    const uint32_t tick) -> ZRangerFilter
+            {
+                static constexpr float EXP_COEFF =
+                    logf(EXP_STD_B / EXP_STD_A) / (EXP_POINT_B - EXP_POINT_A);
+
+                const auto inrange = range < OUTLIER_LIMIT_MM;
+
+                const float distance_m = range_mm / 1000.f;
+
+                // check if range is feasible and push into the estimator the
+                // sensor should not be able to measure >5 [m], and outliers
+                // typically occur as >8 [m] measurements
+
+                if (range < OUTLIER_LIMIT_MM) {
+
+                    float distance = range / 1000; // Scale from [mm] to [m]
+
+                    float stdDev = EXP_STD_A * (
+                            1 + expf(EXP_COEFF * (distance - EXP_POINT_A)));
+
+                    tofData.timestamp = tick;
+                    tofData.distance = distance;
+                    tofData.stdDev = stdDev;
+
+                    return true;
+                }
+
+                return false;
+            }*/
+
+        private:
+
+            bool inrange;
+            uint32_t timestamp;
+            float distance;
+            float stdDev;
     };
 
 }
