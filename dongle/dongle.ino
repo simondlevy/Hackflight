38cb196426f97ca76de35334a4ab78be533f5e7f
Simon D. Levy <simon.d.levy@gmail.com>
2026-04-12 14:57:35 -0400

    deleted:    src/firmware/espnow.h

--- dongle/dongle.ino
+++ dongle/dongle.ino
@@ -9,76 +9,72 @@
    of this software and associated documentation files.  The above copyright
    notice and this permission notice shall be included in all copies or
    substantial portions of the Software.
-*/
+ */
 
 
 #include <esp_now.h>
 #include <WiFi.h>
 
+#include <hackflight.h>
+
 // REPLACE WITH YOUR RECEIVER MAC Address
 uint8_t broadcastAddress[] = {0x54, 0x32, 0x04, 0x33, 0x0D, 0xF0};
-
-#include <hackflight.h>
-#include <firmware/espnow.h>
-
-// Create a espnow_message_t called myData
-static espnow_message_t myData;
-
-static esp_now_peer_info_t peerInfo;
 
 // callback when data is sent
 static void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
 {
-  Serial.print("\r\nLast Packet Send Status:\t");
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+    Serial.print("\r\nLast Packet Send Status:\t");
+    Serial.println(status == ESP_NOW_SEND_SUCCESS ?
+            "Delivery Success" : "Delivery Fail");
 }
- 
+
 void setup()
 {
-  // Init Serial Monitor
-  Serial.begin(115200);
- 
-  // Set device as a Wi-Fi Station
-  WiFi.mode(WIFI_STA);
+    // Init Serial Monitor
+    Serial.begin(115200);
 
-  // Init ESP-NOW
-  if (esp_now_init() != ESP_OK) {
-    Serial.println("Error initializing ESP-NOW");
-    return;
-  }
+    // Set device as a Wi-Fi Station
+    WiFi.mode(WIFI_STA);
 
-  // Once ESPNow is successfully Init, we will register for Send CB to
-  // get the status of Trasnmitted packet
-  esp_now_register_send_cb(esp_now_send_cb_t(OnDataSent));
-  
-  // Register peer
-  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 0;  
-  peerInfo.encrypt = false;
-  
-  // Add peer        
-  if (esp_now_add_peer(&peerInfo) != ESP_OK){
-    Serial.println("Failed to add peer");
-    return;
-  }
+    // Init ESP-NOW
+    if (esp_now_init() != ESP_OK) {
+        Serial.println("Error initializing ESP-NOW");
+        return;
+    }
+
+    // Once ESPNow is successfully Init, we will register for Send CB to
+    // get the status of Trasnmitted packet
+    esp_now_register_send_cb(esp_now_send_cb_t(OnDataSent));
+
+    // Register peer
+    esp_now_peer_info_t peerInfo = {};
+    memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+    peerInfo.channel = 0;  
+    peerInfo.encrypt = false;
+
+    // Add peer        
+    if (esp_now_add_peer(&peerInfo) != ESP_OK){
+        Serial.println("Failed to add peer");
+        return;
+    }
 }
- 
+
 void loop()
 {
-  // Set values to send
-  strcpy(myData.a, "THIS IS A CHAR");
-  myData.b = random(1,20);
-  myData.c = 1.2;
-  myData.d = false;
-  
-  // Send message via ESP-NOW
-  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
-   
-  if (result == ESP_OK) {
-    Serial.println("Sent with success");
-  }
-  else {
-    Serial.println("Error sending the data");
-  }
-  delay(2000);
+    static uint8_t k;
+
+    const auto c = (char)('A' +k);
+
+    // Send message via ESP-NOW
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &c, 1);
+
+    k = (k + 1) % 26;
+
+    if (result == ESP_OK) {
+        Serial.println("Sent with success");
+    }
+    else {
+        Serial.println("Error sending the data");
+    }
+    delay(2000);
 }
--- xiao/xiao.ino
+++ xiao/xiao.ino
@@ -15,48 +15,37 @@
 #include <WiFi.h>
 
 #include <hackflight.h>
-#include <firmware/espnow.h>
-
-// Create a espnow_message_t called myData
-static espnow_message_t myData;
 
 // callback function that will be executed when data is received
 static void OnDataRecv(
         const uint8_t * mac, const uint8_t *incomingData, int len)
 {
-  memcpy(&myData, incomingData, sizeof(myData));
-  Serial.print("Bytes received: ");
-  Serial.println(len);
-  Serial.print("Char: ");
-  Serial.println(myData.a);
-  Serial.print("Int: ");
-  Serial.println(myData.b);
-  Serial.print("Float: ");
-  Serial.println(myData.c);
-  Serial.print("Bool: ");
-  Serial.println(myData.d);
-  Serial.println();
+    (void)mac;
+
+    for (int k=0; k<len; ++k) {
+        Serial.println((char)incomingData[k]);
+    }
 }
- 
+
 void setup()
 {
-  // Initialize Serial Monitor
-  Serial.begin(115200);
-  
-  // Set device as a Wi-Fi Station
-  WiFi.mode(WIFI_STA);
+    // Initialize Serial Monitor
+    Serial.begin(115200);
 
-  // Init ESP-NOW
-  if (esp_now_init() != ESP_OK) {
-    Serial.println("Error initializing ESP-NOW");
-    return;
-  }
-  
-  // Once ESPNow is successfully Init, we will register for recv CB to
-  // get recv packer info
-  esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
+    // Set device as a Wi-Fi Station
+    WiFi.mode(WIFI_STA);
+
+    // Init ESP-NOW
+    if (esp_now_init() != ESP_OK) {
+        Serial.println("Error initializing ESP-NOW");
+        return;
+    }
+
+    // Once ESPNow is successfully Init, we will register for recv CB to
+    // get recv packer info
+    esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
 }
- 
+
 void loop()
 {
 
