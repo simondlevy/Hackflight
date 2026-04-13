be131243ac51d99218038bf0356533125470cba0
Simon D. Levy <simon.d.levy@gmail.com>
2026-04-13 00:10:21 -0400

    renamed:    teensy/poshold/elrs.ino -> teensy/poshold/poshold.ino

--- teensy/poshold/poshold.ino
+++ teensy/poshold/poshold.ino
@@ -23,7 +23,6 @@
 
 #include <hackflight.h>
 #include <datatypes.hpp>
-#include <mixers/bfquadx.hpp>
 #include <pidcontrol/pids/position.hpp>
 #include <pidcontrol/stabilizer.hpp>
 
@@ -36,23 +35,13 @@
 #include <firmware/opticalflow/filter.hpp>
 #include <firmware/opticalflow/sensor.hpp>
 #include <firmware/profiling.hpp>
-#include <firmware/rx.hpp>
 #include <firmware/safety.hpp>
 #include <firmware/timer.hpp>
-#include <firmware/setpoint.hpp>
 #include <firmware/zranger/filter.hpp>
 #include <firmware/zranger/sensor.hpp>
 using namespace hf;
 
-//#define _DEBUG
-//#define _PROFILE
-//#define _POSHOLD
-
-#ifdef _POSHOLD
 static const uint8_t LED_PIN = 9;
-#else
-static const uint8_t LED_PIN = LED_BUILTIN;
-#endif
 
 // Rate constants
 static constexpr float EKF_PREDICTION_RATE_HZ       = 100;
@@ -62,8 +51,6 @@ static constexpr float FLOWDECK_ACQUISITION_RATE_HZ = 100;
 // Devices
 static IMU _imu;
 static auto _led = LED(LED_PIN);
-static auto _rx = RX(&Serial5);
-static auto _motors = DshotTeensy4({2, 3, 4, 5});
 static ZRanger _zranger;
 static OpticalFlowSensor _flowsensor;
 
@@ -75,69 +62,37 @@ static auto _flowdeckTimer = Timer(FLOWDECK_ACQUISITION_RATE_HZ);
 // Setup
 void setup()
 {
-    _rx.begin();
     _imu.begin();
-    _motors.begin(); 
     _led.begin(); 
-
-#ifdef _POSHOLD
     _zranger.begin();
     _flowsensor.begin();
-#else
-    (void)_zranger;
-    (void)_flowsensor;
-#endif
 }
 
 // Loop
 void loop()
 {
-
-    // Debugging
     static Debugger _debugger;
-    static Profiler _profiler;
 
-    // Computation
     static EKF _ekf;
     static FlyingCheck _flyingCheck;
     static ImuFilter _imuFilter;
-    static Mixer _mixer;
-    static StabilizerPid _stabilizerPid;
     static OpticalFlowFilter _opticalFlowFilter;
     static ZRangerFilter _zrangerFilter;
 
-    // Flight mode
-    static mode_e _mode;
-
-    const auto dt = Timer::getDt();
-
     _led.blink(_imuFilter.isGyroCalibrated);
-
-    // Disable arming while gyro is calibrating
-    const auto rxdata =
-        _imuFilter.isGyroCalibrated ? _rx.read() : RX::Data();
 
     const auto imuraw = _imu.read();
 
     _imuFilter = ImuFilter::step(_imuFilter, millis(), imuraw,
             _imu.gyroRangeDps(), _imu.accelRangeGs());
 
-#ifdef _POSHOLD
+    // Slower EKF update with range, optical flow
     if (_flowdeckTimer.ready()) {
-
         _zrangerFilter = ZRangerFilter::update(_zrangerFilter, _zranger.read());
-
         _opticalFlowFilter = OpticalFlowFilter::update(_opticalFlowFilter,
                 micros(), _flowsensor.read());
-
-        // Slower EKF update with range, optical flow
         _ekf = EKF::update(_ekf, _zrangerFilter, _opticalFlowFilter);
     }
-#else
-    (void)_flowdeckTimer;
-    (void)_zrangerFilter;
-    (void)_opticalFlowFilter;
-#endif
 
     // Run the system dynamics to predict the state forward.
     if (_ekfPredictionTimer.ready()) {
@@ -147,34 +102,5 @@ void loop()
     // Faster EKF update with IMU readings
     _ekf = EKF::update(_ekf, _imuFilter.output, millis());
 
-    const auto state = EKF::getVehicleState(_ekf);
-
-    _mode = Safety::updateMode(state, rxdata, _imuFilter, _mode);
-
-#ifdef _DEBUG
-#ifdef _POSHOLD
-    _debugger.report(state, true);
-#else
-    _debugger.report(state);
-#endif
-#endif
-
-#ifdef _PROFILE
-    _profiler.report();
-#endif
-
-    const auto setpoint = mksetpoint(rxdata.axes);
-    _stabilizerPid = StabilizerPid::run( _stabilizerPid,
-            !rxdata.is_throttle_down, dt, state, setpoint);
-
-    _mixer = Mixer::run(_mixer, _stabilizerPid.setpoint);
-
-    if (_mode != MODE_PANIC) {
-        _motors.run(rxdata.is_armed, _mixer.motorvals);
-    }
-
-    if (_flyingCheckTimer.ready()) {
-        _flyingCheck = FlyingCheck::run(
-                _flyingCheck, millis(), _mixer.motorvals, 4);
-    }
+    _debugger.report(EKF::getVehicleState(_ekf), true);
 }
