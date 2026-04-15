e942d155bf5b9fe2fdfd3e32e06295094ee1282b
Simon D. Levy <simon.d.levy@gmail.com>
2026-04-15 18:02:57 -0400

    new file:   commtest/dongle/dongle.ino

---
+++ commtest/xiao/xiao.ino
@@ -0,0 +1,119 @@
+/*
+   Adapted from Rui Santos & Sara Santos - Random Nerd Tutorials
+
+   Complete project details at
+https://RandomNerdTutorials.com/esp-now-two-way-communication-esp32/ Permission
+is hereby granted, free of charge, to any person obtaining a copy of this
+software and associated documentation files.  The above copyright notice and
+this permission notice shall be included in all copies or substantial portions
+of the Software.
+ */
+
+
+#include <esp_now.h>
+#include <WiFi.h>
+
+static const uint8_t BROADCAST_ADDRESS[] = {0xD4,0xD4,0xDA,0x83,0x97,0x90};
+
+// Define variables to store BME280 readings to be sent
+float temperature;
+float humidity;
+float pressure;
+
+// Define variables to store incoming readings
+float incomingTemp;
+float incomingHum;
+float incomingPres;
+
+// Variable to store if sending data was successful
+String success;
+
+//Structure example to send data
+//Must match the receiver structure
+typedef struct struct_message {
+    float temp;
+    float hum;
+    float pres;
+} struct_message;
+
+// Create a struct_message called BME280Readings to hold sensor readings
+struct_message BME280Readings;
+
+// Create a struct_message to hold incoming sensor readings
+struct_message incomingReadings;
+
+esp_now_peer_info_t peerInfo;
+
+// Callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  if (status ==0){
+    success = "Delivery Success :)";
+  }
+  else{
+    success = "Delivery Fail :(";
+  }
+}
+
+// Callback when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  incomingTemp = incomingReadings.temp;
+  incomingHum = incomingReadings.hum;
+  incomingPres = incomingReadings.pres;
+}
+
+static void getReadings(){
+  temperature = 0;
+  humidity = 1;
+  pressure = 2;
+}
+
+void setup() {
+  // Init Serial Monitor
+  Serial.begin(115200);
+
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(esp_now_send_cb_t(OnDataSent));
+
+  // Register peer
+  memcpy(peerInfo.peer_addr, BROADCAST_ADDRESS, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+
+  // Add peer
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+  // Register for a callback function that will be called when data is received
+  esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
+}
+
+void loop() {
+
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(BROADCAST_ADDRESS, (uint8_t *) &BME280Readings, sizeof(BME280Readings));
+
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+  delay(10000);
+}
+
