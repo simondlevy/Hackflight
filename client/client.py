3fa0191a89e7e12a4d2ee918192a90b8e3e2fc47
simondlevy <simon.d.levy@gmail.com>
2026-05-21 14:55:04 -0400

    modified:   client.py

--- client/client.py
+++ client/client.py
@@ -15,20 +15,35 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 '''
 
+import time
+from threading import Thread
+
 from gamepad import Gamepad
 from telemetry import Telemetry
+
+
+def telemetry_threadfun(telemetry):
+
+    while True:
+
+        telemetry.step()
+
+        time.sleep(0)  # yield
+
 
 def main():
 
     # Telemetry parser does all the initialization
-    telemetryParser = Telemetry()
+    telemetry = Telemetry()
 
-    gamepad = Gamepad(telemetryParser.port)
+    thread = Thread(target=telemetry_threadfun, args=(telemetry, ))
+    thread.daemon = True
+    thread.start()
+
+    gamepad = Gamepad(telemetry.port)
 
     while gamepad.connected:
 
         gamepad.step()
-
-        telemetryParser.step()
 
 main()
