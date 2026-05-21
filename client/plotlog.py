b3d9d5c3940ad03603c9613c156ca7d3ad075353
simondlevy <simon.d.levy@gmail.com>
2026-05-20 21:08:58 -0400

    new file:   plotlog.py

---
+++ client/plotlog.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''
+Copyright (C) 2026 Simon D. Levy
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, in version 3.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+
+    data = np.loadtxt('log.csv', delimiter=',', skiprows=1)
+
+    plt.subplot(2, 1, 1)
+    plt.plot(data[:,2])
+
+    plt.subplot(2, 1, 2)
+    plt.plot(data[:,3])
+
+    plt.show()
+
+
+main()
