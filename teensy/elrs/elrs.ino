f5c525cd3d68bde63cb1acd04bd3551392c377cc
simondlevy <simon.d.levy@gmail.com>
2026-05-21 16:56:03 -0400

    deleted:    teensy/elrs/receiver.cpp

--- teensy/elrs/elrs.ino
+++ teensy/elrs/elrs.ino
@@ -16,10 +16,50 @@
    along with this program. If not, see <http:--www.gnu.org/licenses/>.
  */
 
+// Third-party libraries
+#include <CRSFforArduino.hpp>
+
+// Hackflight library
 #include <hackflight.h>
 #include <firmware/quadcore.hpp>
+#include <firmware/receiver.hpp>
 using namespace hf;
 
+static CRSFforArduino _crsf = CRSFforArduino(&Serial2);
+
+static Receiver::Data _rxdata;
+
+static void onReceiveRcChannels(serialReceiverLayer::rcChannels_t *rcChannels)
+{
+    if (!rcChannels->failsafe) {
+
+        _rxdata = Receiver::Data::update(
+                _rxdata,
+                _crsf.readRcChannel(3),
+                _crsf.readRcChannel(1),
+                _crsf.readRcChannel(2),
+                _crsf.readRcChannel(4),
+                _crsf.readRcChannel(5),
+                millis());
+    }
+}
+
+void Receiver::begin()
+{
+    if (!_crsf.begin()) {
+        _crsf.end();
+        Debugger::reportForever("Unable to start ELRS receiver");
+    }
+
+    _crsf.setRcChannelsCallback(onReceiveRcChannels);
+}
+
+auto Receiver::read() -> Receiver::Data
+{
+    _crsf.update();
+
+    return _rxdata;
+}
 static QuadCore _core;
 
 void setup()
