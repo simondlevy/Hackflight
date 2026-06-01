1c299e1ced4d4953bdc5eacb03546b31b3b999b5
simondlevy <simon.d.levy@gmail.com>
2026-06-01 02:12:59 -0400

    modified:   telemetry.py cleanup

--- client/telemetry.py
+++ client/telemetry.py
@@ -81,7 +81,7 @@ class Telemetry(MspParser):
 
             except Exception as e:
                 print('Unable to open log file %s: %s' %
-                        (args.outfile, str(e)))
+                      (args.outfile, str(e)))
                 exit(1)
 
         self.plotter = None
@@ -107,8 +107,8 @@ class Telemetry(MspParser):
                             self.PLOTTER_Z_RANGE,
                             (self.PLOTTER_DZ_RANGE[0],
                                 0, self.PLOTTER_DZ_RANGE[1])),
-                            ylabels=('Z (m)', 'dZ/dt (m/s)'),
-                            styles=('b-', 'g-'))
+                        ylabels=('Z (m)', 'dZ/dt (m/s)'),
+                        styles=('b-', 'g-'))
 
         self.running = True
 
@@ -126,16 +126,16 @@ class Telemetry(MspParser):
         return self.plotter_data
 
     def handle_TELEMETRY(self, thrust, roll, pitch, yaw,
-            dx, dy, z, dz, phi, dphi, theta, dtheta, psi, dpsi):
+                         dx, dy, z, dz, phi, dphi, theta, dtheta, psi, dpsi):
 
         if self.outfile is None and self.plotter is None:
 
             print(('thrust=%+3.3f roll=%+3.3f pitch=%+3.3f yaw=%+3.3f | ' +
-                'dx=%+03.2f dy=%+03.2f z=%+03.2f dz=%+03.2f ' +
-                'phi=%+5.1f dphi=%+6.1f theta=%+5.1f dtheta=%+6.1f ' +
-                'psi=%+5.1f dpsi=%+5.1f') %
-                (thrust, roll, pitch, yaw,
-                    dx, dy, z, dz, phi, dphi, theta, dtheta, psi, dpsi))
+                   'dx=%+03.2f dy=%+03.2f z=%+03.2f dz=%+03.2f ' +
+                   'phi=%+5.1f dphi=%+6.1f theta=%+5.1f dtheta=%+6.1f ' +
+                   'psi=%+5.1f dpsi=%+5.1f') %
+                  (thrust, roll, pitch, yaw,
+                   dx, dy, z, dz, phi, dphi, theta, dtheta, psi, dpsi))
 
         elif self.outfile is not None:
 
