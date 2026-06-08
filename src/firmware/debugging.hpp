b2843e3ad2ac61a65be061494513e20a47de3486
Simon D. Levy <simon.d.levy@gmail.com>
2026-06-08 17:00:44 -0400

    =More trailing underscore

--- src/firmware/debugging.hpp
+++ src/firmware/debugging.hpp
@@ -35,7 +35,7 @@ namespace hf {
 
                 private:
 
-                    Timer _timer = Timer(FREQ);
+                    Timer timer_ = Timer(FREQ);
 
                 public:
 
@@ -43,7 +43,7 @@ namespace hf {
 
                     bool ready()
                     {
-                        if (_timer.ready()) {
+                        if (timer_.ready()) {
                             count++;
                             return true;
                         }
@@ -72,59 +72,59 @@ namespace hf {
 
             void report(const Mode mode)
             {
-                if (_helper.ready()) {
+                if (helper_.ready()) {
 
-                    printf("%5lu | mode=%s\n", _helper.count, MODENAMES[mode]);
+                    printf("%5lu | mode=%s\n", helper_.count, MODENAMES[mode]);
                 }
             }
 
             void reportFloat(const char * label, const float value)
             {
-                if (_helper.ready()) {
+                if (helper_.ready()) {
 
-                    printf("%5lu | %s=%f\n", _helper.count, label, value);
+                    printf("%5lu | %s=%f\n", helper_.count, label, value);
                 }
             }
 
             void reportBool(const char * label, const bool flag)
             {
-                if (_helper.ready()) {
+                if (helper_.ready()) {
 
                     printf("%5lu | %s=%s\n",
-                            _helper.count, label, flag ? "true" : "false");
+                            helper_.count, label, flag ? "true" : "false");
                 }
             }
 
             void report(const Setpoint & setpoint)
             {
-                if (_helper.ready()) {
+                if (helper_.ready()) {
 
                     printf("%5lu | thrust=%+3.3f roll=%+3.3f pitch=%+3.3f "
                             "yaw=%+3.3f\n",
-                            _helper.count, setpoint.thrust, setpoint.roll,
+                            helper_.count, setpoint.thrust, setpoint.roll,
                             setpoint.pitch, setpoint.yaw); }
             }
 
             void reportHover(const VehicleState & state)
             {
-                if (_helper.ready()) {
+                if (helper_.ready()) {
 
                     printf("%5lu | dx=%+3.3f dy=%+3.3f z=%6.3f dz=%+5.3f\n",
-                            _helper.count, state.dx, state.dy, state.z,
+                            helper_.count, state.dx, state.dy, state.z,
                             state.dz);
                 }
             }
 
             void report(const VehicleState & state, const bool full=false)
             {
-                if (_helper.ready()) {
+                if (helper_.ready()) {
 
                     if (full) {
                         printf("%5lu | "
                                 "dx=%+03.3f dy=%+03.3f z=%6.3f dz=%+5.3f "
                                 "phi=%+03.0f dphi=%+04.0f theta=%+03.0f "
                                 " dtheta=%+04.0f psi=%+04.0f dpsi=%+04.0f\n",
-                                _helper.count, state.dx, state.dy, state.z,
+                                helper_.count, state.dx, state.dy, state.z,
                                 state.dz, state.phi, state.dphi, state.theta,
                                 state.dtheta, state.psi, state.dpsi);
                     }
@@ -132,7 +132,7 @@ namespace hf {
                         printf("%5lu | "
                                 "phi=%+03.0f dphi=%+04.0f theta=%+03.0f "
                                 "dtheta=%+04.0f psi=%+04.0f dpsi=%+04.0f\n",
-                                _helper.count, state.phi, state.dphi,
+                                helper_.count, state.phi, state.dphi,
                                 state.theta, state.dtheta, state.psi,
                                 state.dpsi);
                     }
@@ -141,45 +141,45 @@ namespace hf {
 
             void report(const IMU::ThreeAxisRaw & raw)
             {
-                if (_helper.ready()) {
+                if (helper_.ready()) {
 
                     printf("%5lu | x=%+05d y=%+05d z=%+05d\n",
-                            _helper.count, raw.x, raw.y, raw.z);
+                            helper_.count, raw.x, raw.y, raw.z);
                 }
             }
 
             void report(const ThreeAxis & vec)
             {
-                if (_helper.ready()) {
+                if (helper_.ready()) {
 
                     printf("%5lu | x=%+04.0f y=%+04.0f z=%+04.0f\n",
-                            _helper.count, vec.x, vec.y, vec.z);
+                            helper_.count, vec.x, vec.y, vec.z);
                 }
             }
 
             void report(const ImuFilter::Data & imufilt)
             {
-                if (_helper.ready()) {
+                if (helper_.ready()) {
 
                     const auto g = imufilt.gyro_dps;
                     const auto a = imufilt.accel_gs;
 
                     printf("%5lu | gx=%+04.0f gy=%+04.0f gz=%+04.0f DPS | "
                             "ax=%+05.3f ay=%+05.3f az=%+05.3f Gs\n",
-                            _helper.count, g.x, g.y, g.z, a.x, a.y, a.z);
+                            helper_.count, g.x, g.y, g.z, a.x, a.y, a.z);
                 }
             }
 
             void report(const IMU::RawData & imuraw)
             {
-                if (_helper.ready()) {
+                if (helper_.ready()) {
 
                     const auto gyro = imuraw.gyro;
                     const auto accel = imuraw.accel;
 
                     printf("%5lu | gx=%+05d gy=%+05d gz=%+05d | "
                             "ax=%+05d ay=%+05d az=%+05d\n",
-                            _helper.count, gyro.x, gyro.y, gyro.z,
+                            helper_.count, gyro.x, gyro.y, gyro.z,
                             accel.x, accel.y, accel.z);
                 }
             }
@@ -189,9 +189,9 @@ namespace hf {
                     const uint8_t count,
                     const char * fmt="%f")
             {
-                if (_helper.ready()) {
+                if (helper_.ready()) {
 
-                    printf("%5lu | ", _helper.count);
+                    printf("%5lu | ", helper_.count);
 
                     for (uint8_t k=0; k<count; ++k) {
                         printf("m%d=", k+1);
@@ -205,7 +205,7 @@ namespace hf {
 
         private:
 
-            Helper _helper;
+            Helper helper_;
 
 
     };
