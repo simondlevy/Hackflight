27381723b4b30b3e59c2778dc0de5adbf320d8ef
Simon D. Levy <simon.d.levy@gmail.com>
2026-06-16 01:33:29 -0400

    SEVJr tail rotor spinning nicely

--- webots/controllers/standard/sevjr.py
+++ webots/controllers/standard/sevjr.py
@@ -24,8 +24,8 @@ def main():
 
     robot = makeRobot()
 
-    startMotor(robot, 'motor_front_left', -1)
-    startMotor(robot, 'motor_front_right', +1)
+    #startMotor(robot, 'motor_front_left', -1)
+    #startMotor(robot, 'motor_front_right', +1)
     startMotor(robot, 'motor_rear', +1)
 
     run(robot)
