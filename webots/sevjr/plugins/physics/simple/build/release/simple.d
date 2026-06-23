3bfb51a73a90cb465d92899ef0e4e3ac4a165c7e
simondlevy <simon.d.levy@gmail.com>
2026-06-23 18:40:24 -0400

    renamed:    webots/apex2/meshes/stl_files/apex2/battery.stl -> webots/apex2/meshes/stl_files/battery.stl

---
+++ webots/sevjr/plugins/physics/simple/build/release/simple.d
@@ -0,0 +1,26 @@
+build/release/simple.o: simple.cpp helper.hpp \
+ /usr/local/webots/include/plugins/physics.h \
+ /usr/local/webots/include/ode/ode/ode.h \
+ /usr/local/webots/include/ode/ode/odeconfig.h \
+ /usr/local/webots/include/ode/ode/precision.h \
+ /usr/local/webots/include/ode/ode/compatibility.h \
+ /usr/local/webots/include/ode/ode/common.h \
+ /usr/local/webots/include/ode/ode/error.h \
+ /usr/local/webots/include/ode/ode/odeinit.h \
+ /usr/local/webots/include/ode/ode/contact.h \
+ /usr/local/webots/include/ode/ode/memory.h \
+ /usr/local/webots/include/ode/ode/odemath.h \
+ /usr/local/webots/include/ode/ode/odemath_legacy.h \
+ /usr/local/webots/include/ode/ode/matrix.h \
+ /usr/local/webots/include/ode/ode/timer.h \
+ /usr/local/webots/include/ode/ode/rotation.h \
+ /usr/local/webots/include/ode/ode/mass.h \
+ /usr/local/webots/include/ode/ode/misc.h \
+ /usr/local/webots/include/ode/ode/objects.h \
+ /usr/local/webots/include/ode/ode/collision_space.h \
+ /usr/local/webots/include/ode/ode/collision.h \
+ /usr/local/webots/include/ode/ode/collision_trimesh.h \
+ /usr/local/webots/include/ode/ode/export-dif.h \
+ /usr/local/webots/include/ode/ode/version.h \
+ /usr/local/webots/include/ode/ode/odecpp.h \
+ /usr/local/webots/include/ode/ode/odecpp_collision.h
