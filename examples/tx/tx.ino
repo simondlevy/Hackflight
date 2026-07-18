79dc73106f2db74aff4733894d985228b8be18e3
simondlevy <simon.d.levy@gmail.com>
2026-07-18 19:35:43 -0400

    modified:   tx.ino

--- examples/tx/tx.ino
+++ examples/tx/tx.ino
@@ -12,16 +12,25 @@
  * along with this program. If not, see <http:--www.gnu.org/licenses/>.
  */
 
+#include <TinyPICO.h>
+
+#include <hackflight.h>
+#include <firmware/voltage_divider.hpp>
+
 #include "pushbutton.hpp"
 
-static const uint8_t THROTTLE_INPUT_PIN = 25;
-static const uint8_t ROLL_INPUT_PIN = 33;
-static const uint8_t PITCH_INPUT_PIN = 32;
-static const uint8_t YAW_INPUT_PIN = 4;
+static const uint8_t THROTTLE_PIN = 25;
+static const uint8_t ROLL_PIN = 33;
+static const uint8_t PITCH_PIN = 32;
+static const uint8_t YAW_PIN = 4;
 
-static const uint8_t ARMING_INPUT_PIN = 23;
-static const uint8_t HOVER_INPUT_PIN = 27;
-static const uint8_t AUTOPILOT_INPUT_PIN = 19;
+static const uint8_t ARMING_PIN = 23;
+static const uint8_t HOVER_PIN = 27;
+static const uint8_t AUTOPILOT_PIN = 19;
+
+static const uint8_t VOLTAGE_DIVIDER_PIN = 14;
+static const float VOLTAGE_DIVIDER_R1_OHMS = 1000;
+static const float VOLTAGE_DIVIDER_R2_OHMS = 2200;
 
 //static const uint8_t LED_PIN = 15;
 
@@ -30,12 +39,17 @@ static const float ANALOG_MAX = 3900;
 
 static bool arming_prev_;
 
-static PushButton hoverButton_ = PushButton(HOVER_INPUT_PIN);;
-static PushButton autopilotButton_ = PushButton(AUTOPILOT_INPUT_PIN);;
+static PushButton hoverButton_ = PushButton(HOVER_PIN);;
+static PushButton autopilotButton_ = PushButton(AUTOPILOT_PIN);;
+
+static hf::VoltageDivider voltage_divider_ = hf::VoltageDivider(
+        VOLTAGE_DIVIDER_PIN,
+        VOLTAGE_DIVIDER_R2_OHMS,
+        VOLTAGE_DIVIDER_R1_OHMS);
 
 static auto ReadArmingSwitch() -> bool
 {
-    return digitalRead(ARMING_INPUT_PIN);
+    return digitalRead(ARMING_PIN);
 }
 
 static auto ReadGimbal(const uint8_t pin) -> float
@@ -43,13 +57,15 @@ static auto ReadGimbal(const uint8_t pin) -> float
     return (analogRead(pin) - ANALOG_MIN) / (ANALOG_MAX - ANALOG_MIN);
 }
 
+static TinyPICO tp = TinyPICO();
+
 void setup()
 {
     Serial.begin(115200);
 
-    //pinMode(ARMING_INPUT_PIN, INPUT);
-    pinMode(HOVER_INPUT_PIN, INPUT);
-    pinMode(AUTOPILOT_INPUT_PIN, INPUT);
+    //pinMode(ARMING_PIN, INPUT);
+    pinMode(HOVER_PIN, INPUT);
+    pinMode(AUTOPILOT_PIN, INPUT);
 
     //pinMode(LED_PIN, OUTPUT);
 
@@ -57,6 +73,8 @@ void setup()
     autopilotButton_.begin();
 
     arming_prev_ = ReadArmingSwitch();
+
+    tp.DotStar_SetPixelColor(255, 0, 0);
 }
 
 void loop()
@@ -72,15 +90,18 @@ void loop()
 
     const auto autopilot = autopilotButton_.read();
 
-    const auto throttle = 1 - ReadGimbal(THROTTLE_INPUT_PIN);
+    const auto throttle = 1 - ReadGimbal(THROTTLE_PIN);
 
-    const auto roll = 2 * (0.5 - ReadGimbal(ROLL_INPUT_PIN));
+    const auto roll = 2 * (0.5 - ReadGimbal(ROLL_PIN));
 
-    const auto pitch = 2 * (ReadGimbal(PITCH_INPUT_PIN) - 0.5);
+    const auto pitch = 2 * (ReadGimbal(PITCH_PIN) - 0.5);
 
-    const auto yaw = 2 * ReadGimbal(YAW_INPUT_PIN) - 1;
+    const auto yaw = 2 * ReadGimbal(YAW_PIN) - 1;
 
+    Serial.printf("%f\n", voltage_divider_.read());
+
+    /*
     Serial.printf("throttle=%3.2f roll=%+3.2f pitch=%+3.2f yaw=%+3.2f | "
             "armed=%d hovering=%d autopilot=%d\n",
-            throttle, roll, pitch, yaw, armed_, hovering, autopilot);
+            throttle, roll, pitch, yaw, armed_, hovering, autopilot);*/
 }
