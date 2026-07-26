8a01251f4fd07fc21c86e4824e501bc3c624170e
simondlevy <simon.d.levy@gmail.com>
2026-07-26 01:03:28 -0400

    new file:   attic/kicad/tx/tx.kicad_pcb

--- examples/tx/tx.ino
+++ examples/tx/tx.ino
@@ -16,20 +16,24 @@
 #include <firmware/blink_timer.hpp>
 #include <firmware/espnow.hpp>
 #include <firmware/voltage_divider.hpp>
-#include <firmware/analog_pushbutton.hpp>
 
 static const uint8_t kReceiverAddress[6] = {0x98,0x3D,0xAE,0xEF,0x0E,0xAC};
 
 static const bool kDebug = true;
 
-static const uint8_t kYawPin = 4;
+
 static const uint8_t kVoltageDividerPin = 14;
+
 static const uint8_t kLedPin = 15;
+
 static const uint8_t kArmingPin = 23;
+static const uint8_t kHoverPin = 22;
+static const uint8_t kAutopilotPin = 1;
+
 static const uint8_t kThrottlePin = 25;
-static const uint8_t kHoverPin = 27;
 static const uint8_t kPitchPin = 32;
 static const uint8_t kRollPin = 33;
+static const uint8_t kYawPin = 4;
 
 static const float kVoltageDividerR1Ohms = 1000;
 static const float kVoltageDividerR2Ohms = 2200;
@@ -40,8 +44,6 @@ static const float kAnalogMax = 3900;
 static const float kLowVoltage = 3.0;
 
 static const float kTransmitHz = 100;
-
-static const uint16_t kAnalogPushbuttonThreshold = 4094;
 
 static auto blink_timer_ = hf::BlinkTimer();
 
@@ -60,9 +62,6 @@ static auto ReadGimbal(const uint8_t pin) -> float
     return (analogRead(pin) - kAnalogMin) / (kAnalogMax - kAnalogMin);
 }
 
-static AnalogPushButton hoverButton_ = AnalogPushButton(kHoverPin,
-        kAnalogPushbuttonThreshold);
-
 void setup()
 {
     Serial.begin(115200);
@@ -74,8 +73,8 @@ void setup()
 
     arming_prev_ = digitalRead(kArmingPin);
 
-    hf::EspNow::WifiSetup();
-    hf::EspNow::WifiAddPeer(kReceiverAddress);
+    //hf::EspNow::WifiSetup();
+    //hf::EspNow::WifiAddPeer(kReceiverAddress);
 }
 
 void loop()
@@ -98,17 +97,16 @@ void loop()
         digitalWrite(kLedPin, blink_timer_.On());
     }
 
-    //if (kDebug) {
-    //    Serial.printf("throttle=%3.2f roll=%+3.2f pitch=%+3.2f yaw=%+3.2f | "
-    //            "armed=%d hovering=%d | voltage=%3.3f\n",
-    //            throttle, roll, pitch, yaw, armed_, hovering, volts);
-    //}
+    if (kDebug) {
+        Serial.printf("armed=%d\n", armed_);
+    }
 
     const uint8_t data = 'A';
 
+    /*
     if (transmit_timer_.Ready()) {
         if (esp_now_send(kReceiverAddress, &data, 1) != ESP_OK) {
             Serial.println("Error sending the data");
         }
-    }
+    }*/
 }
