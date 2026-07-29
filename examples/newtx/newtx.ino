7a1ccd8c8b46088d7ee2a78d79597e75419388f1
simondlevy <simon.d.levy@gmail.com>
2026-07-29 18:59:39 -0400

    modified:   newtx.ino

--- examples/newtx/newtx.ino
+++ examples/newtx/newtx.ino
@@ -3,6 +3,12 @@
 
 static const uint8_t kReceiverAddress[6] = {0x98,0x3D,0xAE,0xEF,0x0E,0xAC};
 
+static const uint8_t kThrottleAnalogPin = A0;
+static const uint8_t kRollAnalogPin = A2;
+static const uint8_t kPitchAnalogPin = A3;
+static const uint8_t kYawAnalogPin = A1;
+static const uint8_t kDividerAnalogPin = A4;
+
 static const uint8_t kArmingDigitalPin = 35;
 static const uint8_t kHoverDigitalPin = 9;
 static const uint8_t kAutopilotDigitalPin = 8;
@@ -25,20 +31,20 @@ void loop()
             "Arm=%d "
             "Hover=%d "
             "Autopilot=%d "
-            "A0=%04d "
-            "A1=%04d "
-            "A2=%04d "
-            "A3=%04d "
-            "A4=%04d "
+            "Throttle=%04d "
+            "Roll=%04d "
+            "Pitch=%04d "
+            "Yaw=%04d "
+            "Divider=%04d "
             "\n"
             , digitalRead(kArmingDigitalPin)
             , digitalRead(kHoverDigitalPin)
             , digitalRead(kAutopilotDigitalPin)
-            , analogRead(A0)
-            , analogRead(A1)
-            , analogRead(A2)
-            , analogRead(A3)
-            , analogRead(A4)
+            , analogRead(kThrottleAnalogPin)
+            , analogRead(kRollAnalogPin)
+            , analogRead(kPitchAnalogPin)
+            , analogRead(kYawAnalogPin)
+            , analogRead(kDividerAnalogPin)
             );
 
 }
