7a893c4ddc062ffef95e2cb41af67f9f70c4bb81
simondlevy <simon.d.levy@gmail.com>
2026-07-29 18:52:34 -0400

    modified:   newtx.ino

--- examples/newtx/newtx.ino
+++ examples/newtx/newtx.ino
@@ -3,9 +3,9 @@
 
 static const uint8_t kReceiverAddress[6] = {0x98,0x3D,0xAE,0xEF,0x0E,0xAC};
 
-static const uint8_t DPIN1 = 35;
-static const uint8_t DPIN2 = 9;
-static const uint8_t DPIN3 = 8;
+static const uint8_t kArmingDigitalPin = 35;
+static const uint8_t kHoverDigitalPin = 9;
+static const uint8_t kAutopilotDigitalPin = 8;
 
 void setup()
 {
@@ -14,26 +14,26 @@ void setup()
     hf::EspNow::WifiSetup();
     hf::EspNow::WifiAddPeer(kReceiverAddress);
 
-    pinMode(DPIN1, INPUT);
-    pinMode(DPIN2, INPUT);
-    pinMode(DPIN3, INPUT);
+    pinMode(kArmingDigitalPin, INPUT);
+    pinMode(kHoverDigitalPin, INPUT);
+    pinMode(kAutopilotDigitalPin, INPUT);
 }
 
 void loop()
 {
     Serial.printf(
-            "D%d=%d "
-            "D%d=%d "
-            "D%d=%d "
+            "Arm=%d "
+            "Hover=%d "
+            "Autopilot=%d "
             "A0=%04d "
             "A1=%04d "
             "A2=%04d "
             "A3=%04d "
             "A4=%04d "
             "\n"
-            , DPIN1, digitalRead(DPIN1)
-            , DPIN2, digitalRead(DPIN2)
-            , DPIN3, digitalRead(DPIN3)
+            , digitalRead(kArmingDigitalPin)
+            , digitalRead(kHoverDigitalPin)
+            , digitalRead(kAutopilotDigitalPin)
             , analogRead(A0)
             , analogRead(A1)
             , analogRead(A2)
